40a607133d6de0a7e1d92fdad9749f497beee017
Christoph Deil <Deil.Christoph@gmail.com>
2013-05-15 15:01:50 +0200

    update

--- tevpy/background/reflected.py
+++ tevpy/background/reflected.py
@@ -1,3 +1,11 @@
 """
 Reflected region background estimation
 """
+
+__all__ = ['ReflectedBgMaker']
+
+
+class ReflectedBgMaker(object):
+    """Finds reflected regions and does computations with them"""
+    def __init__(self, theta=0.1, exclusions=None):
+        pass
