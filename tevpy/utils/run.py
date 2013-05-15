9aa89b8be1dc036e7b26ffcaa3af85d1eeedd15a
Christoph Deil <Deil.Christoph@gmail.com>
2013-05-15 14:58:48 +0200

    update

---
+++ tevpy/utils/run.py
@@ -0,0 +1,26 @@
+class Run(object):
+    """
+    Run parameters container
+    """
+    def __init__(self, GLON, GLAT, livetime=1800,
+                 eff_area=1e12, background=0):
+        self.GLON = GLON
+        self.GLAT = GLAT
+        self.livetime = livetime
+
+    def wcs_header(self, system='FOV'):
+        """
+        Create a WCS FITS header for an image centered on the
+        run position in one of these systems:
+        FOV, Galactic, Equatorial
+        """
+        raise NotImplementedError
+
+
+class RunList(list):
+    """
+    Run list container
+    """
+    pass
+
+
