11af4c7436bb79f8e2cae8d0441693232eebe1ba
Christoph Deil <Deil.Christoph@gmail.com>
2013-05-15 10:44:55 +0200

    update

--- licenses/LICENSE.rst
+++ licenses/LICENSE.rst
@@ -1,4 +1,4 @@
-Copyright (c) 2011, Astropy Developers
+Copyright (c) 2011, tevpy Developers
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
--- setup.py
+++ setup.py
@@ -34,13 +34,13 @@ from astropy.setup_helpers import (register_commands, adjust_compiler,
 from astropy.version_helpers import get_git_devstr, generate_version_py
 
 # Set affiliated package-specific settings
-PACKAGENAME = 'packagename'
-DESCRIPTION = 'Astropy affiliated package'
+PACKAGENAME = 'tevpy'
+DESCRIPTION = 'TeV high-level data analysis method demo'
 LONG_DESCRIPTION = ''
 AUTHOR = ''
 AUTHOR_EMAIL = ''
 LICENSE = 'BSD'
-URL = 'http://astropy.org'
+URL = 'https://github.com/gammapy/tevpy'
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
 VERSION = '0.0.dev'
