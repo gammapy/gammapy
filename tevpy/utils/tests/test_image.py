b749a6d81cfa624c91d5c45cdf032bf3e60cd3de
Christoph Deil <Deil.Christoph@gmail.com>
2013-05-18 22:15:27 +0200

    Add some test to utils.image

---
+++ tevpy/utils/tests/test_image.py
@@ -0,0 +1,19 @@
+import numpy as np
+from numpy.testing import assert_equal
+from .. import image 
+
+def test_binary_disk():
+    actual = image.binary_disk(1)
+    desired = np.array([[False, True, False],
+                        [True, True, True],
+                        [False, True, False]])
+    assert_equal(actual, desired)
+
+def test_binary_ring():
+    actual = image.binary_ring(1, 2)
+    desired = np.array([[False, False, True, False, False],
+                        [False, True, True, True, False],
+                        [True, True, False, True, True],
+                        [False, True, True, True, False],
+                        [False, False, True, False, False]])
+    assert_equal(actual, desired)
