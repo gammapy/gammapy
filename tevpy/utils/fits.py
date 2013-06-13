c9e86f95e0b30d92cd85be93833960f866a0eced
Christoph Deil <Deil.Christoph@gmail.com>
2013-06-13 12:58:53 +0200

    Add FITS utils functions

---
+++ tevpy/utils/fits.py
@@ -0,0 +1,27 @@
+"""FITS utility functions"""
+from astropy.io import fits
+
+__all__ = ['get_hdu', 'get_image_hdu', 'get_table_hdu']
+
+def get_hdu(location):
+    """Get one HDU for a given location
+    
+    location should be either a `file_name` or a file 
+    and HDU name in the format `file_name[hdu_name]`.
+    """ 
+    # TODO: Test all cases and give good exceptions / error messages
+    if '[' in location:
+        tokens = location.split('[')
+        file_name = tokens[0]
+        hdu_name = tokens[1][:-1]  # split off ']' at the end
+        return fits.open(file_name)[hdu_name]
+    else:
+        return fits.open(file_name)[0]
+
+def get_image_hdu():
+    """Get the first image HDU"""
+    raise NotImplementedError
+
+def get_table_hdu():
+    """Get the first table HDU"""
+    raise NotImplementedError
