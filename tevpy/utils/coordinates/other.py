4edc0aa12dc6cd4e0c4450c05b9270853c0b323e
Christoph Deil <Deil.Christoph@gmail.com>
2013-07-04 11:20:15 +0200

    Rewrite utils.coordinates to use astropy units

--- tevpy/utils/coordinates/other.py
+++ tevpy/utils/coordinates/other.py
@@ -3,8 +3,11 @@
 from numpy import (cos, sin, arcsin, sqrt,
                    tan, arctan, arctan2,
                    radians, degrees, pi)
-from const import (r_sun, sec_per_year, km_per_kpc,
-                   cm_per_kpc, pc_per_kpc)
+from astropy.constants import R_sun
+from astropy.units import pc, kpc, cm, km, second, year
+
+__all__ = ['cartesian', 'galactic', 'luminosity_to_flux', 'flux_to_luminosity',
+           'radius_to_angle', 'angle_to_radius', 'spherical_velocity', 'motion_since_birth']
 
 
 def cartesian(r, theta, dx=0, dy=0):
@@ -15,43 +18,44 @@ def cartesian(r, theta, dx=0, dy=0):
     return x, y
 
 
-def spherical(x, y, z):
-    """Compute spherical coordinates lon, lat (deg) and distance (kpc)
+def galactic(x, y, z):
+    """Compute galactic coordinates lon, lat (deg) and distance (kpc)
     for given position in cartesian coordinates (kpc)"""
-    d = sqrt(x ** 2 + (y - r_sun) ** 2 + z ** 2)
-    lon = degrees(arctan2(x, r_sun - y))
+    d = sqrt(x ** 2 + (y - R_sun) ** 2 + z ** 2)
+    lon = degrees(arctan2(x, R_sun - y))
     lat = degrees(arcsin(z / d))
     return lon, lat, d
 
 
 def luminosity_to_flux(luminosity, distance):
     """Distance is assumed to be in kpc"""
-    return luminosity / (4 * pi * (distance * cm_per_kpc) ** 2)
+    return luminosity / (4 * pi * (kpc.to(cm) * distance) ** 2)
 
 
 def flux_to_luminosity(flux, distance):
     """Distance is assumed to be in kpc"""
-    return flux * (4 * pi * (distance * cm_per_kpc) ** 2)
+    return flux * (4 * pi * (kpc.to(cm) * distance) ** 2)
 
 
 def radius_to_angle(radius, distance):
     """Radius (pc), distance(kpc), angle(deg)"""
-    return degrees(arctan(radius * pc_per_kpc / distance))
+    return degrees(arctan(kpc.to(pc) * radius / distance))
 
 
 def angle_to_radius(angle, distance):
     """Radius (pc), distance(kpc), angle(deg)"""
-    return tan(radians(angle)) * distance * pc_per_kpc
+    return tan(radians(angle)) * kpc.to(pc) * distance
 
 
 def spherical_velocity(x, y, z, vx, vy, vz):
     """Computes the projected angular velocity in spherical coordinates."""
     d = sqrt(x ** 2 + y ** 2 + z ** 2)
     r = sqrt(x ** 2 + y ** 2)
-    v_lon = degrees(1. / (r * km_per_kpc) * (-y * vx + x * vy)) * sec_per_year * 1e6
-    v_lat = (degrees(vz / (sqrt(1 - (z / d) ** 2) * d * km_per_kpc) - 
+    
+    v_lon = degrees(1. / (kpc.to(km) * r) * (-y * vx + x * vy)) * year.to(second) * 1e6
+    v_lat = (degrees(vz / (sqrt(1 - (z / d) ** 2) * kpc.to(km) * d ) - 
               sqrt(vx ** 2 + vy ** 2 + vz ** 2) * z / 
-              (km_per_kpc * (sqrt(1 - (z / d) ** 2) * d ** 2))) * sec_per_year * 1e6)
+              (kpc.to(km) * (sqrt(1 - (z / d) ** 2) * d ** 2))) * year.to(second) * 1e6)
     return v_lon, v_lat
 
 
@@ -63,8 +67,11 @@ def motion_since_birth(x, y, z, v, age, theta, phi):
     vy = v * sin(phi) * sin(theta)
     vz = v * cos(theta)
 
+    age = year.to(second) * age
+
     # Compute new positions
-    x = x + vx * age * sec_per_year / km_per_kpc
-    y = y + vy * age * sec_per_year / km_per_kpc
-    z = z + vz * age * sec_per_year / km_per_kpc
+    x = x + kpc.to(km) * vx * age
+    y = y + kpc.to(km) * vy * age
+    z = z + kpc.to(km) * vz * age
+
     return x, y, z, vx, vy, vz
