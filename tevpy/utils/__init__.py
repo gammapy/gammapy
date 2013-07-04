1e3eafcf8b79f99c4ccbe5a7bbea2f051d5c23da
Christoph Deil <Deil.Christoph@gmail.com>
2013-07-04 09:05:11 +0200

    Minor docstring edit.

--- tevpy/utils/__init__.py
+++ tevpy/utils/__init__.py
@@ -1,4 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-# This sub-module is destined for common non-package specific utility
-# functions that will ultimately be merged into `astropy.utils`
+"""
+Utility functions and classes that are used in many places
+or didn't fit anywhere else.
+"""
