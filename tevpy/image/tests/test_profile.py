d526c697fee11c1016131a1f5bcce7bc8d3249ce
Christoph Deil <Deil.Christoph@gmail.com>
2013-07-08 19:05:52 +0200

    Add test for image profile

--- tevpy/image/tests/test_profile.py
+++ tevpy/image/tests/test_profile.py
@@ -1,5 +1,20 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+from numpy.testing import assert_almost_equal
+import pytest
 from .. import profile
 
-def test_dummy():
-    pass
+try:
+    import pandas
+    HAS_PANDAS = True
+except ImportError:
+    HAS_PANDAS = False
+
+@pytest.mark.skipif('not HAS_PANDAS')
+def test_compute_binning():
+    data = [1, 3, 2, 2, 4]
+    bin_edges = profile.compute_binning(data, n_bins=3, method='equal width')
+    assert_almost_equal(bin_edges, [1, 2, 3, 4])
+    
+    bin_edges = profile.compute_binning(data, n_bins=3, method='equal entries')
+    assert_almost_equal(bin_edges, [1, 2, 3, 4])
+    
