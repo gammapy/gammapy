77c79bb689ecb223ec6ee747db4fc164b5e5f2a3
Christoph Deil <Deil.Christoph@gmail.com>
2013-07-11 13:49:29 +0200

    Add tevpy/detect/tests/test_sex.py

---
+++ tevpy/detect/tests/test_sex.py
@@ -0,0 +1,20 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+import subprocess
+import pytest
+from astropy.utils.data import get_pkg_data_filename
+from ..sex import sex
+
+try:
+    subprocess.call('sex')
+    HAS_SEX = True
+except OSError:
+    HAS_SEX = False
+
+
+@pytest.mark.skipif('not HAS_SEX')
+def test_sex():
+    """Run SExtractor an example image and check number of detected sources"""
+    filename = get_pkg_data_filename('../../data/poisson_stats_image/counts.fits.gz')
+    catalog, checkimage = sex(filename)
+    assert len(catalog) == 42
+    assert checkimage.data.max() == 42
