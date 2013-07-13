b3e1d69979c0fab06b9e352e719eaf87e3f9dd19
Christoph Deil <Deil.Christoph@gmail.com>
2013-07-13 16:05:13 +0200

    Add license line to spectrum files

--- tevpy/spectrum/__init__.py
+++ tevpy/spectrum/__init__.py
@@ -1,5 +1,5 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
 Spectrum estimation and modeling methods.
 """
 from . import crab
-# from .crab import *
--- tevpy/spectrum/tests/test_crab.py
+++ tevpy/spectrum/tests/test_crab.py
@@ -1,3 +1,4 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 import pytest
 import numpy as np
 from astropy.units import Unit
