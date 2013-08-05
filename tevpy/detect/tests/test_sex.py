08e392fae7b6c239ae480936b60c582396618609
Christoph Deil <Deil.Christoph@gmail.com>
2013-08-05 20:00:00 +0200

    Clean up comment in SExtractor test describing an issue

--- tevpy/detect/tests/test_sex.py
+++ tevpy/detect/tests/test_sex.py
@@ -2,7 +2,6 @@
 import subprocess
 import tempfile
 import pytest
-#from astropy.utils.data import get_pkg_data_filename
 from astropy.io import fits
 from ...data import poisson_stats_image
 from ..sex import sex
@@ -16,14 +15,13 @@ except OSError:
     HAS_SEX = False
 
 
-# TODO: this test doesn't work in the test runner, because get_pkg_data_filename
-# doesn't work ... on my machine it returns a filename which doesn't exist:
-# /private/var/folders/sb/4qv5j4m90pz1rw7m70rj1b1r0000gn/T/astropy-test-Zg88hJ/lib.macosx-10.8-intel-2.7/tevpy/detect/tests/../../data/poisson_stats_image/counts.fits.gz
 @pytest.mark.skipif('not HAS_SEX')
 def test_sex():
     """Run SExtractor an example image and check number of detected sources"""
-    #filename = get_pkg_data_filename('../../data/poisson_stats_image/counts.fits.gz')
-    #filename = poisson_stats_image(return_filenames=True)
+    # SExtractor can't process zipped files, so we can't simply use this:
+    # from astropy.utils.data import get_pkg_data_filename
+    # filename = get_pkg_data_filename('../../data/poisson_stats_image/counts.fits.gz')
+    # Instead we make a non-zipped copy of the file:
     data = poisson_stats_image()
     filename = tempfile.mktemp('.fits')
     fits.writeto(filename, data=data)
