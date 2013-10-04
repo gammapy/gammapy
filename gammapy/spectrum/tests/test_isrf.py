080c9a5822c0b9f33b3c625e59fca32879ba96cf
Christoph Deil <Deil.Christoph@gmail.com>
2013-10-04 13:34:19 +0200

    Clean up test_isrf.py a bit

--- gammapy/spectrum/tests/test_isrf.py
+++ gammapy/spectrum/tests/test_isrf.py
@@ -1,28 +1,24 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-import pytest
+from numpy.testing import assert_almost_equal
 from ..isrf import Schlickeiser, Galprop
 
+# TODO
+def _test_Schlickeiser_omega_g_over_b():
+    """ Check that CMB has the energy density it is
+    supposed to have according to its temperature """
+    actual = Schlickeiser()._omega_g_over_b('CMB')
+    assert_almost_equal(actual, 1, places=2)
 
-@pytest.skip('TODO')
-class TestSchlickeiser:
-
-    def test_omega_g_over_b(self):
-        """ Check that CMB has the energy density it is
-        supposed to have accoding to its temperature """
-        actual = Schlickeiser()._omega_g_over_b('CMB')
-        self.assertAlmostEqual(actual, 1, places=2)
-
-    def test_call(self):
-        """ Check that we roughly get the same value
-        as in Fig. 3.9 of Hillert's diploma thesis.
-        TODO: The check should be made against a published
-        value instead """
-        actual = Schlickeiser()(1e-3)
-        self.assertAlmostEqual(actual / 189946, 1, places=5)
+# TODO
+def _test_Schlickeiser_call():
+    """ Check that we roughly get the same value
+    as in Fig. 3.9 of Hillert's diploma thesis.
+    TODO: The check should be made against a published
+    value instead """
+    actual = Schlickeiser()(1e-3)
+    assert_almost_equal(actual / 189946, 1, places=5)
 
 
-class TestGalprop:
-
-    def test_call(self):
-        Galprop()
-
+def test_Galprop_call():
+    Galprop()
+    # TODO
