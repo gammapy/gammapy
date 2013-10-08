80c0935e06416eddc82566371dae143c46caa8e3
Christoph Deil <Deil.Christoph@gmail.com>
2013-10-08 11:33:26 +0200

    Add Fermi PSF class (work in progress)

---
+++ gammapy/morphology/tests/test_fermi_psf.py
@@ -0,0 +1,15 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import print_function, division
+from astropy.utils.data import get_pkg_data_filename
+from ..fermi_psf import FermiPSF
+
+def test_FermiPSF():
+    filename = get_pkg_data_filename('../../data/fermi/psf.fits')
+    psf = FermiPSF.read(filename)
+
+def interactive_test():
+    filename = get_pkg_data_filename('../../data/fermi/psf.fits')
+    psf = FermiPSF.read(filename)
+    #psf.plot_containment('fermi_psf_containment.pdf')
+    #psf.plot_exposure('fermi_psf_exposure.pdf')
+    psf.plot_theta('fermi_psf_theta.pdf')
