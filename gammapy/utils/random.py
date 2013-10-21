361ae7be3affbfb650965154dac798a627c35b08
Christoph Deil <Deil.Christoph@gmail.com>
2013-10-21 13:47:11 +0200

    Fix and extend `random_sphere` function

--- gammapy/utils/random.py
+++ gammapy/utils/random.py
@@ -6,7 +6,7 @@ import numpy as np
 __all__ = ['sample_sphere', 'sample_powerlaw']
 
 
-def sample_sphere(size, unit='radians'):
+def sample_sphere(size, lon_range=None, lat_range=None, unit='radians'):
     """Sample random points on the sphere.
     
     Reference: http://mathworld.wolfram.com/SpherePointPicking.html
@@ -15,22 +15,43 @@ def sample_sphere(size, unit='radians'):
     ----------
     size : int
         Number of samples to generate
+    lon_range : tuple
+        Longitude range (min, max) tuple in range (0, 360)
+    lat_range : tuple
+        Latitude range (min, max) tuple in range (-180, 180)
     units : {'rad', 'deg'}
-        Units of returned angles
+        Units of input range and returned angles
 
     Returns
     -------
     lon, lat: arrays
         Longitude and latitude coordinate arrays
     """
+    # Convert inputs to internal format (all radians)
     size = int(size)
 
+    if (lon_range != None) and (unit == 'deg'):
+        lon_range = np.radians(lon_range)
+    else:
+        lon_range = 0, 2 * np.pi
+
+    if (lat_range != None) and (unit == 'deg'):
+        lat_range = np.radians(lat_range)
+    else:
+        lat_range = -np.pi / 2, np.pi / 2
+
+    # Sample random longitude
     u = np.random.random(size)
-    lon = 2 * np.pi * u
+    lon = lon_range[0] + (lon_range[1] - lon_range[0]) * u
 
+    # Sample random latitude
     v = np.random.random(size)
-    lat = np.arccos(2 * v - 1)
+    z_range = np.sin(np.array(lat_range)) 
+    z = z_range[0] + (z_range[1] - z_range[0]) * v
+    # This is not the formula given in the reference, but it is equivalent.
+    lat = np.arcsin(z)
 
+    # Return result
     if unit in ['rad', 'radian', 'radians']:
         return lon, lat
     elif unit in ['deg', 'degree', 'degrees']:
