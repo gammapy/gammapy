8aabac0eb74729d89a39976b45de306b58b8c28b
Christoph Deil <Deil.Christoph@gmail.com>
2013-10-22 21:37:59 +0200

    Improve healpix reprojection function a bit

--- gammapy/image/healpix.py
+++ gammapy/image/healpix.py
@@ -2,20 +2,43 @@
 
 This is a thin wrapper convenience functions around
 `healpy` (http://code.google.com/p/healpy/) functionality.
+
+Really these utility functions belong in `healpy` ... I've made a feature request here:
+https://github.com/healpy/healpy/issues/129
 """
 from __future__ import print_function, division
 from .utils import coordinates
 
 
-def healpix_to_image(healpix_data, other_image):
-    """Convert image in HPX format to some other format (e.g. CAR or AIT).
+def healpix_to_image(healpix_data, reference_image):
+    """Convert image in HEALPIX format to a normal FITS projection image (e.g. CAR or AIT).
 
-    @param healpix_data: numpy.ndarray containing the HEALPIX data
-    @param other_image: kapteyn.maputils.FITSimage containing the other image"""
-    import healpy
-    glon, glat = coordinates(other_image, glon_sym=False, radians=True)
-    data = healpy.get_interp_val(healpix_data, glon, glat)
-    other_image.dat = data
+    Parameters
+    ----------
+    healpix_data : `numpy.ndarray`
+        HEALPIX data array
+    reference_image : `astropy.io.fits.ImageHDU`
+        A reference image to project to.
+
+    Returns
+    -------
+    reprojected_data : `numpy.ndarray`
+        HEALPIX image resampled onto the reference image
+    
+    Examples
+    --------
+    >>> import healpy as hp
+    >>> from astropy.io import fits
+    >>> from gammapy.image.healpix import healpix_to_image
+    >>> healpix_data = hp.read_map('healpix.fits')
+    >>> reference_image = fits.open('reference_image.fits')[0]
+    >>> reprojected_data = healpix_to_image(healpix_data, reference_image)
+    >>> fits.writeto('new_image.fits', reprojected_data, reference_image.header)
+    """
+    import healpy as hp
+    lon, lat = coordinates(reference_image, lon_sym=False, radians=True)
+    data = hp.get_interp_val(healpix_data, lon, lat)
+    return data
 
 
 def other_to_healpix(other_image, healpix_data):
