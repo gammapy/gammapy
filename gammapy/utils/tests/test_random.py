e7ba120f170b81a9d01d0d0e7e47b8dcc3278613
Christoph Deil <Deil.Christoph@gmail.com>
2013-10-29 09:32:01 +0100

    Fix test_sample_sphere

--- gammapy/utils/tests/test_random.py
+++ gammapy/utils/tests/test_random.py
@@ -8,7 +8,7 @@ def test_sample_sphere():
     np.random.seed(0)
     lon, lat = random.sample_sphere(size=2)
     assert_allclose(lon, [ 3.44829694,  4.49366732])
-    assert_allclose(lat, [ 1.36379441,  1.48090897])
+    assert_allclose(lat, [ 0.20700192,  0.08988736])
 
 def test_sample_powerlaw():
     np.random.seed(0)
