5b355a555d5f8ab5f2f7298a15e071bb9a0c63df
Christoph Deil <Deil.Christoph@gmail.com>
2013-12-08 21:19:41 +0100

    Clean up stats/data.py

--- gammapy/stats/data.py
+++ gammapy/stats/data.py
@@ -1,48 +1,71 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-"""On-off bin stats computations"""
+"""On-off bin stats computations."""
 from __future__ import print_function, division
 import numpy as np
 
-__all__ = ['Stats', 'SpectrumStats', 'make_stats', 'combine_stats']
+__all__ = ['Stats', 'make_stats', 'combine_stats']
 
 
 class Stats(object):
-    """Contains count info"""
+    """Container for an on-off observation.
+    
+    Parameters
+    ----------
+    n_on : array-like
+        Observed number of counts in the on region
+    n_off : array-like
+        Observed number of counts in the off region
+    a_on : array-like
+        Relative background exposure of the on region
+    a_off : array-like
+        Relative background exposure in the off region
+    """
+    # TODO: use numpy arrays and properties
+    # TODO: add gamma exposure
 
-    def __init__(self, n_on, n_off, e_on, e_off):
+    def __init__(self, n_on, n_off, a_on, a_off):
         self.n_on = float(n_on)
         self.n_off = float(n_off)
-        self.e_on = float(e_on)
-        self.e_off = float(e_off)
+        self.a_on = float(a_on)
+        self.a_off = float(a_off)
 
+    @property
     def alpha(self):
-        return self.e_on / self.e_off
+        r"""Background exposure ratio.
+        
+        .. math:: \alpha = a_{on} / a_{off}
+        """
+        return self.a_on / self.a_off
 
+    @property
     def background(self):
-        return self.alpha() * self.n_off
+        r"""Background estimate.
+        
+        .. math:: \mu_{background} = \alpha\ n_{off}
+        """
+        return self.alpha * self.n_off
 
+    @property
     def excess(self):
-        return self.n_on - self.background()
+        r"""Excess.
+        
+        .. math:: n_{excess} = n_{on} - \mu_{background}
+        """
+        return self.n_on - self.background
 
     def __str__(self):
-        keys = ['n_on', 'n_off', 'e_on', 'e_off',
+        keys = ['n_on', 'n_off', 'a_on', 'a_off',
                 'alpha', 'background', 'excess']
-        values = [self.n_on, self.n_off, self.e_on, self.e_off,
-                  self.alpha(), self.background(), self.excess()]
+        values = [self.n_on, self.n_off, self.a_on, self.a_off,
+                  self.alpha, self.background(), self.excess()]
         return '\n'.join(['%s = %s' % (k, v)
                           for (k, v) in zip(keys, values)])
 
 
-class SpectrumStats(Stats):
-
-    """Contains count and exposure info"""
-    # TODO: implement me!
-    pass
-
-
 def make_stats(signal, background, area_factor, weight_method="background",
                poisson_fluctuate=False):
-    """Fill using some weight method for the exposure"""
+    """Fill using some weight method for the exposure.
+    """
     # Compute counts
     n_on = signal + background
     n_off = area_factor * background
@@ -61,13 +84,28 @@ def make_stats(signal, background, area_factor, weight_method="background",
         raise ValueError("Invalid weight_method: {0}".format(weight_method))
 
     # Compute exposure
-    e_on = weight
-    e_off = weight * area_factor
-    return Stats(n_on, n_off, e_on, e_off)
+    a_on = weight
+    a_off = weight * area_factor
+    return Stats(n_on, n_off, a_on, a_off)
 
 
 def combine_stats(stats_1, stats_2, weight_method="none"):
-    """Combine using some weight method for the exposure."""
+    """Combine using some weight method for the exposure.
+    
+    Parameters
+    ----------
+    stats_1 : `Stats`
+        Observation 1
+    stats_2 : `Stats`
+        Observation 2
+    weight_method : {'none', 'background', 'n_off'}
+        Observation weighting method.
+
+    Returns
+    -------
+    stats : `Stats`
+        Combined Observation 1 and 2
+    """
     # Compute counts
     n_on = stats_1.n_on + stats_2.n_on
     n_off = stats_1.n_off + stats_2.n_off
@@ -86,6 +124,7 @@ def combine_stats(stats_1, stats_2, weight_method="none"):
         raise ValueError("Invalid weight_method: {0}".format(weight_method))
 
     # Compute exposure
-    e_on = weight_1 * stats_1.e_on + weight_2 * stats_2.e_on
-    e_off = weight_1 * stats_1.e_off + weight_2 * stats_2.e_off
-    return Stats(n_on, n_off, e_on, e_off)
+    a_on = weight_1 * stats_1.a_on + weight_2 * stats_2.a_on
+    a_off = weight_1 * stats_1.a_off + weight_2 * stats_2.a_off
+
+    return Stats(n_on, n_off, a_on, a_off)
