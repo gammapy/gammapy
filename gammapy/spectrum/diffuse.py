5ec5a2abca48cc6b48cf6d7083da77b944af0681
Christoph Deil <Deil.Christoph@gmail.com>
2013-12-11 14:39:25 +0100

    Update diffuse gamma-ray emission function.

--- gammapy/spectrum/diffuse.py
+++ gammapy/spectrum/diffuse.py
@@ -1,13 +1,15 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import print_function, division
 import numpy as np
+from astropy.units import Quantity
 from astropy.io import fits
 from astropy.table import Table
 from astropy.wcs import WCS
 from .utils import EnergyAxis
 from . import powerlaw as pl
 
-__all__ = ['GalacticDiffuse']
+__all__ = ['GalacticDiffuse', 'diffuse_gamma_ray_flux']
+
 
 class GalacticDiffuse(object):
     """Lookup diffuse emission flux from FITS cube as used e.g. by Fermi or GALPROP.
@@ -108,3 +110,44 @@ class GalacticDiffuse(object):
         x = np.round(x).astype(int)
         y = np.round(y).astype(int)
         return x, y
+
+def _power_law(E, N, k):
+    E = Quantity(E, 'TeV')
+    E0 = Quantity(1, 'TeV')
+    N = Quantity(N, 'm^-2 s^-1 TeV^-1 sr^-1')
+    flux = N * (E / E0) ** (-k)
+    return flux
+
+def diffuse_gamma_ray_flux(energy, component='isotropic'):
+    """Diffuse gamma ray flux.
+    
+    TODO: describe available spectra.
+
+    References:
+    * 'isotropic':  http://adsabs.harvard.edu/abs/2010PhRvL.104j1101A
+    
+    Parameters
+    ----------
+    energy : `~astropy.units.Quantity`
+        Gamma-ray energy
+    component : {'isotropic', 'bubble', 'galactic_fermi2', 'galactic_fermi4'}
+        Diffuse model component
+    
+    Returns
+    -------
+    flux : `~astropy.units.Quantity`
+        Gamma-ray flux in unit `m^-2 s^-1 TeV^-1 sr^-1`
+    """
+    #flux = Quantity(1, 'm^-2 s^-1 TeV^-1 sr^-1')
+    if component == 'isotropic':
+        # Reference: abstract from this Fermi paper:
+        # http://adsabs.harvard.edu/abs/2010PhRvL.104j1101A
+        integral_flux = Quantity(1.03e-5, 'cm^-2 s^-1 sr^-1')
+        gamma = 2.41
+        return _power_law(energy, 1, 2)
+    elif component == 'bubble':
+        raise NotImplementedError
+    elif component == 'galactic_fermi2':
+        raise NotImplementedError
+    else:
+        raise ValueError('Invalid argument for component: {0}'.format(component))
