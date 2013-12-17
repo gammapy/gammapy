20cb0a554ca6fed6d9bb3e4185efef6b44d7edd4
Christoph Deil <Deil.Christoph@gmail.com>
2013-12-17 20:06:01 +0100

    Add functions to image.measure

--- gammapy/image/measure.py
+++ gammapy/image/measure.py
@@ -3,7 +3,11 @@
 from __future__ import print_function, division
 import numpy as np
 
-__all__ = ['measure_labeled_regions']
+__all__ = ['aperphot', 'find_max',
+           'lookup', 'lookup_max',
+           'measure_labeled_regions'
+           ]
+
 
 def _split_xys(pos):
     """Helper function to work with `scipy.ndimage`."""
@@ -25,7 +29,7 @@ def _split_slices(slices):
     return xmin, xmax, ymin, ymax
 
 
-def get_area(labels):
+def _measure_area(labels):
     """Measure the area in pix of each segment."""
     nsegments = labels.max()
     area = np.zeros(nsegments)
@@ -61,7 +65,7 @@ def measure_labeled_regions(data, labels, tag='IMAGE',
     x, y = _split_xys(nd.center_of_mass(data, labels, index))
     xpeak, ypeak = _split_xys(nd.maximum_position(data, labels, index))
     xmin, xmax, ymin, ymax = _split_slices(nd.find_objects(labels))
-    area = get_area(labels)
+    area = _measure_area(labels)
     # Use FITS convention, i.e. start counting at 1
     FITS_OFFSET = 1 if fits_offset else 0
     # Use SExtractor convention, i.e. slice max is inside
@@ -87,3 +91,142 @@ def measure_labeled_regions(data, labels, tag='IMAGE',
         table.add_column(Column(data=mean, name=tag + '_MEAN'))
 
     return table
+
+
+def find_max(image):
+    """Find position of maximum in an image.
+    
+    Parameters
+    ----------
+    image : `astropy.io.fits.ImageHDU`
+        Input image
+    
+    Returns
+    -------
+    lon, lat, value : float
+        Maximum value and its position 
+    """
+    from scipy.ndimage import maximum_position
+    from astropy.wcs import WCS
+    proj = WCS(image.header)
+    data = image.data
+    data[np.isnan(data)] = -np.inf
+    y, x = maximum_position(data)
+    GLON, GLAT = proj.wcs_pix2world(x, y, 0)
+    val = data[int(y), int(x)]
+    return GLON, GLAT, val
+
+
+def aperphot(img, x, y, aper, sky, sky_type='median', verbose=False):
+    """Perform circular aperture photometry on a set of images.
+
+    Performs the aperture photometry of a given x,y point in an image.
+    Note: Doesn't handle subpixel integration.
+
+    img: numpy 2D array of the image to perform the photometry on.
+        y_dimension, x_dimension = img.shape
+        # Note: x is the second axis as opposed to the first in IDL!
+    x: x coordinate of the position to perform the photometry on.
+        # Note: the first pixel as a coordinate 0, not 1!
+    y: y coordinate of the position to perform the photometry on.
+    aper: aperture size in pixel.
+    sky: 2-element list/tuple/array providing the inner and outer radii
+        to calculate the sky from.
+    sky_type ('median'): if 'median' will use a median for the sky level
+        determination within the sky annulus, otherwise will use a mean.
+    verbose (False): If true, will print some information.
+
+    >>> flux, err_flux = aperphot(img, 32.2, 35.6, 5., [10.,15.], sky_type='median')
+    """
+    dimy, dimx = img.shape
+    indy, indx = np.mgrid[0:dimy, 0:dimx]
+    r = np.sqrt((indy - y) ** 2 + (indx - x) ** 2)
+    ind_aper = r < aper
+    n_counts = ind_aper.sum()
+    tot_counts = img[ind_aper].sum()
+    ind_bkg = (sky[0] < r) * (r < sky[1])
+    n_bkg = ind_bkg.sum()
+    if sky_type.lower() == 'median':
+        bkg = np.median(img[ind_bkg])
+        std_bkg = img[ind_bkg].std()
+    else:
+        tot_bkg = img[ind_bkg].sum()
+        bkg = tot_bkg / n_bkg
+        std_bkg = img[ind_bkg].std()
+    flux = tot_counts - n_counts * bkg
+    if verbose:
+        print('%8.2f %8.2f %10.2f %10.2f %10.2f %10.2f %10.2f' %
+              (x, y, tot_counts, n_counts, bkg, flux, std_bkg) * np.sqrt(n_counts))
+    return flux, std_bkg * np.sqrt(n_counts)
+
+
+def _lookup_pix(image, x, y):
+    """Look up values in an image for given pixel coordinates.
+    
+    image = numpy array
+    x, y = array_like of pixel coordinates (floats OK)
+    """
+    # Find the right pixel
+    x_int = np.round(x).astype(int)
+    y_int = np.round(y).astype(int)
+
+    # Return it's value
+    # Note that numpy has index order (y, x)
+    values = image[y_int, x_int]
+    return values
+
+
+def _lookup_world(image, lon, lat):
+    """Look up values in an image for given world coordinates.
+    
+    image = astropy.io.fits.HDU
+    lon, lat = world coordinates (float OK)
+    """
+    from astropy.wcs import WCS
+    wcs = WCS(image.header)
+    x, y = wcs.wcs_world2pix(lon, lat, 0)
+    return _lookup_pix(image.data, x, y)
+
+
+def lookup(image, x, y, world=True):
+    """Look up values in an image.
+
+    Parameters
+    ----------
+    image : array_like if world=False, astropy.io.fits.ImageHDU if world=True
+        Array or image to look up the value
+    x : array_like
+        Array of X lookup positions
+    y : array_like
+        Array of Y lookup positions
+    world : bool
+        Are (x, y) WCS coordinates?
+    """
+    if world:
+        return _lookup_world(image, x, y)
+    else:
+        return _lookup_pix(image, x, y)
+
+
+def lookup_max(image, GLON, GLAT, theta):
+    """Look up the max image values within a circle of radius theta
+    around lists of given positions (nan if outside)"""
+    from .utils import coordinates
+    GLON = np.asarray(GLON)
+    GLON = np.where(GLON > 180, GLON - 360, GLON)
+    GLAT = np.asarray(GLAT)
+    n_pos = len(GLON)
+    theta = np.asarray(theta) * np.ones(n_pos, dtype='float32')
+
+    ll, bb = coordinates(image)
+
+    val = np.nan * np.ones(n_pos, dtype='float32')
+    for ii in range(n_pos):
+        mask = ((GLON[ii] - ll) ** 2 +
+                (GLAT[ii] - bb) ** 2 <=
+                theta[ii] ** 2)
+        try:
+            val[ii] = image.dat[mask].max()
+        except ValueError:
+            pass
+    return val
