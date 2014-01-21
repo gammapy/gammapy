97a1242445aff3af8156dac6f936d9ad79549ad1
Christoph Deil <Deil.Christoph@gmail.com>
2014-01-21 20:56:23 +0100

    Add missing license line in utils/distributions files

--- gammapy/utils/distributions/__init__.py
+++ gammapy/utils/distributions/__init__.py
@@ -1,5 +1,7 @@
-"""Utility functions / classes for working with distributions
-(e.g. probability density functions)
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""Utility functions / classes for working with distributions.
+
+For example probability density functions.
 """
 from .general_random import *
 from .general_random_array import *
--- gammapy/utils/distributions/utils.py
+++ gammapy/utils/distributions/utils.py
@@ -1,3 +1,4 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Helper functions to work with distributions."""
 from __future__ import print_function, division
 
