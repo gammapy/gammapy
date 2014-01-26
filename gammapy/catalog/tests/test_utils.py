848b5f785b74b308eb5fead34769a3c32868fdec
Christoph Deil <Deil.Christoph@gmail.com>
2014-01-26 16:00:23 +0100

    Add coordinate / RA / DEC IAU string format functions

--- gammapy/catalog/tests/test_utils.py
+++ gammapy/catalog/tests/test_utils.py
@@ -1,14 +1,75 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import print_function, division
-from astropy.coordinates import ICRS
-from .. import make_source_designation
+import astropy.coordinates as coord 
+from .. import coordinate_iau_format, ra_iau_format, dec_iau_format
 
 
 def test_make_source_designation():
-    coordinate = ICRS('05h34m31.93830s +22d00m52.1758s')
-    designation = make_source_designation(coordinate, ra_digits=4, acronym='HESS')
-    assert designation == 'HESS J0534+220'
-        
-    coordinate = ICRS('21h58m52.06511s -30d13m32.1182s')
-    designation = make_source_designation(coordinate, ra_digits=5, acronym='')
-    assert designation == 'J21588.7-3013'
+    # Crab pulsar position for HESS
+    coordinate = coord.ICRS('05h34m31.93830s +22d00m52.1758s')
+    strrep = coordinate_iau_format(coordinate, ra_digits=4)
+    assert strrep == '0534+220'
+    
+    # PKS 2155-304 AGN position for 2FGL
+    coordinate = coord.ICRS('21h58m52.06511s -30d13m32.1182s')
+    strrep = coordinate_iau_format(coordinate, ra_digits=5)
+    assert strrep == '2158.8-3013'
+
+    # Check the example from Section 3.2.1 of the IAU spec: 
+    # http://cdsweb.u-strasbg.fr/Dic/iau-spec.html
+    icrs = coord.ICRS('00h51m09.38s -42d26m33.8s')
+    fk4 = coord.FK4(icrs)
+
+    strrep = coordinate_iau_format(icrs, ra_digits=6)
+    assert strrep == '005109-4226.5'
+    
+    strrep = coordinate_iau_format(fk4, ra_digits=6)
+    assert strrep == '004848-4242.8'
+    
+    strrep = coordinate_iau_format(fk4, ra_digits=4)
+    assert strrep == '0048-427'
+
+    strrep = coordinate_iau_format(fk4, ra_digits=4, dec_digits=2)
+    assert strrep == '0048-42'
+
+
+def test_ra_iau_format():
+    # Test various number of digits (output not verified)
+    ra = coord.Longitude('05h34m31.93830s')
+    assert ra_iau_format(ra, digits=2) == '05'
+    assert ra_iau_format(ra, digits=3) == '055'
+    assert ra_iau_format(ra, digits=4) == '0534'
+    assert ra_iau_format(ra, digits=5) == '0534.5'
+    assert ra_iau_format(ra, digits=6) == '053431'
+    assert ra_iau_format(ra, digits=7) == '053431.9'
+    assert ra_iau_format(ra, digits=8) == '053431.93'
+
+    ra = coord.Longitude('00h51m09.38s')
+    assert ra_iau_format(ra, digits=2) == '00'
+    assert ra_iau_format(ra, digits=3) == '008'
+    assert ra_iau_format(ra, digits=4) == '0051'
+    assert ra_iau_format(ra, digits=5) == '0051.1'
+    assert ra_iau_format(ra, digits=6) == '005109'
+    assert ra_iau_format(ra, digits=7) == '005109.3'
+    assert ra_iau_format(ra, digits=8) == '005109.38'
+
+
+def test_dec_iau_format():
+    # Test various number of digits (output not verified)
+    dec = coord.Latitude('+22d00m52.1758s')
+    assert dec_iau_format(dec, digits=2) == '+22'
+    assert dec_iau_format(dec, digits=3) == '+220'
+    assert dec_iau_format(dec, digits=4) == '+2200'
+    assert dec_iau_format(dec, digits=5) == '+2200.8'
+    assert dec_iau_format(dec, digits=6) == '+2200.52'
+    assert dec_iau_format(dec, digits=7) == '+220052.1'
+    assert dec_iau_format(dec, digits=8) == '+220052.17'
+
+    dec = coord.Latitude('-42d26m33.8s')
+    assert dec_iau_format(dec, digits=2) == '-42'
+    assert dec_iau_format(dec, digits=3) == '-424'
+    assert dec_iau_format(dec, digits=4) == '-4226'
+    assert dec_iau_format(dec, digits=5) == '-4226.5'
+    assert dec_iau_format(dec, digits=6) == '-4226.33'
+    assert dec_iau_format(dec, digits=7) == '-422633.7'
+    assert dec_iau_format(dec, digits=8) == '-422633.79'
--- gammapy/catalog/utils.py
+++ gammapy/catalog/utils.py
@@ -2,82 +2,185 @@
 """Catalog utility functions / classes."""
 from __future__ import print_function, division
 
-__all__ = ['make_source_designation', 'parse_source_designation']
+__all__ = ['coordinate_iau_format',
+           'ra_iau_format',
+           'dec_iau_format']
 
 
-def make_source_designation(coordinate, ra_digits, acronym=''):
-    """Compute source designation following the IAU specification.
+def coordinate_iau_format(coordinate, ra_digits, dec_digits=None):
+    """Coordinate format as an IAU source designation.
 
-    Following the IAU specification, the following formats are
-    used for a given number of right ascension digits (`ra_digits`):
-    
-    --------- ------
-    ra_digits format
-    --------- ------
-    4         HHMM+DDd
-    5         HHMM.m+DDMM
-    6         HHMMSS+DDMM.m
-    7         HHMMSS.s+DDMMSS
-    8         HHMMSS.ss+DDMMSS.s
-    --------- ------------------
-    
-    Note that the declination part always has one digit less than
-    the right ascension part. 
-    
     Reference: http://cdsweb.u-strasbg.fr/Dic/iau-spec.html
     
     Parameters
     ----------
     coordinate : `astropy.coordinate.ICRS`
-        Source position
-    ra_digits : int (>= 4)
-        Number of digits for the right ascension part
-    acronym : str
-        Source acronym (default: no acronym)
+        Source coordinate
+    ra_digits : int (>=2)
+        Number of digits for the Right Ascension part
+    dec_digits : int (>=2) or None
+        Number of digits for the declination part
+        Default is `dec_digits` = None, meaning `dec_digits` = `ra_digits` - 1
     
     Returns
     -------
-    designation : str
-        Source designation
+    strrepr : str
+        IAU format string representation of the coordinate
     
     Examples
     --------
-    >>> from astropy.coordinates import ICRS
-    >>> from gammapy.catalog import make_source_designation
+    >>> from astropy.coordinates import ICRS, FK4
+    >>> from gammapy.catalog import coordinate_iau_format
     
+    Example position from IAU specification
+
+    >>> coordinate = ICRS('00h51m09.38s -42d26m33.8s')
+    >>> designation = 'QSO J' + coordinate_iau_format(coordinate, ra_digits=6)
+    >>> print(designation)
+    QSO J005109-4226.5
+    >>> designation = 'QSO B' + coordinate_iau_format(FK4(coordinate), ra_digits=6)
+    >>> print(designation)
+    QSO B004848-4242.8
+
     Example: Crab pulsar position (positive declination)
+
     >>> coordinate = ICRS('05h34m31.93830s +22d00m52.1758s')
-    >>> designation = make_source_designation(coordinate, ra_digits=4, acronym='HESS')
+    >>> designation = 'HESS J' + coordinate_iau_format(coordinate, ra_digits=4)
     >>> print(designation)
     HESS J0534+220
     
     Example: PKS 2155-304 AGN position (negative declination)
+
     >>> coordinate = ICRS('21h58m52.06511s -30d13m32.1182s')
-    >>> designation = make_source_designation(coordinate, ra_digits=5, acronym='')
+    >>> designation = '2FGL J' + coordinate_iau_format(coordinate, ra_digits=5)
     >>> print(designation)
-    J21588.7-3013
+    2FGL J2158.8-3013
     """
-    ra = coordinate.ra
-    dec = coordinate.dec
+    if dec_digits == None:
+        dec_digits = max(2, ra_digits - 1) 
+    
+    ra_str = ra_iau_format(coordinate.ra, ra_digits)
+    dec_str = dec_iau_format(coordinate.dec, dec_digits)
 
-    if ra_digits == 4: # format: HHMM+DDd
-        ra_str = '{0:02d}{1:02d}'.format(int(ra.hms.h), int(ra.hms.m))
-        dec_str = '{0:+03d}'.format(int(10 * dec.deg))
-    elif ra_digits == 5: # format : HHMM.m+DDMM
-        ra_str = '{0:02d}{1:02.1f}'.format(int(ra.hms.h), 10 * (ra.hms.m + ra.hms.s / 60.))
-        dec_str = '{0:+02d}{1:02d}'.format(int(dec.dms.d), abs(int(dec.dms.m)))
-    elif ra_digits >= 6:
-        raise NotImplementedError()
-    else:
-        raise ValueError('Invalid ra_digits: {0}'.format(ra_digits))
-
-    if acronym:
-        designation = acronym + ' J' + ra_str + dec_str
-    else:
-        designation = 'J' + ra_str + dec_str
-
-    return designation
+    return ra_str + dec_str
 
 
-def parse_source_designation(designation):
-    raise NotImplementedError
+def ra_iau_format(ra, digits):
+    """Right Ascension part of an IAU source designation.
+    
+    Reference: http://cdsweb.u-strasbg.fr/Dic/iau-spec.html
+
+    ====== ========
+    digits format
+    ====== ========
+    2      HH
+    3      HHh
+    4      HHMM
+    5      HHMM.m
+    6      HHMMSS
+    7      HHMMSS.s
+    ====== ========
+
+    Parameters
+    ----------
+    ra : `astropy.coordinate.Longitude`
+        Right ascension
+    digits : int (>=2)
+        Number of digits
+    
+    Returns
+    -------
+    strrepr : str
+        IAU format string representation of the angle
+    """
+    if (not isinstance(digits, int)) and (digits >= 2): 
+        raise ValueError('Invalid digits: {0}. Valid options: int >= 2'.format(digits))
+    
+    # Note that Python string formatting always rounds the last digit,
+    # but the IAU spec requires to truncate instead.
+    # That's why integers with the correct digits are computed and formatted
+    # instead of formatting floats directly
+
+    ra_h = int(ra.hms[0])
+    ra_m = int(ra.hms[1])
+    ra_s = ra.hms[2]
+
+    if digits == 2:  # format: HH
+        ra_str = '{0:02d}'.format(ra_h)
+    elif digits == 3:  # format: HHh
+        ra_str = '{0:03d}'.format(int(10 * ra.hour))
+    elif digits == 4:  # format: HHMM
+        ra_str = '{0:02d}{1:02d}'.format(ra_h, ra_m)
+    elif digits == 5:  # format : HHMM.m
+        ra_str = '{0:02d}{1:02d}.{2:01d}'.format(ra_h, ra_m, int(ra_s / 6))
+    elif digits == 6:  # format: HHMMSS
+        ra_str = '{0:02d}{1:02d}{2:02d}'.format(ra_h, ra_m, int(ra_s))
+    else:  # format: HHMMSS.s
+        SS = int(ra_s)
+        s_digits = digits - 6
+        s = int(10 ** s_digits * (ra_s - SS))
+        fmt = '{0:02d}{1:02d}{2:02d}.{3:0' + str(s_digits) + 'd}'
+        ra_str = fmt.format(ra_h, ra_m, SS, s)
+
+    return ra_str
+
+
+def dec_iau_format(dec, digits):
+    """Declination part of an IAU source designation.
+    
+    Reference: http://cdsweb.u-strasbg.fr/Dic/iau-spec.html
+
+    ====== =========
+    digits format
+    ====== =========
+    2      +DD
+    3      +DDd
+    4      +DDMM
+    5      +DDMM.m
+    6      +DDMMSS
+    7      +DDMMSS.s
+    ====== =========
+
+    Parameters
+    ----------
+    dec : `astropy.coordinate.Latitude`
+        Declination
+    digits : int (>=2)
+        Number of digits
+    
+    Returns
+    -------
+    strrepr : str
+        IAU format string representation of the angle
+    """
+    if not isinstance(digits, int) and digits >= 2: 
+        raise ValueError('Invalid digits: {0}. Valid options: int >= 2'.format(digits))
+    
+    # Note that Python string formatting always rounds the last digit,
+    # but the IAU spec requires to truncate instead.
+    # That's why integers with the correct digits are computed and formatted
+    # instead of formatting floats directly
+
+    dec_sign = '+' if dec.deg >= 0 else '-'
+    dec_d = int(abs(dec.dms[0]))
+    dec_m = int(abs(dec.dms[1]))
+    dec_s = abs(dec.dms[2])
+
+    if digits == 2:  # format: +DD
+        dec_str = '{0}{1:02d}'.format(dec_sign, dec_d)
+    elif digits == 3:  # format: +DDd
+        dec_str = '{0:+04d}'.format(int(10 * dec.deg))
+    elif digits == 4:  # format : +DDMM
+        dec_str = '{0}{1:02d}{2:02d}'.format(dec_sign, dec_d, dec_m)
+    elif digits == 5:  # format: +DDMM.m
+        dec_str = '{0}{1:02d}{2:02d}.{3:01d}'.format(dec_sign, dec_d, dec_m, int(dec_s / 6))
+    elif digits == 6:  # format: +DDMMSS
+        dec_str = '{0}{1:02d}{2:02d}.{3:02d}'.format(dec_sign, dec_d, dec_m, int(dec_s))
+    else:  # format: +DDMMSS.s
+        SS = int(dec_s)
+        s_digits = digits - 6
+        s = int(10 ** s_digits * (dec_s - SS))
+        fmt = '{0}{1:02d}{2:02d}{3:02d}.{4:0' + str(s_digits) + 'd}'
+        dec_str = fmt.format(dec_sign, dec_d, dec_m, SS, s)
+
+    return dec_str
