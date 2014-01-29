5605270f40478321d01d0cb341de33f89af0d344
Christoph Deil <Deil.Christoph@gmail.com>
2014-01-30 00:19:06 +0100

    Add gammapy/morphology/tests/test_model.py

---
+++ gammapy/morphology/tests/test_model.py
@@ -0,0 +1,6 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import print_function, division
+from .. import model
+
+def test_MorphModelImageCreator():
+    pass
