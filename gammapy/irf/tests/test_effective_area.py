9b4b4ec4af152683515f04bb8814864b3bc05d77
Christoph Deil <Deil.Christoph@gmail.com>
2014-02-12 15:53:01 +0100

    Rename function in effective area test

--- gammapy/irf/tests/test_effective_area.py
+++ gammapy/irf/tests/test_effective_area.py
@@ -1,16 +1,16 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import print_function, division
 from numpy.testing import assert_allclose
-from ..effective_area import effective_area
+from ..effective_area import abramowski_effective_area
 
 
-def test_effective_area():
+def test_abramowski_effective_area():
     energy = 0.1
-    area = effective_area(energy, 'HESS')
+    area = abramowski_effective_area(energy, 'HESS')
     assert_allclose(area, 16546957.901469307)
     
 
-def plot_effective_area():
+def plot_abramowski_effective_area():
     import numpy as np
     import matplotlib.pyplot as plt
     # Plot the effective area curves of the three experiments
@@ -18,9 +18,11 @@ def plot_effective_area():
     # Build a vector of energies (TeV) with equal log spacing
     loge = np.linspace(0, np.log10(elim[1]), 100)
     energy = 10 ** loge
+
     for instrument in ['HESS', 'HESS2', 'CTA']:
-        a_eff_hess = effective_area(energy, instrument)
+        a_eff_hess = abramowski_effective_area(energy, instrument)
         plt.plot(energy, a_eff_hess, label=instrument)
+
     plt.loglog()
     plt.xlabel('Energy (TeV)')
     plt.ylabel('Effective Area (cm^2)')
