dd9cf4f38d153454a57c9b171179205a51601ec5
Christoph Deil <Deil.Christoph@gmail.com>
2014-03-18 18:31:53 +0100

    Add docs/image/survey_example.py

---
+++ docs/image/survey_example.py
@@ -0,0 +1,20 @@
+"""Plot a Galactic plane survey image in two panels."""
+from aplpy import FITSFigure
+from gammapy.data import fermi_galactic_center
+from gammapy.image import GalacticPlaneSurveyPanelPlot
+
+
+class GPSFermiPlot(GalacticPlaneSurveyPanelPlot):    
+
+    def main(self):
+        filename = fermi_galactic_center()['counts']
+        self.fits_figure = FITSFigure(filename, figure=self.figure, subplot=self.box)
+        self.fits_figure.show_colorscale(vmin=1, vmax=10)
+        
+        # TODO: it's not possible to set this here ...
+        # should change callbacks 
+        #self.fits_figure.ticks.set_xspacing(2)
+
+plot = GPSFermiPlot(npanels=3, center=(0, 0), fov=(30, 3))
+plot.draw_panels('all')
+plot.figure.savefig('survey_example.png')
