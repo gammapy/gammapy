544320c27a1862b6865cd65c6f15cf5db256790d
Christoph Deil <Deil.Christoph@gmail.com>
2014-03-31 14:27:00 +0200

    Update setup_package.py files

--- gammapy/irf/setup_package.py
+++ gammapy/irf/setup_package.py
@@ -1,5 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 def get_package_data():
-    files = ['data/*.txt']
-    return {'gammapy.irf.tests': files}
+    formats = 'fits root xml json conf txt csv'.split()
+    files = ['data/*.{0}'.format(_) for _ in formats]
+    return {'gammapy.morphology.tests': files}
