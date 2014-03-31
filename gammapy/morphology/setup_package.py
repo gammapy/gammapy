1741e0123bf83c779fe0a021844cd8bb148aa47b
Christoph Deil <Deil.Christoph@gmail.com>
2014-03-31 13:22:49 +0200

    Add fermi_model.xml

--- gammapy/morphology/setup_package.py
+++ gammapy/morphology/setup_package.py
@@ -1,5 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 def get_package_data():
-    files = ['data/*.xml']
+    files = ['data/*.xml', 'data/*.json', 'data/*.conf']
     return {'gammapy.morphology.tests': files}
