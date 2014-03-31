51803199b59abadf84eed98dd075e7aa47011235
Christoph Deil <Deil.Christoph@gmail.com>
2014-03-31 12:10:04 +0200

    Add setup_package.py to gammapy/irf

--- gammapy/irf/setup_package.py
+++ gammapy/irf/setup_package.py
@@ -1,5 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 def get_package_data():
-    files = ['data/*.xml']
-    return {'gammapy.morphology.tests': files}
+    files = ['data/*.txt']
+    return {'gammapy.irf.tests': files}
