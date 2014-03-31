842f574f2650b8adb04ef51fa1158c6e60a61da9
Christoph Deil <Deil.Christoph@gmail.com>
2014-03-31 14:36:48 +0200

    Fix PSF imports after renaming

--- gammapy/irf/tests/test_psf_fermi.py
+++ gammapy/irf/tests/test_psf_fermi.py
@@ -1,11 +1,12 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import print_function, division
 from astropy.utils.data import get_pkg_data_filename
-from ..fermi_psf import FermiPSF
+from ..psf_fermi import FermiPSF
 
 def test_FermiPSF():
     filename = get_pkg_data_filename('../../datasets/fermi/psf.fits')
     psf = FermiPSF.read(filename)
+
 
 def interactive_test():
     filename = get_pkg_data_filename('../../datasets/fermi/psf.fits')
