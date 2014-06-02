e74e012b432d9310da2723e6b8ab30e81e6a4d0b
Christoph Deil <Deil.Christoph@gmail.com>
2014-06-02 21:44:17 +0200

    Add spectral_cube sub-package

---
+++ gammapy/spectral_cube/core.py
@@ -0,0 +1,16 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""GammaSpectralCube class.
+"""
+from __future__ import print_function, division
+from spectral_cube import SpectralCube
+
+__all__ = ['GammaSpectralCube']
+
+    
+class GammaSpectralCube(SpectralCube):
+    """Spectral cube with for gamma-ray astronomy.
+    
+    TODO: https://github.com/radio-astro-tools/spectral-cube/issues/110
+    http://spectral-cube.readthedocs.org/en/latest/index.html
+    """
+    pass
---
+++ gammapy/spectral_cube/tests/test_core.py
@@ -0,0 +1,16 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import print_function, division
+from astropy.tests.helper import pytest
+from ..core import GammaSpectralCube
+
+try:
+    import spectral_cube
+    HAS_SPECTRAL_CUBE = True
+except ImportError:
+    HAS_SPECTRAL_CUBE = False
+
+
+@pytest.mark.skipif('not HAS_SPECTRAL_CUBE')
+def test_SpectralCube():
+    #spectral_cube = GammaSpectralCube()
+    pass
