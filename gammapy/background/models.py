e424f5f44aa8433be30849543b8e0d443b1f39c2
Christoph Deil <Deil.Christoph@gmail.com>
2014-07-02 19:07:29 +0200

    Add GaussianBand2D model

---
+++ gammapy/background/models.py
@@ -0,0 +1,69 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import print_function, division
+import numpy as np
+from astropy.modeling.models import Gaussian1D
+
+DEFAULT_SPLINE_KWARGS = dict(k=1, s=0)
+
+
+class GaussianBand2D(object):
+    """Gaussian band model.
+
+    This 2-dimensional model is Gaussian in ``y`` for a given ``x``,
+    and the Gaussian parameters can vary in ``x``.
+
+    One application of this model is the diffuse emission along the
+    Galactic plane, i.e. ``x = GLON`` and ``y = GLAT``.
+
+    Parameters
+    ----------
+    table : `~astropy.table.Table`
+        Table of Gaussian parameters.
+        ``x``, ``amplitude``, ``mean``, ``stddev``.
+    spline_kwargs : dict
+        Keyword arguments passed to `~scipy.interpolate.UnivariateSpline`
+    """
+
+    def __init__(self, table, spline_kwargs=DEFAULT_SPLINE_KWARGS):
+        self.table = table
+        self.parnames = ['amplitude', 'mean', 'stddev']
+
+        from scipy.interpolate import UnivariateSpline
+        s = dict()
+        for parname in self.parnames:
+            x = self.table['x']
+            y = self.table[parname]
+            s[parname] = UnivariateSpline(x, y, **spline_kwargs)
+        self._par_model = s
+
+    def _eval_y(self, y, pars):
+        """Evaluate Gaussian model at a given ``y`` position.
+        """
+        return Gaussian1D.eval(y, **pars)
+
+    def parvals(self, x):
+        """Interpolated parameter values at a given ``x``.
+        """
+        x = np.asanyarray(x, dtype=float)
+        parvals = dict()
+        for parname in self.parnames:
+            par_model = self._par_model[parname]
+            shape = x.shape
+            parvals[parname] = par_model(x.flat).reshape(shape)
+
+        return parvals
+
+    def y_model(self, x):
+        """Create model at a given ``x`` position.
+        """
+        x = np.asanyarray(x, dtype=float)
+        parvals = self.parvals(x)
+        return Gaussian1D(**parvals)
+
+    def eval(self, x, y):
+        """Evaluate model at a given position ``(x, y)`` position.
+        """
+        x = np.asanyarray(x, dtype=float)
+        y = np.asanyarray(y, dtype=float)
+        parvals = self.parvals(x)
+        return self._eval_y(y, parvals)
---
+++ gammapy/background/tests/test_models.py
@@ -0,0 +1,45 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import print_function, division
+import numpy as np
+from numpy.testing import assert_allclose
+from astropy.tests.helper import pytest
+from astropy.table import Table
+from astropy.modeling.models import Gaussian1D
+from ..models import GaussianBand2D
+
+try:
+    import scipy
+    HAS_SCIPY = True
+except ImportError:
+    HAS_SCIPY = False
+
+
+@pytest.mark.skipif('not HAS_SCIPY')
+class TestGaussianBand2D():
+
+    def setup(self):
+        table = Table()
+        table['x'] = [-30, -10, 10, 20]
+        table['amplitude'] = [0, 1, 10, 0]
+        table['mean'] = [-1, 0, 1, 0]
+        table['stddev'] = [0.4, 0.5, 0.3, 1.0]
+        self.table = table
+        self.model = GaussianBand2D(table)
+
+    def test_eval(self):
+        x = np.linspace(-100, 20, 5)
+        y = np.linspace(-2, 2, 7)
+        x, y = np.meshgrid(x, y)
+        image = self.model.eval(x, y)
+        assert_allclose(image.sum(), 1.223962643740966)
+
+    def test_parvals(self):
+        par = self.model.parvals(-30)
+        assert_allclose(par['amplitude'], 0)
+        assert_allclose(par['mean'], -1)
+        assert_allclose(par['stddev'], 0.4)
+
+    def test_y_model(self):
+        model = self.model.y_model(-30)
+        assert isinstance(model, Gaussian1D)
+        assert_allclose(model.parameters, [0, -1, 0.4])
