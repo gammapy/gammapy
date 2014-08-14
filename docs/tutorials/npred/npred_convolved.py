03d44dae6f57ccb1c4aa55f3e6051454d277046f
eowen <ellis.owen@mpi-hd.mpg.de>
2014-08-14 20:14:42 +0100

    Adds predicted counts cube tutorial, including plotting scripts and updates doc index.rst file to accomodate new addition

---
+++ docs/tutorials/npred/npred_convolved.py
@@ -0,0 +1,37 @@
+"""Runs commands to produce convolved predicted counts map in current directory
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from npred_general import prepare_images
+
+model, gtmodel, ratio, counts = prepare_images()
+
+# Plotting
+
+fig, axes = plt.subplots(nrows=1, ncols=3)
+
+results = [model, gtmodel, ratio]
+titles = ['Gammapy Background', 'Fermi Tools Background', 'Ratio: \n Gammapy/Fermi Tools']
+
+vmins = [0, 0, 0.9]
+vmaxs = [0.3, 0.3, 1.2]
+
+for i in np.arange(3):
+    im = axes.flat[i].imshow(results[i],
+                             interpolation='nearest',
+                             origin="lower", vmin=vmins[i], vmax=vmaxs[i],
+                             cmap=plt.get_cmap())
+
+    axes.flat[i].set_title(titles[i], fontsize=12)
+
+fig.subplots_adjust(right=0.8)
+cbar_ax = fig.add_axes([0.85, 0.3, 0.025, 0.4])
+fig.colorbar(im, cax=cbar_ax)
+a = fig.get_axes()[0]
+b = fig.get_axes()[1]
+c = fig.get_axes()[2]
+a.set_axis_off()
+b.set_axis_off()
+c.set_axis_off()
---
+++ docs/tutorials/npred/npred_general.py
@@ -0,0 +1,56 @@
+"""Runs commands to produce convolved predicted counts map in current directory
+"""
+
+import numpy as np
+from astropy.coordinates import Angle
+from astropy.units import Quantity
+from astropy.io import fits
+from astropy.wcs import WCS
+from gammapy.spectral_cube import (compute_npred_cube, GammaSpectralCube,
+                                   convolve_cube)
+from gammapy.datasets import FermiVelaRegion
+from gammapy.irf import EnergyDependentTablePSF
+
+__all__ = ['prepare_images']
+
+def prepare_images():
+
+    # Reads in data
+    background_file = FermiVelaRegion.filenames()['diffuse_model']
+    exposure_file = FermiVelaRegion.filenames()['exposure_cube']
+    counts_file = FermiVelaRegion.filenames()['counts_cube']
+    background_model = GammaSpectralCube.read(background_file)
+    exposure_cube = GammaSpectralCube.read(exposure_file)
+    # Add correct units
+    exposure_cube.data = Quantity(exposure_cube.data, '1/(cm2 deg2 s GeV)')
+    # Re-project background cube
+    repro_bg_cube = background_model.reproject_to(exposure_cube)
+    # Define energy band required for output
+    energies = Quantity([10, 500], 'GeV')
+
+    # Compute the predicted counts cube
+    npred_cube = compute_npred_cube(repro_bg_cube, exposure_cube, energies)
+
+    # Convolve with Energy-dependent Fermi LAT PSF
+    
+    psf = EnergyDependentTablePSF.read(FermiVelaRegion.filenames()['psf'])
+    convolved_npred_cube = convolve_cube(npred_cube, psf,
+                                               Angle(3, 'deg'), Angle(1, 'deg'))
+
+    # Counts data
+    counts_data = fits.open(counts_file)[0].data
+    counts_wcs = WCS(fits.open(counts_file)[0].header)
+    counts_cube = GammaSpectralCube(data=Quantity(counts_data, ''), wcs=counts_wcs,
+                                    energy=energies)
+    counts_cube = counts_cube.reproject_to(npred_cube)
+
+    counts = counts_cube.data[0]
+    model = convolved_npred_cube.data[0]
+
+    # Load Fermi tools gtmodel background-only result
+    
+    gtmodel = fits.open(FermiVelaRegion.filenames()['background_image'])[0].data.astype(float)
+    # Ratio for the two background images
+    ratio = np.nan_to_num(model / gtmodel)
+
+    return model, gtmodel, ratio, counts
