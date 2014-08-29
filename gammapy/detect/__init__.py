94d39e77474a1fdbbe897baf9ac71aaa785d82f0
Axel Donath <adonath@mpi-hd.mpg.de>
2014-08-29 18:29:37 +0200

    Removed blob detection

--- gammapy/detect/__init__.py
+++ gammapy/detect/__init__.py
@@ -1,10 +1,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Source detection and measurement methods.
 """
-from .blob import *
 from .cwt import *
 from .iterfind import *
 from .matched_filter import *
 from .sex import *
 from .test_statistic import *
-from .scale_cube import *
--- gammapy/detect/tests/test_blob.py
+++
@@ -1,7 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-from __future__ import print_function, division
-from .. import blob
-
-
-def test_dummy():
-    assert 42 == 42
