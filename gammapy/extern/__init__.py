6df55964abba873c6622949967597598a0a58c93
Christoph Deil <Deil.Christoph@gmail.com>
2014-11-11 11:32:12 +0100

    Bundle xmltodict.py in gammapy/extern

--- gammapy/extern/__init__.py
+++ gammapy/extern/__init__.py
@@ -5,4 +5,10 @@ package but are external to the affiliated package, and hence are developed in
 a separate source tree. Note that this package is distinct from the /cextern
 directory of the source code distribution, as that directory only contains C
 extension code.
+
+The following Python packages were bundled with Gammapy.
+* ``xmltodict.py`` for easily converting XML from / to Python dicts
+  Origin: https://github.com/martinblech/xmltodict/blob/master/xmltodict.py
+* ``validator.py`` for `~astropy.units.Quantity` validation helper functions.
+  Origin: https://github.com/astrofrog/sedfitter/blob/master/sedfitter/utils/validator.py
 """
