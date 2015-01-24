126f2c2350d85610a1c10d78ac0a1b3368b73569
Christoph Deil <Deil.Christoph@gmail.com>
2015-01-24 19:09:54 +0100

    Add event list test

--- gammapy/data/tests/test_event_list.py
+++ gammapy/data/tests/test_event_list.py
@@ -1,9 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import print_function, division
-from ...data import EventList
+from ...data import EventListDataset
+from ...datasets import get_path
 
 
 def test_EventList():
-    event_list = EventList('TODO')
+    filename = get_path('hess/run_0023037_hard_eventlist.fits.gz')
+    ds = EventListDataset.read(filename)
 
-    # TODO: add tests
+    assert len(ds.event_list) == 49
---
+++ gammapy/datasets/data/hess/README.rst
@@ -0,0 +1,10 @@
+HESS data files
+===============
+
+Here's some files exported by the HESS software to test if they can be read OK by Gammapy.
+
+Note that they don't contain any internal information or useful data, e.g. the
+event list contains just a few gamma-like background events.
+
+I'm doing this because faking all the info (header keywords and columns) would
+be more work than implementing the data classes themselves...
