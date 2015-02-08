92fe1bb9dcf61c5678f09b4f1ad826c94cc0d7c3
Christoph Deil <Deil.Christoph@gmail.com>
2015-02-08 14:49:21 +0100

    Implement dataset catalog load functions

--- gammapy/datasets/catalogs.py
+++ gammapy/datasets/catalogs.py
@@ -4,70 +4,116 @@
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-from astropy.utils.data import download_file
 from astropy.table import Table
 from ..datasets import get_path
 
 __all__ = ['load_catalog_atnf',
+           'load_catalog_hess_galactic',
+           # 'load_catalog_hgps',
            'load_catalog_green',
            'load_catalog_snrcat',
            'load_catalog_tevcat',
            ]
 
 
-def load_catalog_atnf(small_sample=False):
+def load_catalog_atnf():
     """Load ATNF pulsar catalog.
 
     The `ATNF pulsar catalog <http://www.atnf.csiro.au/people/pulsar/psrcat/>`__
     is **the** collection of information on all pulsars.
 
+    Unfortunately it's only available in a database format that can only
+    be read with their software.
 
+    This function loads a FITS copy of version 1.51 of the ATNF catalog:
     http://www.atnf.csiro.au/research/pulsar/psrcat/catalogueHistory.html
 
-    For details, see
-    `README file for FermiVelaRegion
-    <https://github.com/gammapy/gammapy-extra/blob/master/datasets/vela_region/README.rst>`_.
+    The ``ATNF_v1.51.fits.gz`` file and ``make_atnf.py`` script are available
+    `here <https://github.com/gammapy/gammapy-extra/blob/master/datasets/catalogs/>`__.
 
     Returns
     -------
     catalog : `~astropy.table.Table`
         Source catalog
     """
-    if small_sample:
-        filename = get_path('atnf/atnf_sample.txt')
-        return Table.read(filename, format='ascii.csv', delimiter=' ')
-    else:
-        filename = get_path('catalogs/ATNF_v51.fits', location='remote')
-        return Table.read(filename)
+    filename = get_path('catalogs/ATNF_v1.51.fits.gz', location='remote')
+    return Table.read(filename)
+
+
+def load_catalog_hess_galactic():
+    """Load catalog with info on HESS Galactic sources from individual publications.
+
+    Note that this is different from the
+    `official "H.E.S.S. source catalog" <http://www.mpi-hd.mpg.de/hfm/HESS/pages/home/sources/>`__.
+
+    The main difference is that we only include Galactic sources (and the ones
+    from the large Magellanic cloud) and that we collect more information
+    (morphological and spectral parameters, associations, paper links to ADS),
+    using the latest publication for each source.
+
+    Please drop us an email if you have any questions or find something incorrect or outdated.
+
+    Returns
+    -------
+    catalog : `~astropy.table.Table`
+        Source catalog
+    """
+    filename = get_path('catalogs/hess_galactic_catalog.fits.gz', location='remote')
+    return Table.read(filename)
+
+
+def load_catalog_hgps():
+    """Load the HESS Galactic plane survey (HGPS) catalog.
+
+    Returns
+    -------
+    catalog : `~astropy.table.Table`
+        Source catalog
+    """
+    raise NotImplementedError
 
 
 def load_catalog_green():
     """Load Green's supernova remnant catalog.
 
-    TODO: document
+    This is the May 2014 version of the catalog, which contains 294 sources.
+
+    References:
+
+    - http://www.mrao.cam.ac.uk/surveys/snrs/
+    - http://vizier.u-strasbg.fr/viz-bin/VizieR?-source=VII/272
+    - http://adsabs.harvard.edu/abs/2014BASI...42...47G
+
+    The ``Green_2014-05.fits.gz`` file and ``make_green.py`` script are available
+    `here <https://github.com/gammapy/gammapy-extra/blob/master/datasets/catalogs/>`__.
 
     Returns
     -------
     catalog : `~astropy.table.Table`
         Source catalog
     """
-    filename = get_path('catalogs/Green_2014-05.fits', location='remote')
+    filename = get_path('catalogs/Green_2014-05.fits.gz', location='remote')
     return Table.read(filename)
 
 
 def load_catalog_tevcat():
     """Load TeVCat source catalog.
 
-    This is a dump of TeVCat (http://tevcat.uchicago.edu/)
-    as of 2014-009-24 created by Christoph Deil using this code:
-    https://github.com/astropy/astroquery/pull/41
+    `TeVCat <http://tevcat.uchicago.edu/>`__ is an online catalog
+    for TeV astronomy.
+
+    Unfortunately the TeVCat is not available in electronic format.
+
+    This is a dump of TeVCat as of 2014-09-24 created by scraping their
+    web page using the script available
+    `here <https://github.com/astropy/astroquery/pull/41>`__.
 
     Returns
     -------
     catalog : `~astropy.table.Table`
         Source catalog
     """
-    filename = get_path('tev_catalogs/tevcat.fits.gz')
+    filename = get_path('catalogs/tevcat.fits.gz', location='remote')
     return Table.read(filename)
 
 
@@ -77,16 +123,18 @@ def load_catalog_snrcat():
     `SNRcat <http://www.physics.umanitoba.ca/snr/SNRcat/>`__
     is a census of high-energy observations of Galactic supernova remnants.
 
-    Unfortunately the full information is not available for
-    programmatic access (only as html pages via their webpage).
+    Unfortunately the SNRCat information is not available in electronic format.
+
+    This is a dump of http://www.physics.umanitoba.ca/snr/SNRcat/SNRlist.php?textonly from 2015-02-08.
+    It doesn't contain position and extension columns and is really a HTML page with lots of hidden stuff that
+    would have to be scraped, i.e. we can't be simply ``requests.get`` the latest version from there ... sigh.
+    I've contacted them and they might provide a useful version of their catalog in the future ...
 
     Returns
     -------
     catalog : `~astropy.table.Table`
         Source catalog
     """
-    url = 'http://www.physics.umanitoba.ca/snr/SNRcat/SNRlist.php?textonly'
-    filename = download_file(url, cache=True)
-    # filename = get_path('catalogs/SNRcat.fits', location='remote')
-    return Table.read(filename, format='ascii')
+    filename = get_path('catalogs/SNRCat.csv', location='remote')
+    return Table.read(filename, format='ascii.csv')
 
--- gammapy/datasets/tests/test_catalogs.py
+++ gammapy/datasets/tests/test_catalogs.py
@@ -2,32 +2,35 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from astropy.tests.helper import remote_data
-from ...datasets import (
-    load_catalog_atnf,
-    load_catalog_green,
-    load_catalog_snrcat,
-    load_catalog_tevcat,
-)
+from ... import datasets
 
 
+@remote_data
 def test_load_catalog_atnf():
-    catalog = load_catalog_atnf(small_sample=True)
-    assert len(catalog) == 10
+    catalog = datasets.load_catalog_atnf()
+    assert len(catalog) == 2399
+
+
+# TODO: activate test when available
+@remote_data
+def _test_load_catalog_hess_galactic():
+    catalog = datasets.load_catalog_hess_galactic()
+    assert len(catalog) == 42
 
 
 @remote_data
 def test_load_catalog_green():
-    catalog = load_catalog_green()
-    assert len(catalog) == 173
+    catalog = datasets.load_catalog_green()
+    assert len(catalog) == 294
 
 
 @remote_data
 def test_load_catalog_snrcat():
-    catalog = load_catalog_snrcat()
-    assert len(catalog) == 173
+    catalog = datasets.load_catalog_snrcat()
+    assert len(catalog) == 338
 
 
 @remote_data
 def test_load_catalog_tevcat():
-    catalog = load_catalog_tevcat()
+    catalog = datasets.load_catalog_tevcat()
     assert len(catalog) == 173
