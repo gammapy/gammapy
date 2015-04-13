834567f4fb1481340fc548f6c079ccb1a947b2d2
Christoph Deil <Deil.Christoph@gmail.com>
2015-04-13 16:29:24 +0200

    Skip test_fill_acceptance_image if scipy not available

--- gammapy/background/tests/test_fov.py
+++ gammapy/background/tests/test_fov.py
@@ -1,40 +1,39 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import print_function, division
-
 import numpy as np
-
-from astropy.io import fits
+from astropy.tests.helper import pytest
 from astropy.coordinates import SkyCoord, Angle
 from astropy.wcs import WCS
 from astropy.wcs.utils import pixel_to_skycoord, skycoord_to_pixel
-import astropy.units as u
-from astropy.units.quantity import Quantity
 from astropy.modeling import models
-
 from ...background import fill_acceptance_image
 from ...image import make_empty_image
 from ...image.utils import coordinates
 
+try:
+    import scipy
+    HAS_SCIPY = True
+except ImportError:
+    HAS_SCIPY = False
 
+
+@pytest.mark.skipif('not HAS_SCIPY')
 def test_fill_acceptance_image():
-
-    from scipy.integrate import quad
-
     # create empty image
     # odd number of pixels needed for having the center in its own pixel
     n_pix_x = 101
     n_pix_y = 101
     bin_size = Angle(0.1, 'degree')
-    image = make_empty_image(n_pix_x, n_pix_y, bin_size.to(u.degree).value,
+    image = make_empty_image(n_pix_x, n_pix_y, bin_size.degree,
                              xref=0, yref=0, fill=0,
                              proj='CAR', coordsys='GAL',
                              xrefpix=None, yrefpix=None, dtype='float32')
 
     # define center coordinate of the image in wolrd and pixel coordinates
-    x = image.header['CRVAL1']
-    y = image.header['CRVAL2']
+    lon = image.header['CRVAL1']
+    lat = image.header['CRVAL2']
 
-    center = SkyCoord(l=x*u.degree, b=y*u.degree, frame='galactic')
+    center = SkyCoord(lon, lat, unit='deg', frame='galactic')
 
     # initialize WCS to the header of the image
     w = WCS(image.header)
@@ -42,19 +41,19 @@ def test_fill_acceptance_image():
     x_center_pix, y_center_pix = skycoord_to_pixel(center, w, origin=0)
 
     # define pixel sizes
-    x_pix_size = Angle(abs(image.header['CDELT1'])*u.degree)
-    y_pix_size = Angle(abs(image.header['CDELT2'])*u.degree)
+    # x_pix_size = Angle(abs(image.header['CDELT1']), 'deg')
+    # y_pix_size = Angle(abs(image.header['CDELT2']), 'deg')
 
     # define radial acceptance and offset angles
     # using bin_size for the offset step makes the test comparison easier
-    offset = Angle(np.arange(0., 30., bin_size.to(u.degree).value), 'degree')
-    acceptance = np.zeros_like(offset)
+    offset = Angle(np.arange(0., 30., bin_size.degree), 'deg')
+    # acceptance = np.zeros_like(offset)
     sigma = Angle(1.0, 'degree')  # gaussian width
     amplitude = 1.
     mean = 0.
-    stddev = sigma.to(u.radian).value
+    stddev = sigma.radian
     gaus_model = models.Gaussian1D(amplitude, mean, stddev)
-    acceptance = gaus_model(offset.to(u.radian).value)
+    acceptance = gaus_model(offset.radian)
 
     # fill acceptance in the image
     image = fill_acceptance_image(image, center, offset, acceptance)
@@ -79,7 +78,7 @@ def test_fill_acceptance_image():
     # cut offset and acceptance arrays to match image size
     # this is only valid if the offset step matches the pixel size
     n = pix_off_x_axis.size
-    offset_cut = offset[0:n]
+    # offset_cut = offset[0:n]
     acceptance_cut = acceptance[0:n]
 
     # check acceptance of the image:
