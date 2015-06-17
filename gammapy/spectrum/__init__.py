fe55effba38c3c01f13b38e5d0e61c1b074b4eba
Johannes King <johannes.king@mpi-hd.mpg.de>
2015-06-17 11:05:17 +0200

    Suggestions for energy binning handling

--- gammapy/spectrum/__init__.py
+++ gammapy/spectrum/__init__.py
@@ -6,11 +6,11 @@ from .cosmic_ray import *
 from .crab import *
 from .diffuse import *
 from .fitter import *
-from . import fitting_utils
+from .fitting_utils import *
 from .flux_point import *
 from .isrf import *
-from . import models
-from . import powerlaw
+from .models import *
+from .powerlaw import *
 from .sed import *
-from . import sherpa_chi2asym
+from .sherpa_chi2asym import *
 from .utils import *
