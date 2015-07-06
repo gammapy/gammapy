68a5f8a390b9d5a2df39af66558a55d7163ee195
mapaz <mapazarr@users.noreply.github.com>
2015-07-06 14:16:25 -0700

    CubeBackgroundModel class: using an astropy Table in the to_fits method.

--- examples/plot_background_model.py
+++ examples/plot_background_model.py
@@ -7,6 +7,7 @@ def plot_example():
     filename = '../test_datasets/background/bg_cube_model_test.fits'
     filename = datasets.get_path(filename, location='remote')
     bg_model = CubeBackgroundModel.read_bin_table(filename)
+    print("Plotting all images and spectra (can take a couple of mins).")
     bg_model.plot_images()
     bg_model.plot_spectra()
     outname = 'cube_background_model'
