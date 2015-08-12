0d2b6c23920f36146c67484348a34d976b83d29e
Dirk Lennarz <dirk.lennarz@pa.msu.edu>
2015-08-12 10:44:21 -0400

    Revert "Add Feldman Cousins to import. Order alphabetically."
    This reverts commit e5b43328265c2ba49faa4398ab157a633e5dfbc1.

--- gammapy/stats/__init__.py
+++ gammapy/stats/__init__.py
@@ -2,10 +2,9 @@
 """
 Statistics utility functions and classes
 """
+from .significance import *
 from .data import *
-from .feldman_cousins import *
 from .fit_statistics import *
 from .fitting import *
 from .poisson import *
-from .significance import *
 from .utils import *
