ddfceafe7a217b873e60352d3e00d3b98a138998
Johannes King <johannes.king@mpi-hd.mpg.de>
2015-08-24 19:26:26 +0200

    added plot function, evaluate function, chages to load function

--- gammapy/irf/tests/test_energy_dispersion.py
+++ gammapy/irf/tests/test_energy_dispersion.py
@@ -1,10 +1,10 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_equal
 from astropy.tests.helper import pytest
-from ...irf import EnergyDispersion
-
+from ...irf import EnergyDispersion, EnergyDispersion2D
+from ...datasets import load_edisp2D_fits_table
 
 @pytest.mark.xfail
 def test_EnergyDispersion():
@@ -12,22 +12,30 @@ def test_EnergyDispersion():
     pdf = edisp(3, 4)
     assert_allclose(pdf, 42)
 
-
-
 def test_EnergyDispersion2D():
 
     # Read test effective area file
-    effarea = EffectiveAreaTable2D.from_fits(
-        load_aeff2D_fits_table())
-
-    effarea.interpolation_method = method
+    edisp = EnergyDispersion2D.from_fits(
+        load_edisp2D_fits_table())
 
     # Check that nodes are evaluated correctly
-    e_node = 42
+    e_node = 12
     off_node = 3
-    offset = effarea.offset[off_node]
-    energy = effarea.energy[e_node]
-    actual = effarea.evaluate(offset, energy)
-    desired = effarea.eff_area[off_node, e_node]
-    assert_allclose(actual, desired)
+    m_node = 5
+    offset = edisp.offset[off_node]
+    energy = edisp.energy.log_centers[e_node]
+    migra = edisp.migra[m_node]
+    actual = edisp.evaluate(offset, energy, migra)
+    desired = edisp.dispersion[off_node, m_node, e_node]
+    assert_allclose(actual, desired, rtol=1e-06)
+
+
+    # Check that values between node make sense
+    #THINK ABOUT WHAT MAKES SENSE
+    energy2 = edisp.energy.log_centers[e_node + 1]
+    upper = edisp.evaluate(offset, energy, migra)
+    lower = edisp.evaluate(offset, energy2, migra)
+    e_val = (energy + energy2) / 2
+    actual = edisp.evaluate(offset, e_val, migra)
+    #assert_equal(lower > actual and actual > upper, True)
 
