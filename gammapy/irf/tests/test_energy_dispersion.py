b19b0dc6ee4a1fe5ed3582d64a68ccac5d1e0b79
Johannes King <johannes.king@mpi-hd.mpg.de>
2015-08-24 17:37:34 +0200

    add testing

--- gammapy/irf/tests/test_energy_dispersion.py
+++ gammapy/irf/tests/test_energy_dispersion.py
@@ -11,3 +11,23 @@ def test_EnergyDispersion():
     edisp = EnergyDispersion()
     pdf = edisp(3, 4)
     assert_allclose(pdf, 42)
+
+
+
+def test_EnergyDispersion2D():
+
+    # Read test effective area file
+    effarea = EffectiveAreaTable2D.from_fits(
+        load_aeff2D_fits_table())
+
+    effarea.interpolation_method = method
+
+    # Check that nodes are evaluated correctly
+    e_node = 42
+    off_node = 3
+    offset = effarea.offset[off_node]
+    energy = effarea.energy[e_node]
+    actual = effarea.evaluate(offset, energy)
+    desired = effarea.eff_area[off_node, e_node]
+    assert_allclose(actual, desired)
+
