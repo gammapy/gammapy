c750d7fe2987933c7e6981a9192332eb4ecb1fa0
JonathanDHarris <jonharris24@gmail.com>
2015-08-30 13:18:01 +0100

    Attempt to get catalog_query working

--- gammapy/scripts/catalog_query.py
+++ gammapy/scripts/catalog_query.py
@@ -6,11 +6,13 @@ __all__ = ['catalog_query']
 
 def main(args=None):
     parser = get_parser(catalog_query)
-    parser.add_argument('--catalog',
+    parser.add_argument('catalog',
+                        choices=['3FGL'],
                         help='Catalog for the source e.g. 3FGL')
-    parser.add_argument('--source',
+    parser.add_argument('source',
                         help='Source name e.g. J0349.9-2102')
-    parser.add_argument('--querytype',
+    parser.add_argument('querytype',
+                        choices=['info', 'lightcurve', 'spectrum'],
                         help='The query type: info, lightcurve, or spectrum')
     catalog_query(**vars(args))
 
@@ -23,24 +25,24 @@ def catalog_query(catalog, source, querytype):
 
     if (catalog=='3FGL'):
         catalog_object = fermi.Fermi3FGLObject(source)
-    else:
-        raise NotImplementedError
 
     if querytype == 'info':
-        print(source)
-        print ("RA (J2000) " + str(catalog_object.ra))
-        print ("Dec (J2000) " + str(catalog_object.dec))
-        print ("l " + str(catalog_object.glon))
-        print ("b " + str(catalog_object.glat))
-        print ("Flux " + str(catalog_object.int_flux) + " +/- " + str(catalog_object.unc_int_flux)
-               + " ph /cm2 /MeV /s")
-        print ("Detection significance: " + str(catalog_object.signif) + " sigma")
+        #print(source)
+        #print ("RA (J2000) " + str(catalog_object.ra))
+        #print ("Dec (J2000) " + str(catalog_object.dec))
+        #print ("l " + str(catalog_object.glon))
+        #print ("b " + str(catalog_object.glat))
+        #print ("Flux " + str(catalog_object.int_flux) + " +/- " + str(catalog_object.unc_int_flux)
+        #       + " ph /cm2 /MeV /s")
+        #print ("Detection significance: " + str(catalog_object.signif) + " sigma")
+        raise NotImplementedError
 
     elif querytype == 'lightcurve':
-        raise NotImplementedError#
+        raise NotImplementedError
 
     elif querytype == 'spectrum':
         raise NotImplementedError
 
     else:
-        print("Valid query types are info, light_curve, and spectrum.")
+        #print_function("Valid query types are info, light_curve, and spectrum.")
+        raise NotImplementedError
