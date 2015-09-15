84505b1fcea7bd02341e3619b552ca2cf09e55d1
Johannes King <johannes.king@mpi-hd.mpg.de>
2015-09-15 18:30:06 +0200

    Fix RMF reader, add tests

--- gammapy/irf/tests/test_energy_dispersion.py
+++ gammapy/irf/tests/test_energy_dispersion.py
@@ -1,9 +1,12 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_equal
 from astropy.tests.helper import pytest, remote_data
 from ...irf import EnergyDispersion, EnergyDispersion2D
 from ...datasets import get_path
+from astropy.coordinates import  Angle
+from gammapy.spectrum.energy import EnergyBounds        
+import numpy as np
 
 try:
     import scipy
@@ -11,13 +14,23 @@ try:
 except ImportError:
     HAS_SCIPY = False
 
-
-@pytest.mark.xfail
+@remote_data
 def test_EnergyDispersion():
-    edisp = EnergyDispersion()
-    pdf = edisp(3, 4)
-    assert_allclose(pdf, 42)
+    filename = get_path("../test_datasets/irf/hess/ogip/run_rmf60741.fits",
+                        location='remote')
 
+    edisp = EnergyDispersion.read(filename)
+
+    #Set PDF threshold
+    threshold = 1e-2
+    edisp.pdf_threshold = threshold
+    a = edisp._pdf_matrix > threshold
+    b = edisp._pdf_matrix == 0
+    c = a+b
+    actual = np.sum(c)
+    desired = edisp._pdf_matrix.flatten().shape[0]
+    assert_equal(actual, desired)
+    
 
 @pytest.mark.skipif('not HAS_SCIPY')
 @remote_data
@@ -48,3 +61,12 @@ def test_EnergyDispersion2D():
     e_val = (energy + energy2) / 2
     actual = edisp.evaluate(offset, e_val, migra)
     # assert_equal(lower > actual and actual > upper, True)
+
+    # Check RMF exporter
+    offset = Angle([0.612], 'deg')    
+    e_reco = EnergyBounds.equal_log_spacing(1,10,5,'TeV')
+    rmf = edisp.to_energy_dispersion(e_reco, offset)
+
+    actual = rmf._pdf_matrix[5]
+    desired = edisp.get_response(offset, edisp.energy[5], e_reco=e_reco)
+    assert_equal(actual, desired)
