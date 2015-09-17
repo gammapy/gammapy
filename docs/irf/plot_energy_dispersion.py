816b5d03a0d399ce1f4ff32ceb72ddb0c1b18171
Johannes King <johannes.king@mpi-hd.mpg.de>
2015-09-17 13:37:55 +0200

    added from_gaus alternative constructor

--- docs/irf/plot_energy_dispersion.py
+++ docs/irf/plot_energy_dispersion.py
@@ -1,12 +1,11 @@
 """Plot energy dispersion example."""
 import numpy as np
 import matplotlib.pyplot as plt
-from gammapy import irf
+from gammapy.irf import EnergyDispersion
 from gammapy.spectrum import EnergyBounds
 
 ebounds = EnergyBounds.equal_log_spacing(0.1, 100, 100, 'TeV')
-pdf_matrix = irf.gauss_energy_dispersion_matrix(ebounds, sigma=0.2)
-energy_dispersion = irf.EnergyDispersion(pdf_matrix, ebounds)
+energy_dispersion = EnergyDispersion.from_gauss(ebounds, sigma=0.3)
 
 energy_dispersion.plot(type='matrix')
 plt.show()
