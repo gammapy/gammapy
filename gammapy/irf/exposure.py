4103a102e20c5037720afa8696ad8f29b9617e80
Luigi Tibaldo <ltibaldo@slac.stanford.edu>
2015-12-09 13:50:25 +0100

    added exposure cube calculation

---
+++ gammapy/irf/exposure.py
@@ -0,0 +1,54 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import absolute_import, division, print_function, unicode_literals
+import numpy as np
+from ..data import SpectralCube
+from astropy.coordinates import SkyCoord, Angle
+
+import logging
+
+__all__ = [
+    'exposure_cube'
+]
+
+log = logging.getLogger(__name__)
+
+
+def exposure_cube(pointing,
+                  livetime,
+                  aeff2D,
+                  ref_cube):
+    """Exposure cube
+
+        Parameters
+        ----------
+        pointing : `~astropy.coordinates.SkyCoord`
+            pointing direction
+        livetime : `~astropy.units.Quantity`
+            livetime
+        aeff2D: 'EffectiveAreaTable2D'
+            effective area table
+        ref_cube: 'SpectralCube'
+            reference cube used to define geometry
+
+        Returns
+        -------
+        expcube : 'SpectralCube'
+            3D exposure
+    """
+    ny, nx = ref_cube.data.shape[1:]
+    xx, yy = np.meshgrid(np.arange(nx), np.arange(ny))
+    lon, lat, en = ref_cube.pix2world(xx, yy, 0)
+    coord = SkyCoord(lon, lat, frame=ref_cube.wcs.wcs.radesys.lower())  # don't care about energy
+    offset = coord.separation(pointing)
+    ###this is a hack to make the current code work in spite of issues with offset binning from HAP export
+    ###TODO remove once the issue has been fixed
+    offset = np.clip(offset, Angle(0.2, 'deg'), Angle(2.2, 'deg'))
+
+    exposure = aeff2D.evaluate(offset, ref_cube.energy)
+    exposure = np.rollaxis(exposure, 2)
+    exposure *= livetime
+
+    expcube = SpectralCube(data=exposure,
+                           wcs=ref_cube.wcs,
+                           energy=ref_cube.energy)
+    return expcube
