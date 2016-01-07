4e6a7d0d9fe9fca36c8098954b6d07b1bd183c4e
Johannes King <johannes.king@mpi-hd.mpg.de>
2016-01-07 09:59:26 +0100

    remove dummy exclusion deps

--- docs/spectrum/run_spectrum_analysis.py
+++ docs/spectrum/run_spectrum_analysis.py
@@ -17,8 +17,8 @@ on_region = SkyCircleRegion(pos=center, radius=radius)
 
 bkg_method = dict(type='reflected')
 
-exclusion_file = gammapy_extra.filename("test_datasets/spectrum/"
-                                        "dummy_exclusion.fits")
+exclusion_file = gammapy_extra.filename("datasets/exclusion_masks/"
+                                        "tevcat_exclusion.fits")
 excl = ExclusionMask.from_fits(exclusion_file)
 
 bounds = EnergyBounds.equal_log_spacing(1, 10, 40, unit='TeV')
