fde3b4e0451552f3b7692c19ad10c4186a4acea1
Lea Jouvin <lea.jouvin@gmail.com>
2016-01-18 17:51:52 +0100

    defining the testing function

--- gammapy/background/tests/test_energy_offset_array.py
+++ gammapy/background/tests/test_energy_offset_array.py
@@ -1,11 +1,22 @@
 from ..energy_offset_array import EnergyOffsetArray
+from gammapy.data import DataStore
 from ...datasets import gammapy_extra
+import numpy as np
+import pylab as pt
+pt.ion()
 
 @requires_data('gammapy-extra')
 def test_energy_offset_array():
-    a = EnergyOffsetArray(1, 2, 3)
+    dir=str(gammapy_extra.dir)+'/datasets/hess-crab4'
+    data_store = DataStore.from_dir(dir)
+    Observation_Table=data_store.obs_table
+    event_list_files = data_store.make_table_of_files(Observation_Table,
+                                                          filetypes=['events'])
+    energy=np.logspace(-1,2 , 100)
+    offset=np.linspace(0, 2.5, 100)
+    Array=EnergyOffsetArray(energy,offset)
+    Array.fill_events(event_list_files)
+    Array.plot_image()
 
 
-    assert a.energy == 1
-    assert a.offset == 2.4
-    assert a.data == 3
+    
