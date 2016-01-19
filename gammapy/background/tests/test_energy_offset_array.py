7edb7156437dceab80fbb3c3fa2edf05f63f6a19
Lea Jouvin <lea.jouvin@gmail.com>
2016-01-19 15:04:03 +0100

    pass now a list of Object eventList to the fill_events method

--- gammapy/background/tests/test_energy_offset_array.py
+++ gammapy/background/tests/test_energy_offset_array.py
@@ -1,11 +1,13 @@
 from ..energy_offset_array import EnergyOffsetArray
 from gammapy.data import DataStore
+from gammapy.data import EventList
 from ...datasets import gammapy_extra
 from ...utils.testing import requires_dependency, requires_data
 import numpy as np
-import pylab as pt
-pt.ion()
+#import pylab as pt
 
+
+@requires_dependency('matplotlib')
 @requires_data('gammapy-extra')
 def test_energy_offset_array():
     dir=str(gammapy_extra.dir)+'/datasets/hess-crab4'
@@ -13,11 +15,14 @@ def test_energy_offset_array():
     Observation_Table=data_store.obs_table
     event_list_files = data_store.make_table_of_files(Observation_Table,
                                                           filetypes=['events'])
-    energy=np.logspace(-1,2 , 100)
-    offset=np.linspace(0, 2.5, 100)
-    Array=EnergyOffsetArray(energy,offset)
-    Array.fill_events(event_list_files)
-    Array.plot_image()
-    pt.savefig("test_energy_offset_array.jpg")
+    ev_list=[]
+    for (i,i_ev_file) in enumerate(event_list_files['filename']):
+        ev_list.append(EventList.read(i_ev_file))
+        energy=np.logspace(-1,2 , 100)
+        offset=np.linspace(0, 2.5, 100)
+        Array=EnergyOffsetArray(energy,offset)
+        Array.fill_events(ev_list)
+        Array.plot_image()
+
 
     
