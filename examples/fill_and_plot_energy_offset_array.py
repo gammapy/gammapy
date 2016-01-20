2658eebbed3236582dcc32e3f652e488219eeef4
Lea Jouvin <lea.jouvin@gmail.com>
2016-01-20 17:13:00 +0100

    add the docu for the energy offset array

---
+++ examples/fill_and_plot_energy_offset_array.py
@@ -0,0 +1,33 @@
+import numpy as np
+from gammapy.background import EnergyOffsetArray
+from astropy.table import Table
+from gammapy.data import DataStore
+from gammapy.data import EventList
+from gammapy.datasets import gammapy_extra
+import pylab as pt
+pt.ion()
+
+#Create the Observation Table for the four Crab observations
+dir=str(gammapy_extra.dir)+'/datasets/hess-crab4'
+#Observation_Table=Table.read(dir+"/observations.fits.gz")
+data_store = DataStore.from_dir(dir)
+Observation_Table=data_store.obs_table
+
+#Select the event fits files for these observations
+event_list_files = data_store.make_table_of_files(Observation_Table,
+                                                          filetypes=['events'])
+#List of EventList Object for each observation
+ev_list=[]
+for (i,i_ev_file) in enumerate(event_list_files['filename']):
+    ev_list.append(EventList.read(i_ev_file))
+            
+            
+#Define the bining in energy and offset           
+energy=np.logspace(-1,2 , 100)
+offset=np.linspace(0, 2.5, 100)
+Array=EnergyOffsetArray(energy,offset)
+
+#Fill the EnergyArray and plot the result
+Array.fill_events(ev_list)
+Array.plot_image()
+
