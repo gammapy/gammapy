9027d1e554a0bf7dca85fac8b49870d4190c9788
Lea Jouvin <lea.jouvin@gmail.com>
2016-01-22 14:36:32 +0100

    add ebound.equal_log_spacing

--- examples/example_energy_offset_array.py
+++ examples/example_energy_offset_array.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 from gammapy.data import DataStore
 from gammapy.datasets import gammapy_extra
 from gammapy.background import EnergyOffsetArray
+from gammapy.utils.energy import EnergyBounds
 
 
 def make_counts_array():
@@ -10,9 +12,9 @@ def make_counts_array():
     data_store = DataStore.from_dir(gammapy_extra.dir / 'datasets/hess-crab4')
 
     event_lists = data_store.load_all('events')
-    energy = np.logspace(-1, 2, 100)
+    ebounds = EnergyBounds.equal_log_spacing(0.1, 100, 100, 'TeV')
     offset = np.linspace(0, 2.5, 100)
-    array = EnergyOffsetArray(energy, offset)
+    array = EnergyOffsetArray(ebounds, offset)
     array.fill_events(event_lists)
 
     return array
--- gammapy/background/tests/test_energy_offset_array.py
+++ gammapy/background/tests/test_energy_offset_array.py
@@ -5,6 +5,7 @@ from ..energy_offset_array import EnergyOffsetArray
 from ...data import DataStore, EventList
 from ...datasets import gammapy_extra
 from ...utils.testing import requires_dependency, requires_data
+from ...utils.energy import EnergyBounds
 
 
 @requires_dependency('matplotlib')
@@ -13,8 +14,8 @@ def test_energy_offset_array():
     dir = str(gammapy_extra.dir) + '/datasets/hess-crab4'
     data_store = DataStore.from_dir(dir)
     ev_list = data_store.load_all('events')
-    energy = np.logspace(-1, 2, 100)
+    ebounds = EnergyBounds.equal_log_spacing(0.1, 100, 100, 'TeV')
     offset = np.linspace(0, 2.5, 100)
-    Array = EnergyOffsetArray(energy, offset)
+    Array = EnergyOffsetArray(ebounds, offset)
     Array.fill_events(ev_list)
     Array.plot_image()
