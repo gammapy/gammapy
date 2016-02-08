5004fb2ff2092e352f8c7b527383fc26ad2d67c0
Lea Jouvin <lea.jouvin@gmail.com>
2016-02-08 18:17:50 +0100

    add the write,read method

--- gammapy/background/energy_offset_array.py
+++ gammapy/background/energy_offset_array.py
@@ -3,10 +3,31 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import numpy as np
 from astropy.coordinates import Angle
 from astropy.units import Quantity
+from astropy.table import Table
+from ..utils.fits import table_to_fits_table
 
 __all__ = [
     'EnergyOffsetArray',
 ]
+
+def _make_bin_edges_array(lo, hi):
+    """Make bin edges array from a low values and a high values array.
+
+    TODO: move this function to somewhere else? (i.e. utils?)
+
+    Parameters
+    ----------
+    lo : `~numpy.ndarray`
+        Lower boundaries.
+    hi : `~numpy.ndarray`
+        Higher boundaries.
+
+    Returns
+    -------
+    bin_edges : `~numpy.ndarray`
+        Array of bin edges as ``[[low], [high]]``.
+    """
+    return np.append(lo.flatten(), hi.flatten()[-1:])
 
 
 class EnergyOffsetArray(object):
@@ -20,7 +41,7 @@ class EnergyOffsetArray(object):
     offset : `~astropy.coordinates.Angle`
         offset bin vector
     data : `~numpy.ndarray`
-        data array (2D)
+        data array (2D):
 
     """
 
@@ -104,3 +125,81 @@ class EnergyOffsetArray(object):
         image = ax.imshow(self.data, extent=extent, **kwargs)
         plt.colorbar(image)
         return ax
+
+    @classmethod
+    def read(cls, filename):
+        """Create `EnergyOffsetArray` from background rate 2D FITS file.
+
+        Parameters
+        ----------
+        filename : str
+            File name
+        """
+        hdu_list = fits.open(filename)
+        return cls.from_fits(hdu_list)
+
+    
+        @classmethod
+    def from_fits(cls, hdu, scheme=None):
+        """Read EnergyOffsetArray from a fits binary table.
+
+        Parameters
+        ----------
+        hdu : `~astropy.io.fits.BinTableHDU`
+            HDU binary table for the EnergyOffsetArray.
+        Returns
+        -------
+        cube : `~gammapy.background.EnergyOffsetArray`
+            EnergyOffsetArray object.
+        """
+
+        header = hdu.header
+        data = hdu.data
+        
+        # get offset and energy binning
+        offset_edges = _make_bin_edges_array(data['THETA_LO'], data['THETA_HI'])
+        energy_edges = _make_bin_edges_array(data['ENERG_LO'], data['ENERG_HI'])
+        
+        offset_edges = Angle(offset_edges, header['TUNIT1'])
+        energy_edges = Quantity(energy_edges, header['TUNIT3'])
+
+        # get data
+        bkg_rate=data['bkg']
+        #bkg_rate = Quantity(data, header['TUNIT5'])
+
+        return cls(offset_edges, energy_edges, data=bkg_rate)
+
+    def to_table(self):
+        """Convert EnergyOffsetArray to astropy table format.
+
+        Returns
+        -------
+        table : `~astropy.table.Table`
+            Table containing the EnergyOffsetArray.
+        """
+        # table
+        table = Table()
+        table['THETA_LO']=self.offset[:-1]
+        table['THETA_HI']=self.offset[1:]
+        table[data['ENERG_LO']=self.energy[:-1]
+        table[data['ENERG_HI']=self.energy[1:]
+        table['bkg']=Quantity(self.data, "s-1 MeV-1 sr-1")
+        table.meta['name'] = 'BACKGROUND'
+
+        return table
+
+    def to_fits(self):
+        """Convert EnergyOffsetArray to binary table fits format.
+
+        Returns
+        -------
+        tbhdu : `~astropy.io.fits.BinTableHDU`
+            Table containing the EnergyOffsetArray.
+        """
+        return table_to_fits_table(self.to_table())
+
+    def write(self, filename, **kwargs):
+             """ Write EnergyOffsetArray to fits file"""
+        self.to_fits().writeto(filename, **kwargs)
+        
+        
