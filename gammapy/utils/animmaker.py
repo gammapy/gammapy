428fedaecffe548c8df2030f139575675c9bfec4
Alankar Kotwal <alankarkotwal13@gmail.com>
2016-02-13 07:19:36 -0500

    Correct code to adhere to Astropy coding standards

--- gammapy/utils/animmaker.py
+++ gammapy/utils/animmaker.py
@@ -1,82 +1,86 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Utility class to export animations from Gammapy for visualization"""
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 from matplotlib import animation
-import numpy as np
+
 
 class AnimMaker:
-	"""
-	Utility class to export animation from NumPy arrays
-	
-	The purpose of this class is to generate an animation from a specified
-	sequence of NumPy arrays. Additionally, parameters like frame rate and
-	encoder choice can be specified.
-	
-	Attributes
-	----------
+    """
+    Utility class to export animation from NumPy arrays
 
-	imlist : tuple of ndarrays
-		The input list of ndarrays is stored in this attribute
-	
-	fig : Pyplot figure
-		Container for the figure that is being generated
+    The purpose of this class is to generate an animation from a specified
+    sequence of NumPy arrays. Additionally, parameters like frame rate and
+    encoder choice can be specified.
 
-	im : Pyplot image
-		Container for the pyplot image:w
+    Attributes
+    ----------
 
-	curr_frame : int
-		Frame number of the currently displayed frame
+    imlist : tuple of ndarrays
+        The input list of ndarrays is stored in this attribute
 
-	ani : matplotlib.animation.FuncAnimation object 
-		Animation object for the current animation
+    fig : Pyplot figure
+        Container for the figure that is being generated
 
-	show_anim : function
-		Show the generated animation with the given parameters
+    im : Pyplot image
+        Container for the pyplot image:w
 
-	write_anim : function
-		Write the animation to file with the specified class
-		parameters
+    curr_frame : int
+        Frame number of the currently displayed frame
 
-	Parameters
-	----------
+    ani : matplotlib.animation.FuncAnimation object
+        Animation object for the current animation
 
-	imlist : tuple of ndarrays 
-		Input image NumPy ndarrays in the order in which they
-		have to be put in the output animation.
-	duration : float, optional
-		Time (ms) between consecutive animation frames
-	"""
+    show_anim : function
+        Show the generated animation with the given parameters
 
-	def __init__(self, imlist, duration=200):
-		self.fig = plt.figure()
-		self.imlist = imlist
-		self.im = plt.imshow(self.imlist[0], animated=True)
-		plt.axis('off')
-		self.curr_frame = 0
+    write_anim : function
+        Write the animation to file with the specified class
+        parameters
 
-		def updatefig(*args):
-			self.curr_frame = (self.curr_frame + 1)%len(self.imlist)
-			self.im.set_array(self.imlist[self.curr_frame])
+    Parameters
+    ----------
 
-		self.ani = animation.FuncAnimation(self.fig, updatefig, interval=duration, blit=False)
+    imlist : tuple of ndarrays
+        Input image NumPy ndarrays in the order in which they
+        have to be put in the output animation.
+    duration : float, optional
+        Time (ms) between consecutive animation frames
+    """
 
-	def show_anim(self):
-		"""
-		Show the generated animation
-		"""
-		plt.show()
+    def __init__(self, imlist, duration=200):
+        self.fig = plt.figure()
+        self.imlist = imlist
+        self.im = plt.imshow(self.imlist[0], animated=True)
+        plt.axis('off')
+        self.curr_frame = 0
 
-	def write_anim(self, fname, enc='imagemagick'):
-		"""
-		Write the image list to file 
+        def updatefig(*args):
+            self.curr_frame = (self.curr_frame + 1) % len(self.imlist)
+            self.im.set_array(self.imlist[self.curr_frame])
 
-		Parameters
-		----------
-		
-		fname : string
-			Output file name
-		enc : string, optional
-			Name of the encoder to use while writing the output
-		"""
-		self.ani.save(fname, writer=enc)
+        self.ani = animation.FuncAnimation(self.fig,
+                                           updatefig,
+                                           interval=duration,
+                                           blit=False)
+
+    def show_anim(self):
+        """
+        Show the generated animation
+        """
+        plt.show()
+
+    def write_anim(self, fname, enc='imagemagick'):
+        """
+        Write the image list to file
+
+        Parameters
+        ----------
+
+        fname : string
+            Output file name
+        enc : string, optional
+            Name of the encoder to use while writing the output
+        """
+        self.ani.save(fname, writer=enc)
