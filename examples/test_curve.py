d74ee93e86c4ff19dbb4bf82ea1422b45d5733bf
Lea Jouvin <lea.jouvin@gmail.com>
2016-02-17 19:02:35 +0100

    square root offset distribution

--- examples/test_curve.py
+++ examples/test_curve.py
@@ -3,6 +3,7 @@
 import numpy as np
 from astropy.coordinates import Angle
 from astropy.table import Table
+from astropy.units import Quantity
 from gammapy.datasets import gammapy_extra
 from gammapy.background import EnergyOffsetBackgroundModel
 from gammapy.utils.energy import EnergyBounds, Energy
@@ -14,9 +15,13 @@ def make_model():
     dir = str(gammapy_extra.dir) + '/datasets/hess-crab4-hd-hap-prod2'
     data_store = DataStore.from_dir(dir)
     obs_table = data_store.obs_table
-
     ebounds = EnergyBounds.equal_log_spacing(0.1, 100, 100, 'TeV')
-    offset = Angle(np.linspace(0, 2.5, 100), "deg")
+    theta2_min=Quantity(0, "deg2")
+    theta2_max=Quantity(2.5**2, "deg2")
+    theta2_bin=100
+    theta2=Quantity(np.linspace(theta2_min.value, theta2_max.value, theta2_bin), theta2_min.unit)
+    offset=Angle(np.sqrt(theta2))
+    #offset = Angle(np.linspace(0, 2.5, 100), "deg")
     multi_array = EnergyOffsetBackgroundModel(ebounds, offset)
     multi_array.fill_obs(obs_table, data_store)
     multi_array.compute_rate()
@@ -42,10 +47,10 @@ def plot_model():
     multi_array.livetime.plot_image()
     pt.figure(3)
     multi_array.bg_rate.plot_image()
-
+    pt.figure(4)
     pt.plot(table["offset"], table["Acceptance"])
     pt.xlabel("offset (deg)")
-    pt.ylabel("Acceptance (s-1)")
+    pt.ylabel("Acceptance (s-1 sr-1)")
 
     input()
 
