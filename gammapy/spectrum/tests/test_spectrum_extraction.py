de3410196c5d3d63f17ae41567d09ae2a06e1066
Lea Jouvin <lea.jouvin@gmail.com>
2016-02-19 18:00:34 +0100

    change the method to have a subset of a SpectrumObservationList and ass a test for it

--- gammapy/spectrum/tests/test_spectrum_extraction.py
+++ gammapy/spectrum/tests/test_spectrum_extraction.py
@@ -11,11 +11,9 @@ from ...region import SkyCircleRegion
 from ...spectrum import SpectrumExtraction
 from ...utils.energy import EnergyBounds
 from ...utils.testing import requires_dependency, requires_data
+from ...spectrum.spectrum_extraction import SpectrumObservationList
 
-
-@requires_dependency('scipy')
-@requires_data('gammapy-extra')
-def test_spectrum_extraction(tmpdir):
+def make_spectrum_extraction():
     # Construct w/o config file
     center = SkyCoord(83.63, 22.01, unit='deg', frame='icrs')
     radius = Angle('0.3 deg')
@@ -36,10 +34,38 @@ def test_spectrum_extraction(tmpdir):
     ana = SpectrumExtraction(datastore=ds, obs_ids=obs, on_region=on_region,
                            bkg_method=bkg_method, exclusion=excl,
                            ebounds=bounds)
+    return ana
+
+@requires_dependency('scipy')
+@requires_data('gammapy-extra')
+def test_spectrum_extraction(tmpdir):
+    # Construct w/o config file
+    center = SkyCoord(83.63, 22.01, unit='deg', frame='icrs')
+    radius = Angle('0.3 deg')
+    on_region = SkyCircleRegion(pos=center, radius=radius)
+
+    bkg_method = dict(type='reflected', n_min=2)
+
+    exclusion_file = gammapy_extra.filename(
+        "datasets/exclusion_masks/tevcat_exclusion.fits")
+    excl = ExclusionMask.from_fits(exclusion_file)
+
+    bounds = EnergyBounds.equal_log_spacing(1, 10, 40, unit='TeV')
+
+    obs = [23523, 23559, 11111, 23592]
+    store = gammapy_extra.filename("datasets/hess-crab4-hd-hap-prod2")
+    ds = DataStore.from_dir(store)
+
+    ana = SpectrumExtraction(datastore=ds, obs_ids=obs, on_region=on_region,
+                           bkg_method=bkg_method, exclusion=excl,
+                           ebounds=bounds)
 
     #test methods on SpectrumObservationList
     obs = ana.observations
-    assert len(obs) == 2
-    obs23523 = obs.get_obs_by_id(23523)
+    assert len(obs) == 3
+    obs23523 = obs.get_obslist_from_obsid([23523])[0]
     assert obs23523.on_vector.total_counts == 123
+    new_list = obs.get_obslist_from_obsid([23523, 23592])
+    assert new_list[0].obs_id == 23523
+    assert new_list[1].obs_id == 23592
 
