35b4c08a45ae351f11c03b1681f5c9fb924c7097
Johannes King <johannes.king@mpi-hd.mpg.de>
2016-02-23 17:22:24 +0100

    changes to effective area

--- gammapy/irf/tests/test_effective_area.py
+++ gammapy/irf/tests/test_effective_area.py
@@ -8,6 +8,7 @@ from astropy.utils.data import get_pkg_data_filename
 from astropy.tests.helper import pytest
 from astropy.io import fits
 from ...utils.testing import requires_dependency, requires_data
+from ...utils.energy import EnergyBounds
 from ...irf import EffectiveAreaTable2D, EffectiveAreaTable, abramowski_effective_area
 from ...datasets import gammapy_extra
 
@@ -38,6 +39,8 @@ def test_EffectiveAreaTable():
     arf = EffectiveAreaTable.read(filename)
     assert arf.info() == info_str
 
+    assert (arf.evaluate() == arf.effective_area).all() == True
+
 
 @requires_data('gammapy-extra')
 def test_EffectiveAreaTable_write(tmpdir):
@@ -59,8 +62,13 @@ INTERPOLATION_METHODS = ['linear']
 @requires_dependency('scipy')
 @requires_data('gammapy-extra')
 def test_EffectiveAreaTable2D(method):
-    filename = gammapy_extra.filename('test_datasets/unbundled/irfs/aeff2D.fits')
+    filename = gammapy_extra.filename('datasets/hess-crab4-hd-hap-prod2/run023400-023599/run023523/hess_aeff_2d_023523.fits.gz')
     aeff = EffectiveAreaTable2D.read(filename)
+
+    filename = get_pkg_data_filename('data/aeff2D_info.txt')
+    info_str = open(filename, 'r').read()
+
+    assert aeff.info() == info_str
 
     aeff.interpolation_method = method
 
@@ -68,13 +76,13 @@ def test_EffectiveAreaTable2D(method):
     e_node = 42
     off_node = 3
     offset = aeff.offset[off_node]
-    energy = aeff.energy[e_node]
+    energy = aeff.ebounds.log_centers[e_node]
     actual = aeff.evaluate(offset, energy)
     desired = aeff.eff_area[off_node, e_node]
     assert_allclose(actual, desired)
 
     # Check that values between node make sense
-    energy2 = aeff.energy[e_node + 1]
+    energy2 = aeff.ebounds.log_centers[e_node + 1]
     upper = aeff.evaluate(offset, energy)
     lower = aeff.evaluate(offset, energy2)
     e_val = (energy + energy2) / 2
@@ -87,7 +95,7 @@ def test_EffectiveAreaTable2D(method):
     # Case 1: offset = scalar, energy = None
     offset = Angle(0.234, 'deg')
     actual = aeff.evaluate(offset=offset).shape
-    desired = aeff.energy.shape
+    desired = aeff.ebounds.log_centers.shape
     assert_equal(actual, desired)
 
     # Case 2: offset = scalar, energy = 1Darray
@@ -123,30 +131,27 @@ def test_EffectiveAreaTable2D(method):
 
     # case 6: offset = 2Darray, energy = 1Darray
     nbinse = 16
-    nx, ny = (12,3)
-    offset=np.linspace(1,0,nx*ny).reshape(nx,ny)
+    nx, ny = (12, 3)
+    offset = np.linspace(1, 0, nx * ny).reshape(nx, ny)
     offset = Angle(offset, 'deg')
     energy = Quantity(np.logspace(0, 1, nbinse), 'TeV')
     actual = aeff.evaluate(offset=offset, energy=energy).shape
     desired = np.zeros([nx, ny, nbinse]).shape
     assert_equal(actual, desired)
 
-
     # Test ARF export
     offset = Angle(0.236, 'deg')
     e_axis = Quantity(np.logspace(0, 1, 20), 'TeV')
-    energy_lo = e_axis[:-1]
-    energy_hi = e_axis[1:]
 
-    effareafrom2d = aeff.to_effective_area_table(offset, energy_lo, energy_hi)
+    effareafrom2d = aeff.to_effective_area_table(offset, e_axis)
 
-    energy = Quantity(np.sqrt(energy_lo.value * energy_hi.value), 'TeV')
+    energy = EnergyBounds(e_axis).log_centers
     area = aeff.evaluate(offset, energy)
     effarea1d = EffectiveAreaTable(e_axis, area)
 
     test_energy = Quantity(2.34, 'TeV')
-    actual = effareafrom2d.effective_area_at_energy(test_energy)
-    desired = effarea1d.effective_area_at_energy(test_energy)
+    actual = effareafrom2d.evaluate(test_energy)
+    desired = effarea1d.evaluate(test_energy)
     assert_equal(actual, desired)
 
     # Test ARF export #2
