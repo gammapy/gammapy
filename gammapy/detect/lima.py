2601af28c150153baedba2b1d917714afe2e9550
adonath <axel.donath@mpi-hd.mpg.de>
2016-02-26 15:17:29 +0100

    Rework lima functions tests

--- gammapy/detect/lima.py
+++ gammapy/detect/lima.py
@@ -1,5 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
+from copy import deepcopy
 import logging
 
 import numpy as np
@@ -13,19 +14,24 @@ __all__ = ['compute_lima_map', 'compute_lima_on_off_map']
 log = logging.getLogger(__name__)
 
 
-def _fftconvolve_boundary_nan(array, kernel):
+def _convolve_boundary_nan(array, kernel, fft=True):
     """
-    Wrapper for `~scipy.signal.fftconvolve` that sets all values, that would
-    require any kind of boundary handling to NaN.
+    Wrapper for `~scipy.signal.convolve` an `~scipy.signal.fftconvolve` that
+    sets all values in the results array, that would require any kind of
+    boundary handling to NaN and thus leaves the total size of the resulting
+    array unchanged.
     """
-    from scipy.signal import fftconvolve
-    result = fftconvolve(array, kernel, mode='valid')
+    from scipy.signal import convolve, fftconvolve
+
+    _convolve = fftconvolve if fft else convolve
+
+    result = _convolve(array, kernel, mode='valid')
     padding = [ _ // 2 for _ in kernel.shape]
     return np.pad(result, (padding, padding), mode=str('constant'),
                   constant_values=np.nan)
 
 
-def compute_lima_map(counts, background, kernel, exposure=None):
+def compute_lima_map(counts, background, kernel, exposure=None, fft=False):
     """
     Compute Li&Ma significance and flux maps for known background.
 
@@ -41,62 +47,95 @@ def compute_lima_map(counts, background, kernel, exposure=None):
         convolution kernel. 
     exposure : `~numpy.ndarray`
         Exposure map
+    fft : bool
+        Use fast fft convolution. Default is False.
     
     Returns
     -------
     Bunch : `gammapy.extern.bunch.Bunch`
         Bunch of result maps.
     """
-    
+    # Kernel is modified later make a copy here
+    kernel = deepcopy(kernel)
+
     if not kernel.is_bool:
         log.warn('Using weighted kernels can lead to biased results.')
 
     kernel.normalize('peak')
-    counts_ = _fftconvolve_boundary_nan(counts, kernel.array)
-    background_ = _fftconvolve_boundary_nan(background, kernel.array)
+    counts_ = _convolve_boundary_nan(counts, kernel.array, fft=fft)
+    background_ = _convolve_boundary_nan(background, kernel.array, fft=fft)
 
     significance_lima = significance(counts_, background_, method='lima') 
 
+    result = Bunch(significance=significance_lima,
+                   counts=counts_,
+                   background=background_,
+                   excess= counts_ - background_)
+
     if not exposure is None:
         kernel.normalize('integral')
-        exposure_ = _fftconvolve_boundary_nan(exposure, kernel.array)
+        exposure_ = _convolve_boundary_nan(exposure, kernel.array, fft=fft)
         flux = (counts_ - background_) / exposure_
-        # TODO: return correlated counts and background map?   
-        return Bunch(significance=significance_lima, flux=flux)
+        result.flux = flux
 
-    return Bunch(significance=significance_lima)
+    return result
 
 
-def compute_lima_on_off_map(counts, background, kernel, alpha, exposure=None):
+def compute_lima_on_off_map(counts, off, a_on, a_off, kernel, exposure=None,
+                            fft=False):
     """
-    Compute Li&Ma significance and flux maps for known background.
+    Compute Li&Ma significance and flux maps on off observation.
 
     Parameters
     ----------
     counts : `~numpy.ndarray`
         Count map
-    background : `~numpy.ndarray`
-        Background map
-    alpha : `~numpy.ndarray`
+    off : `~numpy.ndarray`
+        Off counts map.
+    a_on : `~numpy.ndarray`
+        Relative .
+    a_off : `~numpy.ndarray`
         Exposure ratio map.
     kernel : `astropy.convolution.Kernel2D`
         convolution kernel. 
     exposure : `~numpy.ndarray`
-        Exposure map
+        Exposure map.
+    fft : bool
+        Use fast fft convolution. Default is False.
     
     Returns
     -------
     Bunch : `gammapy.extern.bunch.Bunch`
         Bunch of result maps.   
     """
+ 
+    # Kernel is modified later make a copy here
+    kernel = deepcopy(kernel)
+
+    if not kernel.is_bool:
+        log.warn('Using weighted kernels can lead to biased results.')
+
+    kernel.normalize('peak')
+    counts_ = _convolve_boundary_nan(counts, kernel.array, fft=fft)
+    a_ = _convolve_boundary_nan(a_on, kernel.array, fft=fft)
+    alpha = a_ / a_off
+    background = alpha * off
+
+    significance_lima = significance_on_off(counts_, off, alpha, method='lima')
     
+    # safe significance threshold. Is this worse making an option?
+    significance_lima[counts_ < 5] = 0
 
-    flux = 0
-    significance_lima = significance_on_off() 
+    result = Bunch(significance=significance_lima,
+                   counts=counts_,
+                   background=background,
+                   excess=counts_ - background,
+                   alpha=alpha)
 
+    if not exposure is None:
+        kernel.normalize('integral')
+        exposure_ = _convolve_boundary_nan(exposure, kernel.array, fft=fft)
+        flux = (counts_ - background_) / exposure_
+        result.flux = flux
 
-    return Bunch(significance=significance, flux=flux, alpha=alpha)
-
-
-
-
+    return result
--- gammapy/detect/tests/test_lima.py
+++ gammapy/detect/tests/test_lima.py
@@ -5,10 +5,15 @@ import numpy as np
 from numpy.testing.utils import assert_allclose, assert_equal
 
 from astropy.convolution import Tophat2DKernel
+from astropy.io import fits
 
 from ...utils.testing import requires_dependency, requires_data
-from ...detect import compute_ts_map, compute_lima_map
+from ...detect import compute_ts_map, compute_lima_map, compute_lima_on_off_map
+from ...datasets.core import _GammapyExtra
 from ...datasets import load_poisson_stats_image
+from ...data import FitsMapBunch
+
+from pathlib import Path
 
 
 @requires_dependency('scipy')
@@ -26,9 +31,25 @@ def test_compute_lima_map():
     result_ts = compute_ts_map(data['counts'], data['background'], data['exposure'],
                             kernel)
     
-    # Ignore NaN values by setting them to zero 
-    assert_allclose(np.nan_to_num(result_ts.sqrt_ts),
-                    np.nan_to_num(result_lima.significance), atol=1E-3)
-    assert_allclose(np.nan_to_num(result_ts.amplitude),
-                    np.nan_to_num(result_lima.flux), atol=3E-12)
+    assert_allclose(result_ts.sqrt_ts, result_lima.significance, atol=1E-3)
+    assert_allclose(result_ts.amplitude, result_lima.flux, atol=3E-12)
 
+
+@requires_dependency('scipy')
+@requires_data('gammapy-extra')
+def test_compute_lima_on_off_map():
+    """
+    Test Li&Ma map with snippet from the H.E.S.S. survey data.
+    """
+    filename = _GammapyExtra().filename('test_datasets/unbundled/'
+                                        'hess/survey/hess_survey_snippet.fits.gz')
+    data = FitsMapBunch.read(filename)
+
+    kernel = Tophat2DKernel(5)
+
+    result_lima = compute_lima_on_off_map(data.On, data.Off, data.OnExposure,
+                                          data.OffExposure, kernel)
+    
+    # Set boundary to NaN in reference image
+    data.Significance[np.isnan(result_lima.significance)] = np.nan
+    assert_allclose(result_lima.significance, data.Significance, atol=1E-5)
