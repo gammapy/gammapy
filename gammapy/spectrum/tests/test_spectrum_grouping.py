dc3f7406acc0c66172364ab27bea75a7aea84d5c
Christoph Deil <Deil.Christoph@gmail.com>
2016-03-01 16:07:46 +0100

    Add missing requires_dependency to test_spectrum_grouping

--- gammapy/spectrum/tests/test_spectrum_grouping.py
+++ gammapy/spectrum/tests/test_spectrum_grouping.py
@@ -1,16 +1,14 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-
+from __future__ import absolute_import, division, print_function, unicode_literals
 from astropy.tests.helper import assert_quantity_allclose
-
+from ...utils.testing import requires_data, requires_dependency
 from ...datasets import gammapy_extra
 from ...spectrum import SpectrumObservationList
 from ...spectrum.spectrum_fit import SpectrumFit
 from ...spectrum.spectrum_grouping import SpectrumGrouping
-from ...utils.testing import requires_data
 
 
+@requires_dependency('sherpa')
 @requires_data('gammapy-extra')
 def test_define_groups_and_stack(tmpdir):
     phadir = gammapy_extra.filename('datasets/hess-crab4_pha')
@@ -50,13 +48,11 @@ def test_define_groups_and_stack(tmpdir):
     assert_quantity_allclose(fit.result.parameters["norm"],
                              fit_band2.result.parameters["norm"], rtol=1e-1)
 
-
     # Test that if we stack all the runs in one band we get a result close than before
     obs_list2 = group.define_groups_and_stack(offset_range=[0, 2.5],
                                               n_off_bin=1, eff_range=[0, 100],
                                               n_eff_bin=1,
                                               zen_range=[0., 70.], n_zen_bin=1)
-
 
     obs_list2.write_ogip_data(outdir='ogip_data_grouped_all')
     band_obs2 = obs_list2.to_observation_table()
@@ -71,6 +67,7 @@ def test_define_groups_and_stack(tmpdir):
                              fit_band3.result.parameters["index"], rtol=1e0)
     assert_quantity_allclose(fit_band3.result.parameters["norm"],
                              fit.result.parameters["norm"], rtol=1e0)
+
 
 @requires_data('gammapy-extra')
 def test_define_spectral_groups():
