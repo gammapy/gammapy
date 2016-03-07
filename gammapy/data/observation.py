018783ae7d21cb39f285a7f016c933bc18e52c8d
Johannes King <johannes.king@mpi-hd.mpg.de>
2016-03-07 12:48:09 +0100

    add todo

--- gammapy/data/observation.py
+++ gammapy/data/observation.py
@@ -49,6 +49,7 @@ class ObservationTable(Table):
     @lazyproperty
     def index_dict(self):
         """Dict containing row index for all obs ids"""
+        # TODO: Switch to http://astropy.readthedocs.org/en/latest/table/indexing.html once it is more stable
         temp = (zip(self['OBS_ID'], np.arange(len(self))))
         return dict(temp)
 
