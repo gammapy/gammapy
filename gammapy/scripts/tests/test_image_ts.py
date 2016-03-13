0a90bd5e0991553df3d5c802e62817bb989a8404
Olga Vorokh <olga.vorokh@yandex.by>
2016-03-14 02:18:54 +0300

    Remove dead code and regenerate image_ts expected files

--- gammapy/scripts/tests/test_image_ts.py
+++ gammapy/scripts/tests/test_image_ts.py
@@ -3,12 +3,10 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import json
 import numpy as np
-from numpy.testing.utils import assert_allclose, assert_equal
-from astropy.convolution import Gaussian2DKernel
+from numpy.testing.utils import assert_allclose
 from ...utils.testing import requires_dependency, requires_data
-from ...detect import compute_ts_map, TSMapResult
+from ...detect import TSMapResult
 from ...datasets import load_poisson_stats_image
-from ...image.utils import upsample_2N, downsample_2N
 from ..image_ts import image_ts_main
 from astropy.io import fits
 from ...datasets.core import gammapy_extra
@@ -44,53 +42,19 @@ def test_command_line_gammapy_image_ts(tmpdir):
     """Minimal test of gammapy_image_ts using testcase that
     guaranteed to work with compute_ts_map"""
     data = load_poisson_stats_image(extra_info=True)
-
-    kernel = Gaussian2DKernel(2.5, mode='oversample')
-    kernel.normalize()
     data['exposure'] = np.ones(data['counts'].shape) * 1E12
-    for _, func in zip(['counts', 'background', 'exposure'], [np.nansum, np.nansum, np.mean]):
-        data[_] = downsample_2N(data[_], 2, func)
-
-    result = compute_ts_map(data['counts'], data['background'], data['exposure'],
-                            kernel)
-    for name, order in zip(['ts', 'amplitude', 'niter'], [2, 5, 0]):
-        result[name] = np.nan_to_num(result[name])
-        result[name] = upsample_2N(result[name], 2, order=order)
-
-    assert_allclose(1705.840212274973, result.ts[99, 99], rtol=1e-3)
-    assert_allclose([[99], [99]], np.where(result.ts == result.ts.max()))
-    assert_allclose(6, result.niter[99, 99])
-    assert_allclose(1.0227934338735763e-09, result.amplitude[99, 99], rtol=1e-2)
-
-    filename = str(tmpdir / 'ts_test_method.fits')
-    result.write(filename, header=data['header'])
 
     input_filename = str(tmpdir / 'input_all.fits.gz')
-    output_filename = str(tmpdir / "output.fits")
-    output_filename_upsampled2x = str(tmpdir / "output_upsampled2x.fits")
+    create_test_input_file(input_filename, data)
+
     psf_filename = init_psf_file(tmpdir)
 
-    create_test_input_file(input_filename, data)
-    image_ts_main([input_filename,
-                   output_filename,
-                   "--psf",
-                   psf_filename])
+    output_filename = str(tmpdir / "output.fits")
+    output_filename_without_nan = str(tmpdir / "output_without_nan.fits")
+    expected_filename = str(gammapy_extra.dir /
+                         'test_datasets/unbundled/poisson_stats_image/expected_output.fits')
 
-    read_console = TSMapResult.read(output_filename)
-    for name, order in zip(['ts', 'amplitude', 'niter'], [2, 5, 0]):
-            read_console[name] = np.nan_to_num(read_console[name])
-            read_console[name] = upsample_2N(read_console[name], 2, order=order)
-    read_console.write(output_filename_upsampled2x, header=data['header'])
-
-    method_hdu = fits.open(filename)[0]
-    console_hdu = fits.open(output_filename_upsampled2x)[0]
-
-    assert_allclose(method_hdu.data, console_hdu.data, rtol=1e-2, atol=1e-15)
-
-    # check correct work with different scales
-    scales_test_list = ['0.025', '0.075', '0.111']
-    correct_filename = str(gammapy_extra.dir /
-                         'test_datasets/unbundled/poisson_stats_image/correct_output.fits')
+    scales_test_list = ['0.000', '0.050', '0.100', '0.200']
     image_ts_main([input_filename,
                    output_filename,
                    "--psf",
@@ -98,22 +62,21 @@ def test_command_line_gammapy_image_ts(tmpdir):
                    "--scales",
                    scales_test_list[0],
                    scales_test_list[1],
-                   scales_test_list[2]])
+                   scales_test_list[2],
+                   scales_test_list[3]])
 
     for scale_test in scales_test_list:
         output_filename_ = output_filename.replace('.fits', '_{0}.fits'.format(scale_test))
-        correct_filename_ = correct_filename.replace('.fits', '_{0}.fits'.format(scale_test))
+        expected_filename_ = expected_filename.replace('.fits', '_{0}.fits'.format(scale_test))
 
         read_console = TSMapResult.read(output_filename_)
-
-        for name, order in zip(['ts', 'amplitude', 'niter'], [2, 5, 0]):
-            read_console[name] = np.nan_to_num(read_console[name])
-            read_console[name] = upsample_2N(read_console[name], 2, order=order)
-        output_filename_upsampled2x_ = output_filename_upsampled2x.replace('.fits',
+        for name, order in zip(['ts', 'sqrt_ts', 'amplitude', 'niter'], [1, 1, 1, 0]):
+                read_console[name] = np.nan_to_num(read_console[name])
+        output_filename_without_nan_ = output_filename_without_nan.replace('.fits',
                                                                            '_{0}.fits'.format(scale_test))
-        read_console.write(output_filename_upsampled2x_, header=data['header'])
+        read_console.write(output_filename_without_nan_, header=data['header'])
 
-        correct_hdu = fits.open(correct_filename_)[0]
-        console_hdu = fits.open(output_filename_upsampled2x_)[0]
+        expected_hdu = fits.open(expected_filename_)[0]
+        console_hdu = fits.open(output_filename_without_nan_)[0]
 
-        assert_allclose(correct_hdu.data, console_hdu.data, rtol=1e-2, atol=1e-15)
+        assert_allclose(expected_hdu.data, console_hdu.data, rtol=1e-2, atol=1e-15)
