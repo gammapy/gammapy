6165d5f93644941fb3a4618295905a971632a9e1
Lea Jouvin <lea.jouvin@gmail.com>
2016-03-23 09:31:12 +0100

    quantity ok

--- gammapy/spectrum/spectrum_grouping.py
+++ gammapy/spectrum/spectrum_grouping.py
@@ -58,7 +58,7 @@ def group_obs_table(obs_table, offset_range=[0, 2.5], n_off_bin=5,
     val.append(ObservationGroupAxis('OFFSET', offtab, 'edges'))
 
     obs_groups = ObservationGroups(val)
-    cos_zen = np.cos(obs_table['ZEN_PNT'].quantity.value * np.pi / 180.)
+    cos_zen = np.cos(obs_table['ZEN_PNT'].quantity)
     obs_table.add_column(Column(cos_zen, 'COSZEN'))
     grouped_table = obs_groups.apply(obs_table)
 
