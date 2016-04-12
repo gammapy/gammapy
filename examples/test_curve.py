b17e3bb0f1bcd59c857ca2ffb97491a7c42d3c01
Lea Jouvin <lea.jouvin@gmail.com>
2016-04-12 12:25:56 +0200

    reformat ok

--- examples/test_curve.py
+++ examples/test_curve.py
@@ -164,13 +164,13 @@ def make_image_from_2d_bg():
     # TODO: fix `binarize` implementation
     # exclusion_mask = exclusion_mask.binarize()
     image = SkyMap.empty(nxpix=1000, nypix=1000, binsz=0.01, xref=center.l.deg,
-                         yref=center.b.deg, proj='TAN', coordsys = 'GAL')
+                         yref=center.b.deg, proj='TAN', coordsys='GAL')
 
     refheader = image.to_image_hdu().header
     exclusion_mask = ExclusionMask.read('$GAMMAPY_EXTRA/datasets/exclusion_masks/tevcat_exclusion.fits')
     exclusion_mask = exclusion_mask.reproject(refheader=refheader)
     images = ImageAnalysis(image, energy_band=energy_band, offset_band=offset_band, data_store=data_store,
-                           exclusion_mask = exclusion_mask)
+                           exclusion_mask=exclusion_mask)
 
     exclusion_mask = ExclusionMask.read('$GAMMAPY_EXTRA/datasets/exclusion_masks/tevcat_exclusion.fits')
     exclusion_mask = exclusion_mask.reproject(refheader=refheader)
@@ -182,10 +182,6 @@ def make_image_from_2d_bg():
     log.info('Writing {}'.format(filename))
 
     images.maps.write(filename, clobber=True)
-
-    #images.make_total_exposure(2.3)
-    # s_image = images.make_significance(maps["counts"], maps["bkg"], 10.)
-    # s_image.writeto("significance_image.fits", clobber=True)
 
 
 if __name__ == '__main__':
--- gammapy/scripts/image_pipe.py
+++ gammapy/scripts/image_pipe.py
@@ -6,13 +6,11 @@ import numpy as np
 from astropy.coordinates import Angle, SkyCoord
 from astropy.units import Quantity
 from astropy.wcs.utils import pixel_to_skycoord
-from astropy.io import fits
-from ..utils.scripts import get_parser
 from ..background import fill_acceptance_image
 from ..utils.energy import EnergyBounds
 from ..image import SkyMap, SkyMapCollection, disk_correlate
 from ..stats import significance
-from ..image.utils import coordinates
+
 __all__ = ['ImageAnalysis']
 
 log = logging.getLogger(__name__)
@@ -31,31 +29,29 @@ class ImageAnalysis(object):
         Offset Band where you compute the image
     data_store : `~gammapy.data.DataStore`
         `DataStore` where are situated the events
+    obs_table : `~astropy.table.Table`
+            Required columns: OBS_ID
     exclusion_mask : `~gammapy.image.ExclusionMask`
             Exclusion regions
-    counts_image : ~gammapy.image.SkyMap`
-            count image
-    bkg_image : ~gammapy.image.SkyMap`
-            bkg image
     """
 
     def __init__(self, empty_image=None,
                  energy_band=None, offset_band=None,
-                 data_store=None, exclusion_mask = None, counts_image=None, bkg_image=None):
+                 data_store=None, obs_table=None, exclusion_mask=None):
         self.maps = SkyMapCollection()
-        if counts_image:
-            self.maps["total_counts"] = counts_image
-        else:
-            self.data_store = data_store
-            self.energy_band = energy_band
-            self.offset_band = offset_band
 
-            self.empty_image = empty_image
-            self.header = self.empty_image.to_image_hdu().header
-            if exclusion_mask:
-                self.maps['exclusion'] = exclusion_mask
-        if bkg_image:
-            self.maps["total_bkg"] = bkg_image
+        self.data_store = data_store
+        if not obs_table:
+            self.obs_table = self.data_store.obs_table
+        else:
+            self.obs_table = obs_table
+        self.energy_band = energy_band
+        self.offset_band = offset_band
+
+        self.empty_image = empty_image
+        self.header = self.empty_image.to_image_hdu().header
+        if exclusion_mask:
+            self.maps['exclusion'] = exclusion_mask
 
     def make_counts(self, obs_id):
         """Fill the counts image for the events of one observation.
@@ -79,7 +75,7 @@ class ImageAnalysis(object):
 
         """
         self.maps["total_counts"] = SkyMap.empty_like(self.empty_image)
-        for obs_id in self.data_store.obs_table['OBS_ID']:
+        for obs_id in self.obs_table['OBS_ID']:
             self.make_counts(obs_id)
             self.maps["total_counts"].data += self.maps["counts"].data
 
@@ -121,39 +117,26 @@ class ImageAnalysis(object):
         scale = counts_sum / bkg_sum
         self.maps["bkg"].data = scale * self.maps["bkg"].data
 
-
     def make_total_bkg(self):
         """Stack the total bkg from the observation in the 'DataStore'.
         """
         self.maps["total_bkg"] = SkyMap.empty_like(self.empty_image)
-        for obs_id in self.data_store.obs_table['OBS_ID']:
+        for obs_id in self.obs_table['OBS_ID']:
             self.make_background_normalized_offcounts(obs_id)
             self.maps["total_bkg"].data += self.maps["bkg"].data
 
-    def make_significance(self, radius, counts_image=None, bkg_image=None):
-        """Make the dignificance image from the counts and kbg images.
+    def make_significance(self, radius):
+        """Make the significance image from the counts and kbg images.
 
         Parameters
         ----------
         radius : float
             Disk radius in pixels.
-        counts_image : `~gammapy.image.SkyMap`
-            count image
-        bkg_image : `~gammapy.image.SkyMap`
-            bkg image
 
         """
-
-        if not counts_image:
-            self.maps["significance"] = SkyMap.empty_like(self.empty_image)
-            counts_image = self.maps["total_counts"]
-        else:
-            self.maps["significance"] = SkyMap.empty_like(counts_image)
-        if not bkg_image:
-            bkg_image = self.maps["total_bkg"]
-
-        counts = disk_correlate(counts_image.data, radius)
-        bkg = disk_correlate(bkg_image.data, radius)
+        self.maps["significance"] = SkyMap.empty_like(self.empty_image)
+        counts = disk_correlate(self.maps["total_counts"].data, radius)
+        bkg = disk_correlate(self.maps["total_bkg"].data, radius)
         s = significance(counts, bkg)
         self.maps["significance"].data = s
         log.info('Making significance image ...')
@@ -176,55 +159,5 @@ class ImageAnalysis(object):
     def make_psf(self):
         log.info('Making PSF ...')
 
-    def make_exposure_one_obs(self, obs_id, spectral_index):
-        """
-
-        Parameters
-        ----------
-        obs_id
-        spectral_index
-        Returns
-        -------
-
-        """
-        self.maps["exposure"] = SkyMap.empty_like(self.empty_image)
-        xpix_coord_grid, ypix_coord_grid = self.maps["exposure"].coordinates(coord_type='pix')
-        # calculate pixel offset from center (in world coordinates)
-        coord = pixel_to_skycoord(xpix_coord_grid, ypix_coord_grid, self.maps["exposure"].wcs, origin=0)
-        ##TODO:quoi prendre exactement comme centre de pixel pour retrouver exactement le crab
-        center = SkyCoord.from_pixel(self.header["NAXIS1"]/2., self.header["NAXIS2"]/2., self.maps["exposure"].wcs)
-        offset = coord.separation(center)
-        #offset = np.clip(pix_off, Angle(0, 'deg'), self.offset_band[1])
-
-        obs = self.data_store.obs(obs_id=obs_id)
-        livetime = obs.observation_live_time_duration
-
-        energy = EnergyBounds(np.linspace(self.energy_band[0].value, self.energy_band[1].value,100), self.energy_band.unit)
-        energy_band = energy.bands
-        energy_bin = energy.lin_centers
-        spectrum = energy_bin.value**(-spectral_index)
-        aeff2d = obs.aeff
-        for i in range(len(energy_bin)):
-            print(i)
-            self.maps["exposure"].data += aeff2d.evaluate(offset, energy_bin[i]).to("cm2") * spectrum[i]*energy_band[i]
-        self.maps["exposure"].data= self.maps["exposure"].data/np.sum(spectrum*energy_band)
-        self.maps["exposure"].data *= livetime
-        self.maps["exposure"].data[offset >= self.offset_band[1]] = 0
-        self.maps["exposure"].data = self.maps["exposure"].data.value
-
-
-    def make_total_exposure(self, spectral_index):
-        """
-
-        Parameters
-        ----------
-        spectral_index
-
-        Returns
-        -------
-
-        """
-        self.maps["total_exposure"] = SkyMap.empty_like(self.empty_image)
-        for obs_id in self.data_store.obs_table['OBS_ID']:
-                self.make_exposure_one_obs(obs_id, spectral_index)
-                self.maps["total_exposure"].data += self.maps["exposure"].data
+    def make_exposure(self):
+        log.info('Making EXPOSURE ...')
