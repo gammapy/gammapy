5637902f5be3a5abdfd655a576248327bc0e0d84
Christoph Deil <Deil.Christoph@gmail.com>
2016-04-18 09:58:04 +0200

    Minor cleanup of ImageAnalysis

--- gammapy/scripts/image_pipe.py
+++ gammapy/scripts/image_pipe.py
@@ -3,7 +3,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import logging
 import numpy as np
-from scipy import interpolate
 from astropy.units import Quantity
 from astropy.wcs.utils import pixel_to_skycoord
 from astropy.coordinates import SkyCoord, Angle
@@ -20,7 +19,8 @@ log = logging.getLogger(__name__)
 class ImageAnalysis(object):
     """Gammapy 2D image based analysis.
 
-    Fill a `~gammapy.image.SkyMapCollection`:
+    The computed images are stored in a ``maps`` attribute of type `~gammapy.image.SkyMapCollection`
+    with the following keys:
 
     * counts : counts for one obs
     * bkg : bkg model for one obs
@@ -47,7 +47,6 @@ class ImageAnalysis(object):
             Exclusion regions
     ncounts_min : int
             Minimum counts required for one observation
-
     """
 
     def __init__(self, empty_image=None,
@@ -74,16 +73,16 @@ class ImageAnalysis(object):
         ----------
         obs_id : int
             Observation ID
-
         """
-        log.debug('Making counts image ...')
         counts_map = SkyMap.empty_like(self.empty_image)
         obs = self.data_store.obs(obs_id=obs_id)
         events = obs.events.select_energy(self.energy_band)
         events = events.select_offset(self.offset_band)
         counts_map.fill(events=events)
         counts_map.data = counts_map.data.value
+
         self.maps["counts"] = counts_map
+        return counts_map
 
     def _low_counts(self, obs_id):
         """Check if there are events for this observation after applying the offset and energy range.
@@ -95,13 +94,13 @@ class ImageAnalysis(object):
 
         Returns
         -------
-        bool :
-            True if there is no events for this observation for the energy range used to compute the image
-
+        low_counts : bool
+            Does this observation contain low counts?
         """
         obs = self.data_store.obs(obs_id=obs_id)
         events = obs.events.select_energy(self.energy_band)
         events = events.select_offset(self.offset_band)
+
         if len(events) <= self.ncounts_min:
             log.warning('Skipping obs ' + str(obs_id) + ' because it only has ' + str(len(events)) + ' counts.')
             return True
@@ -112,12 +111,15 @@ class ImageAnalysis(object):
         """Stack the total count from the observation in the 'DataStore'.
         """
         total_counts = SkyMap.empty_like(self.empty_image)
+
         for obs_id in self.obs_table['OBS_ID']:
             if self._low_counts(obs_id):
                 continue
             self.make_counts(obs_id)
             total_counts.data += self.maps["counts"].data
+
         self.maps["total_counts"] = total_counts
+        return total_counts
 
     def make_background(self, obs_id, bkg_norm=True):
         """Make the background map for one observation from a bkg model.
@@ -128,10 +130,7 @@ class ImageAnalysis(object):
             Observation ID
         bkg_norm : bool
             If true, apply the scaling factor  to the bkg map
-
-
         """
-        log.debug('Making background image ...')
         bkg_map = SkyMap.empty_like(self.empty_image)
         obs = self.data_store.obs(obs_id=obs_id)
         table = obs.bkg.acceptance_curve_in_energy_band(energy_band=self.energy_band)
@@ -141,15 +140,19 @@ class ImageAnalysis(object):
         bkg_map.data = Quantity(bkg_hdu.data, table["Acceptance"].unit) * bkg_map.solid_angle() * livetime
         bkg_map.data = bkg_map.data.decompose()
         bkg_map.data = bkg_map.data.value
-        self.maps["bkg"] = bkg_map
+
         if bkg_norm:
-            self.make_counts(obs_id)
-            scale = self.background_norm_factor(self.maps["counts"], bkg_map)
-            self.maps["bkg"].data = scale * self.maps["bkg"].data
+            counts_map = self.make_counts(obs_id)
+            scale = self.background_norm_factor(counts_map, bkg_map)
+            bkg_map.data = scale * bkg_map.data
+
+        self.maps["bkg"] = bkg_map
+        return bkg_map
 
     def background_norm_factor(self, counts, bkg):
-        """Determine the scaling factor to apply to the background map by comparing the events in the counts maps
-        and the bkg map outside the exclusion maps.
+        """Determine the scaling factor to apply to the background map.
+
+        Compares the events in the counts maps and the bkg map outside the exclusion maps.
 
         Parameters
         ----------
@@ -166,6 +169,7 @@ class ImageAnalysis(object):
         counts_sum = np.sum(counts.data * self.maps['exclusion'].data)
         bkg_sum = np.sum(bkg.data * self.maps['exclusion'].data)
         scale = counts_sum / bkg_sum
+
         return scale
 
     def make_total_bkg(self, bkg_norm=True):
@@ -177,12 +181,15 @@ class ImageAnalysis(object):
             If true, apply the scaling factor  to the bkg map
         """
         total_bkg = SkyMap.empty_like(self.empty_image)
+
         for obs_id in self.obs_table['OBS_ID']:
             if self._low_counts(obs_id):
                 continue
             self.make_background(obs_id, bkg_norm)
             total_bkg.data += self.maps["bkg"].data
+
         self.maps["total_bkg"] = total_bkg
+        return total_bkg
 
     def make_significance(self, radius):
         """Make the significance image from the counts and bkg images.
@@ -196,19 +203,18 @@ class ImageAnalysis(object):
         -------
         s_maps : `~gammapy.image.SkyMap`
             significance map
-
         """
-        log.debug('Making significance image ...')
-        s_maps = SkyMap.empty_like(self.empty_image)
+        s_map = SkyMap.empty_like(self.empty_image)
         counts = disk_correlate(self.maps["total_counts"].data, radius)
         bkg = disk_correlate(self.maps["total_bkg"].data, radius)
         s = significance(counts, bkg)
-        s_maps.data = s
-        self.maps["significance"] = s_maps
-        return s_maps
+        s_map.data = s
+
+        self.maps["significance"] = s_map
+        return s_map
 
     def make_maps(self, radius, bkg_norm=True, spectral_index=2.3, for_integral_flux=False):
-        """Compute the counts, bkg, exlusion_mask and significance images for a set of observation
+        """Compute the counts, bkg, exclusion_mask and significance images for a set of observation.
 
         Parameters
         ----------
@@ -228,20 +234,24 @@ class ImageAnalysis(object):
         self.make_total_exposure(spectral_index, for_integral_flux)
 
     def make_psf(self):
-        log.info('Making PSF ...')
+        """Make PSF.
+        """
+        raise NotImplementedError
 
     def make_total_excess(self):
         """Compute excess between counts and bkg map."""
-        self.maps["total_excess"] = SkyMap.empty_like(self.empty_image)
-        excess = self.maps["total_counts"].data - self.maps["total_bkg"].data
-        self.maps["total_excess"].data = excess
+        total_excess = SkyMap.empty_like(self.empty_image)
+        total_excess.data = self.maps["total_counts"].data - self.maps["total_bkg"].data
+
+        self.maps["total_excess"] = total_excess
+        return total_excess
 
     def make_exposure(self, obs_id, spectral_index=2.3, for_integral_flux=False):
         r"""Compute the exposure map for one observation.
 
-        Excess/exposure will give the differential flux at the energy Eref at the middle of the self.energy_band
+        Excess/exposure will give the differential flux at the energy Eref at the middle of the ``self.energy_band``
 
-        If for_integral_flux is true, it will give the integrated flux over the self.energy_band
+        If ``for_integral_flux`` is true, it will give the integrated flux over the ``self.energy_band``
 
         Exposure is define as follow:
 
@@ -249,13 +259,13 @@ class ImageAnalysis(object):
 
             EXPOSURE = \int_{E_1}^{E_2} A(E) \phi(E) * T \, dE
 
-        with T the observation livetime, A(E) the surface area, the energy integration range :math: `[E_1,E_2]`=
-        self.energy_range and assuming a power law for the flux
-        :math:`\phi(E) = \phi_{Eref} \times \frac{E}{E_ref}^{\gamma}` with :math:
-        `\gamma` the spectral index of the assumed power law.
+        with ``T`` the observation livetime, ``A(E)`` the effective area,
+        the energy integration range :math:`[E_1,E_2]` given by ``self.energy_range``
+        and assuming a power law for the flux :math:`\phi(E) = \phi_{Eref} \times \frac{E}{E_{ref}}^{\gamma}`
+        with :math:`\gamma` the spectral index of the assumed power law.
 
-        If for_integral_flux is true, :math:`EXPOSURE = \int_{E_1}^{E_2} A(E) \phi_{E} * T \, dE / \int \phi_{E} \, dE`
-
+        If ``for_integral_flux`` is true,
+        :math:`EXPOSURE = \int_{E_1}^{E_2} A(E) \phi_{E} * T \, dE / \int \phi_{E} \, dE`
 
         Parameters
         ----------
@@ -265,8 +275,9 @@ class ImageAnalysis(object):
             Assumed power-law spectral index
         for_integral_flux : bool
             True if you want that the total excess / exposure gives the integrated flux
-
         """
+        from scipy.interpolate import interp1d
+
         # TODO: should be re-implemented using the exposure_cube function
         exposure = SkyMap.empty_like(self.empty_image)
 
@@ -295,11 +306,13 @@ class ImageAnalysis(object):
             exposure_tab /= norm
 
         # Interpolate for the offset of each pixel
-        f = interpolate.interp1d(offset_tab, exposure_tab, bounds_error=False, fill_value=0)
+        f = interp1d(offset_tab, exposure_tab, bounds_error=False, fill_value=0)
         exposure.data = f(offset)
         exposure.data *= livetime
         exposure.data[offset >= self.offset_band[1]] = 0
+
         self.maps["exposure"] = exposure
+        return exposure
 
     def make_total_exposure(self, spectral_index=2.3, for_integral_flux=False):
         """Compute the exposure map for all the observations in the obs_table.
@@ -312,9 +325,12 @@ class ImageAnalysis(object):
             True if you want that the total excess / exposure gives the integrated flux
         """
         exposure_map = SkyMap.empty_like(self.empty_image)
+
         for obs_id in self.obs_table['OBS_ID']:
             if self._low_counts(obs_id):
                 continue
             self.make_exposure(obs_id, spectral_index, for_integral_flux)
             exposure_map.data += self.maps["exposure"].data
+
         self.maps["total_exposure"] = exposure_map
+        return exposure_map
