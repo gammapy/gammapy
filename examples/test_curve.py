206c249b73832e113a886594c9284f00fa8c7b38
Lea Jouvin <lea.jouvin@gmail.com>
2016-05-02 15:31:47 +0200

    clean test_curve

--- examples/test_curve.py
+++ examples/test_curve.py
@@ -1,21 +1,12 @@
 """Example how to make an acceptance curve and background model image.
 """
-from astropy.table import Table
-import astropy.units as u
-from astropy.io import fits
+
 from astropy.coordinates import SkyCoord, Angle
-from astropy.units import Quantity
-from gammapy.datasets import gammapy_extra
-from gammapy.background import EnergyOffsetBackgroundModel
-from gammapy.utils.energy import EnergyBounds, Energy
+from gammapy.utils.energy import Energy
 from gammapy.data import DataStore
-from gammapy.utils.axis import sqrt_space
-from gammapy.image import bin_events_in_image, disk_correlate, SkyMap, ExclusionMask
-from gammapy.background import fill_acceptance_image
-from gammapy.region import SkyCircleRegion
-from gammapy.stats import significance
+from gammapy.image import SkyMap, ExclusionMask
 from gammapy.background import OffDataBackgroundMaker
-from gammapy.scripts import ImageAnalysis, ObsImage, MosaicImage
+from gammapy.scripts import MosaicImage
 import matplotlib.pyplot as plt
 
 plt.ion()
@@ -25,96 +16,6 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-
-def make_excluded_sources():
-    # centers = SkyCoord([84, 82], [23, 21], unit='deg')
-    centers = SkyCoord([83.63, 83.63], [22.01, 22.01], unit='deg', frame='icrs')
-    radius = Angle('0.3 deg')
-    sources = SkyCircleRegion(pos=centers, radius=radius)
-    catalog = Table()
-    catalog["RA"] = sources.pos.data.lon
-    catalog["DEC"] = sources.pos.data.lat
-    catalog["Radius"] = sources.radius
-    return catalog
-
-
-def make_model():
-    dir = str(gammapy_extra.dir) + '/datasets/hess-crab4-hd-hap-prod2'
-    data_store = DataStore.from_dir(dir)
-    obs_table = data_store.obs_table
-    ebounds = EnergyBounds.equal_log_spacing(0.1, 100, 100, 'TeV')
-    offset = sqrt_space(start=0, stop=2.5, num=100) * u.deg
-
-    excluded_sources = make_excluded_sources()
-
-    multi_array = EnergyOffsetBackgroundModel(ebounds, offset)
-    multi_array.fill_obs(obs_table, data_store, excluded_sources)
-    # multi_array.fill_obs(obs_table, data_store)
-    multi_array.compute_rate()
-    bgarray = multi_array.bg_rate
-    energy_range = Energy([1, 10], 'TeV')
-    table = bgarray.acceptance_curve_in_energy_band(energy_range, energy_bins=10)
-
-    multi_array.write('energy_offset_array.fits', overwrite=True)
-    table.write('acceptance_curve.fits', overwrite=True)
-
-
-def make_image():
-    table = Table.read('acceptance_curve.fits')
-    table.pprint()
-    center = SkyCoord(83.63, 22.01, unit='deg').galactic
-
-    counts_image = SkyMap.empty(nxpix=1000, nypix=1000, binsz=0.01, xref=center.l.deg, yref=center.b.deg,
-                                proj='TAN').to_image_hdu()
-    bkg_image = counts_image.copy()
-    data_store = DataStore.from_dir('$GAMMAPY_EXTRA/datasets/hess-crab4-hd-hap-prod2')
-
-    for events in data_store.load_all("events"):
-        center = events.pointing_radec.galactic
-        livetime = events.observation_live_time_duration
-        solid_angle = Angle(0.01, "deg") ** 2
-
-        counts_image.data += bin_events_in_image(events, counts_image).data
-
-        # interp_param = dict(bounds_error=False, fill_value=None)
-
-        acc_hdu = fill_acceptance_image(bkg_image.header, center, table["offset"], table["Acceptance"])
-        acc = Quantity(acc_hdu.data, table["Acceptance"].unit) * solid_angle * livetime
-        bkg_image.data += acc.decompose()
-        print(acc.decompose().sum())
-
-    counts_image.writeto("counts_image_save.fits", clobber=True)
-    bkg_image.writeto("bkg_image_save.fits", clobber=True)
-
-    # result = compute_ts_map(counts_stacked_image.data, bkg_stacked_image.data,
-    #  maps['ExpGammaMap'].data, kernel)
-
-
-def make_significance_image():
-    counts_image = fits.open("counts_image.fits")[1]
-    bkg_image = fits.open("bkg_image.fits")[1]
-    counts = disk_correlate(counts_image.data, 10)
-    bkg = disk_correlate(bkg_image.data, 10)
-    s = significance(counts, bkg)
-    s_image = fits.ImageHDU(data=s, header=counts_image.header)
-    s_image.writeto("significance_image.fits", clobber=True)
-
-
-def plot_model():
-    multi_array = EnergyOffsetBackgroundModel.read('energy_offset_array.fits')
-    table = Table.read('acceptance_curve.fits')
-    plt.figure(1)
-    multi_array.counts.plot_image()
-    plt.figure(2)
-    multi_array.livetime.plot_image()
-    plt.figure(3)
-    multi_array.bg_rate.plot_image()
-    plt.figure(4)
-    plt.plot(table["offset"], table["Acceptance"])
-    plt.xlabel("offset (deg)")
-    plt.ylabel("Acceptance (s-1 sr-1)")
-
-    input()
 
 
 def make_bg_model_two_groups():
@@ -165,39 +66,14 @@ def make_image_from_2d_bg():
     refheader = image.to_image_hdu().header
     exclusion_mask = ExclusionMask.read('$GAMMAPY_EXTRA/datasets/exclusion_masks/tevcat_exclusion.fits')
     exclusion_mask = exclusion_mask.reproject(reference=refheader)
-    images = ImageAnalysis(image, energy_band=energy_band, offset_band=offset_band, data_store=data_store,
+    mosaic_images=MosaicImage(image, energy_band=energy_band, offset_band=offset_band, data_store=data_store,
                            obs_table=data_store.obs_table, exclusion_mask=exclusion_mask)
-
-    #import IPython; IPython.embed()
-    events= data_store.obs(23526).events
-    obsimage=ObsImage(events, data_store, image, energy_band, offset_band, exclusion_mask, 0)
-    obsimage.counts_map()
-    obsimage.bkg_map()
-    obsimage.exposure_map()
-
-    images.make_counts(23526)
-    images.make_background(23526)
-    images.make_exposure(23526)
-    import numpy as np
-    #import IPython; IPython.embed()
-    print(np.where(images.maps["counts"].data!=obsimage.maps["counts"].data))
-    print(np.where(images.maps["bkg"].data!=obsimage.maps["bkg"].data))
-    print(np.where(images.maps["exposure"].data!=obsimage.maps["exposure"].data))
-    images.make_maps(radius=10., bkg_norm=True, spectral_index=2.3, for_integral_flux = True)
-    mosaicimages=MosaicImage(image, energy_band=energy_band, offset_band=offset_band, data_store=data_store,
-                           obs_table=data_store.obs_table, exclusion_mask=exclusion_mask)
-    mosaicimages.make_images(make_background_image = True, for_integral_flux = True, radius=10.)
+    mosaic_images.make_images(make_background_image = True, for_integral_flux = True, radius=10.)
     filename = 'fov_bg_maps_test.fits'
     log.info('Writing {}'.format(filename))
-    import IPython; IPython.embed()
-    mosaicimages.maps.write(filename, clobber=True)
+    mosaic_images.maps.write(filename, clobber=True)
 
 
 if __name__ == '__main__':
-    # make_model()
-    # plot_model()
-    # make_image()
-    # make_significance_image()
-
     # make_bg_model_two_groups()
     make_image_from_2d_bg()
