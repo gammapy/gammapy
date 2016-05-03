cc74f0c3aa7816cb8829727bc065dc2651b66eb5
Lea Jouvin <lea.jouvin@gmail.com>
2016-05-03 11:38:51 +0200

    add test on counts value..etc

--- gammapy/scripts/tests/test_image_pipe.py
+++ gammapy/scripts/tests/test_image_pipe.py
@@ -1,6 +1,7 @@
 """Example how to make an acceptance curve and background model image.
 """
 from astropy.coordinates import SkyCoord, Angle
+from numpy.testing import assert_equal
 from gammapy.utils.energy import Energy
 from gammapy.data import DataStore
 from gammapy.image import SkyMap, ExclusionMask
@@ -60,6 +61,11 @@ def test_image_pipe(tmpdir):
     exclusion_mask = exclusion_mask.reproject(reference=refheader)
     # Pb with the load psftable for one of the run that is not implemented yet...
     data_store.hdu_table.remove_row(14)
-    mosaic_images = MosaicImage(image, energy_band=energy_band, offset_band=offset_band, data_store=data_store,
-                                obs_table=data_store.obs_table, exclusion_mask=exclusion_mask)
-    mosaic_images.make_images(make_background_image=True, for_integral_flux=True, radius=10.)
+    mosaic = MosaicImage(image, energy_band=energy_band, offset_band=offset_band, data_store=data_store,
+                         obs_table=data_store.obs_table, exclusion_mask=exclusion_mask)
+    mosaic.make_images(make_background_image=True, for_integral_flux=True, radius=10.)
+    assert_equal(mosaic.maps['counts'].data.sum(), 2334.0)
+    assert_equal(mosaic.maps['bkg'].data.sum(), 1987.2088283446637)
+    assert_equal(mosaic.maps['exposure'].data.sum(), 5.9292301086257267e17)
+    assert_equal(mosaic.maps['significance'].lookup(center), 33.699068269123039)
+    assert_equal(mosaic.maps['excess'].data.sum(), 346.79117165533626)
