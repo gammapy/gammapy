1f0f62f343c02e39b11fe80ca0d50542658b73fe
hijlk <julien.lefaucheur@obspm.fr>
2016-05-18 14:53:11 +0200

    Add Offset plot and summary informations

--- gammapy/data/observation_summary.py
+++ gammapy/data/observation_summary.py
@@ -1,6 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
 import numpy as np
+from astropy.units import Quantity
 from astropy.coordinates import SkyCoord
 
 from .observation import ObservationTable
@@ -12,9 +13,8 @@ __all__ = [
 class ObservationTableSummary(object):
     """Observation table summary.
 
-    This is an `~astropy.table.Table` sub-class, with a few
-    convenience methods. The format of the observation table
-    is described in :ref:`dataformats_observation_lists`.
+    Class allowing to summarize informations conatained in 
+    Observation index table (`~gammapy.data.ObservationTable`)
 
     Parameters:
     -----------
@@ -28,13 +28,28 @@ class ObservationTableSummary(object):
         self.obs_table = obs_table
         self.target_pos = target_pos
 
+    def get_offset(self):
+        """
+        Compute offsets of the different observations
+        
+        Returns:
+        --------
+        vector: `~numpy.array`
+            Offsets
+        """
+        pnt_coord = SkyCoord(np.array(self.obs_table['RA_PNT']),
+                             np.array(self.obs_table['DEC_PNT']),
+                             unit='deg')
+
+        return pnt_coord.separation(self.target_pos)
+    
     def plot_zenith_distribution(self, ax=None, bins=100, range=(0,100.)):
         """
         Construct the zenith distribution of the observations
 
         Returns:
         --------
-        ax : `~matplolib.axes`
+        ax: `~matplolib.axes`
             Axis
         """
         import matplotlib.pyplot as plt
@@ -49,19 +64,24 @@ class ObservationTableSummary(object):
         
         return ax
 
-    def plot_offset_distribution(self, ax=None):
+    def plot_offset_distribution(self, ax=None, bins=10, range=(0,3.)):
         """
         Construct the zenith distribution of the observations
 
         Returns:
         --------
-        ax : `~matplolib.axes`
+        ax: `~matplolib.axes`
             Axis
         """
         import matplotlib.pyplot as plt
         ax = plt.gca() if ax is None else ax
 
-        
+        offset = self.get_offset()
+
+        ax.hist(offset, range=range, bins=bins)
+        ax.set_title('Offset distribution')
+        ax.set_xlabel('Offset (Deg)')
+        ax.set_ylabel('#Entries')
         
         return ax
 
@@ -69,9 +89,18 @@ class ObservationTableSummary(object):
         """Summary report"""
         ss = '*** Observation summary ***\n'
         ss += 'Target position: {}\n'.format(self.target_pos)
-
-        # TODO: print some summary stats here ...
-
+        
+        ss += 'Number of observations: {}\n'.format(len(self.obs_table))
+        
+        livetime = Quantity(sum(self.obs_table['LIVETIME']), 'second')
+        ss += 'Livetime: {:.2f}\n'.format(livetime.to('hour'))
+        zenith = np.array(self.obs_table['ZEN_PNT'])
+        ss += 'Zenith angle: (mean={:.2f}, std={:.2f})\n'.format(zenith.mean(),
+                                                                 zenith.std())    
+        offset = self.get_offset()
+        ss += 'Offset: (mean={:.2f}, std={:.2f})\n'.format(offset.mean(),
+                                                           offset.std())  
+        
         return ss
 
     def show_in_browser(self):
