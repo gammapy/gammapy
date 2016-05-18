1615fddb29be1cff250299af560bb92c577a6cc7
hijlk <julien.lefaucheur@obspm.fr>
2016-05-18 16:31:38 +0200

    Fix doc format, add property to offset function, raise exception for to be implemented function and add default binning for plots

--- gammapy/data/observation_summary.py
+++ gammapy/data/observation_summary.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import numpy as np
 from astropy.units import Quantity
-from astropy.coordinates import SkyCoord
+from astropy.coordinates import SkyCoord, Angle
 
 from .observation import ObservationTable
 
@@ -28,57 +28,81 @@ class ObservationTableSummary(object):
         self.obs_table = obs_table
         self.target_pos = target_pos
 
-    def get_offset(self):
+    @property
+    def _offset(self):
         """
         Compute offsets of the different observations
         
-        Returns:
-        --------
-        vector: `~numpy.array`
+        Returns
+        -------
+        vector : `~numpy.array`
             Offsets
         """
-        pnt_coord = SkyCoord(np.array(self.obs_table['RA_PNT']),
-                             np.array(self.obs_table['DEC_PNT']),
-                             unit='deg')
+        pnt_pos = SkyCoord(self.obs_table['RA_PNT'],
+                           self.obs_table['DEC_PNT'],
+                           unit='deg')
 
-        return pnt_coord.separation(self.target_pos)
+        offset = pnt_pos.separation(self.target_pos)
+        
+        return offset.degree
     
-    def plot_zenith_distribution(self, ax=None, bins=100, range=(0,100.)):
+    def plot_zenith_distribution(self, ax=None, bins=None):
         """
         Construct the zenith distribution of the observations
+        
+        Parameters
+        ----------
+        ax : `~matplolib.axes`, optional
+            Axis
+        bins : integer
+            number of bins, optional
+        range : `range`
+            range of the x axis, optional
 
-        Returns:
+        Returns
         --------
-        ax: `~matplolib.axes`
+        ax : `~matplolib.axes`
             Axis
         """
         import matplotlib.pyplot as plt
         ax = plt.gca() if ax is None else ax
 
-        zenith = np.array(self.obs_table['ZEN_PNT'])
+        zenith = self.obs_table['ZEN_PNT']
 
-        ax.hist(zenith, range=range, bins=bins)
+        if bins is None:
+            bins = np.linspace(0, zenith.max() + 5, 30)
+
+        ax.hist(zenith, bins=bins)
         ax.set_title('Zenith distribution')
         ax.set_xlabel('Zenith (Deg)')
         ax.set_ylabel('#Entries')
         
         return ax
 
-    def plot_offset_distribution(self, ax=None, bins=10, range=(0,3.)):
+    def plot_offset_distribution(self, ax=None, bins=None):
         """
-        Construct the zenith distribution of the observations
+        Construct the offset distribution of the observations
+        
+        Parameters
+        ----------
+        ax : `~matplolib.axes`, optional
+            Axis
+        bins : integer
+            number of bins, optional
 
-        Returns:
-        --------
-        ax: `~matplolib.axes`
+        Returns
+        -------
+        ax : `~matplolib.axes`
             Axis
         """
         import matplotlib.pyplot as plt
         ax = plt.gca() if ax is None else ax
+        
+        offset = self._offset
 
-        offset = self.get_offset()
-
-        ax.hist(offset, range=range, bins=bins)
+        if bins is None:
+            bins = np.linspace(0, offset.max()+0.5, 10)
+        ax.hist(offset, bins=bins)
         ax.set_title('Offset distribution')
         ax.set_xlabel('Offset (Deg)')
         ax.set_ylabel('#Entries')
@@ -94,10 +118,10 @@ class ObservationTableSummary(object):
         
         livetime = Quantity(sum(self.obs_table['LIVETIME']), 'second')
         ss += 'Livetime: {:.2f}\n'.format(livetime.to('hour'))
-        zenith = np.array(self.obs_table['ZEN_PNT'])
+        zenith = self.obs_table['ZEN_PNT']
         ss += 'Zenith angle: (mean={:.2f}, std={:.2f})\n'.format(zenith.mean(),
                                                                  zenith.std())    
-        offset = self.get_offset()
+        offset = self._offset
         ss += 'Offset: (mean={:.2f}, std={:.2f})\n'.format(offset.mean(),
                                                            offset.std())  
         
@@ -105,3 +129,4 @@ class ObservationTableSummary(object):
 
     def show_in_browser(self):
         """Make HTML file and images in tmp dir, open in browser"""
+        raise NotImplementedError
