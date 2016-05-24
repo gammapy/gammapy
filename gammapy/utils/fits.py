add7e5d50c4536fb7c646b9bef12649aa6e8bf43
Christoph Deil <Deil.Christoph@gmail.com>
2016-05-24 18:42:31 +0200

    Fix table_to_fits_table

--- gammapy/utils/fits.py
+++ gammapy/utils/fits.py
@@ -5,13 +5,11 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from astropy.io import fits
 from astropy.table import Table
 
-__all__ = ['get_hdu',
-           'get_image_hdu',
-           'get_table_hdu',
-           'fits_table_to_pandas',
-           'table_to_fits_table',
-           'fits_table_to_table',
-           ]
+__all__ = [
+    'get_hdu',
+    'table_to_fits_table',
+    'fits_table_to_table',
+]
 
 
 def get_hdu(location):
@@ -39,80 +37,38 @@ def get_hdu(location):
         return fits.open(file_name)[0]
 
 
-def get_image_hdu():
-    """Get the first image HDU."""
-    raise NotImplementedError
-
-
-def get_table_hdu():
-    """Get the first table HDU."""
-    raise NotImplementedError
-
-
-def fits_table_to_pandas(filename, index_columns):
-    """Convert a FITS table HDU to a `pandas.DataFrame`.
-
-    Parameters
-    ----------
-    TODO
-
-    Returns
-    -------
-    TODO
-    """
-    # TODO: really make this work for an astropy Table (not a TableHDU or filename).
-    from pandas import DataFrame
-    data = fits.getdata(filename)
-
-    # Convert byte order to native ... this is required by pandas
-    # See https://github.com/astropy/astropy/issues/1156
-    # and http://pandas.pydata.org/pandas-docs/dev/faq.html#byte-ordering-issues
-    data = data.byteswap().newbyteorder()
-    table = DataFrame(data)
-
-    # Strip whitespace for string columns that will become indices
-    for index_column in index_columns:
-        table[index_column].map(str.strip)
-    table = table.set_index(index_columns)
-
-    return table
-
-
 def table_to_fits_table(table):
-    """Convert astropy table to binary table FITS format.
+    """Convert `~astropy.table.Table` to `astropy.io.fits.BinTableHDU`.
 
-    This is a generic method to convert a `~astropy.table.Table`
-    to a `~astropy.io.fits.BinTableHDU`.
     The name of the table can be stored in the Table meta information
     under the ``name`` keyword.
 
     Parameters
     ----------
     table : `~astropy.table.Table`
-        astropy table containing the desired columns
+        Table
 
     Returns
     -------
     hdu : `~astropy.io.fits.BinTableHDU`
-        FITS bin table containing the astropy table columns
+        Binary table HDU
     """
     # read name and drop it from the meta information, otherwise
     # it would be stored as a header keyword in the BinTableHDU
-    if 'name' in table.meta:
-        name = table.meta.popitem('name')[1]
-    else:
-        name = None
+    name = table.meta.pop('name', None)
 
+    table.convert_unicode_to_bytestring(python3_only=True)
     data = table.as_array()
 
     header = fits.Header()
     header.update(table.meta)
 
-    tbhdu = fits.BinTableHDU(data, header, name=name)
+    hdu = fits.BinTableHDU(data, header, name=name)
 
     # Copy over column meta-data
     for colname in table.colnames:
-        tbhdu.columns[colname].unit = str(table[colname].unit)
+        if table[colname].unit is not None:
+            hdu.columns[colname].unit = str(table[colname].unit)
 
     # TODO: this method works fine but the order of keywords in the table
     # header is not logical: for instance, list of keywords with column
@@ -122,7 +78,7 @@ def table_to_fits_table(table):
     # only when calling the write method and opening the output file.
     # https://github.com/gammapy/gammapy/issues/298
 
-    return tbhdu
+    return hdu
 
 
 def fits_table_to_table(tbhdu):
