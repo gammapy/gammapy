5f46215d6a89e7ead7b84da5dcb36456ff4a5a47
Nachiketa Chakraborty <cnachi@mpi-hd.mpg.de>
2016-06-09 17:20:12 +0200

    Add lightcurve plotting functionality

--- examples/example_lightcurve.py
+++ examples/example_lightcurve.py
@@ -3,11 +3,13 @@ import astropy.units as u
 from astropy.units import Quantity
 from astropy.table import QTable
 import gammapy.time
-from gammapy.time import LightCurve
+from gammapy.time.lightcurve import LightCurve
 table = QTable()
 table['TSTART'] = [1, 4, 7, 9] * u.s
 table['TSTOP'] = [1, 4, 7, 9] * u.s
 table['FLUX'] = Quantity([1, 4, 7, 9], 'cm^-2 s^-1')
+table['ERRORS'] = Quantity([0.1, 0.4, 0.7, 0.9], 'cm^-2 s^-1')
 
 lc = LightCurve(table=table)
 print(lc.flux_mean())
+lc.lc_plot()
