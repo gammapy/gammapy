d18562f3f02db5f54ca1c0bb5b93736944b7f4c6
Olga Vorokh <olga.vorokh@yandex.by>
2016-06-20 02:28:03 +0300

    Rearrange asserts

--- gammapy/image/tests/test_lists.py
+++ gammapy/image/tests/test_lists.py
@@ -1,22 +1,18 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
+from gammapy.utils.testing import requires_data
 from gammapy.datasets import FermiGalacticCenter
-import numpy as np
+from numpy.testing import assert_array_equal
 
 
+@requires_data("gammapy-extra")
 def test_to_cube():
     sky_cube_original = FermiGalacticCenter.diffuse_model()
     image_list = sky_cube_original.to_image_list()
-    assert np.array_equal(image_list.skymaps[0].data, sky_cube_original.data[0])
     sky_cube_restored = image_list.to_cube()
 
-    for key in set(sky_cube_original.__dict__.keys() + sky_cube_restored.__dict__.keys()):
-        # the __init__ of SkyCube initializes energy_axis from energy, but the original cube may not have this field.
-        # Thus the deep equality is not possible and we have to go through it's fields
-        if key == 'energy_axis':
-            continue
+    assert_array_equal(image_list.skymaps[0].data, sky_cube_original.data[0])
 
-        if isinstance(sky_cube_original.__dict__[key], np.ndarray):
-            assert np.array_equal(sky_cube_original.__dict__[key], sky_cube_restored.__dict__[key]),\
-                "differs on {} ".format(key)
-        else:
-            assert sky_cube_original.__dict__[key] == sky_cube_restored.__dict__[key], "differs on {} ".format(key)
+    assert_array_equal(sky_cube_restored.data, sky_cube_original.data)
+    assert sky_cube_restored.energy == sky_cube_original.energy
+    assert sky_cube_restored.name == sky_cube_original.name
+    assert sky_cube_restored.wcs == sky_cube_original.wcs
