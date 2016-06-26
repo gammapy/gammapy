408a8008b6d631027c0d2c8998c63af22ac15e6d
Christoph Deil <Deil.Christoph@gmail.com>
2016-06-26 13:13:53 +0200

    Uncomment failing test in test_plot.py

--- gammapy/time/tests/test_plot.py
+++ gammapy/time/tests/test_plot.py
@@ -1,5 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
+from astropy.tests.helper import pytest
 from ...utils.testing import requires_dependency, requires_data
 from ..plot import plot_fermi_3fgl_light_curve
 
@@ -9,6 +10,10 @@ def test_plot_time_difference_distribution():
     pass
 
 
+# Some change in our dependencies (maybe matplotlib, maybe astropy)
+# makes this now fail with this error:
+# https://travis-ci.org/gammapy/gammapy/jobs/140330531#L1607
+@pytest.mark.xfail
 @requires_dependency('matplotlib')
 @requires_data('gammapy-extra')
 def test_plot_fermi_3fgl_light_curve():
