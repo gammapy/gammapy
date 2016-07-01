3756ba34d98f135847bc034ab8ce791230f7325b
Helen Poon <china_108@yahoo.com>
2016-07-01 16:17:02 +0200

    exptest_for_run changed to exptest

--- gammapy/time/exptest.py
+++ gammapy/time/exptest.py
@@ -3,10 +3,10 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import numpy as np
 
 __all__ = [
-    'exptest_for_run',
+    'exptest',
 ]
 
-def exptest_for_run(time_delta):
+def exptest(time_delta):
     """Compute Mr value, the level of variability for a certain period of time.
 
      A single Mr value can be calculated, which shows the level of variability
@@ -28,14 +28,16 @@ def exptest_for_run(time_delta):
     mean_time = np.mean(time_delta)
     normalized_time_delta = time_delta / mean_time
     sum_time = []
+
+    mask = normalized_time_delta < 1
+    sum_time = 1 - normalized_time_delta[mask] / 1
+    """
     for i in range(len(normalized_time_delta)):
         if normalized_time_delta[i] < 1:
             sum_time.append(1 - normalized_time_delta[i] / 1.0)
+    """   
     mean_normalized_time=np.mean(normalized_time_delta)
-    M_value=0
-    Mr=0
     sum_time_all = np.sum([sum_time])
-    if len(time_delta)!=0:
-        M_value = sum_time_all / len(time_delta)
-        Mr = (M_value - (1 / 2.71828 - 0.189 / len(time_delta))) / (0.2427 / np.sqrt(len(time_delta)))
+    M_value = sum_time_all / len(time_delta)
+    Mr = (M_value - (1 / 2.71828 - 0.189 / len(time_delta))) / (0.2427 / np.sqrt(len(time_delta)))
     return Mr
