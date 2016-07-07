55c7136171a8d6bc81cb235b5d97c0b9ac0de9e9
Christoph Deil <Deil.Christoph@gmail.com>
2016-07-07 16:24:54 +0200

    Simplify examples/example_exptest.py

--- examples/example_exptest.py
+++ examples/example_exptest.py
@@ -6,64 +6,91 @@ from scipy.stats import norm
 import numpy as np
 
 
+def simulation(n_obs):
+    """Simulate time series data.
 
-
-
-def exptest_multi(table):
+    Produce table with one row per event.
+    - obs_id -- Observation ID
+    - mjd -- event time
+      - mjd is filled randomly between 0 and 1
+    - expCount -- expected counts, aka acceptance
+      - expcount is filled with 1
     """
+    table = Table()
+
+    # For every observation, decide somewhat randomly how many events to simulate
+    n_event_per_obs = np.random.random_integers(20, 30, n_obs)
+    n_events = n_event_per_obs.sum()
+
+    table['obs_id'] = np.repeat(np.arange(len(n_event_per_obs)), n_event_per_obs)
+
+    mjd_random = np.random.uniform(0, 1, n_events)
+    mjd = np.sort(mjd_random)
+    table['mjd'] = mjd
+
+    table['expCount'] = 1
+
+    return table
+
+
+def exptest_multi(table_events):
+    """
+    Compute mr value for each run and whole dataset.
+
     Parameter
-    ----------
-     table : astropy.table as the input
+    ---------
+    table : `astropy.table.Table`
+        Input data
 
-     plot : This function returns a histogram for the distribution of the Mr value of each run.
-            A normal distribution is expected for non-flaring sources.
+    Returns
+    -------
+    mr_list : list
+        List of `mr` values.
     """
-    m_value = []
-    size = len(table['runnum'])
-    individual_run = []
-    num_of_events = []
+    # Make table with one row per observation
+    table_obs = Table()
+    table_obs['obs_id'] = np.unique(table_events['obs_id'])
+    res = table_events.group_by('obs_id')
+    n_events = np.array(res.groups.aggregate(sum)['expCount'])
+    table_obs['n_events'] = n_events
+
+    mr_list = []
+
     time_delta_all = []
 
-    for i in range(0, size - 1):
-        if table['runnum'][i] != table['runnum'][i + 1]:
-            individual_run.append(table['runnum'][i])
-    individual_run.append(table['runnum'][size - 1])
-    num_of_runs = len(individual_run)
-
-    for i in range(0, len(individual_run)):
-        num_of_events_temp = 0
-        for j in range(0, size):
-            if individual_run[i] == table['runnum'][j]:
-                num_of_events_temp += 1
-        num_of_events.append(num_of_events_temp)
-
-    for i in range(0, num_of_runs):
+    for i in range(0, len(table_obs)):
         time_delta_each_run = []
-        for j in range(0, size - 1):
-            if num_of_events[i] > 20 and individual_run[i] == table[
-                    'runnum'][j] and table['runnum'][j] == table['runnum'][j + 1]:
+        for j in range(0, len(table_events) - 1):
+            if table_obs['n_events'][i] > 20 and table_obs['obs_id'][i] == table_events[
+                'obs_id'][j] and table_events['obs_id'][j] == table_events['obs_id'][j + 1]:
                 time_delta_each_run.append(
-                    (table['mjd'][
-                        j + 1] - table['mjd'][j]) * 0.5 * (
-                        table['expCount'][
-                            j + 1] + table['expCount'][j]))
+                    (table_events['mjd'][
+                         j + 1] - table_events['mjd'][j]) * 0.5 * (
+                        table_events['expCount'][
+                            j + 1] + table_events['expCount'][j]))
                 time_delta_all.append(
-                    (table['mjd'][
-                        j + 1] - table['mjd'][j]) * 0.5 * (
-                        table['expCount'][
-                            j + 1] + table['expCount'][j]))
+                    (table_events['mjd'][
+                         j + 1] - table_events['mjd'][j]) * 0.5 * (
+                        table_events['expCount'][
+                            j + 1] + table_events['expCount'][j]))
         if len(time_delta_each_run) == 0:
             continue
-        m = exptest(time_delta_each_run)
-        m_value.append(m)
-        print("m value: ", m, "   ", individual_run[i])
+        mr = exptest(time_delta_each_run)
+        mr_list.append(mr)
+        print("mr value: ", mr, "   ", table_obs['obs_id'][i])
         del time_delta_each_run[:]
+
     overallm = exptest(time_delta_all)
     print("Mr for the whole dataset: ", overallm)
-    return m_value
+
+    return mr_list
 
 
 def plot(m_value):
+    """Plot histogram of mr value for each run.
+
+    A normal distribution is expected for non-flaring sources.
+    """
     (mu, sigma) = norm.fit(m_value)
     n, bins, patches = plt.hist(
         m_value, bins=30, normed=1, facecolor='green', alpha=0.75)
@@ -76,29 +103,6 @@ def plot(m_value):
         (mu, sigma))
     plt.grid(True)
     plt.show()
-
-
-def simulation(number_of_runs):
-    """
-
-    """
-    table = Table()
-    num_of_events_per_run = np.random.random_integers(20, 30, number_of_runs)
-    size_cumsum = np.cumsum(num_of_events_per_run)
-    size = size_cumsum[number_of_runs - 1]
-    runnum = []
-    for i in range(0, number_of_runs):
-        for j in range(0, num_of_events_per_run[i]):
-            runnum.append(i + 1)
-    table['runnum'] = runnum
-    mjd_random = np.random.uniform(0, size, size)
-    mjd = np.sort(mjd_random)
-    table['mjd'] = mjd
-    for i in range(0, size):
-        expcount = 1
-    table['expCount'] = expcount
-
-    return table
 
 
 if __name__ == '__main__':
