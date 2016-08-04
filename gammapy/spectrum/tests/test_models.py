b9c9ca99e5e725b79f5f7dc9ca2021fa231cfda6
Christoph Deil <Deil.Christoph@gmail.com>
2016-08-04 20:39:24 +0200

    Fix spectral model test parametrization on Python 3

--- gammapy/spectrum/tests/test_models.py
+++ gammapy/spectrum/tests/test_models.py
@@ -1,54 +1,54 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
-from collections import OrderedDict
 import astropy.units as u
 from astropy.tests.helper import assert_quantity_allclose, pytest
 from ..models import PowerLaw, ExponentialCutoffPowerLaw, LogParabola
 from ...utils.testing import requires_dependency
 
-
-TEST_MODELS = OrderedDict(
-    powerlaw = dict(
+TEST_MODELS = [
+    dict(
+        name='powerlaw',
         model=PowerLaw(
             index=2.3 * u.Unit(''),
             amplitude=4 / u.cm ** 2 / u.s / u.TeV,
-            reference = 1 * u.TeV
+            reference=1 * u.TeV
         ),
         val_at_2TeV=u.Quantity(4 * 2. ** (-2.3), 'cm-2 s-1 TeV-1'),
         integral_1_10TeV=u.Quantity(2.9227116204223784, 'cm-2 s-1'),
         eflux_1_10TeV=u.Quantity(6.650836884969039, 'TeV cm-2 s-1'),
     ),
-
-    ecpl = dict(
+    dict(
+        name='ecpl',
         model=ExponentialCutoffPowerLaw(
-                          index=2.3 * u.Unit(''),
-                          amplitude=4 / u.cm ** 2 / u.s / u.TeV,
-                          reference = 1 * u.TeV,
-                          lambda_=0.1 / u.TeV
+            index=2.3 * u.Unit(''),
+            amplitude=4 / u.cm ** 2 / u.s / u.TeV,
+            reference=1 * u.TeV,
+            lambda_=0.1 / u.TeV
         ),
 
         val_at_2TeV=u.Quantity(0.6650160161581361, 'cm-2 s-1 TeV-1'),
         integral_1_10TeV=u.Quantity(2.3556579120286796, 'cm-2 s-1'),
         eflux_1_10TeV=u.Quantity(4.83209019773561, 'TeV cm-2 s-1'),
     ),
-
-    logpar = dict(
-        model = LogParabola(
-                    alpha=2.3 * u.Unit(''),
-                    amplitude= 4 / u.cm ** 2 / u.s / u.TeV,
-                    reference = 1 * u.TeV,
-                    beta= 0.5 * u.Unit('')
+    dict(
+        name='logpar',
+        model=LogParabola(
+            alpha=2.3 * u.Unit(''),
+            amplitude=4 / u.cm ** 2 / u.s / u.TeV,
+            reference=1 * u.TeV,
+            beta=0.5 * u.Unit('')
         ),
 
         val_at_2TeV=u.Quantity(0.6387956571420305, 'cm-2 s-1 TeV-1'),
         integral_1_10TeV=u.Quantity(2.255689748270628, 'cm-2 s-1'),
         eflux_1_10TeV=u.Quantity(3.9586515834989267, 'TeV cm-2 s-1')
     ),
-)
+]
 
-@pytest.mark.parametrize("spectrum",
-    list(TEST_MODELS.values()),
-    ids=list(TEST_MODELS.keys()))
+
+@pytest.mark.parametrize(
+    "spectrum", TEST_MODELS, ids=[_.name for _ in TEST_MODELS]
+)
 def test_models(spectrum):
     model = spectrum['model']
     energy = 2 * u.TeV
@@ -71,5 +71,5 @@ def test_to_sherpa(spectrum):
         model.to_sherpa()
     except AttributeError:
         pass
-    energy_range = [1,10] * u.TeV
-    model.plot(energy_range = energy_range)
+    energy_range = [1, 10] * u.TeV
+    model.plot(energy_range=energy_range)
