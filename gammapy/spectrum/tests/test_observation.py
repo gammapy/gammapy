bb0c9e9b1bb8972bfa317fed81e2be8fb82c0bda
Johannes King <johannes.king@mpi-hd.mpg.de>
2016-08-31 12:31:18 +0200

    add kick-ass test

--- gammapy/spectrum/tests/test_observation.py
+++ gammapy/spectrum/tests/test_observation.py
@@ -6,9 +6,7 @@ from numpy.testing import assert_allclose
 from astropy.tests.helper import pytest, assert_quantity_allclose
 from ...datasets import gammapy_extra
 from ...utils.testing import requires_dependency, requires_data
-from ...data import ObservationTable
-from ...spectrum import SpectrumObservation, SpectrumObservationList
-
+from ...spectrum import SpectrumObservation, models
 
 @requires_data('gammapy-extra')
 @requires_dependency('matplotlib')
@@ -19,42 +17,31 @@ def test_spectrum_observation():
     obs.peek()
 
 
-@pytest.mark.xfail(reason='This needs some changes to the API')
 @requires_data('gammapy-extra')
 def test_observation_stacking():
-    obs_table_file = gammapy_extra.filename(
-        'datasets/hess-crab4_pha/observation_table.fits')
+    obs1 = SpectrumObservation.read(
+        '$GAMMAPY_EXTRA/datasets/hess-crab4_pha/pha_obs23523.fits')
+    obs2 = SpectrumObservation.read(
+        '$GAMMAPY_EXTRA/datasets/hess-crab4_pha/pha_obs23592.fits')
 
-    obs_table = ObservationTable.read(obs_table_file)
-    temp = SpectrumObservationList.from_observation_table(obs_table)
+    # Change threshold to make stuff more interesing
+    obs1.on_vector.lo_threshold = 1.2 * u.TeV
 
-    observations = temp.get_obslist_from_ids([23523, 23592])
-    spectrum_observation_grouped = SpectrumObservation.stack_observation_list(observations, 0)
-    obs0 = observations[0]
-    obs1 = observations[1]
+    stacked_obs = SpectrumObservation.stack([obs1, obs2])
 
-    # Test sum on/off vector and alpha group
-    sum_on_vector = obs0.on_vector.counts + obs1.on_vector.counts
-    sum_off_vector = obs0.off_vector.counts + obs1.off_vector.counts
-    alpha_times_off_tot = obs0.alpha * obs0.off_vector.total_counts + obs1.alpha * obs1.off_vector.total_counts
-    total_off = obs0.off_vector.total_counts + obs1.off_vector.total_counts
+    # Veryfing npred is preserved during the stacking
+    pwl = models.PowerLaw(index=2 * u.Unit(''),
+                          amplitude=2e-11 * u.Unit('cm-2 s-1 TeV-1'),
+                          reference=1 * u.TeV)
 
-    assert_allclose(spectrum_observation_grouped.on_vector.counts, sum_on_vector)
-    assert_allclose(spectrum_observation_grouped.off_vector.counts, sum_off_vector)
-    assert_allclose(spectrum_observation_grouped.alpha, alpha_times_off_tot / total_off)
+    npred1 = obs1.predicted_counts(model=pwl)
+    npred2 = obs2.predicted_counts(model=pwl)
+    npred_stacked = stacked_obs.predicted_counts(model=pwl)
 
-    # Test arf group
-    total_time = obs0.meta.livetime + obs1.meta.livetime
-    arf_times_livetime = obs0.meta.livetime * obs0.effective_area.data \
-        + obs1.meta.livetime * obs1.effective_area.data
-    assert_allclose(spectrum_observation_grouped.effective_area.data, arf_times_livetime / total_time)
-    # Test rmf group
-    rmf_times_arf_times_livetime = obs0.meta.livetime * obs0.effective_area.data \
-        * obs0.energy_dispersion.pdf_matrix.T \
-        + obs1.meta.livetime * obs1.effective_area.data \
-        * obs1.energy_dispersion.pdf_matrix.T
+    # Set npred outside safe range to 0
+    npred1.data[np.nonzero(obs1.on_vector.quality)] = 0
+    npred2.data[np.nonzero(obs2.on_vector.quality)] = 0
 
-    inan = np.isnan(rmf_times_arf_times_livetime / arf_times_livetime)
-    pdf_expexted = rmf_times_arf_times_livetime / arf_times_livetime
-    pdf_expexted[inan] = 0
-    assert_allclose(spectrum_observation_grouped.energy_dispersion.pdf_matrix, pdf_expexted.T, atol=1e-6)
+    npred_summed = npred1 + npred2
+
+    assert_allclose(npred_stacked.data, npred_summed.data)
