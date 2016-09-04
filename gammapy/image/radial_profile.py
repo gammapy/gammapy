372b76e61f1804d0abe602ca716f4345c3e2d6ae
Christoph Deil <Deil.Christoph@gmail.com>
2016-09-04 19:18:03 +0200

    Improve image radial profile code

--- gammapy/image/radial_profile.py
+++ gammapy/image/radial_profile.py
@@ -1,89 +1,153 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-"""Image utility functions"""
 import numpy as np
-from astropy.coordinates import Angle
-from astropy.wcs.utils import skycoord_to_pixel
 from astropy.table import QTable
-from . import SkyImage
+from .core import SkyImage
 
 __all__ = [
-    'radial_profile_label_image',
     'radial_profile',
+    'radial_profile_label_image',
 ]
 
 
-def radial_profile_label_image(image, center, theta_bin=None):
+def radial_profile(image, center, radius):
     """
-   Calculate the labeled image to know which pixel is group together for the radial profile.
+    Image radial profile.
 
-   Parameters
-   ----------
-   image : `~gammapy.image.SkyImage`
-        SkyImage of the image we cant to compute the radial profile
-   center : `~astropy.coordinates.SkyCoord`
-        The `SkyCoord` of the pixel used as the center.
-   theta_bin : `~astropy.coordinates.Angle`
-         Binning for the radial profile
+    TODO: show example and explain handling of "overflow"
+    and "underflow" bins (see ``radial_profile_label_image`` docstring).
 
-    Returns
-    -------
-    label_image : `~gammapy.image.SkyImage`
-        label image
-   """
-    image_bin = Angle(np.fabs(image.meta["CDELT1"]), image.meta["CUNIT1"])
-    # Calculate the indices from the image
-    pix = image.coordinates_pix()
-    x = pix.x
-    y = pix.y
-    center_pix = skycoord_to_pixel(center, image.wcs, origin=0)
-    r = np.hypot(x - center_pix[0], y - center_pix[1])
-    if not theta_bin:
-        theta_bin = image_bin
-    theta_pix = theta_bin / image_bin
-    label_array = (r / theta_pix).astype(np.int)
-    label_image = SkyImage.empty_like(image)
-    label_image.data = label_array
-    return label_image
+    Calls `numpy.digitize` to compute a label image and then
+    `scipy.ndimage.sum` to do measurements.
 
-
-def radial_profile(image, center, theta_bin=None):
-    """
-   Calculate the labeled image to know which pixel is group together for the radial profile.
-
-   Parameters
-   ----------
-   image : `~gammapy.image.SkyImage`
-         SkyImage of the image we cant to compute the radial profile
-   center : `~astropy.coordinates.SkyCoord`
-         The `SkyCoord` of the pixel used as the center.
-   theta_bin : `~astropy.coordinates.Angle`
-         Bining for the radial profile
+    Parameters
+    ----------
+    image : `~gammapy.image.SkyImage`
+        Image
+    center : `~astropy.coordinates.SkyCoord`
+        Center position
+    radius : `~astropy.coordinates.Angle`
+        Offset bin edge array.
 
     Returns
     -------
     table : `~astropy.table.QTable`
 
-        Table with the following fields:
+        Table with the following fields that define the binning:
 
-        *``RADIUS`` : radius bin value of the radial profile
+        * ``RADIUS_BIN_ID`` : Integer bin ID (starts at ``1``).
+        * ``RADIUS_MEAN`` : Radial bin center
+        * ``RADIUS_MIN`` : Radial bin minimum edge
+        * ``RADIUS_MAX`` : Radial bin maximum edge
 
-        *``BIN_VALUE`` : mean of the value of the pixels combined in one radial bin
+        And the following measurements from the pixels in each bin:
 
-        *``BIN_ERR`` : error on each value of the radial profile
-   """
-    from scipy.ndimage.measurements import labeled_comprehension
+        * ``N_PIX`` : Number of pixels
+        * ``SUM`` : Sum of pixel values
+        * ``MEAN`` : Mean of pixel values, computed as ``SUM / N_PIX``
 
-    label_image = radial_profile_label_image(image, center, theta_bin).data
-    index_tab = np.arange(0, label_image.max() + 1)
-    image_bin = Angle(np.fabs(image.meta["CDELT1"]), image.meta["CUNIT1"])
-    if not theta_bin:
-        theta_bin = image_bin
-    theta_bin_tab = index_tab * theta_bin
-    mean_values = labeled_comprehension(image.data, label_image, index_tab, np.mean, np.float, -1.)
-    myerr = lambda x: np.sqrt(np.fabs(x.astype(float).sum())) / len(x)
-    err = labeled_comprehension(image.data, label_image, index_tab, myerr, np.float, -1.)
-    table = QTable([theta_bin_tab,
-                    mean_values,
-                    err],
-                   names=('RADIUS', 'BIN_VALUE', 'BIN_ERR'))
+
+    Examples
+    --------
+
+    Make some example data::
+
+        from astropy.coordinates import Angle
+        from gammapy.image import SkyImage
+        image = SkyImage.empty()
+        image.fill(value=1)
+        center = image.center
+        radius = Angle([0.1, 0.2, 0.4, 0.5, 1.0], 'deg')
+
+    Compute and print a radial profile::
+
+        from gammapy.image import radial_profile
+        table = radial_profile(image, center, radius)
+        table.pprint()
+
+    If your measurement represents counts, you could e.g. use this
+    method to compute errors::
+
+        import numpy as np
+        table['SUM_ERR'] = np.sqrt(table['SUM'])
+        table['MEAN_ERR'] = table['SUM_ERR'] / table['N_PIX']
+
+    If you need to do special measurements or error computation
+    in each bin with access to the pixel values,
+    you could get the label image and then do the measurements yourself::
+
+        labels = radial_profile_label_image(image, center, radius)
+        labels.show()
+    """
+    labels = radial_profile_label_image(image, center, radius)
+
+    # Note: here we could decide to also measure overflow and underflow bins.
+    index = np.arange(1, len(radius))
+    table = _radial_profile_measure(image, labels, index)
+
+    table['RADIUS_BIN_ID'] = index
+    table['RADIUS_MIN'] = radius[:-1]
+    table['RADIUS_MAX'] = radius[1:]
+    table['RADIUS_MEAN'] = 0.5 * (table['RADIUS_MAX'] + table['RADIUS_MIN'])
+
+    meta = dict(
+        type='radial profile',
+        center=center,
+    )
+
+    table.meta.update(meta)
+    return table
+
+
+def radial_profile_label_image(image, center, radius):
+    """
+    Image radial profile label image.
+
+    The ``radius`` array defines ``n_bins = len(radius) - 1`` bins.
+
+    The label image has the following values:
+    * Value ``1`` to ``n_bins`` for pixels in ``(radius[0], radius[-1])``
+    * Value ``0`` for pixels with ``r < radius[0]``
+    * Value ``n_bins`` for pixels with ``r >= radius[-1]``
+
+    Parameters
+    ----------
+    image : `~gammapy.image.SkyImage`
+        Image
+    center : `~astropy.coordinates.SkyCoord`
+        Center position
+    radius : `~astropy.coordinates.Angle`
+        Offset bin edge array.
+
+    Returns
+    -------
+    labels : `~gammapy.image.SkyImage`
+        Label image (1 to max_label; outside pixels have value 0)
+    """
+    radius_image = image.coordinates().separation(center)
+    labels = np.digitize(radius_image.deg, radius.deg)
+
+    return SkyImage(name='labels', data=labels, wcs=image.wcs.copy())
+
+
+def _radial_profile_measure(image, labels, index):
+    """
+    Measurements for radial profile.
+
+    This is a helper function to do measurements.
+
+    TODO: this should call the generic function,
+    nothing radial profile-specific here.
+    """
+    from scipy import ndimage
+
+    # This function takes `SkyImage` objects as inputs
+    # but only operates on their `data`
+    image = image.data
+    labels = labels.data
+
+    table = QTable()
+    table['N_PIX'] = ndimage.sum(np.ones_like(image), labels, index=index)
+    table['SUM'] = ndimage.sum(image, labels, index=index)
+    table['MEAN'] = table['SUM'] / table['N_PIX']
+
     return table
--- gammapy/image/tests/test_radial_profile.py
+++ gammapy/image/tests/test_radial_profile.py
@@ -1,31 +1,22 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
-import numpy as np
-from numpy.testing import assert_equal
+from numpy.testing import assert_allclose
 from astropy.coordinates import Angle
-from astropy.tests.helper import assert_quantity_allclose
-from astropy.coordinates import SkyCoord
 from ...utils.testing import requires_dependency
-from .. import SkyImage
-from .. import radial_profile
+from ..core import SkyImage
+from ..radial_profile import radial_profile, radial_profile_label_image
 
 
 @requires_dependency('scipy')
 def test_radial_profile():
-    center = SkyCoord(0, 0, unit='deg')
-    image = SkyImage.empty(nxpix=250, nypix=250, binsz=0.02, xref=center.galactic.l.deg,
-                           yref=center.galactic.b.deg, proj='TAN', coordsys='GAL')
-    image.data[:] = 1
-    #test with the default theta_bin
-    table = radial_profile(image, center=center, theta_bin=None)
-    offbincenter = table["RADIUS"]
-    offbinsize = offbincenter[1] - offbincenter[0]
-    excess_profile = table["BIN_VALUE"]
-    assert_quantity_allclose(offbinsize, Angle(np.fabs(image.meta["CDELT1"]), image.meta["CUNIT1"]))
-    assert_equal(excess_profile, np.ones(len(excess_profile)))
+    image = SkyImage.empty()
+    image.fill(value=1)
+    center = image.center
+    radius = Angle([0.1, 0.2, 0.4, 0.5, 1.0], 'deg')
 
-    #Test with a defined theta_bin
-    table2 = radial_profile(image, center=center, theta_bin=Angle(0.04, "deg"))
-    offbincenter2 = table2["RADIUS"]
-    offbinsize2 = offbincenter2[1] - offbincenter2[0]
-    assert_quantity_allclose(offbinsize2, Angle(0.04, "deg"))
+    labels = radial_profile_label_image(image, center, radius)
+    assert labels.data.max() == 5
+
+    profile = radial_profile(image, center, radius)
+    assert len(profile) == 4
+    assert_allclose(profile['MEAN'], 1)
