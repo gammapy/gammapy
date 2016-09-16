8dc51dc022288f3a09648d01e51d79b3254a80cd
Christoph Deil <Deil.Christoph@gmail.com>
2016-09-16 15:48:33 +0200

    Code cleanup in test_image_ts.py

--- gammapy/scripts/tests/test_image_ts.py
+++ gammapy/scripts/tests/test_image_ts.py
@@ -7,7 +7,6 @@ from astropy.io import fits
 from ...utils.testing import requires_dependency, requires_data
 from ...image import SkyImageCollection
 from ...datasets import load_poisson_stats_image
-from ...datasets.core import gammapy_extra
 from ..image_ts import image_ts_main
 
 SCALES = ['0.000', '0.050', '0.100', '0.200']
@@ -19,7 +18,7 @@ SCALES = ['0.000', '0.050', '0.100', '0.200']
 class TestImageTS:
     @pytest.fixture(autouse=True)
     def init_test_input_file(self, tmpdir):
-        filename = str(tmpdir / 'input_all.fits.gz')
+        self.input_filename = str(tmpdir / 'input_all.fits.gz')
 
         data = load_poisson_stats_image(extra_info=True)
         header = data['header']
@@ -30,45 +29,40 @@ class TestImageTS:
         for image, name in zip(images, image_names):
             hdu = fits.ImageHDU(data=image, header=header, name=name)
             hdu_list.append(hdu)
-        hdu_list.writeto(filename)
-        self.input_file = filename
+
+        hdu_list.writeto(self.input_filename)
 
     @pytest.fixture(autouse=True)
     def init_psf_file(self, tmpdir):
-        psf_filename = str(tmpdir / 'psf.json')
+        self.psf_filename = str(tmpdir / 'psf.json')
 
         psf_pars = dict()
         psf_pars['psf1'] = dict(ampl=1., fwhm=7.0644601350928475)
         psf_pars['psf2'] = dict(ampl=0, fwhm=1E-5)
         psf_pars['psf3'] = dict(ampl=0, fwhm=1E-5)
-        json.dump(psf_pars, open(psf_filename, "w"))
-        self.psf_file = psf_filename
+        with open(self.psf_filename, 'w') as fh:
+            json.dump(psf_pars, fh)
 
     @pytest.mark.parametrize('scale', SCALES)
     def test_command_line_gammapy_image_ts(self, tmpdir, scale):
         """Minimal test of gammapy_image_ts using testcase that
         guaranteed to work with compute_ts_image"""
 
-        output_filename = str(tmpdir / "output.fits")
-        expected_filename = str(gammapy_extra.dir /
-                                'test_datasets/unbundled/poisson_stats_image/expected_ts.fits.gz')
+        actual_filename = str(tmpdir / 'output.fits')
 
-        image_ts_main([self.input_file,
-                       output_filename,
-                       "--psf",
-                       self.psf_file,
-                       "--scales",
-                       scale])
+        args = [self.input_filename, actual_filename,
+                "--psf", self.psf_filename,
+                "--scales", scale]
+        image_ts_main(args)
 
-        expected_filename_ = expected_filename.replace('.fits', '_{0}.fits'.format(scale))
-        actual = SkyImageCollection.read(output_filename)
-        expected = SkyImageCollection.read(expected_filename_)
+        expected_filename = '$GAMMAPY_EXTRA/test_datasets/unbundled/poisson_stats_image/expected_ts_{}.fits.gz'
+        expected_filename = expected_filename.format(scale)
 
-        assert_allclose(actual['ts'].data, expected['ts'].data, rtol=1e-2, atol=1e-15,
-                        equal_nan=True)
-        assert_allclose(actual['sqrt_ts'].data, expected['sqrt_ts'].data,
-                        rtol=1e-2, atol=1e-15, equal_nan=True)
-        assert_allclose(actual['amplitude'].data, expected['amplitude'].data,
-                        rtol=1e-2, atol=1e-15, equal_nan=True)
-        assert_allclose(actual['niter'].data, expected['niter'].data,
-                        rtol=1e-2, atol=1e-15, equal_nan=True)
+        actual = SkyImageCollection.read(actual_filename)
+        expected = SkyImageCollection.read(expected_filename)
+
+        opts = dict(rtol=1e-2, atol=1e-15, equal_nan=True)
+        assert_allclose(actual['ts'].data, expected['ts'].data, **opts)
+        assert_allclose(actual['sqrt_ts'].data, expected['sqrt_ts'].data, **opts)
+        assert_allclose(actual['amplitude'].data, expected['amplitude'].data, **opts)
+        assert_allclose(actual['niter'].data, expected['niter'].data, **opts)
