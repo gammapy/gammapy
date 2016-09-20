63c6c39838f1e656d89bcafc11e65f8623c9395f
Johannes King <johannes.king@mpi-hd.mpg.de>
2016-09-20 17:58:56 +0200

    fix #688

--- gammapy/spectrum/tests/test_energy_group.py
+++ gammapy/spectrum/tests/test_energy_group.py
@@ -36,13 +36,13 @@ class TestSpectrumEnergyGrouping:
     def test_fixed(self, seg):
         t = seg.groups.to_group_table()
         assert_equal(t['energy_group_idx'], [0, 1, 2, 3, 4, 5])
-        assert_equal(t['bin_idx_min'], [0, 30, 36, 44, 54, 62])
-        assert_equal(t['bin_idx_max'], [29, 35, 43, 53, 61, 71])
+        assert_equal(t['bin_idx_min'], [0, 33, 36, 44, 54, 62])
+        assert_equal(t['bin_idx_max'], [32, 35, 43, 53, 61, 71])
         assert_equal(t['bin_type'], ['underflow', 'normal', 'normal', 'normal', 'normal', 'overflow'])
-        ebounds = [0.01, 0.4641588833612777, 1, 2.7825594022071227, 10, 27.825594022071225, 100]
+        ebounds = [0.01, 0.6812921, 1, 2.7825594022071227, 10, 27.825594022071225, 100]
         assert_allclose(t['energy_min'].value, ebounds[:-1])
         assert_allclose(t['energy_max'].value, ebounds[1:])
-        assert_equal(t['energy_group_n_bins'], [30, 6, 8, 10, 8, 10])
+        assert_equal(t['energy_group_n_bins'], [33, 3, 8, 10, 8, 10])
 
     def _test_adaptive(self, obs):
         seg = SpectrumEnergyGroupMaker(obs=obs)
@@ -59,4 +59,4 @@ class TestSpectrumEnergyGrouping:
 def test_flux_points_binning():
     obs = SpectrumObservation.read('$GAMMAPY_EXTRA/datasets/hess-crab4_pha/pha_obs23523.fits')
     energy_binning = calculate_flux_point_binning(obs_list=[obs], min_signif=3)
-    assert_quantity_allclose(energy_binning[5], 1.668 * u.TeV, rtol=1e-3)
+    assert_quantity_allclose(energy_binning[5], 2.448 * u.TeV, rtol=1e-3)
--- gammapy/spectrum/tests/test_extract.py
+++ gammapy/spectrum/tests/test_extract.py
@@ -1,6 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
 import numpy as np
+import copy
 from numpy.testing import assert_allclose
 from astropy.tests.helper import pytest
 import astropy.units as u
@@ -15,72 +16,101 @@ from ...image import SkyMask
 from ...spectrum import SpectrumExtraction, SpectrumObservation
 
 
-@pytest.mark.parametrize("pars,results", [
-    (dict(containment_correction=False), dict(n_on=172,
-                                              sigma=28.18,
-                                              aeff=549861.8 * u.m ** 2,
-                                              ethresh=0.4327 * u.TeV)),
-    (dict(containment_correction=True), dict(n_on=172,
-                                             sigma=28.18,
-                                             aeff=393356.2 * u.m ** 2,
-                                             ethresh=0.625 * u.TeV)),
-])
-@requires_dependency('scipy')
-@requires_data('gammapy-extra')
-def test_spectrum_extraction(pars, results, tmpdir):
-    center = SkyCoord(83.63, 22.01, unit='deg', frame='icrs')
-    radius = Angle('0.11 deg')
-    on_region = CircleSkyRegion(center, radius)
-    target = Target(on_region)
-
+@pytest.fixture(scope='session')
+def obs():
+    """An example ObservationList for tests."""
     obs_id = [23523, 23592]
     store = gammapy_extra.filename("datasets/hess-crab4-hd-hap-prod2")
     ds = DataStore.from_dir(store)
     obs = ObservationList([ds.obs(_) for _ in obs_id])
+    return obs
 
+
+@pytest.fixture(scope='session')
+def target():
+    """An example Target for tests."""
+    center = SkyCoord(83.63, 22.01, unit='deg', frame='icrs')
+    radius = Angle('0.11 deg')
+    on_region = CircleSkyRegion(center, radius)
+    target = Target(on_region)
+    return target
+
+
+@pytest.fixture(scope='session')
+def bkg():
+    """An example bkg dict for tests."""
     exclusion_file = gammapy_extra.filename(
         "datasets/exclusion_masks/tevcat_exclusion.fits")
     excl = SkyMask.read(exclusion_file)
+    bkg = dict(method='reflected', n_min=2, exclusion=excl)
+    return bkg
 
-    bk = dict(method='reflected', n_min=2, exclusion=excl)
 
-    # Restrict energy binning to a range where the aeff interpolation does not
-    # give none for HAP test files
-    # TODO: set low energies to 0 and extrapolate high energies
+@pytest.fixture(scope='session')
+def extraction():
+    """An example SpectrumExtraction for tests."""
+    # Restrict true energy range covered by HAP exporter
     e_true = np.logspace(-1, 1.9, 70) * u.TeV
 
-    ana = SpectrumExtraction(target,
-                             obs,
-                             bk,
-                             e_true=e_true,
-                             containment_correction=pars['containment_correction'])
+    extraction = SpectrumExtraction(target=target(),
+                                    obs=obs(),
+                                    background=bkg(),
+                                    e_true=e_true
+                                    )
+    return extraction
 
-    ana.run(outdir=tmpdir)
 
-    ana.define_energy_threshold(method_lo_threshold="area_max", percent=10)
+@requires_dependency('scipy')
+@requires_data('gammapy-extra')
+class TestSpectrumExtraction:
 
-    assert_quantity_allclose(ana.observations[0].lo_threshold,
-                             results['ethresh'], rtol=1e-3)
+    @pytest.mark.parametrize("pars, results", [
+        (dict(containment_correction=False), dict(n_on=172,
+                                                  sigma=22.88,
+                                                  aeff=549861.8 * u.m ** 2
+                                                  )),
+        (dict(containment_correction=True), dict(n_on=172,
+                                                 sigma=22.88,
+                                                 aeff=393356.2 * u.m ** 2
+                                                 ))
+    ])
+    def test_extract(self, pars, results, target, obs, bkg, tmpdir):
+        """Test quantitative output for various configs"""
+        extraction = SpectrumExtraction(target=target,
+                                        obs=obs,
+                                        background=copy.deepcopy(bkg),
+                                        **pars)
 
-    assert_quantity_allclose(ana.observations[0].off_vector.lo_threshold,
-                             ana.observations[0].on_vector.lo_threshold)
+        # TODO: Improve API
+        print(extraction.background)
+        extraction.estimate_background(extraction.background)
+        extraction.extract_spectrum()
+        obs = extraction.observations[0]
+        aeff_actual = obs.aeff.evaluate(energy=5 * u.TeV)
 
-    assert_quantity_allclose(ana.observations[0].aeff.evaluate(
-        energy=5 * u.TeV), results['aeff'], rtol=1e-3)
-    assert ana.observations[0].total_stats.n_on == results['n_on']
-    assert_allclose(ana.observations[1].total_stats.sigma, results['sigma'],
-                    atol=1e-2)
+        # TODO: Introduce assert_stats_allclose
+        n_on_actual = obs.total_stats.n_on
+        sigma_actual = obs.total_stats.sigma
 
-    # Write on set of output files to gammapy-extra as input for other tests
-    # and check I/O
-    if not pars['containment_correction']:
-        outdir = gammapy_extra.filename("datasets/hess-crab4_pha")
-        ana.observations.write(outdir, use_sherpa=True)
+        assert_quantity_allclose(aeff_actual, results['aeff'], rtol=1e-3)
+        assert n_on_actual == results['n_on']
+        assert_allclose(sigma_actual, results['sigma'], atol=1e-2)
 
-        testobs = SpectrumObservation.read(make_path(outdir) / 'pha_obs23523.fits')
+    def test_run(self, tmpdir, extraction):
+        """Test the run method and check if files are written correctly"""
+        extraction.run(outdir=tmpdir)
+        testobs = SpectrumObservation.read(tmpdir / 'ogip_data' / 'pha_obs23523.fits')
         assert_quantity_allclose(testobs.aeff.data,
-                                 ana.observations[0].aeff.data)
+                                 extraction.observations[0].aeff.data)
         assert_quantity_allclose(testobs.on_vector.data,
-                                 ana.observations[0].on_vector.data)
+                                 extraction.observations[0].on_vector.data)
         assert_quantity_allclose(testobs.on_vector.energy.nodes,
-                                 ana.observations[0].on_vector.energy.nodes)
+                                 extraction.observations[0].on_vector.energy.nodes)
+
+    def test_define_energy_threshold(self, extraction):
+        # TODO: Find better API for this
+        extraction.define_energy_threshold(method_lo_threshold="area_max",
+                                           percent=10)
+        assert_quantity_allclose(extraction.observations[0].lo_threshold,
+                                 0.4327 * u.TeV,
+                                 rtol=1e-3)
