39d452559e089d2c348f1324e43e13124eff8bb2
Christoph Deil <Deil.Christoph@gmail.com>
2016-09-20 11:19:03 +0200

    Remove old (pre-astropy) stuff from gammapy.utils.coordinates

--- gammapy/utils/coordinates/celestial.py
+++ gammapy/utils/coordinates/celestial.py
@@ -3,132 +3,15 @@
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 import numpy as np
-from numpy import (cos, sin, arccos, arcsin,
-                   arctan2, radians, degrees, pi)
+from astropy.coordinates.angle_utilities import angular_separation
 
-__all__ = ['galactic_to_radec',
-           'radec_to_galactic',
-           'separation',
-           'minimum_separation',
-           'pair_correlation',
-           ]
+__all__ = [
+    'minimum_separation',
+    'pair_correlation',
+]
 
 
-def galactic_to_radec(glon, glat, unit='deg'):
-    """Convert Galactic to Equatorial J2000 coordinates.
-
-    Parameters
-    ----------
-    glon, glat : array_like
-        Galactic coordinates
-    unit : {'deg', 'rad'}
-        Units of input and output coordinates
-
-    Returns
-    -------
-    ra, dec : array_like
-        Equatorial coordinates.
-
-    Notes
-    -----
-    This is a standalone implementation that only uses ``numpy`` for testing.
-    Use `~astropy.coordinates.SkyCoord` instead.
-    Only accurate to ~ 3 digits.
-    """
-    if unit == 'deg':
-        glon, glat = radians(glon), radians(glat)
-
-    ra_gp = radians(192.85948)
-    de_gp = radians(27.12825)
-    lcp = radians(122.932)
-
-    term1 = cos(glat) * sin(lcp - glon)
-    term2 = cos(de_gp) * sin(glat) - sin(de_gp) * cos(glat) * cos(lcp - glon)
-    ramragp = arctan2(term1, term2)
-    ra = (ramragp + ra_gp + 2 * pi) % (2 * pi)
-
-    sin_d = sin(de_gp) * sin(glat) + cos(de_gp) * cos(glat) * cos(lcp - glon)
-    dec = arcsin(sin_d)
-
-    if unit == 'deg':
-        ra, dec = degrees(ra), degrees(dec)
-
-    return ra, dec
-
-
-def radec_to_galactic(ra, dec, unit='deg'):
-    """Convert Equatorial J2000 to Galactic coordinates.
-
-    Parameters
-    ----------
-    ra, dec : array_like
-        Equatorial coordinates.
-    unit : {'deg', 'rad'}
-        Units of input and output coordinates
-
-    Returns
-    -------
-    glon, glat : array_like
-        Galactic coordinates
-
-    Notes
-    -----
-    This is a standalone implementation that only uses ``numpy`` for testing.
-    Use `~astropy.coordinates.SkyCoord` instead.
-    Only accurate to ~ 3 digits.
-    """
-    if unit == 'deg':
-        ra, dec = radians(ra), radians(dec)
-
-    ra_gp = radians(192.85948)
-    de_gp = radians(27.12825)
-    lcp = radians(122.932)
-
-    term1 = cos(dec) * sin(ra - ra_gp)
-    term2 = cos(de_gp) * sin(dec) - sin(de_gp) * cos(dec) * cos(ra - ra_gp)
-    lcpml = arctan2(term1, term2)
-    glon = (lcp - lcpml + 2 * pi) % (2 * pi)
-
-    sin_b = sin(de_gp) * sin(dec) + cos(de_gp) * cos(dec) * cos(ra - ra_gp)
-    glat = arcsin(sin_b)
-
-    if unit == 'deg':
-        glon, glat = degrees(glon), degrees(glat)
-
-    return glon, glat
-
-
-def separation(lon1, lat1, lon2, lat2, unit='deg'):
-    """Angular separation between points on the sphere.
-
-    Parameters
-    ----------
-    lon1, lat1, lon2, lat2 : array_like
-        Coordinates of the two points
-    unit : {'deg', 'rad'}
-        Units of input and output coordinates
-
-    Returns
-    -------
-    separation : array_like
-        Angular separation
-    """
-    if unit == 'deg':
-        lon1, lat1, lon2, lat2 = map(radians, (lon1, lat1, lon2, lat2))
-
-    term1 = cos(lat1) * cos(lon1) * cos(lat2) * cos(lon2)
-    term2 = cos(lat1) * sin(lon1) * cos(lat2) * sin(lon2)
-    term3 = sin(lat1) * sin(lat2)
-    mu = term1 + term2 + term3
-    separation = arccos(mu)
-
-    if unit == 'deg':
-        separation = degrees(separation)
-
-    return separation
-
-
-def minimum_separation(lon1, lat1, lon2, lat2, unit='deg'):
+def minimum_separation(lon1, lat1, lon2, lat2):
     """Compute minimum distance of each (lon1, lat1) to any (lon2, lat2).
 
     Parameters
@@ -151,14 +34,13 @@ def minimum_separation(lon1, lat1, lon2, lat2, unit='deg'):
     theta_min = np.empty_like(lon1, dtype=np.float64)
 
     for i1 in range(lon1.size):
-        thetas = separation(lon1[i1], lat1[i1],
-                            lon2, lat2, unit=unit)
+        thetas = angular_separation(lon1[i1], lat1[i1], lon2, lat2)
         theta_min[i1] = thetas.min()
 
     return theta_min
 
 
-def pair_correlation(lon, lat, theta_bins, unit='deg'):
+def pair_correlation(lon, lat, theta_bins):
     """Compute pair correlation function for points on the sphere.
 
     Parameters
@@ -183,7 +65,7 @@ def pair_correlation(lon, lat, theta_bins, unit='deg'):
     # If there are many points this should have acceptable performance
     # because the inner loop is in np.histogram, not in Python
     for ii in range(len(lon)):
-        theta = separation(lon[ii], lat[ii], lon, lat, unit=unit)
+        theta = angular_separation(lon[ii], lat[ii], lon, lat)
         hist = np.histogram(theta, theta_bins)[0]
         counts += hist
 
--- gammapy/utils/coordinates/tests/test_celestial.py
+++ gammapy/utils/coordinates/tests/test_celestial.py
@@ -1,40 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
-import numpy as np
 from numpy.testing import assert_allclose
-from ...coordinates import (galactic_to_radec,
-                            radec_to_galactic,
-                            separation,
-                            minimum_separation,
-                            )
-
-
-def test_coordinate_conversion_scalar():
-    """Show that it works for the Galactic center by
-    comparing to the results from:
-    http://heasarc.gsfc.nasa.gov/cgi-bin/Tools/convcoord/convcoord.pl
-    """
-    actual = galactic_to_radec(0, 0)
-    expected = (266.404996, -28.936172)
-    assert_allclose(actual, expected, atol=1e-4)
-
-
-def test_coordinate_conversion_array():
-    """Make a grid of positions and make a closure test"""
-    # Note: points near the pole or RA boundary don't work!
-    y, x = np.mgrid[-89:90:1, 1:360:10]
-    for back, forth in [[radec_to_galactic, galactic_to_radec],
-                        [galactic_to_radec, radec_to_galactic]]:
-        a, b = forth(x, y)
-        X, Y = back(a, b)
-        assert_allclose((X, Y), (x, y), atol=1e-10)
-
-
-def test_separation():
-    assert_allclose(separation(0, 0, 180, 0), 180)
-    assert_allclose(separation(270, 0, 180, 0), 90)
-    assert_allclose(separation(0, 0, 0, 90), 90)
-    assert_allclose(separation(0, 89, 180, 89), 2)
+from ...coordinates import minimum_separation
 
 
 def test_minimum_separation():
@@ -44,7 +11,3 @@ def test_minimum_separation():
     lat2 = [0, 0.5]
     separation = minimum_separation(lon1, lat1, lon2, lat2)
     assert_allclose(separation, [1, 0, 0.5])
-
-
-def test_pair_correlation():
-    pass
