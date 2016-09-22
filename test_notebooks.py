1e7de6e8d2f9661a4a1446fcc4f7f53f0625bedb
Christoph Deil <Deil.Christoph@gmail.com>
2016-09-22 15:57:33 +0200

    Update test_notebooks.py

--- test_notebooks.py
+++ test_notebooks.py
@@ -7,7 +7,7 @@ import os
 from astropy.extern.six import PY2
 
 notebooks_for_py2 = [
-    'notebooks/hess_spectrum_analysis.ipynb',
+    'hess_spectrum_analysis.ipynb',
 ]
 
 notebooks_for_py3 = [
@@ -15,7 +15,9 @@ notebooks_for_py3 = [
 ]
 
 notebooks_for_py23 = [
-    'index.ipynb',
+    'hess_image_analysis.ipynb',
+    'fermi_ts_image.ipynb',
+    'fermi_2fhl.ipynb',
 ]
 
 if PY2:
@@ -26,7 +28,7 @@ else:
 print('*** Testing IPython notebooks ...')
 
 for notebook in notebooks:
-    cmd = 'cd $GAMMAPY_EXTRA; '
+    cmd = 'cd $GAMMAPY_EXTRA/notebooks; '
     cmd += 'runipy --quiet {}'.format(notebook)
     print('*** Executing: {}'.format(cmd))
     os.system(cmd)
