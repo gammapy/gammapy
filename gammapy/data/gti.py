42030d25fbba5c35a164e0631663af46af536ff0
Christoph Deil <Deil.Christoph@gmail.com>
2016-11-18 08:59:51 +0100

    Change GTI to table member instead of subclass

--- gammapy/data/gti.py
+++ gammapy/data/gti.py
@@ -1,6 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
-import sys
 from astropy.units import Quantity
 from astropy.table import Table
 from ..utils.time import time_ref_from_dict
@@ -11,16 +10,51 @@ __all__ = [
 ]
 
 
-class GTI(Table):
+class GTI(object):
     """Good time intervals (GTI) `~astropy.table.Table`.
 
     Note: at the moment dead-time and live-time is in the
     EVENTS header ... the GTI header just deals with
     observation times.
+
+    Parameters
+    ----------
+    table : `~astropy.table.Table`
+        GTI table
+
+    Examples
+    --------
+
+    Load GTIs for a H.E.S.S. event list:
+
+    >>> from gammapy.data import GTI
+    >>> filename = '$GAMMAPY_EXTRA/test_datasets/unbundled/hess/run_0023037_hard_eventlist.fits.gz'
+    >>> gti = GTI.read(filename)
+    >>> print(gti)
+    GTI info:
+    - Number of GTIs: 1
+    - Duration: 1568.0 s
+    - Start: 53292.00592592593 MET
+    - Start: 2004-10-14T00:08:32.000(TT)
+    - Stop: 53292.02407407408 MET
+    - Stop: 2004-10-14T00:34:40.000(TT)
+
+    Load GTIs for a Fermi-LAT event list:
+
+    >>> filename = '$GAMMAPY_EXTRA/datasets/fermi_2fhl/2fhl_events.fits.gz'
+    >>> gti = GTI.read(filename)
+    >>> print(gti)
+    GTI info:
+    - Number of GTIs: 36589
+    - Duration: 171273490.97510204 s
+    - Start: 54682.659499814814 MET
+    - Start: 2008-08-04T15:49:40.784(TT)
+    - Stop: 57053.993550740735 MET
+    - Stop: 2015-01-31T23:50:42.784(TT)
     """
 
-    def __init__(self, *args, **kwargs):
-        super(GTI, self).__init__(*args, **kwargs)
+    def __init__(self, table):
+        self.table = table
 
     @classmethod
     def read(cls, filename, **kwargs):
@@ -34,24 +68,24 @@ class GTI(Table):
         filename = make_path(filename)
         if 'hdu' not in kwargs:
             kwargs.update(hdu='GTI')
-        return super(GTI, cls).read(str(filename), **kwargs)
+        table = Table.read(str(filename), **kwargs)
+        return cls(table=table)
 
-    def summary(self, file=None):
-        """Summary info string."""
-        if not file:
-            file = sys.stdout
-
-        print('GTI info:', file=file)
-        print('- Number of GTIs: {}'.format(len(self)), file=file)
-        print('- Duration: {}'.format(self.time_sum), file=file)
-        print('- Start: {}'.format(self.time_start[0]), file=file)
-        print('- Stop: {}'.format(self.time_stop[-1]), file=file)
+    def __str__(self):
+        ss = 'GTI info:\n'
+        ss += '- Number of GTIs: {}\n'.format(len(self.table))
+        ss += '- Duration: {}\n'.format(self.time_sum)
+        ss += '- Start: {} MET\n'.format(self.time_start[0])
+        ss += '- Start: {}\n'.format(self.time_start[0].fits)
+        ss += '- Stop: {} MET\n'.format(self.time_stop[-1])
+        ss += '- Stop: {}\n'.format(self.time_stop[-1].fits)
+        return ss
 
     @property
     def time_delta(self):
         """GTI durations in seconds (`~astropy.units.Quantity`)."""
-        start = self['START'].astype('float64')
-        stop = self['STOP'].astype('float64')
+        start = self.table['START'].astype('float64')
+        stop = self.table['STOP'].astype('float64')
         return Quantity(stop - start, 'second')
 
     @property
@@ -62,13 +96,13 @@ class GTI(Table):
     @property
     def time_start(self):
         """GTI start times (`~astropy.time.Time`)."""
-        met_ref = time_ref_from_dict(self.meta)
-        met = Quantity(self['START'].astype('float64'), 'second')
+        met_ref = time_ref_from_dict(self.table.meta)
+        met = Quantity(self.table['START'].astype('float64'), 'second')
         return met_ref + met
 
     @property
     def time_stop(self):
         """GTI end times (`~astropy.time.Time`)."""
-        met_ref = time_ref_from_dict(self.meta)
-        met = Quantity(self['STOP'].astype('float64'), 'second')
+        met_ref = time_ref_from_dict(self.table.meta)
+        met = Quantity(self.table['STOP'].astype('float64'), 'second')
         return met_ref + met
