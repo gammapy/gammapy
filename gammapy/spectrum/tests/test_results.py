c294521d2e97ea2e7b0d7930daf496d5a5cc7e6b
Christoph Deil <Deil.Christoph@gmail.com>
2016-12-29 23:14:57 +0100

    Declare dependencies in gammapy/spectrum/tests/test_results.py

--- gammapy/spectrum/tests/test_results.py
+++ gammapy/spectrum/tests/test_results.py
@@ -1,31 +1,22 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
-from astropy.tests.helper import pytest, assert_quantity_allclose
-import astropy.units as u
 import numpy as np
-from ...datasets import gammapy_extra
-from gammapy.spectrum import (
-    SpectrumObservation,
-    SpectrumFitResult,
-    models,
-)
-
-from ...utils.testing import (
-    requires_dependency,
-    requires_data,
-)
+from astropy.tests.helper import assert_quantity_allclose
+import astropy.units as u
+from ...utils.testing import requires_dependency, requires_data
+from ..models import PowerLaw
+from .. import SpectrumObservation, SpectrumFitResult
 
 
 @requires_dependency('scipy')
 @requires_data('gammapy-extra')
 class TestSpectrumFitResult:
-
     def setup(self):
-        pha = gammapy_extra.filename("datasets/hess-crab4_pha/pha_obs23592.fits")
-        self.obs = SpectrumObservation.read(pha)
-        self.best_fit_model = models.PowerLaw(index=2 * u.Unit(''),
-                                              amplitude=1e-11 * u.Unit('cm-2 s-1 TeV-1'),
-                                              reference=1 * u.TeV)
+        filename = "$GAMMAPY_EXTRA/datasets/hess-crab4_pha/pha_obs23592.fits"
+        self.obs = SpectrumObservation.read(filename)
+        self.best_fit_model = PowerLaw(index=2 * u.Unit(''),
+                                       amplitude=1e-11 * u.Unit('cm-2 s-1 TeV-1'),
+                                       reference=1 * u.TeV)
         self.npred = self.obs.predicted_counts(self.best_fit_model).data.value
         self.covar_axis = ['index', 'amplitude']
         self.covar = np.diag([0.1 ** 2, 1e-12 ** 2])
@@ -41,22 +32,26 @@ class TestSpectrumFitResult:
             obs=self.obs,
         )
 
+    @requires_dependency('uncertainties')
     def test_basic(self):
         assert 'PowerLaw' in str(self.fit_result)
         assert 'index' in self.fit_result.to_table().colnames
 
-    @requires_dependency('matplotlib')
-    def test_plot(self):
-        self.fit_result.plot()
-
+    @requires_dependency('yaml')
     def test_io(self, tmpdir):
-        self.fit_result.to_yaml(tmpdir / 'test.yaml')
-        read_result = SpectrumFitResult.from_yaml(tmpdir / 'test.yaml')
+        filename = tmpdir / 'test.yaml'
+        self.fit_result.to_yaml(filename)
+        read_result = SpectrumFitResult.from_yaml(filename)
         test_e = 12.5 * u.TeV
         assert_quantity_allclose(self.fit_result.model(test_e),
                                  read_result.model(test_e))
 
+    @requires_dependency('uncertainties')
     def test_model_with_uncertainties(self):
         actual = self.fit_result.model_with_uncertainties.parameters.index.s
         desired = np.sqrt(self.covar[0][0])
         assert actual == desired
+
+    @requires_dependency('matplotlib')
+    def test_plot(self):
+        self.fit_result.plot()
