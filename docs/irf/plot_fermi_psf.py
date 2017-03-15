a93810df9dab398dd5ade502c35fed38ab25f007
adonath <axel.donath@mpi-hd.mpg.de>
2017-03-15 14:15:07 +0100

    Use Astropy simple_norm in docs/irf/plot_fermi_psf.py

--- docs/irf/plot_fermi_psf.py
+++ docs/irf/plot_fermi_psf.py
@@ -1,6 +1,7 @@
 """Plot Fermi PSF."""
 import matplotlib.pyplot as plt
 from astropy import units as u
+from astropy.visualization import simple_norm
 from gammapy.datasets import FermiGalacticCenter
 from gammapy.irf import EnergyDependentTablePSF
 from gammapy.image import SkyImage
@@ -17,7 +18,7 @@ energies = [1] * u.GeV
 for energy in energies:
     psf = fermi_psf.table_psf_at_energy(energy=energy)
     image_psf.data = psf.kernel(image_psf)
-    norm = image_psf.plot_norm('log')
+    norm = simple_norm(image_psf.data, stretch='log')
     image_psf.plot(fig=fig, add_cbar=True, norm=norm)
 
 plt.show()
