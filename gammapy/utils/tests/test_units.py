557b2785c26dc4aa78c9c266cc1d6a510c288c8d
Christoph Deil <Deil.Christoph@gmail.com>
2017-04-18 22:07:06 +0200

    Add gammapy.utils.units with standardise_unit

---
+++ gammapy/utils/tests/test_units.py
@@ -0,0 +1,10 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import absolute_import, division, print_function, unicode_literals
+import astropy.units as u
+from ..units import standardise_unit
+
+
+def test_standardise_unit():
+    assert standardise_unit('ph cm-2 s-1') == u.Unit('cm-2 s-1')
+    assert standardise_unit('ct cm-2 s-1') == u.Unit('cm-2 s-1')
+    assert standardise_unit('cm-2 s-1') == u.Unit('cm-2 s-1')
---
+++ gammapy/utils/units.py
@@ -0,0 +1,44 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""Units and Quantity related helper functions"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+import astropy.units as u
+
+__all__ = [
+    'standardise_unit',
+]
+
+
+def standardise_unit(unit):
+    """Standardise unit.
+    
+    Changes applied by this function:
+
+    * Drop "photon" == "ph" from the unit
+    * Drop "count" == "ct" from the unit
+
+    Parameters
+    ----------
+    unit : `~astropy.units.Unit` or str
+        Any old unit
+
+    Returns
+    -------
+    unit : `~astropy.units.Unit`
+        Shiny new, standardised unit
+
+    Examples
+    --------
+    >>> from gammapy.utils.units import standardise_unit
+    >>> standardise_unit('ph cm-2 s-1')
+    Unit("1 / (cm2 s)")
+    >>> standardise_unit('ct cm-2 s-1')
+    Unit("1 / (cm2 s)")
+    >>> standardise_unit('cm-2 s-1')
+    Unit("1 / (cm2 s)")
+    """
+    unit = u.Unit(unit)
+    for base, power in zip(unit.bases, unit.powers):
+        if base == u.Unit('ph') or base == u.Unit('ct'):
+            unit = unit / (base ** power)
+
+    return unit
