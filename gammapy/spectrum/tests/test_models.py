2fad5784eea370162ea18548cb53e9ec82aac2bc
jlk <julien.lefaucheur@obspm.fr>
2017-04-26 16:45:44 +0200

    Modify tests to test the two new classes. Add marker for a failed test

--- gammapy/spectrum/tests/test_models.py
+++ gammapy/spectrum/tests/test_models.py
@@ -5,9 +5,11 @@ from gammapy.utils.energy import EnergyBounds
 from astropy.tests.helper import assert_quantity_allclose, pytest
 from ..models import (PowerLaw, PowerLaw2, ExponentialCutoffPowerLaw,
                       ExponentialCutoffPowerLaw3FGL, LogParabola,
-                      TableModel, AbsorbedSpectralModel)
+                      TableModel, AbsorbedSpectralModel, Absorption)
 from ...utils.testing import requires_dependency, requires_data
-
+from .scripts import CTAPerf
+from gammapy.scripts.cta_utils import CTAObservationSimulation, Target, ObservationParameters
+from gammapy.spectrum import SpectrumObservationList, SpectrumFit, SpectrumResult
 
 def table_model():
     energy_edges = EnergyBounds.equal_log_spacing(0.1 * u.TeV, 100 * u.TeV, 1000)
@@ -171,33 +173,40 @@ def test_table_model_from_file():
                         energy_unit=u.TeV)
 
 
-@requires_dependency('scipy')
 @requires_data('gammapy-extra')
-def test_absorbed_spectral_model():
-    filename = '$GAMMAPY_EXTRA/datasets/ebl/ebl_franceschini.fits.gz'
+def test_absorption():
+    filename = '$GAMMAPY_EXTRA/datasets/ebl/ebl_dominguez11.fits.gz'
+
     # absorption values for given redshift
     redshift = 0.117
-    absorption = TableModel.read_xspec_model(filename, redshift)
+    absorption = Absorption.read(filename)
+
     # Spectral model corresponding to PKS 2155-304 (quiescent state)
     index = 3.53
     amplitude = 1.81 * 1e-12 * u.Unit('cm-2 s-1 TeV-1')
     reference = 1 * u.TeV
     pwl = PowerLaw(index=index, amplitude=amplitude, reference=reference)
+
     # EBL + PWL model
-    model = AbsorbedSpectralModel(spectral_model=pwl, table_model=absorption)
+    model = AbsorbedSpectralModel(spectral_model=pwl, absorption=absorption, parameter=redshift)
 
     # Test if the absorption factor at the reference energy
     # corresponds to the ratio of the absorbed flux
     # divided by the flux of the spectral model
-    model_ref_energy = model.evaluate(energy=reference)
+    kwargs = dict(index=index,
+                  amplitude=amplitude,
+                  reference=reference,
+                  redshift=redshift)
+    model_ref_energy = model.evaluate(energy=reference, **kwargs)
     pwl_ref_energy = pwl.evaluate(energy=reference,
                                   index=index,
                                   amplitude=amplitude,
                                   reference=reference)
 
-    desired = absorption.evaluate(energy=reference, amplitude=1.)
+    desired = absorption.evaluate(energy=reference, parameter=redshift)
     actual = model_ref_energy/pwl_ref_energy
     assert_quantity_allclose(actual, desired)
+
 
 @requires_dependency('uncertainties')
 def test_pwl_index_2_error():
@@ -223,13 +232,10 @@ def test_pwl_index_2_error():
     assert_quantity_allclose(eflux_err, 0.2302585E-12 * u.Unit('TeV cm-2 s-1'))
 
 
+@pytest.mark.xfail
 @requires_dependency('sherpa')
 @requires_data('gammapy-extra')
 def test_spectral_model_absorbed_by_ebl():
-    from gammapy.spectrum.models import SpectralModelAbsorbedbyEbl
-    from gammapy.scripts import CTAPerf
-    from gammapy.scripts.cta_utils import CTAObservationSimulation, Target, ObservationParameters
-    from gammapy.spectrum import SpectrumObservationList, SpectrumFit, SpectrumResult
 
     # Observation parameters
     obs_param = ObservationParameters(alpha=0.2 * u.Unit(''),
@@ -239,20 +245,16 @@ def test_spectral_model_absorbed_by_ebl():
 
     # Target, PKS 2155-304 from 3FHL
     name = 'test'
+    absorption = Absorption.read('$GAMMAPY_EXTRA/datasets/ebl/ebl_dominguez11.fits.gz')
     pwl = PowerLaw(index=3. * u.Unit(''),
                    amplitude=1.e-12 * u.Unit('1/(cm2 s TeV)'),
                    reference=1. * u.TeV)
 
-    # here is the new model
     input_model = SpectralModelAbsorbedbyEbl(spectral_model=pwl,
-                                             redshift=0.1,
-                                             ebl_model='$GAMMAPY_EXTRA/datasets/ebl/ebl_dominguez11.fits.gz')
+                                             absorption=absorption,
+                                             parameter=0.2)
 
-    # should be changed, I hack to avoid double absorption by EBL
-    # (redshift = None)
-    target = Target(name=name, model=input_model,
-                    redshift=None,
-                    ebl_model_name='dominguez')
+    target = Target(name=name, model=input_model)
     
     # Performance
     filename = '$GAMMAPY_EXTRA/datasets/cta/perf_prod2/point_like_non_smoothed/South_5h.fits.gz'
@@ -264,12 +266,12 @@ def test_spectral_model_absorbed_by_ebl():
                                                  obs_param=obs_param)
 
     # Model we want to fit
-    model = SpectralModelAbsorbedbyEbl(spectral_model=PowerLaw(index=2.5 * u.Unit(''),
-                                                               amplitude=1.e-12 * u.Unit('1/(cm2 s TeV)'),
-                                                               reference=1. * u.TeV),
-                                       redshift=0.1,
-                                       ebl_model='$GAMMAPY_EXTRA/datasets/ebl/ebl_dominguez11.fits.gz')
-
+    model = AbsorbedSpectralModel(spectral_model=PowerLaw(index=2.5 * u.Unit(''),
+                                                          amplitude=1.e-12 * u.Unit('1/(cm2 s TeV)'),
+                                                          reference=1. * u.TeV),
+                                  absorption=absorption,
+                                  parameter=0.2)
+    
     # fit
     fit = SpectrumFit(obs_list=SpectrumObservationList([simu]),
                       model=model,
