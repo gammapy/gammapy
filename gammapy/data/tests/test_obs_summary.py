782ecb0fe8b512e497047a1abe6fd11f07ebba9e
Christoph Deil <Deil.Christoph@gmail.com>
2017-04-26 23:07:05 +0200

    Fix up observation stats summary

--- gammapy/data/tests/test_obs_summary.py
+++ gammapy/data/tests/test_obs_summary.py
@@ -1,27 +1,28 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
 from numpy.testing import assert_allclose
+from astropy.tests.helper import pytest
 from astropy.coordinates import SkyCoord
 import astropy.units as u
 from regions import CircleSkyRegion
 from ...data import DataStore, ObservationTableSummary, ObservationSummary
-from ...data import ObservationStats, ObservationStatsList, ObservationList
-from ...data import Target
+from ...data import ObservationStats
 from ...utils.testing import requires_data, requires_dependency
 from ...background import ReflectedRegionsBackgroundEstimator
 from ...image import SkyImage
 
 
-def table_summary():
-    data_store = DataStore.from_dir('$GAMMAPY_EXTRA/datasets/hess-crab4-hd-hap-prod2/')
-    target_pos = SkyCoord(83.633083, 22.0145, unit='deg')
-    return ObservationTableSummary(data_store.obs_table, target_pos)
-
-
 @requires_data('gammapy-extra')
 class TestObservationSummaryTable:
-    def setup(self):
-        self.table_summary = table_summary()
+    @staticmethod
+    def make_observation_summary_table():
+        data_store = DataStore.from_dir('$GAMMAPY_EXTRA/datasets/hess-crab4-hd-hap-prod2/')
+        target_pos = SkyCoord(83.633083, 22.0145, unit='deg')
+        return ObservationTableSummary(data_store.obs_table, target_pos)
+
+    @classmethod
+    def setup_class(cls):
+        cls.table_summary = cls.make_observation_summary_table()
 
     def test_str(self):
         text = str(self.table_summary)
@@ -41,60 +42,72 @@ class TestObservationSummaryTable:
         self.table_summary.plot_offset_distribution()
 
 
-def obs_summary():
-    datastore = DataStore.from_dir('$GAMMAPY_EXTRA/datasets/hess-crab4-hd-hap-prod2/')
-    run_list = [23523, 23526, 23559, 23592]
-
-    pos = SkyCoord(83.63 * u.deg, 22.01 * u.deg, frame='icrs')
-    on_size = 0.3 * u.deg
-    on_region = CircleSkyRegion(pos, on_size)
-
-    target = Target(position=pos, on_region=on_region,
-                    name='Crab Nebula', tag='crab')
-
-    mask = SkyImage.read('$GAMMAPY_EXTRA/datasets/exclusion_masks/tevcat_exclusion.fits')
-
-    obs_list = ObservationList([datastore.obs(_) for _ in run_list])
-    obs_stats = ObservationStatsList()
-
-    for index, run in enumerate(obs_list):
-        bkg = ReflectedRegionsBackgroundEstimator.process(on_region=on_region,
-                                                          obs=run,
-                                                          exclusion=mask)
-
-        obs_stats.append(ObservationStats.from_target(run, target, bkg))
-
-    summary = ObservationSummary(obs_stats)
-
-    return summary
-
-
 @requires_data('gammapy-extra')
 @requires_dependency('scipy')
-@requires_dependency('matplotlib')
 class TestObservationSummary:
     """
     Test observation summary.
     """
 
-    def setup(self):
-        self.obs_summary = obs_summary()
+    @staticmethod
+    def make_observation_summary():
+        datastore = DataStore.from_dir('$GAMMAPY_EXTRA/datasets/hess-crab4-hd-hap-prod2/')
+        obs_ids = [23523, 23526]
 
-    def test_plot_significance(self):
-        self.obs_summary.plot_significance_vs_livetime()
+        pos = SkyCoord(83.63 * u.deg, 22.01 * u.deg, frame='icrs')
+        on_size = 0.3 * u.deg
+        on_region = CircleSkyRegion(pos, on_size)
 
-    def test_plot_excess(self):
-        self.obs_summary.plot_excess_vs_livetime()
+        exclusion_mask = SkyImage.read('$GAMMAPY_EXTRA/datasets/exclusion_masks/tevcat_exclusion.fits')
 
-    def test_plot_background(self):
-        self.obs_summary.plot_background_vs_livetime()
+        obs_stats_list = []
+        for obs_id in obs_ids:
+            obs = datastore.obs(obs_id)
+            bkg = ReflectedRegionsBackgroundEstimator(
+                on_region=on_region,
+                obs_list=[obs],
+                exclusion_mask=exclusion_mask,
+            )
+            bkg.run()
+            bg_estimate = bkg.result[0]
 
-    def test_plot_gamma_rate(self):
-        self.obs_summary.plot_gamma_rate()
+            obs_stats = ObservationStats.from_obs(obs, bg_estimate)
+            obs_stats_list.append(obs_stats)
 
-    def test_plot_background_rate(self):
-        self.obs_summary.plot_background_rate()
+        return ObservationSummary(obs_stats_list)
+
+    @classmethod
+    def setup_class(cls):
+        cls.obs_summary = cls.make_observation_summary()
+
+    @pytest.mark.xfail
+    def test_results(self):
+        # There's no test asserting on the result numbers yet!!!
+        print(self.obs_summary)
+        from pprint import pprint
+        pprint(self.obs_summary.__dict__)
+        assert 0
 
     def test_obs_str(self):
         text = str(self.obs_summary)
         assert 'Observation summary' in text
+
+    @requires_dependency('matplotlib')
+    def test_plot_significance(self):
+        self.obs_summary.plot_significance_vs_livetime()
+
+    @requires_dependency('matplotlib')
+    def test_plot_excess(self):
+        self.obs_summary.plot_excess_vs_livetime()
+
+    @requires_dependency('matplotlib')
+    def test_plot_background(self):
+        self.obs_summary.plot_background_vs_livetime()
+
+    @requires_dependency('matplotlib')
+    def test_plot_gamma_rate(self):
+        self.obs_summary.plot_gamma_rate()
+
+    @requires_dependency('matplotlib')
+    def test_plot_background_rate(self):
+        self.obs_summary.plot_background_rate()
