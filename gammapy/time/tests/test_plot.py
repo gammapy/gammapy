9ca7df778a53372bebeb0cc72431166a80a73c0b
Christoph Deil <Deil.Christoph@gmail.com>
2017-05-09 21:22:18 +0200

    remove old code in gammapy.time

--- gammapy/time/tests/test_plot.py
+++ gammapy/time/tests/test_plot.py
@@ -5,11 +5,6 @@ from ...utils.testing import requires_dependency, requires_data
 from ..plot import plot_fermi_3fgl_light_curve
 
 
-@requires_dependency('matplotlib')
-def test_plot_time_difference_distribution():
-    pass
-
-
 # Some change in our dependencies (maybe matplotlib, maybe astropy)
 # makes this now fail with this error:
 # https://travis-ci.org/gammapy/gammapy/jobs/140330531#L1607
