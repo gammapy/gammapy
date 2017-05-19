b2aed989361ec59bc503e868b11e03dd782734c6
Christoph Deil <Deil.Christoph@gmail.com>
2017-05-19 14:01:17 +0200

    Remove gammapy/spectrum/sherpa_chi2asym.py

--- gammapy/spectrum/__init__.py
+++ gammapy/spectrum/__init__.py
@@ -9,7 +9,6 @@ from .crab import *
 from .diffuse import *
 from .energy_group import *
 from .flux_point import *
-from .sherpa_chi2asym import *
 from .utils import *
 from .extract import *
 from .simulation import *
