Fix fp dnde computation for spectral models with numerical integration

gammapy/spectrum/models.py
@@ -602,7 +602,7 @@ class PowerLaw2(SpectralModel):
         bottom = emax ** (-index + 1) - emin ** (-index + 1)
         return amplitude * (top / bottom) * np.power(energy, -index)
 
-    def integral(self, emin, emax):
+    def integral(self, emin, emax, **kwargs):
         r"""Integrate power law analytically.
 
         .. math::

gammapy/spectrum/tests/test_flux_point.py
@@ -33,7 +33,7 @@ class LWTestModel(SpectralModel):
     def evaluate(x):
         return 1e4 * np.exp(-6 * x)
 
-    def integral(self, xmin, xmax):
+    def integral(self, xmin, xmax, **kwargs):
         return - 1. / 6 * 1e4 * (np.exp(-6 * xmax) - np.exp(-6 * xmin))
 
     def inverse(self, y):
@@ -47,7 +47,7 @@ class XSqrTestModel(SpectralModel):
     def evaluate(x):
         return x ** 2
 
-    def integral(self, xmin, xmax):
+    def integral(self, xmin, xmax, **kwargs):
         return 1. / 3 * (xmax ** 3 - xmin ** 2)
 
     def inverse(self, y):
@@ -61,7 +61,7 @@ class ExpTestModel(SpectralModel):
     def evaluate(x):
         return np.exp(x * u.Unit('1 / TeV'))
 
-    def integral(self, xmin, xmax):
+    def integral(self, xmin, xmax, **kwargs):
         return np.exp(xmax * u.Unit('1 / TeV')) - np.exp(xmin * u.Unit('1 / TeV'))
 
     def inverse(self, y):