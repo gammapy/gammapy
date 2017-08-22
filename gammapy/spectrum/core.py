f71f90fb28b14d3cedb2483eff056dd78d8d6b15
Johannes King <johannes.king@mpi-hd.mpg.de>
2017-08-22 13:41:05 +0200

    fix bug in CountsSpectrum.plot_hist

--- gammapy/spectrum/core.py
+++ gammapy/spectrum/core.py
@@ -215,7 +215,7 @@ class CountsSpectrum(object):
         kwargs.setdefault('lw', 2)
         kwargs.setdefault('histtype', 'step')
         weights = self.data.data.value
-        bins = self.energy.bins.to(energy_unit).value[:-1]
+        bins = self.energy.bins.to(energy_unit).value
         x = self.energy.nodes.to(energy_unit).value
         ax.hist(x, bins=bins, weights=weights, **kwargs)
         if show_energy is not None:
