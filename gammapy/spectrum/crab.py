be2e4a45d11e93addb0945344a91e1500ebcea9c
cosimoNigro <cosimonigro2@gmail.com>
2017-09-14 17:06:20 +0200

    checked

--- gammapy/spectrum/crab.py
+++ gammapy/spectrum/crab.py
@@ -26,7 +26,7 @@ hegra = {'amplitude': 2.83e-11 * u.Unit('1 / (cm2 s TeV)'),
 
 # MAGIC publication: JHEAP 5-6 (2015), 30-38
 # note that in the paper the beta of the LogParabola is given as negative in  
-# Table 1 (pag. 33), but should be positive to match gammapy LogParabola def
+# Table 1 (pag. 33), but should be positive to match gammapy LogParabola expression
 magic_lp ={'amplitude': 3.23e-11 * u.Unit('1 / (cm2 s TeV)'),
 		   'alpha': 2.47,
 		   'beta': 0.24,
@@ -68,9 +68,9 @@ class CrabSpectrum(object):
 
     The following references are available:
 
- 	* 'meyer', http://adsabs.harvard.edu/abs/2010A%26A...523A...2M, Appendix D
-  	* 'hegra', http://adsabs.harvard.edu/abs/2000ApJ...539..317A
- 	* 'hess_pl' and 'hess_ecpl': http://adsabs.harvard.edu/abs/2006A%26A...457..899A
+    * 'meyer', http://adsabs.harvard.edu/abs/2010A%26A...523A...2M, Appendix D
+    * 'hegra', http://adsabs.harvard.edu/abs/2000ApJ...539..317A
+    * 'hess_pl' and 'hess_ecpl': http://adsabs.harvard.edu/abs/2006A%26A...457..899A
     * 'magic_lp' and 'magic_ecpl': http://www.sciencedirect.com/science/article/pii/S2214404815000038?via%3Dihub
 
     Parameters
