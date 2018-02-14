dece764ac40f1a8c815f8d98f11d7d79dcc43d30
Lab Saha <labsaha@ucm.es>
2018-02-14 14:31:46 +0100

    Updating the class

--- gammapy/time/models.py
+++ gammapy/time/models.py
@@ -1,13 +1,11 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
-import logging
 import numpy as np
 from ..utils.modeling import Parameter, ParameterList
 
 __all__ = [
     'PhaseCurve',
 ]
-
-log = logging.getLogger(__name__)
 
 
 class PhaseCurve(object):
@@ -16,9 +14,6 @@ class PhaseCurve(object):
     This is to calculate phase of a periodic system and to provide the 
     normalization factor for the phase using circular interpolation. 
     The required data for interpolation is given as an astropy table
-
-    # TODO: 1. Checking table values if the phases are in increasing order
-            2. Check normalization constant to set maximum values
 
     Parameters
     ----------
@@ -32,11 +27,6 @@ class PhaseCurve(object):
     Φ(t)=Φ0+f0(t−t0)+(1/2)f1(t−t0)^2+(1/6)f2(t−t0)^3
 
 
-    Returns
-    --------
-    Phase corresponding to the time provided as input
-    It also calculates the normalization constant for that phase
-
     Examples
     --------
     This shows how to get the phase and the normalization constant
@@ -46,20 +36,16 @@ class PhaseCurve(object):
     norm_const = phase_curve.evaluate(time)
     """
 
-
-      
-    def __init__(self,table,reference,phase0,f0,f1,f2):
-
+    def __init__(self, table, reference, phase0, f0, f1, f2):
         self.table = table
         self.parameters = ParameterList([
-        Parameter('reference', reference),
-        Parameter('phase0', phase0),
-        Parameter('f0', f0),
-        Parameter('f1', f1),
-        Parameter('f2', f2)])
-     
+            Parameter('reference', reference),
+            Parameter('phase0', phase0),
+            Parameter('f0', f0),
+            Parameter('f1', f1),
+            Parameter('f2', f2)])
 
-    def phase(self,time):
+    def phase(self, time):
         """ Calculate phase 
         
         Parameters
@@ -70,24 +56,24 @@ class PhaseCurve(object):
         ---------
         phase: array_like
         """
-        
+
         c1 = 0.5
-        c2 = 1.0/6.0
+        c2 = 1.0 / 6.0
         pars = self.parameters
         reference = pars['reference'].value
-        phase0    = pars['phase0'].value
-        f0        = pars['f0'].value
-        f1        = pars['f1'].value
-        f2        = pars['f2'].value
+        phase0 = pars['phase0'].value
+        f0 = pars['f0'].value
+        f1 = pars['f1'].value
+        f2 = pars['f2'].value
 
         t = time - reference
 
-        phase = phase0 + t * (f0 + t * (c1 * f1 +  c2 * f2 * t))
-          
-        phase = np.remainder(phase,1)
+        phase = phase0 + t * (f0 + t * (c1 * f1 + c2 * f2 * t))
+
+        phase = np.remainder(phase, 1)
         return phase
-     
-    def evaluate(self,time):
+
+    def evaluate(self, time):
         """
         To calculate the normalization constant for
         the phase calculated from the given time
@@ -99,11 +85,10 @@ class PhaseCurve(object):
         phase = self.phase(time)
         tbdata = self.table
         phase_col = tbdata['PHASE']
-        norm_col  = tbdata['NORM']
+        norm_col = tbdata['NORM']
 
         x_values = phase_col
         f_values = norm_col
 
-        f_norm = np.interp(x = phase,xp= x_values,fp = f_values, period = 1)
+        f_norm = np.interp(x=phase, xp=x_values, fp=f_values, period=1)
         return f_norm
-
--- gammapy/time/tests/test_models.py
+++ gammapy/time/tests/test_models.py
@@ -1,27 +1,26 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 from astropy.table import Table
-import numpy as np
 import pytest
 from numpy.testing import assert_allclose
-from gammapy.time.models import PhaseCurve
-from gammapy.utils.testing import requires_data
-from gammapy.utils.scripts import make_path
-
+from ..models import PhaseCurve
+from ...utils.scripts import make_path
 
 
 @pytest.fixture(scope='session')
 def phase_curve():
     filename = make_path('$GAMMAPY_EXTRA/test_datasets/phasecurve_LSI_DC.fits')
-    print(filename)
     table = Table.read(str(filename))
-    return PhaseCurve(table,reference=43366.275,phase0 = 0.0,f0 = 0.5,f1 = 0.0,f2 = 0.0
-)
+    return PhaseCurve(table, reference=43366.275, phase0=0.0, f0=0.5, f1=0.0, f2=0.0
+                      )
+
 
 # To check the value of the phase
 def test_phasecurve_phase(phase_curve):
     time = 46300.0
-    assert_allclose(phase_curve.phase(time),0.86,rtol=0.01)
+    assert_allclose(phase_curve.phase(time), 0.86, rtol=0.01)
+
 
 # To check the value of the normalization constant
 def test_phasecurve_norm(phase_curve):
     time = 46300.0
-    assert_allclose(phase_curve.evaluate(time),0.05,rtol=0.01)
+    assert_allclose(phase_curve.evaluate(time), 0.05, rtol=0.01)
