819d3193cfa13efc7034b4eb07b8cf2ef83161bc
Terrier <rterrier@apc.in2p3.fr>
2018-02-16 00:01:25 +0100

    Corrected small issues

--- gammapy/cube/basic_cube.py
+++ gammapy/cube/basic_cube.py
@@ -4,16 +4,15 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 from ..maps import WcsNDMap
 
-def make_map_counts(evts, valid_ref_map):
-    """
-    Build a ``WcsNDMap` with events from an EventList.
+def make_map_counts(event_list, valid_ref_map):
+    """Build a ``WcsNDMap` with events from an EventList.
+
     The energy of the events is used for a non-spatial axis homogeneous to energy.
     The other non-spatial axis names should have an entry in the colum names of the ``EventList``
 
     Parameters
-    __________
-
-    evts : `~gammapy.data.EventList`
+    ----------
+    event_list : `~gammapy.data.EventList`
             the input event list
     valid_ref_map : `~gammapy.maps.WcsNDMap`
         Map containing the valid pixels
@@ -22,18 +21,19 @@ def make_map_counts(evts, valid_ref_map):
     -------
     cntmap : `~gammapy.maps.WcsNDMap`
         Count cube (3D) in true energy bins
-
     """
     # The list will contain the event table entries to be fed into the WcsNDMap
     tmp = list()
     ref_geom = valid_ref_map.geom
     # Convert events coordinates
     if ref_geom.coordsys == 'GAL':
-        tmp.append(evts.galactic.l)
-        tmp.append(evts.galactic.b)
+        galactic_coords = event_list.galactic
+        tmp.append(galactic_coords.l)
+        tmp.append(galactic_coords.b)
     elif ref_geom.coordsys == 'CEL':
-        tmp.append(evts.radec.ra)
-        tmp.append(evts.radec.dec)
+        radec_coords = event_list.radec
+        tmp.append(radec_coords.ra)
+        tmp.append(radec_coords.dec)
     else:
         # should raise an error here. The map is not correct.
         raise ValueError("Incorrect coordsys of input map.")
@@ -41,13 +41,13 @@ def make_map_counts(evts, valid_ref_map):
     for i, axis in enumerate(ref_geom.axes):
         if axis.unit.is_equivalent("eV"):
             # This axis is the energy
-            tmp.append(evts.energy.to(axis.unit))
-        elif axis.name.upper() in evts.table.colnames:
-            # Here we assume that colanmes are all capital
-            tmp.append(evts.table[axis.name.upper()].to(axis.unit))
-        elif axis.name.lower() in evts.table.colnames:
-            # Here we assume that colanmes are not capital
-            tmp.append(evts.table[axis.name.lower()].to(axis.unit))
+            tmp.append(event_list.energy.to(axis.unit))
+        elif axis.name.upper() in event_list.table.colnames:
+            # Here we assume that colnames are all capital
+            tmp.append(event_list.table[axis.name.upper()].to(axis.unit))
+        elif axis.name.lower() in event_list.table.colnames:
+            # Here we assume that colnames are all lower cases
+            tmp.append(event_list.table[axis.name.lower()].to(axis.unit))
         else:
             raise ValueError("Cannot find MapGeom axis {} in EventList", axis.name)
 
