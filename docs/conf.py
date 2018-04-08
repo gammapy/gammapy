85a5576877657d0b6feaf713505a8aa4c49ed118
Jose Enrique Ruiz <jer@iaa.es>
2018-04-08 14:30:22 +0200

    Add gammapy css style

--- docs/conf.py
+++ docs/conf.py
@@ -192,10 +192,17 @@ html_static_path = ['_static']
 
 gammapy_sphinx_ext_activate()
 
-# integration of notebooks from gamapy-extra repo
+# Integration of notebooks from gamapy-extra repo
 gammapy_sphinx_notebooks(setup_cfg)
 
+# Theme style
 #html_style = 'gammapy.css'
+html_context = {
+    'css_files': [
+        '_static/gammapy.css',
+    ],
+}
+html_favicon = ''
 
 # -- Options for LaTeX output --------------------------------------------------
 
