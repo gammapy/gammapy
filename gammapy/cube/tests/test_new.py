c64d11444fdaaf39d8f473cfc05da3fcd739c603
Christoph Deil <Deil.Christoph@gmail.com>
2018-04-12 17:40:59 +0200

    Add gammapy/cube/tests/test_new.py

--- gammapy/cube/tests/test_new.py
+++ gammapy/cube/tests/test_new.py
@@ -9,8 +9,8 @@ from astropy.coordinates import Angle, SkyCoord
 from astropy.tests.helper import assert_quantity_allclose
 from ...utils.testing import requires_dependency, requires_data
 from ...irf import EffectiveAreaTable2D, Background3D
-from .. import make_exposure_cube, make_background_cube
-from .. import SkyCube
+from ...maps import WcsNDMap
+from ..new import make_map_exposure_true_energy, make_map_hadron_acceptance
 
 
 @pytest.fixture(scope='session')
@@ -32,50 +32,40 @@ def counts_cube():
         os.environ['GAMMAPY_EXTRA'],
         'datasets/hess-crab4-hd-hap-prod2/hess_events_simulated_023523_cntcube.fits'
     )
-    return SkyCube.read(filename, format='fermi-counts')
+    return WcsNDMap.read(filename)
 
 
 @requires_dependency('scipy')
 @requires_data('gammapy-extra')
-def test_exposure_cube(aeff, counts_cube):
+def test_make_map_exposure_true_energy(aeff, counts_cube):
     pointing = SkyCoord(83.633, 21.514, unit='deg')
     livetime = Quantity(1581.17, 's')
     offset_max = Angle(2.2, 'deg')
 
-    exp_cube = make_exposure_cube(
-        pointing, livetime, aeff, counts_cube, offset_max=offset_max,
+    m = make_map_exposure_true_energy(
+        pointing, livetime, aeff, counts_cube.geom, offset_max,
     )
-    exp_ref = Quantity(4.7e8, 'm2 s')
-    coordinates = exp_cube.sky_image_ref.coordinates()
-    offset = coordinates.separation(pointing)
 
-    assert np.shape(exp_cube.data)[1:] == np.shape(counts_cube.data)[1:]
-    assert np.shape(exp_cube.data)[0] == np.shape(counts_cube.data)[0]
-    assert exp_cube.wcs == counts_cube.wcs
-    assert_quantity_allclose(np.nanmax(exp_cube.data), exp_ref, rtol=100)
-    assert exp_cube.data.unit == exp_ref.unit
-    assert exp_cube.data[:, offset > offset_max].sum() == 0
+    assert m.data.shape == (15, 120, 200)
+    assert_quantity_allclose(np.nanmax(m.data), 4.7e8, rtol=100)
 
 
 @requires_dependency('scipy')
 @requires_data('gammapy-extra')
-def test_background_cube(bkg_3d, counts_cube):
+def test_make_map_fov_background(bkg_3d, counts_cube):
     pointing = SkyCoord(83.633, 21.514, unit='deg')
     livetime = Quantity(1581.17, 's')
     offset_max = Angle(2.2, 'deg')
 
-    bkg_cube = make_background_cube(
-        pointing, livetime, bkg_3d, counts_cube, offset_max=offset_max,
+    m = make_map_hadron_acceptance(
+        pointing, livetime, bkg_3d, counts_cube.geom, offset_max,
     )
 
-    assert bkg_cube.data.shape == (15, 120, 200)
-    assert bkg_cube.data.unit == ''
+    assert m.data.shape == (15, 120, 200)
+    assert_allclose(m.data[0, 0, 0], 0.013759879207779322)
+    assert_allclose(m.data.sum(), 1301.0242859188463)
 
-    print(bkg_cube.data.sum())
-    assert_allclose(bkg_cube.data[0, 0, 0], 0.013959329891790048)
-    assert_allclose(bkg_cube.data.sum(), 1315.7910319477235)
-
-    # Check that `offset_max` is working properly
-    pos = SkyCoord(85.6, 23, unit='deg')
-    val = bkg_cube.lookup(pos, energy=1 * u.TeV)
-    assert_allclose(val, 0)
+    # TODO: Check that `offset_max` is working properly
+    # pos = SkyCoord(85.6, 23, unit='deg')
+    # val = bkg_cube.lookup(pos, energy=1 * u.TeV)
+    # assert_allclose(val, 0)
