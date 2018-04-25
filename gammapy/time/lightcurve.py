corrected alpha computation, cleaned some code, take care of a special case : last interval created with the last event

gammapy/time/lightcurve.py
@@ -326,7 +326,6 @@ class LightCurveEstimator(object):
                                             spectrum_extraction, separators=[]):
         """
         Create time intervals such that each bin of a light curve reach a given significance
-        ### To Do : check logic ###
         Parameters
         ----------
         significance: float
@@ -352,7 +351,6 @@ class LightCurveEstimator(object):
         def in_list(item, L):
             o, j = np.where(L == item)
             for index in o:
-                # yield L.index(i)
                 yield index
 
         time_holder = []
@@ -363,11 +361,13 @@ class LightCurveEstimator(object):
             time_holder.append([time, 'break'])
 
         for obs in spectrum_extraction.obs_list:
+            # start and end will need to be redefined once the data storage format is fixed
             time_holder.append([obs.events.time.min().value - 0.0000001, 'start'])
             time_holder.append([obs.events.time.max().value + 0.0000001, 'end'])
             obs_properties.append([obs.observation_dead_time_fraction, spectrum_extraction.bkg_estimate[n_obs].a_off])
             n_obs += 1
 
+        # prepare the on and off photon list as in the flux point computation -> should be updated accordingly
         for t_index, obs in enumerate(self.obs_list):
             spec = self.obs_spec[t_index]
             # get ON and OFF evt list
@@ -392,53 +392,46 @@ class LightCurveEstimator(object):
         # sort all elements in the table by time
         time_holder = sorted(time_holder, key=gettime)
         time_holder = np.asarray(time_holder)
+
         intervals = []
         istart = 1
         i = 1
         n = 0
-        count = 0
-        while (time_holder[i][0] < time_holder[-1][0]):
+        while time_holder[i][0] < time_holder[-1][0]:
             i += 1
             if time_holder[i][1] == 'break':
-                n += np.sum(time_holder[istart:i + 1] == 'end')
-                istart = i + 1
-                i = istart
+                while time_holder[i+1][1] != 'on' and time_holder[i+1][1] != 'off':
+                    i += 1
+                n += np.sum(time_holder[istart:i] == 'end')
+                istart = i
                 continue
             if time_holder[i][1] != 'on' and time_holder[i][1] != 'off':
                 continue
 
-            # make a function to compute alpha
+            # compute alpha
             alpha = 0
             tmp = 0
             time = 0
             xm1 = istart
+            # loop over observations
             for x in in_list('end', time_holder[istart:i + 1]):
-                if tmp == 0:
-                    alpha += (1 - obs_properties[n][0]) * (float(time_holder[x][0]) - float(time_holder[istart][0])) * \
-                             obs_properties[n][1]
-                    time += (1 - obs_properties[n][0]) * (float(time_holder[x][0]) - float(time_holder[istart][0]))
-                else:
-                    alpha += (1 - obs_properties[n + tmp][0]) * (
-                                float(time_holder[x][0]) - float(time_holder[xm1][0])) * obs_properties[n][1]
-                    time += (1 - obs_properties[n + tmp][0]) * (float(time_holder[x][0]) - float(time_holder[xm1][0]))
-                xm1 = x
+                alpha += (1 - obs_properties[n + tmp][0]) * (float(time_holder[x][0]) - float(time_holder[xm1][0])) * obs_properties[n+tmp][1]
+                time += (1 - obs_properties[n + tmp][0]) * (float(time_holder[x][0]) - float(time_holder[xm1][0]))
+                xm1 = x+1
                 tmp += 1
-            alpha += (1 - obs_properties[n + tmp][0]) * (float(time_holder[i][0]) - float(time_holder[xm1][0])) * \
-                     obs_properties[n][1]
+            alpha += (1 - obs_properties[n + tmp][0]) * (float(time_holder[i][0]) - float(time_holder[xm1][0])) * obs_properties[n+tmp][1]
             time += (1 - obs_properties[n + tmp][0]) * (float(time_holder[i][0]) - float(time_holder[xm1][0]))
             alpha = time / alpha
             non = np.sum(time_holder[istart:i + 1] == 'on')
             noff = np.sum(time_holder[istart:i + 1] == 'off')
-            ### add try catch
             if non == 0 or noff == 0:
                 continue
             if significance_on_off(non, noff, alpha, method=significance_method) > significance:
-                count += 1
                 n += np.sum(time_holder[istart:i + 1] == 'end')
                 intervals.append(
-                    [Time((float(time_holder[istart][0]) + float(time_holder[istart][0])) / 2, format="mjd"),
+                    [Time((float(time_holder[istart-1][0]) + float(time_holder[istart][0])) / 2, format="mjd"),
                      Time((float(time_holder[i][0]) + float(time_holder[i + 1][0])) / 2, format="mjd")])
-                while time_holder[i + 1][1] != 'on' and time_holder[i + 1][1] != 'off':
+                while time_holder[i + 1][0] < time_holder[-1][0] and time_holder[i + 1][1] != 'on' and time_holder[i + 1][1] != 'off':
                     i += 1
                 istart = i + 1
                 i = istart