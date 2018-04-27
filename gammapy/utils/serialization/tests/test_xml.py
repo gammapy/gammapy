db29df8b37fc45e6f3658e9086579dc9854f2618
Johannes King <johannes.king@mpi-hd.mpg.de>
2018-04-27 10:41:54 +0200

    add fermi_model.xml to gammapy-extra

--- gammapy/utils/serialization/tests/test_xml.py
+++ gammapy/utils/serialization/tests/test_xml.py
@@ -1,14 +1,13 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
 from numpy.testing import assert_allclose
-from astropy.utils.data import get_pkg_data_filename
 from ...scripts import make_path
 from ....cube import SourceLibrary
 from ....spectrum import models as spectral
 from ....image import models as spatial
 
 def test_xml_to_source_library():
-    filename = get_pkg_data_filename('data/fermi_model.xml')
+    filename = '$GAMMAPY_EXTRA/test_datasets/models/fermi_model.xml'
     sourcelib = SourceLibrary.from_xml(filename)
 
     assert len(sourcelib.skymodels) == 4
