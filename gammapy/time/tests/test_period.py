8b7f0c8f0a18188b04863f4549857631c2aaf53f
wegenmat <matthias.wegen@desy.de>
2018-07-02 18:09:14 +0200

    fixed repo after rebase so that docs work now

--- gammapy/time/tests/test_period.py
+++ gammapy/time/tests/test_period.py
@@ -3,22 +3,25 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import pytest
 import numpy as np
 from numpy.testing import assert_allclose
+from collections import OrderedDict
+from astropy.stats.lombscargle import _statistics
+from ..period import robust_periodogram
 from ...utils.testing import requires_dependency
-from ..period import lomb_scargle
 
 
 def simulate_test_data(period, amplitude, t_length, n_data, n_obs, n_outliers):
-    """Function for creating an unevenly data biased by outliers.
+    """
+    Function for creating an unevenly data biased by outliers.
 
-    As underlying model, a single harmonic is used.
+    As underlying model, a single harmonic model is chosen.
     First, an evenly sampled test data set is generated.
-    It the is distorted by randomly choosing data points.
+    It is distorted by randomly drawing data points from an uniform distribution.
     Outliers are simulated as exponential burst with ten times the
     amplitude decreasing with a characteristic time length of 1.
     Flux errors are assumed to be gaussian and homoscedastic.
 
-    It returns arrays for time, flux and flux error and the resolution of the test data.
-    
+    Returns arrays for time, flux and flux error and the resolution of the test data.
+
     Parameters
     ----------
     period : `float`
@@ -45,6 +48,7 @@ def simulate_test_data(period, amplitude, t_length, n_data, n_obs, n_outliers):
     dy : `~numpy.ndarray`
         flux error of observation
     """
+
     rand = np.random.RandomState(42)
     dt = t_length / n_data
     t = np.linspace(0, t_length, n_data)
@@ -63,33 +67,81 @@ def simulate_test_data(period, amplitude, t_length, n_data, n_obs, n_outliers):
     return dict(t=t_obs, dt=dt, y=mag_obs, dy=dmag_obs)
 
 
+def fap_astropy(power, freq, t, y, dy, method=dict(baluev=0)):
+    """
+    Calls the `~astropy`-method `false_alarm_probability`.
+    Function for estimation of periodogram peak significance.
+    Assumes Gaussian white noise light curve.
+
+    Returns an OrderedDict with the false alarm probability for each method.
+
+    Parameters
+    ----------
+    power : `~numpy.ndarray`
+        power of periodogram
+    freq : `~numpy.ndarray`
+        frequencies at withc the periodogram is computed
+    t, y, dy : `~numpy.ndarray`
+        time, flux and flux error or light curve
+    method : `dict`
+        dictionary of methods with their respective false alarm probability
+
+    Returns
+    -------
+    fap : `~collections.OrderedDict`
+        false alarm probability dictionary (see description above).
+    """
+
+    fap = OrderedDict()
+
+    if 'single' in [*method]:
+        fap['single'] = _statistics.false_alarm_probability(power.max(), freq.max(), t, y, dy, 'standard', 'single')
+    if 'naive' in [*method]:
+        fap['naive'] = _statistics.false_alarm_probability(power.max(), freq.max(), t, y, dy, 'standard', 'naive')
+    if 'davies' in [*method]:
+        fap['davies'] = _statistics.false_alarm_probability(power.max(), freq.max(), t, y, dy, 'standard', 'davies')
+    if 'baluev' in [*method]:
+        fap['baluev'] = _statistics.false_alarm_probability(power.max(), freq.max(), t, y, dy, 'standard', 'baluev')
+    if 'bootstrap' in [*method]:
+        fap['bootstrap'] = _statistics.false_alarm_probability(power.max(), freq.max(), t, y, dy, 'standard',
+                                                               'bootstrap', dict(n_bootstraps=100, random_seed=42))
+
+    return fap
+
+
 @requires_dependency('scipy')
 @pytest.mark.parametrize('pars', [
     dict(
-        period=7, amplitude=2, t_length=100, n_data=1000,
-        n_obs=500, n_outliers=0, dt=0.5,
-        max_period=None, criteria='all', n_bootstraps=10,
+        period=7, amplitude=2, t_length=100,
+        n_data=1000, n_obs=500, n_outliers=50,
+        periods=np.linspace(0.5, 100, 200), loss='cauchy', scale=1,
         fap=dict(
-            pre=2.220446049250313e-14,
-            cvm=1.4011014570769476e-11,
-            nll=5.6590954145008254e-09,
-            boot=0,
+            single=2.855680054527823e-93,
+            naive=5.705643031869405e-91,
+            davies=6.752853065345455e-90,
+            baluev=6.752853065345455e-90,
+            bootstrap=0.43,
         ),
     ),
 ])
-def test_lomb_scargle(pars):
+def test_period(pars):
     test_data = simulate_test_data(
         pars['period'], pars['amplitude'], pars['t_length'],
         pars['n_data'], pars['n_obs'], pars['n_outliers'],
     )
 
-    result = lomb_scargle(
-        test_data['t'], test_data['y'], test_data['dy'], pars['dt'],
-        pars['max_period'], pars['criteria'], pars['n_bootstraps'],
+    periodogram = robust_periodogram(
+        test_data['t'], test_data['y'], test_data['dy'],
+        periods=pars['periods'], loss=pars['loss'], scale=pars['scale'],
     )
 
-    assert_allclose(result['period'], pars['period'], atol=pars['dt'])
-    assert_allclose(result['fap']['pre'], pars['fap']['pre'], rtol=1e-3)
-    assert_allclose(result['fap']['cvm'], pars['fap']['cvm'], rtol=1e-3)
-    assert_allclose(result['fap']['nll'], pars['fap']['nll'], rtol=1e-3)
-    assert_allclose(result['fap']['boot'], pars['fap']['boot'], rtol=1e-3)
+    fap = fap_astropy(periodogram['power'], 1. / periodogram['periods'],
+                      test_data['t'], test_data['y'], test_data['dy'], pars['fap']
+                      )
+
+    assert_allclose(periodogram['best_period'], pars['period'], atol=pars['periods'].min())
+    assert_allclose(fap['single'], pars['fap']['single'], rtol=1e-3)
+    assert_allclose(fap['naive'], pars['fap']['naive'], rtol=1e-3)
+    assert_allclose(fap['davies'], pars['fap']['davies'], rtol=1e-3)
+    assert_allclose(fap['baluev'], pars['fap']['baluev'], rtol=1e-3)
+    assert_allclose(fap['bootstrap'], pars['fap']['bootstrap'], rtol=1e-3)
