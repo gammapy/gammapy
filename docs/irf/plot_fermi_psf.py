27c12b0dc4d07d0bde78807313f194f27f1e2e90
Christoph Deil <Deil.Christoph@gmail.com>
2018-07-25 22:21:04 +0200

    Improve PSF IRF docs

--- docs/irf/plot_fermi_psf.py
+++ docs/irf/plot_fermi_psf.py
@@ -1,22 +1,17 @@
 """Plot Fermi PSF."""
 import matplotlib.pyplot as plt
-from astropy import units as u
 from gammapy.datasets import FermiGalacticCenter
 from gammapy.irf import EnergyDependentTablePSF
-from gammapy.image import SkyImage
+from gammapy.maps import WcsGeom
+from gammapy.cube import PSFKernel
 
 filename = FermiGalacticCenter.filenames()['psf']
 fermi_psf = EnergyDependentTablePSF.read(filename)
 
-fig = plt.figure(figsize=(6, 5))
+psf = fermi_psf.table_psf_at_energy(energy='1 GeV')
+geom = WcsGeom.create(npix=100, binsz=0.01)
+kernel = PSFKernel.from_table_psf(psf, geom)
 
-# Compute a PSF kernel image
-# TODO: change this example after introducing PSF kernel class
-# (using SkyImage this way for kernels is weird)
-psf_image = SkyImage.empty()
-energy = 1 * u.GeV
-psf = fermi_psf.table_psf_at_energy(energy=energy)
-psf_image.data = psf.kernel(psf_image, rad_max=1 * u.deg).value
-psf_image.plot(fig=fig, add_cbar=True)
-
+plt.imshow(kernel.data)
+plt.colorbar()
 plt.show()
