da6a3439454f480e0c620aa00b4cd78c25c9099a
Christoph Deil <Deil.Christoph@gmail.com>
2018-07-25 21:23:26 +0200

    Fix docs/background/make_reflected_regions.py

--- docs/background/make_reflected_regions.py
+++ docs/background/make_reflected_regions.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from astropy.coordinates import SkyCoord, Angle
 from regions import CircleSkyRegion
-from gammapy.image import SkyImage, lon_lat_rectangle_mask
+from gammapy.image import SkyImage
 from gammapy.background import ReflectedRegionsFinder
 
 exclusion_mask = SkyImage.empty(
@@ -11,13 +11,9 @@ exclusion_mask = SkyImage.empty(
     coordsys='CEL', xref=83.633, yref=23.014, fill=1
 )
 
-# Exclude Rectangular region
-mask = lon_lat_rectangle_mask(
-    exclusion_mask.coordinates().ra,
-    exclusion_mask.coordinates().dec,
-    lat_max=Angle('24 deg'),
-    lat_min=Angle('23 deg')
-)
+# Exclude a rectangular region
+coords = exclusion_mask.coordinates()
+mask = (Angle('23d') < coords.dec) & (coords.dec < Angle('24d'))
 exclusion_mask.data *= np.invert(mask)
 
 pos = SkyCoord(83.633, 22.014, unit='deg')
