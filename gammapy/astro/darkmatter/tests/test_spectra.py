ee7877efd8933e1fa1855210459c030fc46fc7a0
Johannes King <johannes.king@mpi-hd.mpg.de>
2018-07-30 15:13:16 +0200

    add Test for dark matter utils

--- gammapy/astro/darkmatter/tests/test_spectra.py
+++ gammapy/astro/darkmatter/tests/test_spectra.py
@@ -4,10 +4,14 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from .. import PrimaryFlux
 from ....utils.testing import assert_quantity_allclose
 
+import pytest
 import astropy.units as u
 
 
 def test_primary_flux():
+    with pytest.raises(ValueError):
+        primflux = PrimaryFlux(channel="Spam", mDM=1 * u.TeV)
+
     primflux = PrimaryFlux(channel="W", mDM=1 * u.TeV)
     actual = primflux.table_model(500 * u.GeV)
     desired = 9.324355468682548e-05 / u.GeV
---
+++ gammapy/astro/darkmatter/tests/test_utils.py
@@ -0,0 +1,38 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pytest
+import astropy.units as u
+from ....utils.testing import assert_quantity_allclose
+from ....maps import WcsGeom
+from .. import JFactory, DMFluxMapMaker, profiles, PrimaryFlux
+
+
+@pytest.fixture(scope="session")
+def geom():
+    geom = WcsGeom.create(binsz=0.5, npix=10)
+    return geom
+
+
+@pytest.fixture(scope="session")
+def jfact():
+    jfactory = JFactory(ref_geom=geom(), profile=profiles.NFWProfile(), distance=8 * u.kpc)
+    return jfactory.compute_jfactor()
+
+
+@pytest.fixture(scope="session")
+def prim_flux():
+    return PrimaryFlux(mDM=1 * u.TeV, channel="W")
+
+
+def test_dmfluxmapmaker(jfact, prim_flux):
+    x_section = 1e-26 * u.Unit("cm3 s-1")
+    energy_range = [0.1, 1] * u.TeV
+    dmfmm = DMFluxMapMaker(
+        jfact=jfact, prim_flux=prim_flux, x_section=x_section, energy_range=energy_range
+    )
+    flux = dmfmm.run()
+
+    actual = flux[5, 5]
+    desired = 6.503265562298928e-13 / u.cm **2 / u.s
+    assert_quantity_allclose(actual, desired)
