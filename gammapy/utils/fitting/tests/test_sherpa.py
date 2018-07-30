66466b469b74b4c885030e5e497329ef8be8db02
adonath <axel.donath@mpi-hd.mpg.de>
2018-07-30 16:27:18 +0200

    Add test_sherpa.py

---
+++ gammapy/utils/fitting/tests/test_sherpa.py
@@ -0,0 +1,7 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import absolute_import, division, print_function, unicode_literals
+from numpy.testing import assert_allclose
+
+
+def test_sherpa():
+    pass
