0d41b6f87aa93101c91dd8fb490ff1cafc415718
Christoph Deil <Deil.Christoph@gmail.com>
2018-07-31 10:37:20 +0200

    Fix examples/example_fermi_catalogs.py

--- examples/example_fermi_catalogs.py
+++ examples/example_fermi_catalogs.py
@@ -4,28 +4,22 @@ import matplotlib.pyplot as plt
 from gammapy.catalog import SourceCatalog3FGL, SourceCatalog2FHL, SourceCatalog1FHL, SourceCatalog3FHL
 
 
-def plot_source_spectrum(source, label, color, sed_type):
+def plot_source_spectrum(source, label, color):
+    opts = dict(energy_power=2, flux_unit='erg-1 cm-2 s-1')
     source.spectral_model.plot(
-        energy_range=source.energy_range, energy_power=2, flux_unit='erg-1 cm-2 s-1',
-        label=label, color=color,
+        energy_range=source.energy_range, label=label, color=color, **opts
     )
     source.spectral_model.plot_error(
-        energy_range=source.energy_range, energy_power=2, flux_unit='erg-1 cm-2 s-1',
-        facecolor=color,
+        energy_range=source.energy_range, facecolor=color, **opts
     )
-    energy_power = 0 if sed_type == 'eflux' else 2
-    flux_unit = 'erg cm-2 s-1' if sed_type == 'eflux' else 'cm-2 s-1 erg-1'
-    source.flux_points.plot(
-        sed_type=sed_type, energy_power=energy_power, flux_unit=flux_unit,
-        color=color,
-    )
+    source.flux_points.plot(sed_type='dnde', color=color, **opts)
 
 
 def plot_source_spectra(name):
-    plot_source_spectrum(source=SourceCatalog3FGL()[name], label='Fermi 3FGL', color='r', sed_type='eflux')
-    plot_source_spectrum(source=SourceCatalog2FHL()[name], label='Fermi 2FHL', color='g', sed_type='dnde')
-    plot_source_spectrum(source=SourceCatalog1FHL()[name], label='Fermi 1FHL', color='c', sed_type='dnde')
-    plot_source_spectrum(source=SourceCatalog3FHL()[name], label='Fermi 3FHL', color='b', sed_type='eflux')
+    plot_source_spectrum(source=SourceCatalog3FGL()[name], label='Fermi 3FGL', color='r')
+    plot_source_spectrum(source=SourceCatalog2FHL()[name], label='Fermi 2FHL', color='g')
+    plot_source_spectrum(source=SourceCatalog1FHL()[name], label='Fermi 1FHL', color='c')
+    plot_source_spectrum(source=SourceCatalog3FHL()[name], label='Fermi 3FHL', color='b')
 
     ax = plt.gca()
     ax.set_ylim(1.e-12, 7.e-11)
