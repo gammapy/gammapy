859452775f5d72a9d41b9cee216431f004ab3a8c
Christoph Deil <Deil.Christoph@gmail.com>
2018-08-02 15:13:31 +0200

    Improve map making test cases

--- gammapy/cube/tests/test_background.py
+++ gammapy/cube/tests/test_background.py
@@ -2,14 +2,14 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import pytest
 from numpy.testing import assert_allclose
-from astropy.units import Quantity
-from astropy.coordinates import SkyCoord, Angle
+from astropy.coordinates import SkyCoord
 from ...utils.testing import requires_data
-from ...maps import Map
+from ...maps import WcsGeom, HpxGeom, MapAxis
 from ...irf import Background3D
 from ..background import make_map_background_irf
 
 pytest.importorskip('scipy')
+pytest.importorskip('healpy')
 
 
 @pytest.fixture(scope='session')
@@ -18,21 +18,44 @@ def bkg_3d():
     return Background3D.read(filename, hdu='BACKGROUND')
 
 
-@pytest.fixture(scope='session')
-def counts_cube():
-    filename = '$GAMMAPY_EXTRA/datasets/hess-crab4-hd-hap-prod2/hess_events_simulated_023523_cntcube.fits'
-    return Map.read(filename)
+def geom(map_type, ebounds):
+    axis = MapAxis.from_edges(ebounds, name="energy", unit='TeV', interp='log')
+    if map_type == 'wcs':
+        return WcsGeom.create(npix=(4, 3), binsz=2, axes=[axis])
+    elif map_type == 'hpx':
+        return HpxGeom(256, axes=[axis])
+    else:
+        raise ValueError()
 
 
 @requires_data('gammapy-extra')
-def test_make_map_fov_background(bkg_3d, counts_cube):
-    pointing = SkyCoord(83.633, 21.514, unit='deg')
-    livetime = Quantity(1581.17, 's')
-
+@pytest.mark.parametrize("pars", [
+    {
+        'geom': geom(map_type='wcs', ebounds=[0.1, 1, 10]),
+        'shape': (2, 3, 4),
+        'sum': 4760.562826,
+    },
+    {
+        'geom': geom(map_type='wcs', ebounds=[0.1, 10]),
+        'shape': (1, 3, 4),
+        'sum': 49620.735907,
+    },
+    # TODO: make this work for HPX
+    # 'HpxGeom' object has no attribute 'separation'
+    # {
+    #     'geom': geom(map_type='hpx', ebounds=[0.1, 1, 10]),
+    #     'shape': '???',
+    #     'sum': '???',
+    # },
+])
+def test_make_map_background_irf(bkg_3d, pars):
     m = make_map_background_irf(
-        pointing, livetime, bkg_3d, counts_cube.geom
+        pointing=SkyCoord(2, 1, unit='deg'),
+        livetime='42 s',
+        bkg=bkg_3d,
+        geom=pars['geom'],
     )
 
-    assert m.data.shape == (15, 120, 200)
-    assert_allclose(m.data[0, 0, 0], 0.013959, rtol=1e-4)
-    assert_allclose(m.data.sum(), 1408.573698, rtol=1e-5)
+    assert m.data.shape == pars['shape']
+    assert m.unit == ''
+    assert_allclose(m.data.sum(), pars['sum'], rtol=1e-5)
