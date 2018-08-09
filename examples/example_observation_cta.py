e4ae1d5a3f4f83f46e17190fc5391b18e256f7f8
David Fidalgo <davidc.fidalgo@gmail.com>
2018-08-09 21:43:28 +0200

    removed dict and moved the args to init call

--- examples/example_observation_cta.py
+++ examples/example_observation_cta.py
@@ -8,17 +8,15 @@ cal_file = make_path('$GAMMAPY_EXTRA/datasets/cta-1dc/caldb/data/cta/1dc/bcf/Sou
 
 event_list = EventList.read(data_file)
 
-kwargs = dict(obs_id=event_list.table.meta['OBS_ID'],
-              events=event_list,
-              gti=GTI.read(data_file),
-              psf=EnergyDependentMultiGaussPSF.read(cal_file, hdu='Point Spread Function'),
-              aeff=EffectiveAreaTable2D.read(cal_file),
-              edisp=EnergyDispersion2D.read(cal_file, hdu='Energy Dispersion'),
-              bkg=Background3D.read(cal_file),
-              pointing_radec=event_list.pointing_radec,
-              observation_live_time_duration=event_list.observation_live_time_duration,
-              observation_dead_time_fraction=event_list.observation_dead_time_fraction,
-              )
-
-obs = ObservationCTA(**kwargs)
+obs = ObservationCTA(obs_id=event_list.table.meta['OBS_ID'],
+                     events=event_list,
+                     gti=GTI.read(data_file),
+                     psf=EnergyDependentMultiGaussPSF.read(cal_file, hdu='Point Spread Function'),
+                     aeff=EffectiveAreaTable2D.read(cal_file),
+                     edisp=EnergyDispersion2D.read(cal_file, hdu='Energy Dispersion'),
+                     bkg=Background3D.read(cal_file),
+                     pointing_radec=event_list.pointing_radec,
+                     observation_live_time_duration=event_list.observation_live_time_duration,
+                     observation_dead_time_fraction=event_list.observation_dead_time_fraction
+                     )
 print(obs)
