b9e2bec33f7394267b96535ef42829980331f16b
Terrier <rterrier@apc.in2p3.fr>
2018-08-10 12:32:38 +0200

    added test for non square case

--- gammapy/irf/tests/test_energy_dispersion.py
+++ gammapy/irf/tests/test_energy_dispersion.py
@@ -27,15 +27,25 @@ class TestEnergyDispersion:
         )
 
     def test_from_diagonal_matrix(self):
-        e_true = [1, 3, 7, 10] * u.TeV
-        edisp = EnergyDispersion.from_diagonal_matrix(e_true)
+        e_true = [0.5, 1, 2, 4, 6] * u.TeV
+        e_reco = [2, 4, 6] * u.TeV
 
-        assert edisp.pdf_matrix.shape == (3, 3)
+        edisp = EnergyDispersion.from_diagonal_matrix(e_true, e_reco)
+
+        assert edisp.pdf_matrix.shape == (4, 2)
+        expected = np.array(   [[0, 0],
+                                [0, 0],
+                                [1, 0],
+                                [0, 1]]
+                             )
+        assert_equal(edisp.pdf_matrix, expected)
+
+        # Test square matrix
+        edisp = EnergyDispersion.from_diagonal_matrix(e_true)
+        assert_allclose(edisp.e_reco.bins.value, e_true.value)
+        assert edisp.e_reco.bins.unit == 'TeV'
         assert_equal(edisp.pdf_matrix[0][0], 1)
         assert_equal(edisp.pdf_matrix[2][0], 0)
-
-        assert edisp.e_reco.bins.unit == 'TeV'
-        assert_allclose(edisp.e_reco.bins.value, e_true.value)
 
     def test_str(self):
         assert 'EnergyDispersion' in str(self.edisp)
