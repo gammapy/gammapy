99220ec5d9746af02b5632d3bf8879f3b25db290
Christoph Deil <Deil.Christoph@gmail.com>
2018-09-04 21:41:12 +0200

    Add gammapy.image.find_peaks

--- gammapy/image/measure.py
+++ gammapy/image/measure.py
@@ -2,13 +2,17 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import numpy as np
 from astropy.units import Quantity
+from astropy.coordinates import SkyCoord
+from astropy.table import Table, Column
+from ..maps import WcsNDMap
 
 __all__ = [
     'measure_containment_fraction',
     'measure_containment_radius',
     'measure_image_moments',
     'measure_containment',
-    'measure_curve_of_growth'
+    'measure_curve_of_growth',
+    'find_peaks',
 ]
 
 
@@ -30,7 +34,10 @@ def measure_image_moments(image):
         [A, x_cms, y_cms, x_sigma, y_sigma, sqrt(x_sigma * y_sigma)]
     """
     data = image.quantity
-    x, y = _wrapped_coordinates(image)
+
+    coords = image.geom.get_coord().skycoord
+    x, y = coords.data.lon.wrap_at('180d'), coords.data.lat
+
     A = data[np.isfinite(data)].sum()
 
     # Center of mass
@@ -156,6 +163,87 @@ def measure_curve_of_growth(image, position, radius_max=None, radius_n=10):
     return radii, Quantity(containment)
 
 
-def _wrapped_coordinates(image):
-    coords = image.geom.get_coord().skycoord
-    return coords.data.lon.wrap_at('180d'), coords.data.lat
+def find_peaks(image, threshold, min_distance=1):
+    """Find local peaks in an image.
+
+    This is a very simple peak finder, that finds local peaks
+    (i.e. maxima) in images above a given ``threshold`` within
+    a given ``min_distance`` around each given pixel.
+
+    If you get multiple spurious detections near a peak, usually
+    it's best to smooth the image a bit, or to compute it using
+    a different method in the first place to result in a smooth image.
+    You can also increase the ``min_distance`` parameter.
+
+    The output table contains one row per peak and the following columns:
+
+    - ``x`` and ``y`` are the pixel coordinates (first pixel at zero)
+    - ``ra`` and ``dec`` are the RA / DEC sky coordinates (ICRS frame)
+    - ``value`` is the pixel value
+
+    It is sorted by peak value, starting with the highest value.
+
+    If there are no pixel values above the threshold, an empty table is returned.
+
+    There are more featureful peak finding and source detection methods
+    e.g. in the ``photutils`` or ``scikit-image`` Python packages.
+
+    Parameters
+    ----------
+    image : `~gammapy.maps.WcsNDMap`
+        2D map
+    threshold : float or array-like
+        The data value or pixel-wise data values to be used for the
+        detection threshold.  A 2D ``threshold`` must have the same
+        shape as tha map ``data``.
+    min_distance : int
+        Minimum pixel distance between peaks.
+        Smallest possible value and default is 1 pixel.
+
+    Returns
+    -------
+    output : `~astropy.table.Table`
+        Table with parameters of detected peaks
+    """
+    from scipy.ndimage import maximum_filter
+
+    # Input validation
+
+    if not isinstance(image, WcsNDMap):
+        raise TypeError('find_peaks only supports WcsNDMap')
+
+    if not image.geom.is_image:
+        raise ValueError('find_peaks only supports 2D images')
+
+    size = 2 * min_distance + 1
+
+    data = image.data
+
+    # Handle edge case of constant data; treat as no peak
+    if np.all(data == data.flat[0]):
+        return Table()
+
+    # Run peak finder
+    data_max = maximum_filter(data, size=size, mode='constant')
+    mask = (data == data_max) & (data > threshold)
+    y, x = mask.nonzero()
+    value = data[y, x]
+
+    # Make and return results table
+
+    if len(value) == 0:
+        return Table()
+
+    coord = SkyCoord.from_pixel(x, y, wcs=image.geom.wcs).icrs
+
+    table = Table()
+    table['value'] = value * image.unit
+    table['x'] = x
+    table['y'] = y
+    table['ra'] = coord.ra
+    table['dec'] = coord.dec
+
+    table.sort('value')
+    table.reverse()
+
+    return table
--- gammapy/image/tests/test_measure.py
+++ gammapy/image/tests/test_measure.py
@@ -1,17 +1,19 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function, unicode_literals
 import numpy as np
+from numpy.testing import assert_allclose
 import pytest
 from astropy import units as u
 from astropy.modeling.models import Gaussian2D
 from astropy.coordinates import SkyCoord
 from ...utils.testing import assert_quantity_allclose, requires_dependency
-from ...maps import WcsGeom, WcsNDMap
+from ...maps import WcsGeom, WcsNDMap, Map
 from ...image import (
     measure_containment_radius,
     measure_image_moments,
     measure_containment,
     measure_curve_of_growth,
+    find_peaks,
 )
 
 
@@ -73,3 +75,45 @@ def test_measure_curve_of_growth(gaussian_image):
     sigma = 0.2 * u.deg
     containment_ana = u.Quantity(1 - np.exp(-0.5 * (radius / sigma) ** 2).value, 'cm-2 s-1')
     assert_quantity_allclose(containment, containment_ana, rtol=0.1)
+
+
+@requires_dependency('scipy')
+class TestFindPeaks:
+
+    def test_simple(self):
+        """Test a simple example"""
+        image = Map.create(npix=(10, 5), unit='s')
+        image.data[3, 3] = 11
+        image.data[3, 4] = 10
+        image.data[3, 5] = 12
+        image.data[0, 9] = 99
+
+        table = find_peaks(image, threshold=3)
+
+        assert len(table) == 3
+        assert table['value'].unit == 's'
+        assert table['ra'].unit == 'deg'
+        assert table['dec'].unit == 'deg'
+
+        row = table[0]
+        assert tuple((row['x'], row['y'])) == (9, 0)
+        assert_allclose(row['value'], 99)
+        assert_allclose(row['ra'], 359.55)
+        assert_allclose(row['dec'], -0.2)
+
+        row = table[1]
+        assert tuple((row['x'], row['y'])) == (5, 3)
+        assert_allclose(row['value'], 12)
+
+    def test_no_peak(self):
+        image = Map.create(npix=(10, 5))
+        image.data[3, 5] = 12
+
+        table = find_peaks(image, threshold=12.1)
+        assert len(table) == 0
+
+    def test_constant(self):
+        image = Map.create(npix=(10, 5))
+
+        table = find_peaks(image, threshold=3)
+        assert len(table) == 0
