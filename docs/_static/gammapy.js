55066c1aa2210059223aa86ced2a14c0096a13a6
Jose Enrique Ruiz <jer@iaa.es>
2018-10-11 08:38:50 +0200

    Update gammapy.js to make https requests

--- docs/_static/gammapy.js
+++ docs/_static/gammapy.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
     // add info note for past releases
     var verFile = new XMLHttpRequest();
-    verFile.open("GET", "http://docs.gammapy.org/stable/index.html", true);
+    verFile.open("GET", "https://docs.gammapy.org/stable/index.html", true);
     verFile.onreadystatechange = function() {
       if (verFile.readyState === 4) {  // makes sure the document is ready to parse.
         if (verFile.status === 200) {  // makes sure it's found the file.
@@ -19,11 +19,11 @@ $(document).ready(function() {
           var version = match[1];
           var note = '<div class="admonition note"><p class="first admonition-title">Note</p>'
           note += '<p class="last">You are not reading the most up to date version of Gammapy '
-          note += 'documentation.<br/>Access the <a href="http://docs.gammapy.org/'
+          note += 'documentation.<br/>Access the <a href="https://docs.gammapy.org/'
           note += version
           note += '/">latest stable version v'
           note += version
-          note += '</a> or the <a href="http://gammapy.org/news.html#releases">list of Gammapy releases</a>.</p></div>'
+          note += '</a> or the <a href="https://gammapy.org/news.html#releases">list of Gammapy releases</a>.</p></div>'
 
           var url = window.location.href
           if (url.includes("/dev/") == false && url.includes(version) == false ) {
