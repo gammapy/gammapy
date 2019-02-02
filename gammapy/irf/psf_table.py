91d2d5ebb9b475ffefa2cb9a72e31b8714e30020
Axel Donath <axel.donath@mpi-hd.mpg.de>
2019-02-03 00:04:17 +0100

    Clean up TablePSF

--- gammapy/irf/psf_table.py
+++ gammapy/irf/psf_table.py
@@ -1,8 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import logging
 import numpy as np
-from scipy.interpolate import UnivariateSpline
 from astropy.io import fits
+<<<<<<< HEAD
+=======
+from astropy.units import Quantity
+>>>>>>> Clean up TablePSF
 from astropy import units as u
 from astropy.coordinates import Angle, SkyCoord
 from ..utils.interpolation import ScaledRegularGridInterpolator
@@ -15,19 +18,9 @@ __all__ = ["TablePSF", "EnergyDependentTablePSF"]
 
 log = logging.getLogger(__name__)
 
-# Default PSF spline keyword arguments
-# TODO: test and document
-DEFAULT_PSF_SPLINE_KWARGS = dict(k=1, s=0)
-
 
 class TablePSF:
     r"""Radially-symmetric table PSF.
-
-    This PSF represents a :math:`PSF(r)=dP / d\Omega(r)`
-    spline interpolation curve for a given set of offset :math:`r`
-    and :math:`PSF` points.
-
-    Uses `scipy.interpolate.UnivariateSpline`.
 
     Parameters
     ----------
@@ -35,40 +28,34 @@ class TablePSF:
         Offset wrt source position
     dp_domega : `~astropy.units.Quantity` with sr^-1 units
         PSF value array
-    spline_kwargs : dict
-        Keyword arguments passed to `~scipy.interpolate.UnivariateSpline`
-
-    Notes
-    -----
-    * This PSF class works well for model PSFs of arbitrary shape (represented by a table),
-      but might give unstable results if the PSF has noise.
-      E.g. if ``dp_domega`` was estimated from histograms of real or simulated event data
-      with finite statistics, it will have noise and it is your responsibility
-      to check that the interpolating spline is reasonable.
-    * To customize the spline, pass keyword arguments to `~scipy.interpolate.UnivariateSpline`
-      in ``spline_kwargs``. E.g. passing ``dict(k=1)`` changes from the default cubic to
-      linear interpolation.
-    * TODO: evaluate spline for ``(log(rad), log(PSF))`` for numerical stability?
-    * TODO: merge morphology.theta class functionality with this class.
-    * TODO: add FITS I/O methods
-    * TODO: add ``normalize`` argument to ``__init__`` with default ``True``?
-    * TODO: ``__call__`` doesn't show up in the html API docs, but it should:
-      https://github.com/astropy/astropy/pull/2135
+    interp_kwargs : dict
+        Keyword arguments passed to `ScaledRegularGridInterpolator`
     """
 
-    def __init__(self, rad, dp_domega, spline_kwargs=DEFAULT_PSF_SPLINE_KWARGS):
+    def __init__(self, rad, dp_domega, interp_kwargs=None):
+        self.rad = Angle(rad).to("rad")
+        self.psf_value = u.Quantity(dp_domega).to("sr^-1")
 
-        self._rad = Angle(rad).to("radian")
-        self._dp_domega = u.Quantity(dp_domega).to("sr^-1")
+        self._interp_kwargs = interp_kwargs or {}
+        self._setup_interpolators()
 
-        assert self._rad.ndim == self._dp_domega.ndim == 1
-        assert self._rad.shape == self._dp_domega.shape
+    @property
+    def _rad_axis(self):
+        from ..maps import MapAxis
+        return MapAxis.from_nodes(self.rad.to_value("deg"), unit="deg")
+ 
+    def _setup_interpolators(self):
+        self._interpolate = ScaledRegularGridInterpolator(
+            points=(self.rad.to_value("rad"),), values=self.psf_value, 
+            **self._interp_kwargs)
 
-        # Store input arrays as quantities in default internal units
-        self._dp_dr = (2 * np.pi * self._rad * self._dp_domega).to("radian^-1")
-        self._spline_kwargs = spline_kwargs
+        drad = np.diff(self._rad_axis.edges) * u.deg
+        integral = (2 * np.pi * self.rad * self.psf_value * drad).cumsum()
 
-        self._compute_splines(spline_kwargs)
+        self._interpolate_integral = ScaledRegularGridInterpolator(
+            points=(self.rad.to_value("rad"),), values=integral.to_value(""), fill_value=1,
+            )
+
 
     @classmethod
     def from_shape(cls, shape, width, rad):
@@ -116,39 +103,19 @@ class TablePSF:
 
     def info(self):
         """Print basic info."""
-        ss = array_stats_str(self._rad.degree, "offset")
+        ss = array_stats_str(self.rad.deg, "offset")
         ss += "integral = {}\n".format(self.integral())
 
-        for containment in [50, 68, 80, 95]:
+        for containment in [68, 80, 95]:
             radius = self.containment_radius(0.01 * containment)
             ss += "containment radius {} deg for {}%\n".format(
-                radius.degree, containment
+                radius.deg, containment
             )
 
         return ss
 
-    # TODO: remove because it's not flexible enough?
-    def __call__(self, lon, lat):
-        """Evaluate PSF at a 2D position.
 
-        The PSF is centered on ``(0, 0)``.
-
-        Parameters
-        ----------
-        lon, lat : `~astropy.coordinates.Angle`
-            Longitude / latitude position
-
-        Returns
-        -------
-        psf_value : `~astropy.units.Quantity`
-            PSF value
-        """
-        center = SkyCoord(0, 0, unit="radian")
-        point = SkyCoord(lon, lat)
-        rad = center.separation(point)
-        return self.evaluate(rad)
-
-    def evaluate(self, rad, quantity="dp_domega"):
+    def evaluate(self, rad):
         r"""Evaluate PSF.
 
         The following PSF quantities are available:
@@ -157,47 +124,27 @@ class TablePSF:
 
             .. math:: \frac{dP}{d\Omega}
 
-        * 'dp_dr': PDF per 1-dim offset :math:`r` in radian^-1
-
-            .. math:: \frac{dP}{dr} = 2 \pi r \frac{dP}{d\Omega}
 
         Parameters
         ----------
         rad : `~astropy.coordinates.Angle`
             Offset wrt source position
-        quantity : {'dp_domega', 'dp_dr'}
-            Which PSF quantity?
 
         Returns
         -------
         psf_value : `~astropy.units.Quantity`
             PSF value
         """
-        rad = Angle(rad)
+        rad = np.atleast_1d(u.Quantity(rad, "rad").value)
+        return self._interpolate((rad,))
+        
 
-        shape = rad.shape
-        x = np.array(rad.radian).flat
-
-        if quantity == "dp_domega":
-            y = self._dp_domega_spline(x)
-            unit = "sr^-1"
-        elif quantity == "dp_dr":
-            y = self._dp_dr_spline(x)
-            unit = "radian^-1"
-        else:
-            ss = "Invalid quantity: {}\n".format(quantity)
-            ss += "Choose one of: 'dp_domega', 'dp_dr'"
-            raise ValueError(ss)
-
-        y = np.clip(a=y, a_min=0, a_max=None)
-        return u.Quantity(y, unit).reshape(shape)
-
-    def integral(self, rad_min=None, rad_max=None):
-        """Compute PSF integral, aka containment fraction.
+    def containment(self, rad_max):
+        """Compute PSF containment fraction.
 
         Parameters
         ----------
-        rad_min, rad_max : `~astropy.units.Quantity` with angle units
+        rad_max : `~astropy.units.Quantity`
             Offset angle range
 
         Returns
@@ -205,23 +152,8 @@ class TablePSF:
         integral : float
             PSF integral
         """
-        if rad_min is None:
-            rad_min = self._rad[0]
-        else:
-            rad_min = Angle(rad_min)
-
-        if rad_max is None:
-            rad_max = self._rad[-1]
-        else:
-            rad_max = Angle(rad_max)
-
-        rad_min = self._rad_clip(rad_min)
-        rad_max = self._rad_clip(rad_max)
-
-        cdf_min = self._cdf_spline(rad_min)
-        cdf_max = self._cdf_spline(rad_max)
-
-        return cdf_max - cdf_min
+        rad = np.atleast_1d(u.Quantity(rad_max, "rad").value)
+        return self._interpolate_integral((rad,))
 
     def containment_radius(self, fraction):
         """Containment radius.
@@ -236,8 +168,13 @@ class TablePSF:
         rad : `~astropy.coordinates.Angle`
             Containment radius angle
         """
-        rad = self._ppf_spline(fraction)
-        return Angle(rad, "radian").to("deg")
+        rad_max = np.linspace(0, self.rad[-1].to_value("deg"), 10 * len(self.rad)) * u.deg
+        containment = self.containment(rad_max=rad_max)
+
+        fraction = np.atleast_1d(fraction)
+
+        fraction_idx = np.argmin(np.abs(containment - fraction[:, np.newaxis]), axis=1)
+        return rad_max[fraction_idx].to("deg")
 
     def normalize(self):
         """Normalize PSF to unit integral.
@@ -245,16 +182,8 @@ class TablePSF:
         Computes the total PSF integral via the :math:`dP / dr` spline
         and then divides the :math:`dP / dr` array.
         """
-        integral = self.integral()
-
-        self._dp_dr /= integral
-
-        # Clip to small positive number to avoid divide by 0
-        rad = np.clip(self._rad.radian, 1e-6, None)
-
-        rad = u.Quantity(rad, "radian")
-        self._dp_domega = self._dp_dr / (2 * np.pi * rad)
-        self._compute_splines(self._spline_kwargs)
+        integral = self.containment(self.rad[-1])
+        self.psf_value /= integral 
 
     def broaden(self, factor, normalize=True):
         r"""Broaden PSF by scaling the offset array.
@@ -274,77 +203,30 @@ class TablePSF:
         normalize : bool
             Normalize PSF after broadening
         """
-        self._rad *= factor
-        # We define broadening such that self._dp_domega remains the same
-        # so we only have to re-compute self._dp_dr and the slines here.
-        self._dp_dr = (2 * np.pi * self._rad * self._dp_domega).to("radian^-1")
-        self._compute_splines(self._spline_kwargs)
-
+        self.rad *= factor
+        self._setup_interpolators()
         if normalize:
             self.normalize()
 
-    def plot_psf_vs_rad(self, ax=None, quantity="dp_domega", **kwargs):
+    def plot_psf_vs_rad(self, ax=None,  **kwargs):
         """Plot PSF vs radius.
 
-        TODO: describe PSF ``quantity`` argument in a central place and link to it from here.
+        Parameters
+        ----------
+        ax : ``
+
+        kwargs : dict
+            Keyword arguments passed to `matplotlib.pyplot.plot`
         """
         import matplotlib.pyplot as plt
 
         ax = plt.gca() if ax is None else ax
 
-        x = self._rad.to("deg")
-        y = self.evaluate(self._rad, quantity)
+        ax.plot(self.rad.to_value("deg"), self.psf_value.to_value("sr-1"), **kwargs)
+        ax.set_yscale("log")
+        ax.set_xlabel("Radius (deg)")
+        ax.set_ylabel("PSF (sr-1)")
 
-        ax.plot(x.value, y.value, **kwargs)
-        ax.loglog()
-        ax.set_xlabel("Radius ({})".format(x.unit))
-        ax.set_ylabel("PSF ({})".format(y.unit))
-
-    def _compute_splines(self, spline_kwargs=DEFAULT_PSF_SPLINE_KWARGS):
-        """Compute two splines representing the PSF.
-
-        * `_dp_domega_spline` is used to evaluate the 2D PSF.
-        * `_dp_dr_spline` is not really needed for most applications,
-          but is available via `eval`.
-        * `_cdf_spline` is used to compute integral and for normalisation.
-        * `_ppf_spline` is used to compute containment radii.
-        """
-        # Compute spline and normalize.
-        x, y = self._rad.value, self._dp_domega.value
-        self._dp_domega_spline = UnivariateSpline(x, y, **spline_kwargs)
-
-        x, y = self._rad.value, self._dp_dr.value
-        self._dp_dr_spline = UnivariateSpline(x, y, **spline_kwargs)
-
-        # We use the terminology for scipy.stats distributions
-        # http://docs.scipy.org/doc/scipy/reference/tutorial/stats.html#common-methods
-
-        # cdf = "cumulative distribution function"
-        self._cdf_spline = self._dp_dr_spline.antiderivative()
-
-        # ppf = "percent point function" (inverse of cdf)
-        # Here's a discussion on methods to compute the ppf
-        # http://mail.scipy.org/pipermail/scipy-user/2010-May/025237.html
-        y = self._rad.value
-        x = self.integral(Angle(0, "rad"), self._rad)
-
-        # Since scipy 1.0 the UnivariateSpline requires that x is strictly increasing
-        # So only keep nodes where this is the case (and always keep the first one):
-        x, idx = np.unique(x, return_index=True)
-        y = y[idx]
-
-        # Dummy values, for cases where one really doesn't have a valid PSF.
-        if len(x) < 4:
-            x = [0, 1, 2, 3]
-            y = [0, 0, 0, 0]
-
-        self._ppf_spline = UnivariateSpline(x, y, **spline_kwargs)
-
-    def _rad_clip(self, rad):
-        """Clip to radius support range, because spline extrapolation is unstable."""
-        rad = Angle(rad, "radian").radian
-        rad = np.clip(rad, 0, self._rad[-1].radian)
-        return rad
 
 
 class EnergyDependentTablePSF:
