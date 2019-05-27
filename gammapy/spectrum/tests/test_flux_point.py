1510a401f293a3f5880569896621437b23207a3f
Axel Donath <axel.donath@mpi-hd.mpg.de>
2019-05-27 16:23:42 +0200

    Add FluxPointsDataset.peek() test

--- gammapy/spectrum/tests/test_flux_point.py
+++ gammapy/spectrum/tests/test_flux_point.py
@@ -288,3 +288,12 @@ class TestFluxPointFit:
 
         ts_diff = profile["likelihood"] - result.total_stat
         assert_allclose(ts_diff, [110.1, 0, 110.1], rtol=1e-2, atol=1e-7)
+
+
+    @staticmethod
+    def test_fp_dataset_peek(fit):
+        fp_dataset = fit.datasets.datasets[0]
+
+        with mpl_plot_check():
+            fp_dataset.peek()
+
