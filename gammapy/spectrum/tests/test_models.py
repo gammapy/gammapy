cd545135aa362febb69915e04165095c6d019f07
Axel Donath <axel.donath@mpi-hd.mpg.de>
2019-05-27 11:45:35 +0200

    Fix integrat_spectrum for small energy bins

--- gammapy/spectrum/tests/test_models.py
+++ gammapy/spectrum/tests/test_models.py
@@ -314,7 +314,7 @@ def test_ecpl_intergrate():
     # regresseion test to check the numerical integration for small energy bins
     ecpl = ExponentialCutoffPowerLaw()
     value = ecpl.integral(1 * u.TeV, 1.1 * u.TeV)
-    assert_quantity_allclose(value, 0 * u.Unit("s-1 cm-2"))
+    assert_quantity_allclose(value, 8.380714e-14 * u.Unit("s-1 cm-2"))
 
 
 @requires_dependency("naima")
