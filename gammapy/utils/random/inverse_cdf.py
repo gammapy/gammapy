b43ae4e99019ac60ed4c6bc29913d19ab969e510
Fabio Pintore <fabio.pintore@inaf.it>
2019-06-12 14:28:09 +0200

    Implement InverseCDFSampler class and tests

---
+++ gammapy/utils/random/inverse_cdf.py
@@ -0,0 +1,81 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+import numpy as np
+from .utils import get_random_state
+
+
+#__all__ = [
+#    "normalize",
+#    "density",
+#    "draw",
+#    "pdf",
+#    "get_random_state",
+#    "sample_sphere",
+#    "sample_sphere_distance",
+#    "sample_powerlaw",
+#]
+
+
+class InverseCDFSampler:
+    """Inverse CDF sampler.
+       It determines a set of random numbers and calculate the cumulative distribution function.
+       
+        Parameters
+        ----------
+        pdf : `~`gammapy.maps.Map`
+        predicted source counts
+        
+        """
+    def __init__(self, pdf, axis=None, random_state=0):
+        self.random_state = get_random_state(random_state)
+        self.axis = axis
+        
+        if axis is not None:
+            self.cdf = np.cumsum(pdf, axis=self.axis)
+            self.cdf /= self.cdf[:, [-1]]
+        else:
+            self.pdf_shape = pdf.shape  #gives the shape of the PDF array
+            
+            pdf = pdf.ravel() / pdf.sum()  #flattens the array along one axis
+            self.sortindex = np.argsort(pdf, axis=None) #sorting of the elements and giving the indexes
+            
+            self.pdf = pdf[self.sortindex]  #sort the pdf array
+            self.cdf = np.cumsum(self.pdf)  #evaluate the cumulative sum of the PDF array
+
+    def sample_axis(self):
+        """Sample along a given axis.
+        """
+        choice = self.random_state.uniform(high=1, size=len(self.cdf))
+
+        #find the indices corresponding to this point on the CDF
+        index = np.argmin(np.abs(choice.reshape(-1, 1) - self.cdf), axis=self.axis)
+
+        return index + self.random_state.uniform(low=-0.5, high=0.5,
+                 size=len(self.cdf))
+
+    def sample(self, size):
+        """Draw sample from the given PDF.
+
+        Parameters
+        ----------
+        size : int
+        Number of samples to draw.
+
+        Returns
+        -------
+        index : tuple of `~numpy.ndarray`
+        Coordinates of the drawn sample
+        """
+        #pick numbers which are uniformly random over the cumulative distribution function
+        choice = self.random_state.uniform(high=1, size=size)
+
+        #find the indices corresponding to this point on the CDF
+        index = np.searchsorted(self.cdf, choice)
+        index = self.sortindex[index]
+
+        # map back to multi-dimensional indexing
+        index = np.unravel_index(index, self.pdf_shape) 
+        index = np.vstack(index)
+
+        index = index + self.random_state.uniform(low=-0.5, high=0.5,
+                                              size=index.shape)
+        return index
---
+++ gammapy/utils/random/tests/test_inverse_cdf.py
@@ -0,0 +1,36 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from ..inverse_cdf import InverseCDFSampler
+import numpy as np
+from numpy.testing import assert_allclose
+import scipy.optimize
+import scipy.stats as stats
+
+
+def uniform_dist(n_elem):
+    return np.ones(n_elem)
+
+def gauss_dist(x, mu=0, sigma=0.1):
+    return stats.norm.pdf(x,mu,sigma)
+
+def gaus(x, k, sig, mu):
+    return k * np.exp((-(x-mu)**2)/(2*sig**2))   # 1 gauss
+
+def test_uniform_dist_sampling():
+    for i in np.arange(1,100):
+        idx = InverseCDFSampler(uniform_dist(n_elem=100000), random_state=0)
+        sampled_elem = idx.sample(1000)
+        hist = np.histogram(sampled_elem[0],bins=100)
+        assert_allclose((np.mean(hist[0]) * 100./1000.), 1.0, atol=1e-4)
+
+def test_norm_dist_sampling():
+    for i in np.arange(1,100):
+        x = np.arange(-i,i,i/1000.)
+        sigma = i/100.
+        idx = InverseCDFSampler(gauss_dist(x=x, mu=0, sigma=sigma), random_state=0)
+        sampled_elem = idx.sample(1000)
+        a1, b1 = np.histogram((sampled_elem[0]),bins=100)
+        b2 = (b1 + np.roll(b1,1))/2.0
+        b2 = np.delete(b2, 0)
+        guess = np.array([50, 0.05, 1000])
+        fitParams, fitCovariances = scipy.optimize.curve_fit(gaus, b2, a1, guess, sigma=(np.zeros(len(b2))+0.01), maxfev=100000)
+        assert_allclose(fitParams[2], 1000., atol=0.15)
