abd6c5e6c58b861e2bf8e7dff50122de0d7e2cda
Axel Donath <axel.donath@mpi-hd.mpg.de>
2019-06-15 15:29:24 +0200

    Remove GeneralRandom class

--- gammapy/utils/random/__init__.py
+++ gammapy/utils/random/__init__.py
@@ -1,6 +1,5 @@
 """Utility functions / classes for working with distributions
 (e.g. probability density functions)
 """
-from .general_random import *
 from .utils import *
 from .inverse_cdf import *
