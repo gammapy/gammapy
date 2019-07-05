ca3b234bd818cfb7863e8ca304284abb6fa43e4c
Christoph Deil <Deil.Christoph@gmail.com>
2019-07-05 13:42:58 +0200

    Rename file to avoid conflict with function name

--- gammapy/stats/__init__.py
+++ gammapy/stats/__init__.py
@@ -1,6 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Statistics."""
-from .significance import *
+from .normal import *
 from .data import *
 from .feldman_cousins import *
 from .fit_statistics import *
