d662c955abadf93f2fcb8d021299b70fb20f1fc6
Christoph Deil <Deil.Christoph@gmail.com>
2019-07-13 23:39:56 +0200

    Remove gammapy/_astropy_init.py

--- gammapy/__init__.py
+++ gammapy/__init__.py
@@ -33,18 +33,6 @@ the following sub-packages (e.g. `gammapy.spectrum`):
  `utils`          --- Utility functions and classes
 """
 
-# Affiliated packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *
-
-# ----------------------------------------------------------------------------
-
-# For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:  # pylint: disable=undefined-variable
-    # put top-level package imports here
-    pass
-
 
 def song(karaoke=False):
     """
