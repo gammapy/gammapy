e9bc0f56bc81125ba77e84d594a07a22d361fca4
Jouvin <lea.jouvin@apc.in2p3.fr>
2019-07-15 16:51:24 +0200

    reformat

--- gammapy/spectrum/cosmic_ray.py
+++ gammapy/spectrum/cosmic_ray.py
@@ -10,36 +10,6 @@ from .models import PowerLaw
 
 __all__ = ["cosmic_ray_spectrum"]
 
-proton = {
-    "amplitude": 0.096 * u.Unit("1 / (m2 s TeV sr)"),
-    "index": 2.70,
-    "reference": 1 * u.TeV,
-}
-
-N = {
-    "amplitude": 0.0719 * u.Unit("1 / (m2 s TeV sr)"),
-    "index": 2.64,
-    "reference": 1 * u.TeV,
-}
-
-Si = {
-    "amplitude": 0.0284 * u.Unit("1 / (m2 s TeV sr)"),
-    "index": 2.66,
-    "reference": 1 * u.TeV,
-}
-
-Fe = {
-    "amplitude": 0.0134 * u.Unit("1 / (m2 s TeV sr)"),
-    "index": 2.63,
-    "reference": 1 * u.TeV,
-}
-
-electron = {
-    "amplitude": 6.85e-5 * u.Unit("1 / (m2 s TeV sr)"),
-    "index": 3.21,
-    "reference": 1 * u.TeV,
-}
-
 
 def cosmic_ray_spectrum(particle="proton"):
     """Cosmic ray flux at Earth.
@@ -63,6 +33,39 @@ def cosmic_ray_spectrum(particle="proton"):
     flux : `~astropy.units.Quantity`
         Cosmic ray flux in unit ``m^-2 s^-1 TeV^-1 sr^-1``
     """
+
+    proton = {
+        "amplitude": 0.096 * u.Unit("1 / (m2 s TeV sr)"),
+        "index": 2.70,
+        "reference": 1 * u.TeV,
+    }
+
+    N = {
+        "amplitude": 0.0719 * u.Unit("1 / (m2 s TeV sr)"),
+        "index": 2.64,
+        "reference": 1 * u.TeV,
+    }
+
+    Si = {
+        "amplitude": 0.0284 * u.Unit("1 / (m2 s TeV sr)"),
+        "index": 2.66,
+        "reference": 1 * u.TeV,
+    }
+
+    Fe = {
+        "amplitude": 0.0134 * u.Unit("1 / (m2 s TeV sr)"),
+        "index": 2.63,
+        "reference": 1 * u.TeV,
+    }
+
+    electron = {
+        "amplitude": 6.85e-5 * u.Unit("1 / (m2 s TeV sr)"),
+        "index": 3.21,
+        "reference": 1 * u.TeV,
+        "L": 3.19e-3,
+        "E_p": 0.107,
+        "w": 0.776,
+    }
 
     if particle == "proton":
         model = PowerLaw(**proton)
