8a810a49f2acf1c7241404bc08fe3742b5e61650
Jose Enrique Ruiz <jer@iaa.es>
2019-08-23 14:32:28 +0200

    Add tests for listing, setting and removing config parameters

--- gammapy/scripts/tests/test_analysis.py
+++ gammapy/scripts/tests/test_analysis.py
@@ -1,5 +1,24 @@
 from ..analysis import Analysis
+import copy
 
 
 def test_config():
-    assert Analysis().validate_schema() is None
+    assert Analysis()._validate_schema() is None
+
+
+def test_list_config():
+    assert "global" in Analysis().list_config().keys()
+
+
+def test_set_config():
+    analysis = Analysis()
+    analysis.set_config("global.logging.level", "info")
+    cfgset = copy.deepcopy(analysis.list_config())
+    analysis.set_config("global.test", "test")
+    cfgadd = copy.deepcopy(analysis.list_config())
+    analysis.set_config("global.test", None)
+    cfgdel = copy.deepcopy(analysis.list_config())
+
+    assert cfgset["global"]["logging"]["level"] == "info"
+    assert cfgadd["global"]["test"] == "test"
+    assert "test" not in cfgdel["global"].keys()
