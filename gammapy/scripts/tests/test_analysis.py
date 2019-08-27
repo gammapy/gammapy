458fa417a2ed38aefe44d8d276d927cde4abb409
Jose Enrique Ruiz <jer@iaa.es>
2019-08-27 14:21:43 +0200

    Add test for validation of astropy quantities

--- gammapy/scripts/tests/test_analysis.py
+++ gammapy/scripts/tests/test_analysis.py
@@ -2,7 +2,27 @@ from ..analysis import Analysis
 
 
 def test_config():
+    analysis = Analysis()
+    assert analysis.configuration.settings["general"]["logging"]["level"] == "INFO"
+
     cfg = {"general": {"out_folder": "test"}}
     analysis = Analysis(config=cfg)
     assert analysis.configuration.settings["general"]["logging"]["level"] == "INFO"
     assert analysis.configuration.settings["general"]["out_folder"] == "test"
+
+
+def test_validate_config():
+    analysis = Analysis()
+    assert analysis.configuration.validate() is None
+
+
+def test_validate_astropy_quantities():
+    cfg = {"observations":
+            {
+                "filter": [
+                            {"lon": "1 deg"}
+                          ]
+                }
+            }
+    analysis = Analysis(config=cfg)
+    assert analysis.configuration.validate() is None
