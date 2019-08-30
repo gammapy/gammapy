ec7ce7ed080bbd6d1ee2f8c0dac94b9dc74bf95c
Christoph Deil <Deil.Christoph@gmail.com>
2019-08-30 14:34:04 +0200

    Add 2hwc_j0534+220.txt and 2hwc_j0631+169.txt

--- gammapy/catalog/tests/data/make.py
+++ gammapy/catalog/tests/data/make.py
@@ -11,6 +11,10 @@ open("3fgl_J0835.3-4510.txt", "w").write(str(cat["3FGL J0835.3-4510"]))
 cat = source_catalogs["3fhl"]
 open("3fhl_j2301.9+5855e.txt", "w").write(str(cat["3FHL J2301.9+5855e"]))
 
+cat = source_catalogs["2hwc"]
+open("2hwc_j0534+220.txt", "w").write(str(cat["2HWC J0534+220"]))
+open("2hwc_j0631+169.txt", "w").write(str(cat["2HWC J0631+169"]))
+
 cat = source_catalogs["gamma-cat"]
 open("gammacat_hess_j1813-178.txt", "w").write(str(cat["HESS J1813-178"]))
 open("gammacat_hess_j1848-018.txt", "w").write(str(cat["HESS J1848-018"]))
