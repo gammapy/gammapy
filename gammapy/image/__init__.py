d1b8b7c729843cb5a48fd7c3a983e1322006ca05
Christoph Deil <Deil.Christoph@gmail.com>
2019-09-03 11:52:56 +0200

    Move gammapy/image/asmooth.py -> gammapy/detect/asmooth.py

--- gammapy/image/__init__.py
+++ gammapy/image/__init__.py
@@ -1,6 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Sky images (2-dimensional: lon, lat)."""
-from .asmooth import *
 from .measure import *
 from .plotting import *
 from .profile import *
