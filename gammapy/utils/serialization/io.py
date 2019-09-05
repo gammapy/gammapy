0952771bed5e887347eaf24bccae48880d6f81b8
Axel Donath <axel.donath@mpi-hd.mpg.de>
2019-09-05 11:29:25 +0200

    Set name correctly for background model from SkyDiffuseCube

--- gammapy/utils/serialization/io.py
+++ gammapy/utils/serialization/io.py
@@ -220,7 +220,7 @@ class dict_to_datasets:
                 self.cube_register[component["name"]] = cube
 
             evaluator = MapEvaluator(model=cube, exposure=dataset.exposure, psf=dataset.psf, edisp=dataset.edisp)
-            background_model = BackgroundModel(evaluator.compute_npred())
+            background_model = BackgroundModel(evaluator.compute_npred(), name=cube.name)
         else:
             if component["name"].strip().upper() in bkg_prev:
                 BGind = bkg_prev.index(component["name"].strip().upper())
