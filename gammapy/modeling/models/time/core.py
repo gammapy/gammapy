3d1a0fb63592c8860e47c0fe7b657095c9f3cdea
Christoph Deil <Deil.Christoph@gmail.com>
2019-09-08 23:24:37 +0200

    Fix gammapy.time.models imports

--- gammapy/modeling/models/time/core.py
+++ gammapy/modeling/models/time/core.py
@@ -46,7 +46,7 @@ class PhaseCurveTableModel(Model):
 
         from astropy.table import Table
         from gammapy.utils.scripts import make_path
-        from gammapy.time.models import PhaseCurveTableModel
+        from gammapy.modeling.models import PhaseCurveTableModel
         filename = make_path('$GAMMAPY_DATA/tests/phasecurve_LSI_DC.fits')
         table = Table.read(str(filename))
         phase_curve = PhaseCurveTableModel(table, time_0=43366.275, phase_0=0.0, f0=4.367575e-7, f1=0.0, f2=0.0)
