2e3d685184579a7291485475b1e57d9c818a31d1
Christoph Deil <Deil.Christoph@gmail.com>
2019-10-03 13:02:58 +0200

    Remove plot examples from spatial model docstrings

--- gammapy/modeling/models/spatial.py
+++ gammapy/modeling/models/spatial.py
@@ -165,39 +165,6 @@ class GaussianSpatialModel(SpatialModel):
         Increases counter-clockwise from the North direction.
     frame : {"icrs", "galactic"}
         Center position coordinate frame
-
-    Examples
-    --------
-    .. plot::
-        :include-source:
-
-        import numpy as np
-        import matplotlib.pyplot as plt
-        from astropy.coordinates import Angle
-        from gammapy.modeling.models import GaussianSpatialModel
-        from gammapy.maps import Map, WcsGeom
-
-        m_geom = WcsGeom.create(
-            binsz=0.01, width=(5, 5), skydir=(2, 2), coordsys="GAL", proj="AIT"
-        )
-        phi = Angle("30 deg")
-        model = GaussianSpatialModel("2 deg", "2 deg", "1 deg", 0.7, phi, frame="galactic")
-
-        coords = m_geom.get_coord()
-        vals = model(coords.lon, coords.lat)
-        skymap = Map.from_geom(m_geom, data=vals.value)
-
-        _, ax, _ = skymap.smooth("0.05 deg").plot()
-
-        transform = ax.get_transform("galactic")
-        ax.scatter(2, 2, transform=transform, s=20, edgecolor="red", facecolor="red")
-        ax.text(1.5, 1.85, r"$(l_0, b_0)$", transform=transform, ha="center")
-        ax.plot([2, 2 + np.sin(phi)], [2, 2 + np.cos(phi)], color="r", transform=transform)
-        ax.vlines(x=2, color="r", linestyle="--", transform=transform, ymin=-5, ymax=5)
-        ax.text(2.25, 2.45, r"$\phi$", transform=transform)
-        ax.contour(skymap.data, cmap="coolwarm", levels=10, alpha=0.6)
-
-        plt.show()
     """
 
     __slots__ = ["frame", "lon_0", "lat_0", "sigma", "e", "phi"]
@@ -280,41 +247,6 @@ class DiskSpatialModel(SpatialModel):
         smooth edges of the model drops from 95% to 5% of its amplitude.
     frame : {"icrs", "galactic"}
         Center position coordinate frame
-
-    Examples
-    --------
-    .. plot::
-        :include-source:
-
-        import numpy as np
-        import matplotlib.pyplot as plt
-        from gammapy.maps import Map, WcsGeom
-        from gammapy.modeling.models import DiskSpatialModel
-
-        model = DiskSpatialModel("2 deg", "2 deg", "1 deg", 0.8, "30 deg", frame="galactic")
-
-        m_geom = WcsGeom.create(
-            binsz=0.01, width=(3, 3), skydir=(2, 2), coordsys="GAL", proj="AIT"
-        )
-        coords = m_geom.get_coord()
-        vals = model(coords.lon, coords.lat)
-        skymap = Map.from_geom(m_geom, data=vals.value)
-
-        _, ax, _ = skymap.smooth("0.05 deg").plot()
-
-        transform = ax.get_transform("galactic")
-        ax.scatter(2, 2, transform=transform, s=20, edgecolor="red", facecolor="red")
-        ax.text(1.7, 1.85, r"$(l_0, b_0)$", transform=transform, ha="center")
-        ax.plot(
-            [2, 2 + np.sin(np.pi / 6)],
-            [2, 2 + np.cos(np.pi / 6)],
-            color="r",
-            transform=transform,
-        )
-        ax.vlines(x=2, color="r", linestyle="--", transform=transform, ymin=0, ymax=5)
-        ax.text(2.15, 2.3, r"$\phi$", transform=transform)
-
-        plt.show()
     """
 
     __slots__ = ["frame", "lon_0", "lat_0", "r_0", "e", "phi", "_offset_by"]
