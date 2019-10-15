a51ee0f5e60176ea1c85c5b72ee01cd489ff3b88
Axel Donath <axel.donath@mpi-hd.mpg.de>
2019-10-15 13:55:22 +0200

    Fix regression test

--- gammapy/cube/tests/test_fit.py
+++ gammapy/cube/tests/test_fit.py
@@ -315,9 +315,9 @@ def test_map_fit(sky_model, geom, geom_etrue):
 
     # test model evaluation outside image
 
-    with pytest.raises(ValueError):
-        dataset_1.model.skymodels[0].spatial_model.lon_0.value = 150
-        dataset_1.npred()
+    dataset_1.model.skymodels[0].spatial_model.lon_0.value = 150
+    dataset_1.npred()
+    assert not dataset_1._evaluators[0].contributes
 
     with mpl_plot_check():
         dataset_1.plot_residuals()
