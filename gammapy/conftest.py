6a9528cb24f248559d8c06e1f3f2a7101be3e609
Christoph Deil <Deil.Christoph@gmail.com>
2019-10-16 16:31:08 +0200

    Update gammapy/conftest.py

--- gammapy/conftest.py
+++ gammapy/conftest.py
@@ -3,19 +3,9 @@
 # by importing them here in conftest.py they are discoverable by py.test
 # no matter how it is invoked within the source tree.
 import os
-import astropy.version
 from astropy.tests.helper import enable_deprecations_as_exceptions
 
-if astropy.version.version < "3.0":
-    # With older versions of Astropy, we actually need to import the pytest
-    # plugins themselves in order to make them discoverable by pytest.
-    from astropy.tests.pytest_plugins import *
-else:
-    # As of Astropy 3.0, the pytest plugins provided by Astropy are
-    # automatically made available when Astropy is installed. This means it's
-    # not necessary to import them here, but we still need to import global
-    # variables that are used for configuration.
-    from astropy.tests.plugins.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
+from astropy.tests.plugins.display import PYTEST_HEADER_MODULES
 
 
 # TODO: add numpy again once https://github.com/astropy/regions/pull/252 is addressed
