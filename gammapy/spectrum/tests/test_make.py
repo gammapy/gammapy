cb5f1719c6131b33684eede1dc36d5a9af7a2d2b
Axel Donath <axel.donath@mpi-hd.mpg.de>
2019-10-25 09:05:46 +0200

    Add extract test to test_make.py

--- gammapy/spectrum/tests/test_make.py
+++ gammapy/spectrum/tests/test_make.py
@@ -5,9 +5,10 @@ from numpy.testing import assert_allclose
 import astropy.units as u
 from astropy.coordinates import Angle, SkyCoord
 from regions import CircleSkyRegion
-from gammapy.data import DataStore
-from gammapy.spectrum import SafeMaskMaker, SpectrumDatasetMaker
-from gammapy.utils.testing import requires_data
+from gammapy.data import DataStore, ObservationStats
+from gammapy.spectrum import SafeMaskMaker, SpectrumDatasetMaker, ReflectedRegionsBackgroundMaker
+from gammapy.utils.testing import requires_data, assert_quantity_allclose
+from gammapy.maps import WcsGeom, WcsNDMap
 
 
 @pytest.fixture
@@ -47,6 +48,36 @@ def spectrum_dataset_maker_crab():
     e_reco = np.logspace(0, 2, 5) * u.TeV
     e_true = np.logspace(-0.5, 2, 11) * u.TeV
     return SpectrumDatasetMaker(region=region, e_reco=e_reco, e_true=e_true)
+
+
+@pytest.fixture
+def spectrum_dataset_maker_crab_fine_bins():
+    pos = SkyCoord(83.63, 22.01, unit="deg", frame="icrs")
+    radius = Angle(0.11, "deg")
+    region = CircleSkyRegion(pos, radius)
+    e_true = np.logspace(-2, 2.5, 109) * u.TeV
+    e_reco = np.logspace(-2, 2, 73) * u.TeV
+    return SpectrumDatasetMaker(region=region, e_reco=e_reco, e_true=e_true)
+
+
+@pytest.fixture
+def reflected_regions_bkg_maker():
+    pos = SkyCoord(83.63, 22.01, unit="deg", frame="icrs")
+    radius = Angle(0.11, "deg")
+    region = CircleSkyRegion(pos, radius)
+
+    pos = SkyCoord(83.63, 22.01, unit="deg", frame="icrs")
+    exclusion_region = CircleSkyRegion(pos, Angle(0.3, "deg"))
+    geom = WcsGeom.create(skydir=pos, binsz=0.02, width=10.0)
+    mask = geom.region_mask([exclusion_region], inside=False)
+    exclusion_mask = WcsNDMap(geom, data=mask)
+
+    return ReflectedRegionsBackgroundMaker(
+        region=region,
+        exclusion_mask=exclusion_mask,
+        min_distance_input="0.2 deg",
+    )
+
 
 
 @requires_data()
@@ -115,3 +146,72 @@ def test_safe_mask_maker_dc1(spectrum_dataset_maker_gc, observations_cta_dc1):
     dataset = safe_mask_maker.run(dataset, obs)
     assert_allclose(dataset.energy_range[0].value, 3.162278, rtol=1e-3)
     assert dataset.energy_range[0].unit == "TeV"
+
+
+@requires_data()
+class TestSpectrumMakerChain:
+    @staticmethod
+    @pytest.mark.parametrize(
+        "pars, results",
+        [
+            (
+                dict(containment_correction=False),
+                dict(
+                    n_on=192,
+                    sigma=21.1404,
+                    aeff=580254.9 * u.m ** 2,
+                    edisp=0.236176,
+                ),
+            ),
+            (
+                dict(containment_correction=True),
+                dict(
+                    n_on=192,
+                    sigma=21.1404,
+                    aeff=361924.746081 * u.m ** 2,
+                    edisp=0.236176,
+                ),
+            ),
+        ],
+    )
+    def test_extract(pars, results, observations_hess_dl3, spectrum_dataset_maker_crab_fine_bins, reflected_regions_bkg_maker):
+        """Test quantitative output for various configs"""
+        safe_mask_maker = SafeMaskMaker()
+
+        obs = observations_hess_dl3[0]
+        spectrum_dataset_maker_crab_fine_bins.containment_correction = pars["containment_correction"]
+        dataset = spectrum_dataset_maker_crab_fine_bins.run(obs, selection=["counts", "aeff", "edisp"])
+        dataset = reflected_regions_bkg_maker.run(dataset, obs)
+        dataset = safe_mask_maker.run(dataset, obs)
+
+        aeff_actual = dataset.aeff.data.evaluate(energy=5 * u.TeV)
+        edisp_actual = dataset.edisp.data.evaluate(e_true=5 * u.TeV, e_reco=5.2 * u.TeV)
+
+        assert_quantity_allclose(aeff_actual, results["aeff"], rtol=1e-3)
+        assert_quantity_allclose(edisp_actual, results["edisp"], rtol=1e-3)
+
+        # TODO: Introduce assert_stats_allclose
+        info = dataset._info_dict()
+        info["obs_id"] = info.pop("name")
+        stats = ObservationStats(**info)
+        n_on_actual = stats.n_on
+        sigma_actual = stats.sigma
+
+        assert n_on_actual == results["n_on"]
+        assert_allclose(sigma_actual, results["sigma"], atol=1e-2)
+
+        gti_obs = obs.gti.table
+        gti_dataset = dataset.gti.table
+        assert_allclose(gti_dataset["START"], gti_obs["START"])
+        assert_allclose(gti_dataset["STOP"], gti_obs["STOP"])
+
+    def test_compute_energy_threshold(self, spectrum_dataset_maker_crab_fine_bins, observations_hess_dl3):
+        safe_mask_maker = SafeMaskMaker(methods=["aeff-max"], aeff_percent=10)
+
+        obs = observations_hess_dl3[0]
+        spectrum_dataset_maker_crab_fine_bins.containment_correction = True
+        dataset = spectrum_dataset_maker_crab_fine_bins.run(obs, selection=["counts", "aeff", "edisp"])
+        dataset = safe_mask_maker.run(dataset, obs)
+
+        actual = dataset.energy_range[0]
+        assert_quantity_allclose(actual, 0.8799225 * u.TeV, rtol=1e-3)
