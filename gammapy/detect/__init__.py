9787c27d63900701d3c9afa9fa8ec8748d217187
Christoph Deil <Deil.Christoph@gmail.com>
2019-11-13 14:10:32 +0100

    Remove gammapy/detect/cwt.py

--- gammapy/detect/__init__.py
+++ gammapy/detect/__init__.py
@@ -1,7 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Source detection and measurement methods."""
 from .asmooth import *
-from .cwt import *
 from .find import *
 from .kernel import *
 from .lima import *
