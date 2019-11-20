57a2361a9a6690e78eeb05a02f078fed6f2e26f4
Terrier <rterrier@apc.in2p3.fr>
2019-11-20 11:51:10 +0100

    Removed exptest and relevant documentation

--- gammapy/time/__init__.py
+++ gammapy/time/__init__.py
@@ -1,6 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Time analysis."""
-from .events import *
 from .lightcurve import *
 from .lightcurve_estimator import *
 from .period import *
