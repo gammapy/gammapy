b552361548b749ac531ca066f4b76ff137bc4801
Jose Enrique Ruiz <jer@iaa.es>
2019-11-25 19:49:21 +0100

    add tests to check all types

--- gammapy/analysis/tests/test_config.py
+++ gammapy/analysis/tests/test_config.py
@@ -1,12 +1,62 @@
 import pytest
 from astropy.units import Quantity
-from gammapy.analysis.config import AnalysisConfig, General
+from gammapy.analysis.config import AnalysisConfig
+from gammapy.analysis.config import (
+    General,
+    Log,
+    Data,
+    Datasets,
+    Geom,
+    Wcs,
+    Fov,
+    Selection,
+    Axes,
+    Background,
+    Fit,
+    FluxPoints,
+    TimeRange,
+    EnergyRange,
+    SpatialCircleRange,
+    EnergyAxis,
+    SkyCoordType,
+    BackgroundMethodEnum,
+    FrameEnum,
+    TimeType,
+    EnergyType,
+    AngleType
+)
+
+
 from pathlib import Path
+config_file = Path(__file__).resolve().parent / ".." / "config" / "config.yaml"
 
 
 def test_config_basics():
     config = AnalysisConfig()
     assert isinstance(config.general, General)
+    assert isinstance(config.general.log, Log)
+    assert isinstance(config.data, Data)
+    assert isinstance(config.datasets, Datasets)
+    assert isinstance(config.datasets.geom, Geom)
+    assert isinstance(config.datasets.geom.wcs, Wcs)
+    assert isinstance(config.datasets.geom.wcs.fov, Fov)
+    assert isinstance(config.datasets.geom.selection, Selection)
+    assert isinstance(config.datasets.geom.axes, Axes)
+    assert isinstance(config.datasets.background, Background)
+    assert isinstance(config.fit, Fit)
+    assert isinstance(config.flux_points, FluxPoints)
+    assert isinstance(config.data.obs_time, TimeRange)
+    assert isinstance(config.fit.fit_range, EnergyRange)
+    assert isinstance(config.data.obs_cone, SpatialCircleRange)
+    assert isinstance(config.flux_points.energy, EnergyAxis)
+    config.datasets.geom.wcs.skydir = {"frame": "galactic", "lon": "83.633 deg", "lat": "22.014 deg"}
+    assert isinstance(config.datasets.geom.wcs.skydir, SkyCoordType)
+    assert isinstance(config.datasets.background.method, BackgroundMethodEnum)
+    assert isinstance(config.datasets.geom.wcs.skydir.frame, FrameEnum)
+    config.data.obs_time.start = "2019-12-01"
+    assert isinstance(config.data.obs_time.start, TimeType)
+    assert isinstance(config.fit.fit_range.min, EnergyType)
+    assert isinstance(config.datasets.geom.wcs.binsize, AngleType)
 
 
 def test_config_create_from_dict():
@@ -16,8 +66,7 @@ def test_config_create_from_dict():
 
 
 def test_config_create_from_yaml():
-    cfg = Path(__file__).resolve().parent / ".." / "config" / "config.yaml"
-    config = AnalysisConfig.from_yaml(cfg)
+    config = AnalysisConfig.from_yaml(config_file)
     assert isinstance(config.general, General)
 
 
@@ -28,7 +77,7 @@ def test_config_to_yaml():
 
 def test_config_update_from_dict():
     config1 = AnalysisConfig()
-    data = {"fit": {"fit_range": {"min": "1 TeV",  "max": "100 TeV"}}}
+    data = {"fit": {"fit_range": {"min": "1 TeV", "max": "100 TeV"}}}
     config2 = AnalysisConfig(**data)
     config = config1.update_from_dict(config2)
     assert config.fit.fit_range.min == Quantity("1 TeV")
