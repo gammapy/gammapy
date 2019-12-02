ef15f3ebd348d34f1b3ab40d9aa0974827be6f34
Jose Enrique Ruiz <jer@iaa.es>
2019-12-02 07:43:37 +0100

    make polish

--- gammapy/scripts/analysis.py
+++ gammapy/scripts/analysis.py
@@ -1,6 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-import click
 import logging
+import click
 from gammapy.analysis import Analysis, AnalysisConfig
 
 log = logging.getLogger(__name__)
@@ -11,13 +11,10 @@ log = logging.getLogger(__name__)
     "--filename",
     default="config.yaml",
     help="Filename to store the default configuration values.",
-    show_default=True
+    show_default=True,
 )
 @click.option(
-    "--overwrite",
-    default=False,
-    is_flag=True,
-    help="Overwrite existing file."
+    "--overwrite", default=False, is_flag=True, help="Overwrite existing file."
 )
 def cli_make_config(filename, overwrite):
     """Writes default configuration file."""
@@ -31,19 +28,16 @@ def cli_make_config(filename, overwrite):
     "--filename",
     default="config.yaml",
     help="Filename with default configuration values.",
-    show_default=True
+    show_default=True,
 )
 @click.option(
     "--out",
     default="datasets",
     help="Output folder where reduced datasets are stored.",
-    show_default=True
+    show_default=True,
 )
 @click.option(
-    "--overwrite",
-    default=False,
-    is_flag=True,
-    help="Overwrite existing datasets."
+    "--overwrite", default=False, is_flag=True, help="Overwrite existing datasets."
 )
 def cli_run_analysis(filename, out, overwrite):
     """Performs automated data reduction process."""
@@ -52,4 +46,4 @@ def cli_run_analysis(filename, out, overwrite):
     analysis.get_observations()
     analysis.get_datasets()
     analysis.datasets.write(out, overwrite=overwrite)
-    print(f"Datasets stored in {out} folder.")
+    log.info(f"Datasets stored in {out} folder.")
