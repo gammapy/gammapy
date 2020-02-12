d55bd6262610990b07fcbde01eef9379062829da
Terrier <rterrier@apc.in2p3.fr>
2020-02-12 10:17:55 +0100

    add docstrings

--- gammapy/stats/fit_stat_estimator.py
+++ gammapy/stats/fit_stat_estimator.py
@@ -9,9 +9,19 @@ __all__ = ["WStatEstimator", "CashEstimator"]
 class FitStatisticEstimator(abc.ABC):
     @property
     def significance(self):
+        """Return statistical significance of measured excess."""
         return np.sign(self.excess)*np.sqrt(self.TS_null-self.TS_max)
 
     def compute_errn(self, n_sigma=1):
+        """Compute downward excess uncertainties.
+
+        Searches the signal value for which the test statistics is n_sigma away from the maximum.
+
+        Parameters
+        ----------
+        n_sigma : float
+            Confidence level of the uncertainty expressed in number of sigma. Default is 1.
+        """
         min_range = self.excess - 2 * n_sigma * self.std
 
         try:
@@ -26,6 +36,15 @@ class FitStatisticEstimator(abc.ABC):
         return errn - self.excess
 
     def compute_errp(self, n_sigma=1):
+        """Compute upward excess uncertainties.
+
+        Searches the signal value for which the test statistics is n_sigma away from the maximum.
+
+        Parameters
+        ----------
+        n_sigma : float
+            Confidence level of the uncertainty expressed in number of sigma. Default is 1.
+        """
         max_range = self.excess + 2 * n_sigma * self.std
 
         errp = brentq(
@@ -37,6 +56,16 @@ class FitStatisticEstimator(abc.ABC):
         return errp - self.excess
 
     def compute_upper_limit(self, n_sigma=3):
+        """Compute upper limit on the signal.
+
+        Searches the signal value for which the test statistics is n_sigma away from the maximum
+        or from 0 if the measured excess is negative.
+
+        Parameters
+        ----------
+        n_sigma : float
+            Confidence level of the upper limit expressed in number of sigma. Default is 3.
+        """
         min_range = np.maximum(0, self.excess)
         max_range = min_range + 2 * n_sigma * self.std
         TS_ref = self._stat_fcn(min_range,0.)
@@ -73,12 +102,12 @@ class CashEstimator(FitStatisticEstimator):
 
     @property
     def TS_null(self):
-        """Stat value for null hypothesis, i.e. 0 counts"""
+        """Stat value for null hypothesis, i.e. 0 expected signal counts"""
         return cash(self.n_on, self.mu_bkg + 0)
 
     @property
     def TS_max(self):
-        """Stat value for best fit hypothesis, i.e. mu = n_on - mu_bkg"""
+        """Stat value for best fit hypothesis, i.e. expected signal mu = n_on - mu_bkg"""
         return cash(self.n_on, self.n_on)
 
     def _stat_fcn(self, mu, delta):
@@ -114,10 +143,12 @@ class WStatEstimator(FitStatisticEstimator):
 
     @property
     def TS_null(self):
+        """Stat value for null hypothesis, i.e. 0 expected signal counts"""
         return wstat(self.n_on, self.n_off, self.alpha, 0)
 
     @property
     def TS_max(self):
+        """Stat value for best fit hypothesis, i.e. expected signal mu = n_on - alpha * n_off"""
         return wstat(self.n_on, self.n_off, self.alpha, self.excess)
 
     def _stat_fcn(self, mu, delta):
