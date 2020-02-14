c5fc00b34d5088acebbdc050ed9c4eceefce48f9
Terrier <rterrier@apc.in2p3.fr>
2020-02-14 17:20:42 +0100

    Modify classes to take array as input. Added iterators all over the place...

--- gammapy/stats/fit_stat_estimator.py
+++ gammapy/stats/fit_stat_estimator.py
@@ -22,18 +22,24 @@ class FitStatisticEvaluator(abc.ABC):
         n_sigma : float
             Confidence level of the uncertainty expressed in number of sigma. Default is 1.
         """
+        errn = np.zeros_like(self.n_on, dtype='float')
         min_range = self.excess - 2 * n_sigma * self.std
 
-        try:
-            errn = brentq(
-                self._stat_fcn,
-                min_range,
-                self.excess,
-                args=(self.TS_max + n_sigma))
-        except ValueError:
-            return -self.n_on
+        it = np.nditer(errn, flags=['multi_index'])
+        while not it.finished:
+            try:
+                res = brentq(
+                    self._stat_fcn,
+                    min_range[it.multi_index],
+                    self.excess[it.multi_index],
+                    args=(self.TS_max[it.multi_index] + n_sigma, it.multi_index)
+                )
+                errn[it.multi_index] =res - self.excess[it.multi_index]
+            except ValueError:
+                errn[it.multi_index] = -self.n_on[it.multi_index]
+            it.iternext()
 
-        return errn - self.excess
+        return errn
 
     def compute_errp(self, n_sigma=1):
         """Compute upward excess uncertainties.
@@ -45,13 +51,18 @@ class FitStatisticEvaluator(abc.ABC):
         n_sigma : float
             Confidence level of the uncertainty expressed in number of sigma. Default is 1.
         """
+        errp = np.zeros_like(self.n_on, dtype='float')
         max_range = self.excess + 2 * n_sigma * self.std
 
-        errp = brentq(
-            self._stat_fcn,
-            self.excess,
-            max_range,
-            args=(self.TS_max + n_sigma))
+        it = np.nditer(errp, flags=['multi_index'])
+        while not it.finished:
+            errp[it.multi_index] = brentq(
+                self._stat_fcn,
+                self.excess[it.multi_index],
+                max_range,
+                args=(self.TS_max[it.multi_index] + n_sigma, it.multi_index)
+            )
+            it.iternext()
 
         return errp - self.excess
 
@@ -66,15 +77,23 @@ class FitStatisticEvaluator(abc.ABC):
         n_sigma : float
             Confidence level of the upper limit expressed in number of sigma. Default is 3.
         """
+        ul = np.zeros_like(self.n_on, dtype='float')
+
         min_range = np.maximum(0, self.excess)
         max_range = min_range + 2 * n_sigma * self.std
-        TS_ref = self._stat_fcn(min_range,0.)
-        return brentq(
-            self._stat_fcn,
-            min_range,
-            max_range,
-            args=(TS_ref + n_sigma))
+        it = np.nditer(ul, flags=['multi_index'])
 
+        while not it.finished:
+            TS_ref = self._stat_fcn(min_range[it.multi_index],0.)
+            ul[it.multi_index] = brentq(
+                self._stat_fcn,
+                min_range[it.multi_index],
+                max_range[it.multi_index],
+                args=(TS_ref + n_sigma, it.multi_index)
+            )
+            it.iternext()
+
+        return ul
 
 class CashEvaluator(FitStatisticEvaluator):
     """Class to compute statistics (significance, asymmetric errors , ul) for Poisson distributed variable
@@ -88,8 +107,8 @@ class CashEvaluator(FitStatisticEvaluator):
         Expected level of background
     """
     def __init__(self, n_on, mu_bkg):
-        self.n_on = n_on
-        self.mu_bkg= mu_bkg
+        self.n_on = np.asanyarray(n_on)
+        self.mu_bkg = np.asanyarray(mu_bkg)
 
     @property
     def excess(self):
@@ -110,8 +129,11 @@ class CashEvaluator(FitStatisticEvaluator):
         """Stat value for best fit hypothesis, i.e. expected signal mu = n_on - mu_bkg"""
         return cash(self.n_on, self.n_on)
 
-    def _stat_fcn(self, mu, delta):
-        return cash(self.n_on, self.mu_bkg + mu) - delta
+    def _stat_fcn(self, mu, delta=0, index=None):
+        if index is not None:
+            return cash(self.n_on[index], self.mu_bkg[index] + mu) - delta
+        else:
+            return cash(self.n_on, self.mu_bkg + mu) - delta
 
 
 class WStatEvaluator(FitStatisticEvaluator):
@@ -129,9 +151,9 @@ class WStatEvaluator(FitStatisticEvaluator):
     """
 
     def __init__(self, n_on, n_off, alpha):
-        self.n_on = n_on
-        self.n_off = n_off
-        self.alpha = alpha
+        self.n_on = np.asanyarray(n_on)
+        self.n_off = np.asanyarray(n_off)
+        self.alpha = np.asanyarray(alpha)
 
     @property
     def excess(self):
@@ -151,6 +173,9 @@ class WStatEvaluator(FitStatisticEvaluator):
         """Stat value for best fit hypothesis, i.e. expected signal mu = n_on - alpha * n_off"""
         return wstat(self.n_on, self.n_off, self.alpha, self.excess)
 
-    def _stat_fcn(self, mu, delta):
-        return wstat(self.n_on, self.n_off, self.alpha, mu) - delta
+    def _stat_fcn(self, mu, delta=0, index=None):
+        if index is not None:
+            return wstat(self.n_on[index], self.n_off[index], self.alpha[index], mu) - delta
+        else:
+            return wstat(self.n_on, self.n_off, self.alpha, mu) - delta
 
