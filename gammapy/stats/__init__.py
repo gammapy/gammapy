0235b4c96092f14b05bb420253635d4c0a08b08a
Terrier <rterrier@apc.in2p3.fr>
2020-03-03 11:32:34 +0100

    renamed files

--- gammapy/stats/__init__.py
+++ gammapy/stats/__init__.py
@@ -3,6 +3,6 @@
 from .feldman_cousins import *
 from .fit_statistics import *
 from .fit_statistics_cython import *
-from .fit_stat_estimator import *
+from .counts_statistic import *
 from .normal import *
 from .poisson import *
