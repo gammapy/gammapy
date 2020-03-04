c43e7d722f52d218905948925775d71a134c0ce8
Axel Donath <axel.donath@mpi-hd.mpg.de>
2020-03-04 14:09:38 +0100

    Move LightCurve class

--- docs/time/index.rst
+++ docs/time/index.rst
@@ -40,40 +40,6 @@ The latter gives access to a number of utilities such as plots and access to tim
     >>> lc.plot()
 
 
-Light Curve Extraction
-======================
-
-The extraction of a light curve from gamma-ray data follows the general approach of
-data reduction and modeling/fitting. Observations are first reduced to dataset objects
-(e.g. `~gammapy.cube.MapDataset` or `~gammapy.spectrum.SpectrumDatasetOnOff`). Then, after
-setting the appropriate model the flux is extracted in each time bin with the
-`~gammapy.time.LightCurveEstimator`.
-
-To extract the light curve of a source, the `~gammapy.time.LightCurveEstimator`
-fits a scale factor on the model component representing the source in each time bin
-and returns a `~gammapy.time.LightCurve`. It can work with spectral (1D) datasets as well
-as with map (3D) datasets.
-
-Once a `~gammapy.Fit.Datasets` object is build with a model set, one can call the estimator
-to compute the light curve in the datasets time intervals::
-
-    >>> lc_estimator = LightCurveEstimator(datasets, source="source")
-    >>> lc = lc_estimator.run(e_min=1*u.TeV, emax=10*u.TeV, e_ref=1*u.TeV)
-
-where `source` is the model component describing the source of interest and `datasets` the `~gammapy.modeling.Datasets`
-object produced by data reduction.
-The light curve notebook shows an example of `observation based light curve
-extraction <../notebooks/light_curve.html#Light-Curve-estimation:-by-observation>`__
-
-Similarly, `~gammapy.time.LightCurveEstimator` can be used to extract the light curve in user defined time intervals.
-This can be useful to combine datasets to produce light curve by night, week or month::
-
-    >>> lc_estimator = LightCurveEstimator(datasets, source="source", time_intervals=time_intervals)
-    >>> lc = lc_estimator.run(e_min=1*u.TeV, emax=10*u.TeV, e_ref=1*u.TeV)
-
-where `time_intervals` is a list of time intervals as `~astropy.time.Time` objects.
-The light curve notebook shows an example of `night-wise light curve
-extraction <../notebooks/light_curve.html#Night-wise-LC-estimation>`__
 
 
 Variability and periodicity tests
--- gammapy/estimators/tests/test_lightcurve.py
+++ gammapy/estimators/tests/test_lightcurve.py
@@ -1,15 +1,125 @@
 import pytest
+import datetime
+import numpy as np
 from numpy.testing import assert_allclose
 import astropy.units as u
 from astropy.time import Time
+from astropy.table import Table, Column
 from gammapy.data import GTI
 from gammapy.modeling.models import PowerLawSpectralModel, SkyModel
 from gammapy.estimators.tests.test_flux_point_estimator import (
     simulate_map_dataset,
     simulate_spectrum_dataset,
 )
-from gammapy.estimators import LightCurveEstimator
-from gammapy.utils.testing import requires_data, requires_dependency
+from gammapy.estimators import LightCurveEstimator, LightCurve
+from gammapy.utils.testing import requires_data, requires_dependency, mpl_plot_check
+
+
+@pytest.fixture(scope="session")
+def lc():
+    meta = dict(TIMESYS="utc")
+
+    table = Table(
+        meta=meta,
+        data=[
+            Column(Time(["2010-01-01", "2010-01-03"]).mjd, "time_min"),
+            Column(Time(["2010-01-03", "2010-01-10"]).mjd, "time_max"),
+            Column([1e-11, 3e-11], "flux", unit="cm-2 s-1"),
+            Column([0.1e-11, 0.3e-11], "flux_err", unit="cm-2 s-1"),
+            Column([np.nan, 3.6e-11], "flux_ul", unit="cm-2 s-1"),
+            Column([False, True], "is_ul"),
+        ],
+    )
+
+    return LightCurve(table=table)
+
+
+def test_lightcurve_repr(lc):
+    assert repr(lc) == "LightCurve(len=2)"
+
+
+def test_lightcurve_properties_time(lc):
+    assert lc.time_scale == "utc"
+    assert lc.time_format == "mjd"
+
+    # Time-related attributes
+    time = lc.time
+    assert time.scale == "utc"
+    assert time.format == "mjd"
+    assert_allclose(time.mjd, [55198, 55202.5])
+
+    assert_allclose(lc.time_min.mjd, [55197, 55199])
+    assert_allclose(lc.time_max.mjd, [55199, 55206])
+
+    # Note: I'm not sure why the time delta has this scale and format
+    time_delta = lc.time_delta
+    assert time_delta.scale == "tai"
+    assert time_delta.format == "jd"
+    assert_allclose(time_delta.jd, [2, 7])
+
+
+def test_lightcurve_properties_flux(lc):
+    flux = lc.table["flux"].quantity
+    assert flux.unit == "cm-2 s-1"
+    assert_allclose(flux.value, [1e-11, 3e-11])
+
+
+# TODO: extend these tests to cover other time scales.
+# In those cases, CSV should not round-trip because there
+# is no header info in CSV to store the time scale!
+
+
+@pytest.mark.parametrize("format", ["fits", "ascii.ecsv", "ascii.csv"])
+def test_lightcurve_read_write(tmp_path, lc, format):
+    lc.write(tmp_path / "tmp", format=format)
+    lc = LightCurve.read(tmp_path / "tmp", format=format)
+
+    # Check if time-related info round-trips
+    time = lc.time
+    assert time.scale == "utc"
+    assert time.format == "mjd"
+    assert_allclose(time.mjd, [55198, 55202.5])
+
+
+@requires_dependency("matplotlib")
+def test_lightcurve_plot(lc):
+    with mpl_plot_check():
+        lc.plot()
+
+
+@pytest.mark.parametrize("flux_unit", ["cm-2 s-1"])
+def test_lightcurve_plot_flux(lc, flux_unit):
+    f, ferr = lc._get_fluxes_and_errors(flux_unit)
+    assert_allclose(f, [1e-11, 3e-11])
+    assert_allclose(ferr, ([0.1e-11, 0.3e-11], [0.1e-11, 0.3e-11]))
+
+
+@pytest.mark.parametrize("flux_unit", ["cm-2 s-1"])
+def test_lightcurve_plot_flux_ul(lc, flux_unit):
+    is_ul, ful = lc._get_flux_uls(flux_unit)
+    assert_allclose(is_ul, [False, True])
+    assert_allclose(ful, [np.nan, 3.6e-11])
+
+
+def test_lightcurve_plot_time(lc):
+    t, terr = lc._get_times_and_errors("mjd")
+    assert np.array_equal(t, [55198.0, 55202.5])
+    assert np.array_equal(terr, [[1.0, 3.5], [1.0, 3.5]])
+
+    t, terr = lc._get_times_and_errors("iso")
+    assert np.array_equal(
+        t, [datetime.datetime(2010, 1, 2), datetime.datetime(2010, 1, 6, 12)]
+    )
+    assert np.array_equal(
+        terr,
+        [
+            [datetime.timedelta(1), datetime.timedelta(3.5)],
+            [datetime.timedelta(1), datetime.timedelta(3.5)],
+        ],
+    )
+
+
+
 
 
 def get_spectrum_datasets():
