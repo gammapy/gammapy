fb26c98814337aeda0a01a172ac222f8cfc268a6
Axel Donath <axel.donath@mpi-hd.mpg.de>
2020-03-04 13:47:55 +0100

    Move LightCurveEstimator

--- gammapy/time/__init__.py
+++ gammapy/time/__init__.py
@@ -1,7 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Time analysis."""
 from .lightcurve import *
-from .lightcurve_estimator import *
 from .period import *
 from .simulate import *
 from .variability import *
