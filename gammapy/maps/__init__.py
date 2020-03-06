fb549e587511e3bd34ae448e84207260b2ef7577
Axel Donath <axel.donath@mpi-hd.mpg.de>
2020-03-06 09:20:39 +0100

    Adapt gammapy.maps.__init__

--- gammapy/maps/__init__.py
+++ gammapy/maps/__init__.py
@@ -1,7 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Sky maps."""
 from .base import *
-from .counts_spectrum import *
 from .geom import *
 from .hpx import *
 from .hpxmap import *
