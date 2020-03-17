c8f82d756f9e376c8a4d273a1952c3c42056b3dc
Jose Enrique Ruiz <jer@iaa.es>
2020-03-17 17:45:06 +0100

    make polish in gammapy/analysis

--- gammapy/analysis/core.py
+++ gammapy/analysis/core.py
@@ -7,18 +7,18 @@ from regions import CircleSkyRegion
 from gammapy.analysis.config import AnalysisConfig
 from gammapy.data import DataStore
 from gammapy.datasets import Datasets, FluxPointsDataset, MapDataset, SpectrumDataset
+from gammapy.estimators import FluxPointsEstimator
 from gammapy.makers import (
     FoVBackgroundMaker,
-    RingBackgroundMaker,
     MapDatasetMaker,
     ReflectedRegionsBackgroundMaker,
+    RingBackgroundMaker,
     SafeMaskMaker,
     SpectrumDatasetMaker,
 )
 from gammapy.maps import Map, MapAxis, WcsGeom
 from gammapy.modeling import Fit
-from gammapy.modeling.models import Models, BackgroundModel
-from gammapy.estimators import FluxPointsEstimator
+from gammapy.modeling.models import BackgroundModel, Models
 from gammapy.utils.scripts import make_path
 
 __all__ = ["Analysis"]
@@ -258,15 +258,11 @@ class Analysis:
 
         bkg_method = datasets_settings.background.method
         if bkg_method == "fov_background":
-            log.debug(
-                f"Creating FoVBackgroundMaker with arguments {bkg_maker_config}"
-            )
+            log.debug(f"Creating FoVBackgroundMaker with arguments {bkg_maker_config}")
             bkg_maker = FoVBackgroundMaker(**bkg_maker_config)
         elif bkg_method == "ring":
             bkg_maker = RingBackgroundMaker(**bkg_maker_config)
-            log.debug(
-                f"Creating RingBackgroundMaker with arguments {bkg_maker_config}"
-            )
+            log.debug(f"Creating RingBackgroundMaker with arguments {bkg_maker_config}")
             if datasets_settings.geom.axes.energy.nbins > 1:
                 raise ValueError(
                     "You need to define a single-bin energy geometry for your dataset."
