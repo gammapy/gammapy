9cdcc74ed9c151a19446b9157e7afdc4ac3a8efc
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-05-12 11:43:48 +0200

    Also add numpy and setuptools_scm to build requirements, simnplify setup.cfg/py

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-import sys
 import setuptools
-from distutils.version import LooseVersion
-
-if LooseVersion(setuptools.__version__) < "30.3":
-    sys.stderr.write("ERROR: setuptools 30.3 or later is required by gammapy\n")
-    sys.exit(1)
-
-# TODO: check if setuptools_scm, numpy, ... are OK
-# Exit with good error message telling people to install those first if not
-
-
 from Cython.Build import cythonize
 from distutils.extension import Extension
 import numpy as np
