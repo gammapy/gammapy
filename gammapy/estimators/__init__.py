f393c301a68a543c9e7a806deff27f12eed1768c
Bruno Khelifi <khelifi@khelifi@apc.in2p3.fr>
2020-07-09 10:15:33 +0200

    Update the init function

--- gammapy/estimators/__init__.py
+++ gammapy/estimators/__init__.py
@@ -1,6 +1,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Estimators."""
+from gammapy.utils.registry import Registry
 from .asmooth_map import *
+from .core import *
 from .excess_map import *
 from .flux_point import *
 from .lightcurve import *
@@ -8,3 +10,20 @@ from .sensitivity import *
 from .ts_map import *
 from .image_profile import *
 from .map_profile import *
+
+ESTIMATOR_REGISTRY = Registry(
+    [
+        ExcessMapEstimator,
+        TSMapEstimator,
+        ASmoothMapEstimator,
+        FluxPointsEstimator,
+        LightCurveEstimator,
+        SensitivityEstimator,
+        ExcessProfileEstimator,
+        TSMapEstimator,
+]
+)
+"""Registry of estimator classes in Gammapy."""
+
+__all__ = ["ESTIMATOR_REGISTRY", "FluxPoints", "LightCurve", "ImageProfile", "Estimator"]
+__all__.extend(cls.__name__ for cls in ESTIMATOR_REGISTRY)
