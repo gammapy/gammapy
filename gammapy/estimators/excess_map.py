8b9e5ed9382c788781464dab1102fa260ac93f4a
Terrier <rterrier@apc.in2p3.fr>
2020-09-03 16:46:40 +0200

    Removed returimage option

--- gammapy/estimators/excess_map.py
+++ gammapy/estimators/excess_map.py
@@ -18,9 +18,7 @@ __all__ = [
 log = logging.getLogger(__name__)
 
 
-def convolved_map_dataset_counts_statistics(
-    dataset, kernel, apply_mask_fit=False, return_image=False
-):
+def convolved_map_dataset_counts_statistics(dataset, kernel, apply_mask_fit=False):
     """Return CountsDataset objects containing smoothed maps from the MapDataset"""
     # Kernel is modified later make a copy here
     kernel = copy.deepcopy(kernel)
@@ -35,8 +33,7 @@ def convolved_map_dataset_counts_statistics(
     # fft convolution adds numerical noise, to ensure integer results we call
     # np.rint
     n_on = dataset.counts * mask
-    if return_image:
-        n_on = n_on.sum_over_axes(keepdims=True)
+    n_on = n_on.sum_over_axes(keepdims=True)
     n_on_conv = np.rint(n_on.convolve(kernel.array).data)
 
     if isinstance(dataset, MapDatasetOnOff):
@@ -44,9 +41,8 @@ def convolved_map_dataset_counts_statistics(
         background.data[dataset.acceptance_off.data == 0] = 0.0
         n_off = dataset.counts_off * mask
 
-        if return_image:
-            background = background.sum_over_axes(keepdims=True)
-            n_off = n_off.sum_over_axes(keepdims=True)
+        background = background.sum_over_axes(keepdims=True)
+        n_off = n_off.sum_over_axes(keepdims=True)
 
         background_conv = background.convolve(kernel.array)
         n_off_conv = n_off.convolve(kernel.array)
@@ -57,8 +53,7 @@ def convolved_map_dataset_counts_statistics(
         return WStatCountsStatistic(n_on_conv.data, n_off_conv.data, alpha_conv.data)
     else:
         npred = dataset.npred() * mask
-        if return_image:
-            npred = npred.sum_over_axes(keepdims=True)
+        npred = npred.sum_over_axes(keepdims=True)
         background_conv = npred.convolve(kernel.array)
         return CashCountsStatistic(n_on_conv.data, background_conv.data)
 
@@ -86,12 +81,10 @@ class ExcessMapEstimator(Estimator):
 
         By default all additional quantities are estimated.
     e_edges : `~astropy.units.Quantity`
-        Energy edges of the maps bins.
+        Energy edges of the target excess maps bins.
     apply_mask_fit : Bool
         Apply a mask for the computation.
         A `~gammapy.datasets.MapDataset.mask_fit` must be present on the input dataset
-    return_image : Bool
-        Reduce the input dataset to a 2D image and perform the computations.
     """
 
     tag = "ExcessMapEstimator"
@@ -111,7 +104,6 @@ class ExcessMapEstimator(Estimator):
         self.n_sigma = n_sigma
         self.n_sigma_ul = n_sigma_ul
         self.apply_mask_fit = apply_mask_fit
-        self.return_image = return_image
         self.selection_optional = selection_optional
         self.e_edges = e_edges
 
@@ -192,14 +184,10 @@ class ExcessMapEstimator(Estimator):
         size = self.correlation_radius.deg / pixel_size
         kernel = Tophat2DKernel(size)
 
-        geom = dataset.counts.geom
-
         counts_stat = convolved_map_dataset_counts_statistics(
-            dataset, kernel, self.apply_mask_fit, self.return_image
-        )
+            dataset, kernel, self.apply_mask_fit)
 
-        if self.return_image:
-            geom = dataset.counts.geom.squash("energy")
+        geom = dataset.counts.geom.squash("energy")
 
         n_on = Map.from_geom(geom, data=counts_stat.n_on)
         bkg = Map.from_geom(geom, data=counts_stat.n_on - counts_stat.excess)
@@ -216,8 +204,7 @@ class ExcessMapEstimator(Estimator):
 
         if dataset.exposure:
             reco_exposure = estimate_exposure_reco_energy(dataset)
-            if self.return_image:
-                reco_exposure = reco_exposure.sum_over_axes(keepdims=True)
+            reco_exposure = reco_exposure.sum_over_axes(keepdims=True)
             flux = excess / reco_exposure
             flux.quantity = flux.quantity.to("1 / (cm2 s)")
         else:
--- gammapy/estimators/tests/test_excess_map.py
+++ gammapy/estimators/tests/test_excess_map.py
@@ -125,7 +125,7 @@ def test_significance_map_estimator_map_dataset(simple_dataset):
 
 
 def test_significance_map_estimator_map_dataset_on_off(simple_dataset_on_off):
-    estimator = ExcessMapEstimator(0.11 * u.deg, selection_optional=None)
+    estimator = ExcessMapEstimator(0.11 * u.deg, selection_optional=None, e_edges=[0.1*u.TeV, 1*u.TeV, 10*u.TeV])
     result = estimator.run(simple_dataset_on_off)
 
     assert result["counts"].data.shape == (2, 20, 20)
@@ -134,7 +134,7 @@ def test_significance_map_estimator_map_dataset_on_off(simple_dataset_on_off):
     assert_allclose(result["background"].data[:, 10, 10], 97)
     assert_allclose(result["significance"].data[:, 10, 10], 5.741116, atol=1e-5)
 
-    estimator_image = ExcessMapEstimator(0.11 * u.deg, return_image=True, e_edges=[0.1*u.TeV, 1*u.TeV])
+    estimator_image = ExcessMapEstimator(0.11 * u.deg, e_edges=[0.1*u.TeV, 1*u.TeV])
     result_image = estimator_image.run(simple_dataset_on_off)
     assert result_image["counts"].data.shape == (1, 20, 20)
     assert_allclose(result_image["significance"].data[0, 10, 10], 5.741116, atol=1e-3)
@@ -146,9 +146,7 @@ def test_significance_map_estimator_map_dataset_on_off(simple_dataset_on_off):
     mask_fit.data[:, :, 10] = False
     mask_fit.data[:, 10, :] = False
     simple_dataset_on_off.mask_fit = mask_fit
-    estimator_image = ExcessMapEstimator(
-        0.11 * u.deg, apply_mask_fit=True, return_image=True
-    )
+    estimator_image = ExcessMapEstimator(0.11 * u.deg, apply_mask_fit=True)
     simple_dataset_on_off.exposure.data = (
         np.ones(simple_dataset_on_off.exposure.data.shape) * 1e6
     )
