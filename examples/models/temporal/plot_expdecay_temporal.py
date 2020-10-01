1c5bdb42c7406b7198a6978ab7dee41166030560
Jaleleddine <hajlaouijalel.ig@gmail.com>
2020-10-01 11:49:40 +0200

    added plot expdecay temporal file

---
+++ examples/models/temporal/plot_expdecay_temporal.py
@@ -0,0 +1,46 @@
+r"""
+.. _constant-temporal-model:
+
+ExpDecay Temporal Model
+=======================
+
+This model parametrises an ExpDecay time model.
+
+
+"""
+
+# %%
+# Example plot
+# ------------
+# Here is an example plot of the model:
+
+from astropy import units as u
+from astropy.time import Time
+import matplotlib.pyplot as plt
+from gammapy.modeling.models import Models, ExpDecayTemporalModel, SkyModel
+
+t0= "5 h"
+t_ref = Time("2020-10-01")
+time_range = [t_ref, t_ref + 1 * u.d]
+model = ExpDecayTemporalModel(t_ref = t_ref.mjd * u.d, t0 = t0)
+model.plot(time_range)
+plt.grid(which="both")
+
+
+
+
+# %%
+# YAML representation
+# -------------------
+# Here is an example YAML file using the model:
+
+#model = SkyModel(spectral_model=model, name="power-law-model")
+#models = Models([model])
+
+#print(models.to_yaml())
+
+
+
+
+
+
