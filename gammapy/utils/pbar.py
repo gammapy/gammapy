481c1490affb226197685dc6e8c8ef0c8e9b6262
luca-giunti <giunti@apc.in2p3.fr>
2020-10-11 16:24:39 +0200

    Add pbar.py in gammapy.utils

---
+++ gammapy/utils/pbar.py
@@ -0,0 +1,16 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""Utilities for progress bar display"""
+from contextlib import contextmanager
+from tqdm import tqdm
+
+
+@contextmanager
+def pbar(total=None, show_pbar=False):
+    class dummy():
+        def update(self, x):
+            pass
+
+    if total == None and show_pbar == True:
+        raise AttributeError("Can't set up the progress bar if total is None")
+
+    yield tqdm(total=total, mininterval=0) if show_pbar == True else dummy()
