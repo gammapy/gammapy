732a7ffac23181429d84801fd8fd7c0771adcde9
Axel Donath <axel.donath@mpi-hd.mpg.de>
2021-03-22 14:25:50 +0100

    Fix EnergyDependentMultiGaussPSF example

--- gammapy/irf/psf/parametric.py
+++ gammapy/irf/psf/parametric.py
@@ -292,7 +292,7 @@ class EnergyDependentMultiGaussPSF(ParametricPSF):
         from gammapy.irf import EnergyDependentMultiGaussPSF
         filename = '$GAMMAPY_DATA/cta-1dc/caldb/data/cta/1dc/bcf/South_z20_50h/irf_file.fits'
         psf = EnergyDependentMultiGaussPSF.read(filename, hdu='POINT SPREAD FUNCTION')
-        psf.plot_containment(0.68)
+        psf.plot_containment_radius(fraction=0.68)
         plt.show()
     """
     tag = "psf_3gauss"
