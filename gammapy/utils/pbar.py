2b59ee680bea26192a9dea079531f72c09a66191
luca-giunti <giunti@apc.in2p3.fr>
2021-05-07 11:51:41 +0200

    cleanup

--- gammapy/utils/pbar.py
+++ gammapy/utils/pbar.py
@@ -6,20 +6,14 @@ try:
     from tqdm import tqdm
 except ImportError:
     class tqdm():
-        def __init__(
-                self,
-                total=0,
-                mininterval=0,
-                disable=True,
-                desc=""
-        ):
+        def __init__(self, *args, **kwargs):
             pass
         def update(self, x):
             pass
 
 @contextmanager
 def pbar(total=None, show_pbar=True, desc=None):
-    if total == None and show_pbar == True:
+    if total is None and show_pbar == True:
         raise AttributeError("Can't set up the progress bar if total is None")
 
     yield tqdm(total=total, mininterval=0, disable=not show_pbar, desc=desc)
