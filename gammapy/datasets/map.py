af78a3273c9ea86787999ce905abab61744a75a8
Quentin Remy <quentin.remy@live.fr>
2021-05-13 14:42:31 +0200

    fix

--- gammapy/datasets/map.py
+++ gammapy/datasets/map.py
@@ -2563,8 +2563,6 @@ class MapEvaluator:
     @property
     def needs_update(self):
         """Check whether the model component has drifted away from its support."""
-        values = self.model.spatial_model.parameters.value
-        spaial_changed = ~np.all(self._cached_parameter_values_spatial == values)
         # TODO: simplify and clean up
         if isinstance(self.model, TemplateNPredModel):
             return False
@@ -2576,7 +2574,10 @@ class MapEvaluator:
             return False
         elif len(self.model.spatial_model.parameters.free_parameters) == 0:
             return False
-        elif not spaial_changed:
+        elif self.model.spatial_model is not None and np.all(
+            self._cached_parameter_values_spatial
+            == self.model.spatial_model.parameters.value
+        ):
             return False
         else:
             # Here we do not use SkyCoord.separation to improve performance
