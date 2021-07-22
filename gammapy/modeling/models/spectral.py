ea0b38ed1b29ee0df85425b6d9bb4bfad1514930
Quentin Remy <quentin.remy@live.fr>
2021-07-22 02:57:50 +0200

    "scale10" for Naima

--- gammapy/modeling/models/spectral.py
+++ gammapy/modeling/models/spectral.py
@@ -1762,6 +1762,9 @@ class NaimaSpectralModel(SpectralModel):
             parameters.append(Parameter("B", B))
             parameters.append(Parameter("radius", radius, frozen=True))
 
+        for p in parameters:
+            p.scaler = "scale10"
+
         self.default_parameters = Parameters(parameters)
         super().__init__()
 
