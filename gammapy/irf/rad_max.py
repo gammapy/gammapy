4c20008dc6edabce91640dcc3362568734e46c53
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-10-06 11:23:17 +0200

    Update docstring of RadMax2d

--- gammapy/irf/rad_max.py
+++ gammapy/irf/rad_max.py
@@ -2,7 +2,24 @@ from .core import IRF
 
 
 class RadMax2D(IRF):
-    """small class to exploit the IRF class to directly read in the FITS file the
-    RAD_MAX hud, this has identical axes to the EFFECTIVE AREA HDU"""
+    """2D Rad Max table.
+
+    This is not directly a IRF component but is needed as additional information
+    for point-like IRF components when an energy or field of view
+    dependent directional cut has been applied.
+
+    Data format specification: :ref:`gadf:rad_max_2d`
+
+    Parameters
+    ----------
+    energy_axis : `MapAxis`
+        Reconstructed energy axis
+    offset_axis : `MapAxis`
+        Field of view offset axis.
+    data : `~astropy.units.Quantity`
+        Applied directional cut
+    meta : dict
+        Meta data
+    """
     tag = "rad_max_2d"
     required_axes = ["energy", "offset"]
