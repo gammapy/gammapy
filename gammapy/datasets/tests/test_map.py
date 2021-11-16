dc996bceb2bcb461fa3250a3e300921d863e534f
Quentin Remy <quentin.remy@live.fr>
2021-11-17 00:21:07 +0100

    add npred_background tests

--- gammapy/datasets/tests/test_map.py
+++ gammapy/datasets/tests/test_map.py
@@ -895,7 +895,7 @@ def test_stack(sky_model):
 
 
 @requires_data()
-def test_npred_sig(sky_model, geom, geom_etrue):
+def test_npred(sky_model, geom, geom_etrue):
     dataset = get_map_dataset(geom, geom_etrue)
 
     pwl = PowerLawSpectralModel()
@@ -910,14 +910,24 @@ def test_npred_sig(sky_model, geom, geom_etrue):
     assert_allclose(
         dataset.npred_signal(model_name=model1.name).data.sum(), 150.7487, rtol=1e-3
     )
+    assert dataset._background_cached is None
+    assert_allclose(dataset.npred_background().data.sum(), 4000., rtol=1e-3)
+    assert_allclose(dataset._background_cached.data.sum(), 4000., rtol=1e-3)
+
     assert_allclose(dataset.npred().data.sum(), 9676.047906, rtol=1e-3)
     assert_allclose(dataset.npred_signal().data.sum(), 5676.04790, rtol=1e-3)
+
+    bkg.spectral_model.norm.value = 1.1
+    assert_allclose(dataset.npred_background().data.sum(), 4400., rtol=1e-3)
+    assert_allclose(dataset._background_cached.data.sum(), 4400., rtol=1e-3)
+
 
     with pytest.raises(
         KeyError,
         match="m2",
     ):
         dataset.npred_signal(model_name="m2")
+
 
 
 def test_stack_npred():
