840136d6ec3ab8d6f9a8a3ff0445af8ddb2121dc
lauraolivera <laura.olivera-nieto@mpi-hd.mpg.de>
2022-01-20 19:11:45 +0100

    get psf kernel

--- gammapy/irf/psf/map.py
+++ gammapy/irf/psf/map.py
@@ -597,11 +597,11 @@ class PSFKernelMap(IRFMap):
         return cls.from_gauss(energy_axis_true, psf_lon_axis, psf_lat_axis, sigma=0.1 * u.deg, geom=geom.to_image())
 
     def get_psf_kernel(
-        self, geom, position=None, max_radius=None, containment=0.999, factor=4
+        self, geom=None, position=None,
     ):
-        """Returns a PSF kernel at the given position.
+        """Returns the PSF kernel at the given position.
 
-        The PSF is returned in the form a WcsNDMap defined by the input Geom.
+        The PSF is returned in the form of a PSFKernel which holds a WcsNDMap defined by the input Geom.
 
         Parameters
         ----------
@@ -609,15 +609,7 @@ class PSFKernelMap(IRFMap):
             Target geometry to use
         position : `~astropy.coordinates.SkyCoord`
             Target position. Should be a single coordinate. By default the
-            center position is used.
-        max_radius : `~astropy.coordinates.Angle`
-            maximum angular size of the kernel map
-        containment : float
-            Containment fraction to use as size of the kernel. The max. radius
-            across all energies is used. The radius can be overwritten using
-            the `max_radius` argument.
-        factor : int
-            oversampling factor to compute the PSF
+            center position of the input geom is used.
 
         Returns
         -------
@@ -629,30 +621,25 @@ class PSFKernelMap(IRFMap):
 
         position = self._get_nearest_valid_position(position)
 
-        if max_radius is None:
-            energy_axis = self.psf_kernel_map.geom.axes["energy_true"]
+        kernel_data = self.psf_kernel_map.to_region_nd_map(region=position)
 
-            radii = self.containment_radius(
-                fraction=containment, position=position, energy_true=energy_axis.center
-            )
-            max_radius = np.max(radii)
+        width = 2*np.max(np.abs([kernel_data.geom.axes['psf_lon'].edges.min().to_value('deg'),
+                      kernel_data.geom.axes['psf_lat'].edges.min().to_value('deg'),
+                      kernel_data.geom.axes['psf_lon'].edges.max().to_value('deg'),
+                      kernel_data.geom.axes['psf_lat'].edges.max().to_value('deg')]))
 
-        geom = geom.to_odd_npix(max_radius=max_radius)
-        geom_upsampled = geom.upsample(factor=factor)
-        coords = geom_upsampled.get_coord(sparse=True)
-        rad = coords.skycoord.separation(geom.center_skydir)
 
-        coords = {
-            "energy_true": coords["energy_true"],
-            "rad": rad,
-            "skycoord": position,
-        }
+        kernel_geom = WcsGeom.create(skydir=geom.center_skydir,
+                            binsz=(kernel_data.geom.axes['psf_lon'].bin_width,
+                                  kernel_data.geom.axes['psf_lat'].bin_width),
+                            width = width,
+                            axes = [kernel_data.geom.axes['energy_true']])
 
-        data = self.psf_map.interp_by_coord(
-            coords=coords,
-            method="linear",
-        )
 
-        kernel_map = Map.from_geom(geom=geom_upsampled, data=np.clip(data, 0, np.inf))
-        kernel_map = kernel_map.downsample(factor, preserve_counts=True)
+        kernel_map = Map.from_geom(kernel_geom, data = kernel_data.data[:,:,:,0,0])
+
+        if geom is not None:
+            kernel_map = kernel_map.interp_to_geom(geom)
+        kernel_map.data = np.clip(kernel_map.data,0,np.inf)
+
         return PSFKernel(kernel_map, normalize=True)
