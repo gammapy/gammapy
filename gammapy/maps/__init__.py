8d2216b843fec853374bd55fdb5a85081ac79089
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-01-27 16:47:33 +0100

    Fix pyflakes issues in gammapy.maps

--- gammapy/maps/__init__.py
+++ gammapy/maps/__init__.py
@@ -1,10 +1,38 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Sky maps."""
-from .axes import *
-from .coord import *
-from .core import *
-from .geom import *
-from .hpx import *
-from .maps import *
-from .region import *
-from .wcs import *
+from .axes import MapAxes, MapAxis, TimeMapAxis, LabelMapAxis
+from .coord import MapCoord
+from .core import Map
+from .geom import Geom
+from .hpx import HpxMap, HpxGeom, HpxNDMap
+from .maps import Maps
+from .region import RegionGeom, RegionNDMap
+from .wcs import WcsMap, WcsGeom, WcsNDMap
+
+
+__all__ = [
+    # axes
+    "MapAxes",
+    "MapAxis",
+    "TimeMapAxis",
+    "LabelMapAxis",
+    # coord
+    "MapCoord",
+    # core
+    "Map",
+    # geom
+    "Geom",
+    # hpx
+    "HpxMap",
+    "HpxGeom",
+    "HpxNDMap",
+    # maps
+    "Maps",
+    # region
+    "RegionGeom",
+    "RegionNDMap",
+    # wcs
+    "WcsMap",
+    "WcsGeom",
+    "WcsNDMap",
+]
--- gammapy/maps/region/__init__.py
+++ gammapy/maps/region/__init__.py
@@ -1,2 +1,7 @@
-from .geom import *
-from .ndmap import *
+from .geom import RegionGeom
+from .ndmap import RegionNDMap
+
+__all__ = [
+   "RegionGeom",
+   "RegionNDMap",
+]
