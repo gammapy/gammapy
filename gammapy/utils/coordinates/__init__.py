086bc6061568156b7e8075e676d9c6ab0ddf1b91
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-01-27 16:20:49 +0100

    Fix pyflakes issues in gammapy.utils

--- gammapy/utils/coordinates/__init__.py
+++ gammapy/utils/coordinates/__init__.py
@@ -1,4 +1,22 @@
 """Astronomical coordinate calculation utility functions.
 """
-from .fov import *
-from .other import *
+from .fov import fov_to_sky, sky_to_fov
+from .other import (
+    cartesian,
+    galactic,
+    velocity_glon_glat,
+    motion_since_birth,
+    polar,
+    D_SUN_TO_GALACTIC_CENTER,
+)
+
+__all__ = [
+    "fov_to_sky",
+    "sky_to_fov",
+    "cartesian",
+    "galactic",
+    "velocity_glon_glat",
+    "motion_since_birth",
+    "polar",
+    "D_SUN_TO_GALACTIC_CENTER",
+]
--- gammapy/utils/random/__init__.py
+++ gammapy/utils/random/__init__.py
@@ -1,3 +1,25 @@
 """Random probability distribution helpers."""
-from .inverse_cdf import *
-from .utils import *
+from .inverse_cdf import InverseCDFSampler
+from .utils import (
+    get_random_state,
+    sample_sphere,
+    sample_sphere_distance,
+    sample_powerlaw,
+    sample_times,
+    normalize,
+    draw,
+    pdf,
+)
+
+
+__all__ = [
+    "InverseCDFSampler",
+    "get_random_state",
+    "sample_sphere",
+    "sample_sphere_distance",
+    "sample_powerlaw",
+    "sample_times",
+    "normalize",
+    "draw",
+    "pdf",
+]
