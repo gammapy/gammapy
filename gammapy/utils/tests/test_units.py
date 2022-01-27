b8f9c5eab0feaca480fc46503f343d28529f87bf
facero <fabacero@gmail.com>
2022-01-27 10:48:06 +0100

    Human readable enrgy units string formatting for plot_interactive & plot_grid

--- gammapy/utils/tests/test_units.py
+++ gammapy/utils/tests/test_units.py
@@ -1,8 +1,30 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-from gammapy.utils.units import standardise_unit
-
+from gammapy.utils.units import standardise_unit, energy_unit_format, energy_str_formatting
+import astropy.units as u
+import numpy as np
+from gammapy.maps import MapAxis
 
 def test_standardise_unit():
     assert standardise_unit("ph cm-2 s-1") == "cm-2 s-1"
     assert standardise_unit("ct cm-2 s-1") == "cm-2 s-1"
     assert standardise_unit("cm-2 s-1") == "cm-2 s-1"
+
+def test_energy_str_formatting():
+    assert energy_str_formatting(1530e4 *u.keV) == "15.3 GeV"
+    assert energy_str_formatting(1530 *u.GeV) == "1.53 TeV"
+    assert energy_str_formatting(1530 *u.GeV) == "1.53 TeV"
+    assert energy_str_formatting(1530.5e8 *u.keV) == "153 TeV"
+
+def test_energy_unit_format():
+    E = 1.556e2 *u.TeV
+    assert energy_unit_format(E) == '156 TeV'
+    E = np.array([1e3,3.5e6,400.4e12])*u.eV
+    assert energy_unit_format(E) == ['1.00 keV', '3.50 MeV', '400 TeV']
+    E = [1.54e2*u.GeV, 4300*u.keV, 300.6e12*u.eV] 
+    assert energy_unit_format(E) == ['154 GeV', '4.30 MeV', '301 TeV']
+
+    axis = MapAxis.from_nodes([1e-1, 200, 3.5e3, 4.6e4], name="energy", unit="GeV")
+    assert energy_unit_format(axis.center) == ['100 MeV', '200 GeV', '3.50 TeV', '46.0 TeV']   
+    str_labels=axis.as_plot_labels
+    quant_labels=[u.Quantity(x) for x in str_labels ]
+    energy_unit_format(quant_labels) == ['100 MeV', '200 GeV', '3.50 TeV', '46.0 TeV']
--- gammapy/utils/units.py
+++ gammapy/utils/units.py
@@ -2,6 +2,7 @@
 """Units and Quantity related helper functions"""
 import logging
 import astropy.units as u
+import numpy as np
 
 __all__ = ["standardise_unit", "unit_from_fits_image_hdu"]
 
@@ -64,3 +65,62 @@ def unit_from_fits_image_hdu(header):
         unit = ""
 
     return standardise_unit(unit)
+
+def energy_str_formatting(E):
+    """
+    Format energy quantities to a string representation that is more comfortable to read
+    by swithcing to the most relevant unit (keV, MeV, GeV, TeV) and changing the float precision.
+    Parameters
+    ----------    
+    E: `~astropy.units.Quantity`
+        Quantity
+    Returns
+    -------
+    str : str
+        Returns a string with energy unit formatted        
+    """    
+    E.to('eV')
+    if (E >= 1e3*u.eV) and (E < 1e6*u.eV):
+        E = E.to('keV')
+    elif (E >= 1e6*u.eV) and (E < 1e9*u.eV):
+        E = E.to('MeV')
+    elif (E >= 1e9*u.eV) and (E < 1e12*u.eV):
+        E = E.to('GeV')
+    elif (E >= 1e12*u.eV) and (E < 1e15*u.eV):
+        E = E.to('TeV')
+    elif (E >= 1e15*u.eV):
+        E = E.to('PeV')
+
+    if E.value < 10:
+        prec=2
+    elif (E.value >= 10) and (E.value < 100):
+        prec=1
+    elif (E.value >= 100) and (E.value < 1000):
+        prec=0
+    
+    return f"{E.value:0.{prec}f} {E.unit}"
+
+def energy_unit_format(E):
+    """
+    Format an energy quantity (or a list of quantities) to a string (or list of string) representations.
+    Parameters
+    ----------    
+    E: `~astropy.units.Quantity`
+        Quantity or list of quantities
+    Returns
+    -------
+    str : str
+        Returns a string or list of strings with energy unit formatted        
+    """   
+
+    if (isinstance(E, (list, tuple,np.ndarray)) == False):
+        return energy_str_formatting(E)
+    #Additional test for scalar quantities like 1*u.TeV
+    if isinstance(E, u.quantity.Quantity):
+        if E.shape == ():
+           return energy_str_formatting(E)
+    
+    E_fmt = []     
+    for i, E_ in enumerate(E):
+        E_fmt.append(energy_str_formatting(E[i]))
+    return E_fmt
