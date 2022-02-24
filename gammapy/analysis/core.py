b0d966b0da3b75b137e0fb4ae21e62218e4b0974
Quentin Remy <quentin.remy@live.fr>
2022-02-24 12:15:27 +0100

    removed unused import

--- gammapy/analysis/core.py
+++ gammapy/analysis/core.py
@@ -1,6 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Session class driving the high level interface API"""
-import os
 import logging
 from astropy.coordinates import SkyCoord
 from astropy.table import Table
