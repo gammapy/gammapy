f5631c4c388d92316a4b19e7edac203ce87601e4
Atreyee Sinha <atreyeesinha@Atreyees-MacBook-Pro.local>
2022-04-04 16:08:34 +0200

    make OGIP R/W public

--- gammapy/datasets/__init__.py
+++ gammapy/datasets/__init__.py
@@ -4,6 +4,7 @@ from .flux_points import FluxPointsDataset
 from .map import MapDataset, MapDatasetOnOff, create_map_dataset_geoms
 from .simulate import MapDatasetEventSampler
 from .spectrum import SpectrumDataset, SpectrumDatasetOnOff
+from .io import OGIPDatasetWriter, OGIPDatasetReader
 
 DATASET_REGISTRY = Registry([MapDataset, SpectrumDatasetOnOff, FluxPointsDataset])
 """Registry of dataset classes in Gammapy."""
@@ -17,6 +18,8 @@ __all__ = [
     "MapDataset",
     "MapDatasetEventSampler",
     "MapDatasetOnOff",
+    "OGIPDatasetWriter",
+    "OGIPDatasetReader",
     "SpectrumDataset",
     "SpectrumDatasetOnOff",
 ]
