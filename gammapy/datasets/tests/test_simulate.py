3c4f639e804a1da3b38485f1e17ac0525976713b
Fabio Pintore <fabiopintore@MacBook-Pro-di-Fabio-2.local>
2022-05-03 12:32:42 +0200

    sort models internally in MapDatasetEventSampler and add test

--- gammapy/datasets/tests/test_simulate.py
+++ gammapy/datasets/tests/test_simulate.py
@@ -8,11 +8,13 @@ from astropy.io import fits
 from astropy.table import Table
 from astropy.time import Time
 from gammapy.data import GTI, DataStore, Observation
-from gammapy.datasets import MapDatasetEventSampler
+from gammapy.datasets import MapDatasetEventSampler, MapDataset
 from gammapy.datasets.tests.test_map import get_map_dataset
 from gammapy.irf import load_cta_irfs
+from gammapy.makers import MapDatasetMaker
 from gammapy.maps import MapAxis, WcsGeom
 from gammapy.modeling.models import (
+    Models,
     FoVBackgroundModel,
     GaussianSpatialModel,
     LightCurveTemplateTemporalModel,
@@ -56,6 +58,44 @@ def models():
     bkg_model = FoVBackgroundModel(dataset_name="test")
     return [model, bkg_model]
 
+@pytest.fixture()
+def model_alternative():
+    spatial_model1 = GaussianSpatialModel(
+        lon_0="0 deg", lat_0="0 deg", sigma="0.2 deg", frame="galactic"
+    )
+
+    spectral_model = PowerLawSpectralModel(amplitude="1e-11 cm-2 s-1 TeV-1")
+    
+    mod1 = SkyModel(
+        spatial_model=spatial_model1,
+        spectral_model=spectral_model,
+        name="test-source",
+    )
+
+    spatial_model2 = GaussianSpatialModel(
+        lon_0="0.5 deg", lat_0="0.5 deg", sigma="0.2 deg", frame="galactic"
+    )
+
+    mod2 = SkyModel(
+        spatial_model=spatial_model2,
+        spectral_model=spectral_model,
+        name="test-source2",
+    )
+
+    spatial_model3 = GaussianSpatialModel(
+        lon_0="0.5 deg", lat_0="0.0 deg", sigma="0.2 deg", frame="galactic"
+    )
+
+    mod3 = SkyModel(
+        spatial_model=spatial_model3,
+        spectral_model=spectral_model,
+        name="test-source3",
+    )
+
+    bkg_model = FoVBackgroundModel(dataset_name="test")
+
+    model2 = Models([mod1, mod2, bkg_model, mod3])
+    return model2
 
 @pytest.fixture(scope="session")
 def dataset():
@@ -292,8 +332,9 @@ def test_mde_run(dataset, models):
     assert meta["CONV_DEP"] == 0
     assert meta["CONV_RA"] == 0
     assert meta["CONV_DEC"] == 0
+    assert meta["MID00000"] == 0
+    assert meta["MMN00000"] == "test-bkg"
     assert meta["MID00001"] == 1
-    assert meta["MID00002"] == 2
     assert meta["NMCIDS"] == 2
     assert_allclose(float(meta["ALT_PNT"]), float("-13.5345076464"), rtol=1e-7)
     assert_allclose(float(meta["AZ_PNT"]), float("228.82981620065763"), rtol=1e-7)
@@ -379,3 +420,57 @@ def test_events_datastore(tmp_path, dataset, models):
     hdu_all.writeto(str(tmp_path / "events.fits"))
 
     DataStore.from_events_files([str(tmp_path / "events.fits")])
+
+@requires_data()
+def test_MC_ID(model_alternative):
+    irfs = load_cta_irfs(
+        "$GAMMAPY_DATA/cta-1dc/caldb/data/cta/1dc/bcf/South_z20_50h/irf_file.fits"
+    )
+    livetime = 0.1 * u.hr
+    pointing = SkyCoord(0, 0, unit="deg", frame="galactic")
+    obs = Observation.create(
+        obs_id=1001, pointing=pointing, livetime=livetime, irfs=irfs,
+        location=LOCATION,
+    )
+
+    energy_axis = MapAxis.from_energy_bounds(
+        "1.0 TeV", "10 TeV", nbin=10, per_decade=True
+        )
+    energy_axis_true = MapAxis.from_energy_bounds(
+        "0.5 TeV", "20 TeV", nbin=20, per_decade=True, name="energy_true"
+        )
+    migra_axis = MapAxis.from_bounds(
+        0.5, 2, nbin=150, node_type="edges", name="migra"
+        )
+
+    geom = WcsGeom.create(
+        skydir=pointing,
+        width=(2, 2),
+        binsz=0.06,
+        frame="icrs",
+        axes=[energy_axis],
+    )
+
+    empty = MapDataset.create(
+            geom,
+            energy_axis_true=energy_axis_true,
+            migra_axis=migra_axis,
+            name="test",
+                )
+    maker = MapDatasetMaker(selection=["exposure", "background", "psf", "edisp"])
+    dataset = maker.run(empty, obs)
+
+    dataset.models = model_alternative
+    sampler = MapDatasetEventSampler(random_state=0)
+    events = sampler.run(dataset=dataset, observation=obs)
+    
+    assert len(events.table) == 215
+    assert len(np.where(events.table["MC_ID"] == 0)[0]) == 40
+    
+    meta = events.table.meta
+    assert meta["MID00000"] == 0
+    assert meta["MMN00000"] == "test-bkg"
+    assert meta["MID00001"] == 1
+    assert meta["MID00002"] == 2
+    assert meta["MID00003"] == 3
+    assert meta["NMCIDS"] == 4
