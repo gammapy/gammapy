649019efc3bda2d86e5ac789bfac2c6e15cae093
Bruno Khelifi <khelifi@khelifi@apc.in2p3.fr>
2022-05-10 13:01:22 +0200

    Update the code in order to rationalise the creation process

--- dev/codemeta.py
+++ dev/codemeta.py
@@ -1,7 +1,23 @@
 """Utility script to work with the codemeta.json file."""
+""" A preliminary file is created with https://codemeta.github.io/codemeta-generator/ without the authors list. Then, one add the authors list stored in CITATION.cff."""
+
 import json
+import yaml
 
 FILENAME = "../codemeta.json"
+
+# Update the Author list
+CITATIONFILE = "../CITATION.cff"
+with open(CITATIONFILE, "r") as f:
+    citation = yaml.safe_load(f)
+for author in citation["authors"]:
+   author["@type"] = "Person"
+   author["givenName"] = author.pop("given-names")
+   author["familyName"] = author.pop("family-names")
+   if "orcid" in author:
+       author["@id"] = author.pop("orcid")
+   if "affiliation" in author:
+       author["affiliation"] = {"@type": "Organization", "name": author["affiliation"]}
 
 # add potentially missing content
 with open(FILENAME, "r") as f:
@@ -9,6 +25,8 @@ with open(FILENAME, "r") as f:
 data["maintainer"] = data["author"][0]
 data["readme"] = "https://gammapy.org"
 data["issueTracker"] = "https://github.com/gammapy/gammapy/issues"
+data["author"] = citation["authors"]
+data["identifier"] = citation["identifiers"]
 
 with open(FILENAME, "w") as f:
     json.dump(data, f, indent=4)
