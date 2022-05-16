324ef99cbaf7a788dbd54578b55116894872d611
vuillaut <thomas.vuillaume@gmail.com>
2022-05-16 09:30:29 +0200

    add action workflow to update citation.cff when doing a release

---
+++ dev/citationcff.py
@@ -0,0 +1,8 @@
+import yaml
+from pathlib import Path
+from datetime import date
+
+citation_path = Path(__file__).parent.joinpath('CITATION.cff')
+cita = yaml.safe_load(open(citation_path).read())
+cita['date-released'] = date.today()
+yaml.safe_dump(cita, open(citation_path, 'w'))
