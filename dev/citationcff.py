61aee48706563a3621c9521512a096411d82c7da
vuillaut <thomas.vuillaume@gmail.com>
2022-05-16 09:45:31 +0200

    fix path

--- dev/citationcff.py
+++ dev/citationcff.py
@@ -2,7 +2,7 @@ import yaml
 from pathlib import Path
 from datetime import date
 
-citation_path = Path(__file__).parent.joinpath('CITATION.cff')
+citation_path = Path(__file__).parents[1].joinpath('CITATION.cff')
 cita = yaml.safe_load(open(citation_path).read())
 cita['date-released'] = date.today()
 yaml.safe_dump(cita, open(citation_path, 'w'))
