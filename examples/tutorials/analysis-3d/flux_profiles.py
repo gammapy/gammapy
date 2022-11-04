e9d1931a918c6e4a1a70a19bf75aded9954d4596
Axel Donath <axel.donath@cfa.harvard.edu>
2022-11-04 17:05:13 -0400

    Adapt 3d tutorials

--- examples/tutorials/analysis-3d/flux_profiles.py
+++ examples/tutorials/analysis-3d/flux_profiles.py
@@ -36,17 +36,18 @@ run the actually profile extraction using the `~gammapy.estimators.FluxProfileEs
 """
 
 
-######################################################################
-# Setup
-# -----
-#
-
 import numpy as np
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 
 # %matplotlib inline
 import matplotlib.pyplot as plt
+
+######################################################################
+# Setup
+# -----
+#
+from IPython.display import display
 from gammapy.datasets import MapDataset
 from gammapy.estimators import FluxPoints, FluxProfileEstimator
 from gammapy.maps import RegionGeom
@@ -77,7 +78,6 @@ dataset = MapDataset.read(
 ######################################################################
 # This is what the counts image we will work with looks like:
 #
-plt.figure()
 counts_image = dataset.counts.sum_over_axes()
 counts_image.smooth("0.1 deg").plot(stretch="sqrt")
 
@@ -232,7 +232,7 @@ ax.set_yscale("linear")
 #
 
 table = profile.to_table(format="profile", formatted=True)
-table
+display(table)
 
 
 ######################################################################
@@ -299,3 +299,6 @@ fig, ax = plt.subplots()
 profile_high.plot(ax=ax, sed_type="eflux", color="tab:orange")
 profile_high.plot_ts_profiles(ax=ax, sed_type="eflux")
 ax.set_yscale("linear")
+
+
+plt.show()
