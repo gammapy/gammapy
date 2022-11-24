b81204c67b5efa9164e8fbc00fda43f331837ef7
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2022-11-24 15:53:39 +0100

    remove raise error
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/estimators/tests/test_flux.py
+++ gammapy/estimators/tests/test_flux.py
@@ -148,19 +148,6 @@ def test_flux_estimator_norm_range():
     assert scale_model.norm.interp == "log"
 
 
-def test_flux_estimator_norm_spectral_model():
-    energy = MapAxis.from_energy_bounds(0.1, 10, 3.0, unit="TeV", name="energy_true")
-    template = WcsNDMap.create(npix=10, axes=[energy], unit="cm-2 s-1 sr-1 TeV-1")
-    spatial = TemplateSpatialModel(template, normalize=False)
-    spectral = PowerLawNormSpectralModel()
-    model = SkyModel(spectral_model=spectral, spatial_model=spatial, name="test")
-
-    estimator = FluxEstimator(source="test", selection_optional=[], reoptimize=True)
-
-    with pytest.raises(ValueError, match="`NormSpectralModel` are not supported"):
-        estimator.get_scale_model(Models([model]))
-
-
 def test_flux_estimator_compound_model():
     pl = PowerLawSpectralModel()
     pl.amplitude.min = 1e-15
