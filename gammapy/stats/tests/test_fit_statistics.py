8d0fc8e759c3f79cbefbe8e9f244bd4fa759ac85
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2022-12-07 23:41:07 +0100

    remove unused import
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/stats/tests/test_fit_statistics.py
+++ gammapy/stats/tests/test_fit_statistics.py
@@ -3,7 +3,6 @@ import pytest
 import numpy as np
 from numpy.testing import assert_allclose
 from gammapy import stats
-from gammapy.utils.testing import requires_data
 
 
 @pytest.fixture(scope="session")
