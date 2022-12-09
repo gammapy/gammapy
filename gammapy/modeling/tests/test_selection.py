31b2b90763509cb48710eed8a451defc82efd699
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2022-12-09 09:11:33 +0100

    speed up test
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/modeling/tests/test_selection.py
+++ gammapy/modeling/tests/test_selection.py
@@ -1,8 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import pytest
 from numpy.testing import assert_allclose
+from gammapy.modeling.fit import Fit
+from gammapy.modeling.selection import TestStatisticNested
 from gammapy.utils.testing import requires_data
-from gammapy.modeling.test_statistic import TestStatisticNested
 
 
 @pytest.fixture(scope="session")
@@ -33,10 +34,15 @@ def test_test_statistic_link(fermi_datasets):
     model2.spectral_model.alpha.value = 2.4
     fermi_datasets.models = fermi_datasets.models + [model2]
 
+    fit = Fit()
+    minuit_opts = {"tol": 10, "strategy": 0}
+    fit.backend = "minuit"
+    fit.optimize_opts = minuit_opts
+
     ts_eval = TestStatisticNested(
-        [model.spectral_model.alpha], [model2.spectral_model.alpha]
+        [model.spectral_model.alpha], [model2.spectral_model.alpha], fit=fit
     )
     ts = ts_eval.run(fermi_datasets)
 
-    assert_allclose(ts, -3.602895, rtol=1e-5)
+    assert_allclose(ts, 0.003118, rtol=1e-4)
     assert_allclose(model2.spectral_model.alpha.value, model.spectral_model.alpha.value)
