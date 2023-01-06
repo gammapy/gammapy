fcaaf997be9e8ba19e80de5780c7369d4164fd4f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-06 02:13:36 +0000

    Adding tarfile member sanitization to extractall()

--- gammapy/scripts/download.py
+++ gammapy/scripts/download.py
@@ -96,7 +96,29 @@ def members(tf):
 
 def extract_bundle(bundle, destination):
     with tarfile.open(bundle) as tar:
-        tar.extractall(path=destination, members=members(tar))
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tar, path=destination, members=members(tar))
 
 
 def show_info_notebooks(outfolder, release):
