e79e4121648a2fbe59acf9e88c3918d4be33591f
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2023-02-23 11:04:17 +0100

    Add helper functions for delta TS to significance conversion
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

---
+++ gammapy/stats/utils.py
@@ -0,0 +1,16 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+import numpy as np
+from scipy.stats import chi2
+
+
+def sigma_to_ts(n_sigma, df=1):
+    """Convert sigma to delta ts"""
+    p_value = chi2.sf(n_sigma**2, df=1)
+    return chi2.isf(p_value, df=df)
+
+
+def ts_to_sigma(ts, df=1):
+    """Convert delta ts to sigma"""
+    p_value = chi2.sf(ts, df=df)
+    return np.sqrt(chi2.isf(p_value, df=1))
