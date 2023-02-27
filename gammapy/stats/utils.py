3a42191d08e3ba5f84ab05f6e0c28ec759b03e78
Quentin Remy <quentin.remy@live.fr>
2023-02-27 20:08:18 +0100

    Apply suggestions for docstring
    Co-authored-by: Régis Terrier <regis.terrier@m4x.org>

--- gammapy/stats/utils.py
+++ gammapy/stats/utils.py
@@ -5,12 +5,12 @@ from scipy.stats import chi2
 
 
 def sigma_to_ts(n_sigma, df=1):
-    """Convert sigma to delta ts"""
+    """Convert number of sigma to delta ts."""
     p_value = chi2.sf(n_sigma**2, df=1)
     return chi2.isf(p_value, df=df)
 
 
 def ts_to_sigma(ts, df=1):
-    """Convert delta ts to sigma"""
+    """Convert delta ts to number of sigma"""
     p_value = chi2.sf(ts, df=df)
     return np.sqrt(chi2.isf(p_value, df=1))
