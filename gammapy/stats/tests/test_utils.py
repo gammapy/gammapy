4e8af8ddf6b7418088bd096f5277674ae66540ff
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2023-03-01 13:30:30 +0100

    improve test
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/stats/tests/test_utils.py
+++ gammapy/stats/tests/test_utils.py
@@ -6,13 +6,16 @@ from gammapy.stats.utils import sigma_to_ts, ts_to_sigma
 def test_sigma_ts_conversion():
 
     sigma_ref = 3
+    ts_ref = 9
     df = 1
     ts = sigma_to_ts(3, df=df)
-    assert_allclose(ts, sigma_ref**2.0)
+    assert_allclose(ts, ts_ref)
     sigma = ts_to_sigma(ts, df=df)
     assert_allclose(sigma, sigma_ref)
 
     df = 2
+    ts_ref = 11.829158
     ts = sigma_to_ts(3, df=df)
+    assert_allclose(ts, ts_ref)
     sigma = ts_to_sigma(ts, df=df)
     assert_allclose(sigma, sigma_ref)
