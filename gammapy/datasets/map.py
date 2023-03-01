1109412276d850a5279f8126601dfefd2ae0b0e7
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2023-03-01 20:20:55 +0100

    faster alpha computation
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/datasets/map.py
+++ gammapy/datasets/map.py
@@ -2061,10 +2061,9 @@ class MapDatasetOnOff(MapDataset):
             Alpha map
         """
         with np.errstate(invalid="ignore", divide="ignore"):
-            alpha = self.acceptance / self.acceptance_off
-
-        alpha.data = np.nan_to_num(alpha.data)
-        return alpha
+            data = self.acceptance.quantity / self.acceptance_off.quantity
+        data = np.nan_to_num(data)
+        return Map.from_geom(self._geom, data=data.value, unit=data.unit)
 
     def npred_background(self):
         """Predicted background counts estimated from the marginalized likelihood estimate.
