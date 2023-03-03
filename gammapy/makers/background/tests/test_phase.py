2103f027f9b9f277b0f19fdf189ae0e7c3de463d
Maxime Regeard <regeard@apc.in2p3.fr>
2023-03-03 16:01:55 +0100

    add test rad max for PhaseBackgroundMaker
    Signed-off-by: Maxime Regeard <regeard@apc.in2p3.fr>

--- gammapy/makers/background/tests/test_phase.py
+++ gammapy/makers/background/tests/test_phase.py
@@ -4,7 +4,8 @@ import numpy as np
 from numpy.testing import assert_allclose
 from astropy import units as u
 from astropy.coordinates import Angle, SkyCoord
-from gammapy.data import DataStore
+from regions import PointSkyRegion
+from gammapy.data import DataStore, EventList
 from gammapy.datasets import MapDataset, SpectrumDataset
 from gammapy.makers import MapDatasetMaker, PhaseBackgroundMaker, SpectrumDatasetMaker
 from gammapy.maps import MapAxis, RegionGeom, WcsGeom
@@ -15,8 +16,11 @@ from gammapy.utils.testing import requires_data
 @pytest.fixture(scope="session")
 def observations():
     """Example observation list for testing."""
-    datastore = DataStore.from_dir("$GAMMAPY_DATA/cta-1dc/index/gps")
-    return datastore.get_observations([111630])
+    datastore_cta = DataStore.from_dir("$GAMMAPY_DATA/cta-1dc/index/gps")
+    datastore_magic = DataStore.from_dir("$GAMMAPY_DATA/magic/rad_max/data")
+    observations = datastore_cta.get_observations([111630])
+    observations.append(datastore_magic.get_observations(required_irf="point-like")[0])
+    return observations
 
 
 @pytest.fixture(scope="session")
@@ -79,6 +83,66 @@ def test_run_map(observations, phase_bkg_maker):
 
     assert_allclose(dataset_on_off.counts.data.sum(), 78)
     assert_allclose(dataset_on_off.counts_off.data.sum(), 263)
+
+
+@requires_data()
+def test_rad_max_counts(observations, phase_bkg_maker):
+
+    maker = SpectrumDatasetMaker(
+        containment_correction=False, selection=["counts", "exposure", "edisp"]
+    )
+
+    e_reco = MapAxis.from_energy_bounds(
+        0.05, 100, nbin=5, per_decade=True, unit="TeV", name="energy"
+    )
+    e_true = MapAxis.from_energy_bounds(
+        0.01, 300, nbin=10, per_decade=True, unit="TeV", name="energy_true"
+    )
+
+    pos = SkyCoord(083.6331144560900, +22.0144871383400, frame="icrs", unit="deg")
+    on_region = PointSkyRegion(pos)
+
+    geom = RegionGeom.create(region=on_region, axes=[e_reco])
+    dataset_empty = SpectrumDataset.create(geom=geom, energy_axis_true=e_true)
+
+    obs = observations["5029747"]
+    table = obs.events.table
+    table["PHASE"] = np.linspace(0, 1, len(table["TIME"]))
+    obs._events = EventList(table)
+
+    dataset = maker.run(dataset_empty, obs)
+    dataset_on_off = phase_bkg_maker.run(dataset, obs)
+
+    counts_on = np.reshape(
+        np.array([30, 16, 14, 5, 10, 4, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0]), (17, 1, 1)
+    )
+    counts_off = np.reshape(
+        np.array(
+            [
+                73,
+                61,
+                32,
+                19,
+                20,
+                9,
+                9,
+                0,
+                2,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+            ]
+        ),
+        (17, 1, 1),
+    )
+
+    assert_allclose(dataset_on_off.counts.data, counts_on)
+    assert_allclose(dataset_on_off.counts_off.data, counts_off)
 
 
 @pytest.mark.parametrize(
