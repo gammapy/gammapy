05800c8ce7080ed4cdcd200be6249efe6ec28d74
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2023-03-23 16:05:40 +0100

    add requires_dependency
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/utils/tests/test_parallel.py
+++ gammapy/utils/tests/test_parallel.py
@@ -1,5 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import gammapy.utils.parallel as parallel
+from gammapy.utils.testing import requires_dependency
 
 
 def test_get_multiprocessing():
@@ -8,6 +9,7 @@ def test_get_multiprocessing():
     assert multiprocessing.__name__ == "multiprocessing"
 
 
+@requires_dependency("ray")
 def test_get_multiprocessing_ray():
     parallel.MULTIPROCESSING_BACKEND = "ray"
     multiprocessing = parallel.get_multiprocessing()
