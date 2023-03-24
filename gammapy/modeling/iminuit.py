5d0c3a492a928f93788908ff15fd24efe8fe5eed
Quentin Remy <quentin.remy@live.fr>
2023-03-24 11:05:44 +0100

    Update gammapy/modeling/iminuit.py

--- gammapy/modeling/iminuit.py
+++ gammapy/modeling/iminuit.py
@@ -175,7 +175,7 @@ def contour_iminuit(parameters, function, x, y, numpoints, sigma, **kwargs):
     }
 
 
-# This code is a copied from https://github.com/scikit-hep/iminuit/blob/v2.21.0/src/iminuit/minimize.py#L124-L136
+# This code is copied from https://github.com/scikit-hep/iminuit/blob/v2.21.0/src/iminuit/minimize.py#L124-L136
 def _get_message(m, parameters):
     success = m.valid
     success &= np.all(np.isfinite([par.value for par in parameters]))
