2f629aefd1a629d9492b329b2533f008e530410e
Atreyee Sinha <asinha@ucm.es>
2023-03-31 20:04:28 +0200

    add test
    Signed-off-by: Atreyee Sinha <asinha@ucm.es>

--- gammapy/estimators/points/tests/test_sensitivity.py
+++ gammapy/estimators/points/tests/test_sensitivity.py
@@ -2,7 +2,7 @@
 import pytest
 from numpy.testing import assert_allclose
 from gammapy.datasets import SpectrumDataset, SpectrumDatasetOnOff
-from gammapy.estimators import SensitivityEstimator
+from gammapy.estimators import FluxPoints, SensitivityEstimator
 from gammapy.irf import EDispKernelMap
 from gammapy.maps import MapAxis, RegionNDMap
 
@@ -42,27 +42,55 @@ def test_cta_sensitivity_estimator(spectrum_dataset):
     table = sens.run(dataset_on_off)
 
     assert len(table) == 4
-    assert table.colnames == ["energy", "e2dnde", "excess", "background", "criterion"]
-    assert table["energy"].unit == "TeV"
+    assert table.colnames == [
+        "e_ref",
+        "e_min",
+        "e_max",
+        "e2dnde",
+        "excess",
+        "background",
+        "criterion",
+    ]
+    assert table["e_ref"].unit == "TeV"
     assert table["e2dnde"].unit == "erg / (cm2 s)"
 
     row = table[0]
-    assert_allclose(row["energy"], 1.33352, rtol=1e-3)
+    assert_allclose(row["e_ref"], 1.33352, rtol=1e-3)
     assert_allclose(row["e2dnde"], 2.74559e-08, rtol=1e-3)
     assert_allclose(row["excess"], 270000, rtol=1e-3)
     assert_allclose(row["background"], 3.6e06, rtol=1e-3)
     assert row["criterion"] == "bkg"
 
     row = table[1]
-    assert_allclose(row["energy"], 2.37137, rtol=1e-3)
+    assert_allclose(row["e_ref"], 2.37137, rtol=1e-3)
     assert_allclose(row["e2dnde"], 6.04795e-11, rtol=1e-3)
     assert_allclose(row["excess"], 334.454, rtol=1e-3)
     assert_allclose(row["background"], 3600, rtol=1e-3)
     assert row["criterion"] == "significance"
 
     row = table[3]
-    assert_allclose(row["energy"], 7.49894, rtol=1e-3)
+    assert_allclose(row["e_ref"], 7.49894, rtol=1e-3)
     assert_allclose(row["e2dnde"], 1.42959e-11, rtol=1e-3)
     assert_allclose(row["excess"], 25, rtol=1e-3)
     assert_allclose(row["background"], 3.6, rtol=1e-3)
     assert row["criterion"] == "gamma"
+
+
+def test_integral_estimation(spectrum_dataset):
+    dataset = spectrum_dataset.to_image()
+    geom = dataset.background.geom
+
+    dataset_on_off = SpectrumDatasetOnOff.from_spectrum_dataset(
+        dataset=dataset,
+        acceptance=RegionNDMap.from_geom(geom=geom, data=1),
+        acceptance_off=RegionNDMap.from_geom(geom=geom, data=5),
+    )
+
+    sens = SensitivityEstimator(gamma_min=25, bkg_syst_fraction=0.075)
+    table = sens.run(dataset_on_off)
+    flux_points = FluxPoints.from_table(
+        table, sed_type="e2dnde", reference_model=sens.spectrum
+    )
+
+    assert_allclose(table["excess"].data.squeeze(), 270540, rtol=1e-3)
+    assert_allclose(flux_points.flux.data.squeeze(), 7.52e-9, rtol=1e-3)
