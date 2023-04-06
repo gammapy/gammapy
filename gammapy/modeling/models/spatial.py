56a48f8bf413a9fc7a868f486a3c41cd49dbca81
Atreyee Sinha <asinha@ucm.es>
2023-04-06 17:04:23 +0200

    fix from_position

--- gammapy/modeling/models/spatial.py
+++ gammapy/modeling/models/spatial.py
@@ -393,7 +393,7 @@ class SpatialModel(ModelBase):
             Spatial model
         """
         lon_0, lat_0 = position.data.lon, position.data.lat
-        return cls(lon_0=lon_0, lat_0=lat_0, frame=position.frame, **kwargs)
+        return cls(lon_0=lon_0, lat_0=lat_0, frame=position.frame.name, **kwargs)
 
     @property
     def evaluation_radius(self):
