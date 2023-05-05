caeb8054ba9be678cb12a231826a11218e30f3a0
cgalelli <claudio.galelli@obspm.fr>
2023-05-05 15:51:56 +0200

    Added functions for weighted fvar, fpp, doubling/efolding time and wrappers to use functions form lightcurve objects

--- gammapy/stats/__init__.py
+++ gammapy/stats/__init__.py
@@ -2,6 +2,10 @@
 """Statistics."""
 from .counts_statistic import CashCountsStatistic, WStatCountsStatistic
 from .fit_statistics import cash, cstat, get_wstat_gof_terms, get_wstat_mu_bkg, wstat
+from .variability import (compute_fvar, weighted_fvar, compute_fpp,
+                          compute_etime, compute_2time, compute_chisq,
+                          lc_fpp, lc_fvar, eval_lc_timing)
+
 from .fit_statistics_cython import (
     cash_sum_cython,
     f_cash_root_cython,
@@ -19,4 +23,13 @@ __all__ = [
     "norm_bounds_cython",
     "wstat",
     "WStatCountsStatistic",
+    "compute_fvar",
+    "weighted_fvar",
+    "compute_fpp",
+    "compute_2time",
+    "compute_etime",
+    "compute_chisq",
+    "lc_fvar",
+    "lc_fpp",
+    'eval_lc_time'
 ]
