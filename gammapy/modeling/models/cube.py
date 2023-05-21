390533f1115b5c9aafc8e84ef5f31dc61409de82
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2023-05-21 08:58:23 +0200

    replace copy_covariance
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/modeling/models/cube.py
+++ gammapy/modeling/models/cube.py
@@ -9,7 +9,6 @@ from astropy.nddata import NoOverlapError
 from astropy.time import Time
 from gammapy.maps import Map, MapAxis, WcsGeom
 from gammapy.modeling import Covariance, Parameters
-from gammapy.modeling.covariance import copy_covariance
 from gammapy.modeling.parameter import _get_parameters_str
 from gammapy.utils.fits import LazyFitsData
 from gammapy.utils.scripts import make_name, make_path
@@ -64,6 +63,7 @@ class SkyModel(ModelBase):
         name=None,
         apply_irf=None,
         datasets_names=None,
+        covariance_data=None,
     ):
         self.spatial_model = spatial_model
         self.spectral_model = spectral_model
@@ -84,7 +84,7 @@ class SkyModel(ModelBase):
                 "Spectral model used with SkyModel requires a norm type parameter."
             )
 
-        super().__init__()
+        super().__init__(covariance_data=covariance_data)
 
     @property
     def _models(self):
@@ -397,7 +397,6 @@ class SkyModel(ModelBase):
 
         return Map.from_geom(geom=geom, data=value.value, unit=value.unit)
 
-    @copy_covariance
     def copy(self, name=None, copy_data=False, **kwargs):
         """Copy sky model
 
@@ -431,6 +430,7 @@ class SkyModel(ModelBase):
         kwargs.setdefault("temporal_model", temporal_model)
         kwargs.setdefault("apply_irf", self.apply_irf.copy())
         kwargs.setdefault("datasets_names", self.datasets_names)
+        kwargs.setdefault("covariance_data", self.covariance.data.copy())
 
         return self.__class__(**kwargs)
 
@@ -614,7 +614,13 @@ class FoVBackgroundModel(ModelBase):
 
     tag = ["FoVBackgroundModel", "fov-bkg"]
 
-    def __init__(self, spectral_model=None, dataset_name=None, spatial_model=None):
+    def __init__(
+        self,
+        spectral_model=None,
+        dataset_name=None,
+        spatial_model=None,
+        covariance_data=None,
+    ):
         if dataset_name is None:
             raise ValueError("Dataset name a is required argument")
 
@@ -628,7 +634,7 @@ class FoVBackgroundModel(ModelBase):
 
         self._spatial_model = spatial_model
         self._spectral_model = spectral_model
-        super().__init__()
+        super().__init__(covariance_data=covariance_data)
 
     @staticmethod
     def contributes(*args, **kwargs):
@@ -694,7 +700,6 @@ class FoVBackgroundModel(ModelBase):
         else:
             return value
 
-    @copy_covariance
     def copy(self, name=None, copy_data=False, **kwargs):
         """Copy FoVBackgroundModel
 
@@ -714,6 +719,7 @@ class FoVBackgroundModel(ModelBase):
         """
         kwargs.setdefault("spectral_model", self.spectral_model.copy())
         kwargs.setdefault("dataset_name", self.datasets_names[0])
+        kwargs.setdefault("covariance_data", self.covariance.data.copy())
         return self.__class__(**kwargs)
 
     def to_dict(self, full_output=False):
@@ -815,6 +821,7 @@ class TemplateNPredModel(ModelBase):
         datasets_names=None,
         copy_data=True,
         spatial_model=None,
+        covariance_data=None,
     ):
         if isinstance(map, Map):
             axis = map.geom.axes["energy"]
@@ -847,9 +854,8 @@ class TemplateNPredModel(ModelBase):
                     "Currently background models can only be assigned to one dataset."
                 )
         self.datasets_names = datasets_names
-        super().__init__()
+        super().__init__(covariance_data=covariance_data)
 
-    @copy_covariance
     def copy(self, name=None, copy_data=False, **kwargs):
         """Copy template npred model.
 
@@ -872,6 +878,7 @@ class TemplateNPredModel(ModelBase):
         kwargs.setdefault("spectral_model", self.spectral_model.copy())
         kwargs.setdefault("filename", self.filename)
         kwargs.setdefault("datasets_names", self.datasets_names)
+        kwargs.setdefault("covariance_data", self.covariance.data.copy())
         return self.__class__(name=name, copy_data=copy_data, **kwargs)
 
     @property
