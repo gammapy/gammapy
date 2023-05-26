dd62f4c0733b9936af8009254d34e24830450904
Axel Donath <axel.donath@cfa.harvard.edu>
2023-05-26 11:25:41 -0400

    Clean up utils.parallel

--- gammapy/utils/parallel.py
+++ gammapy/utils/parallel.py
@@ -1,143 +1,202 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Multiprocessing and multithreading setup"""
+import importlib
 import logging
-import os
+import multiprocessing
+from enum import Enum
 from gammapy.utils.pbar import progress_bar
 
 log = logging.getLogger(__name__)
 
 
-MULTIPROCESSING_BACKEND = "multiprocessing"
-N_PROCESSES = 1
-N_THREADS = 1
+class ParallelBackendEnum(Enum):
+    """Enum for parallel backend"""
+
+    multiprocessing = "multiprocessing"
+    ray = "ray"
 
 
-def warning_prototype(module="ray"):
+class PoolMethodEnum(Enum):
+    """Enum for pool method"""
+
+    starmap = "starmap"
+    apply_async = "apply_async"
+
+
+BACKEND_DEFAULT = ParallelBackendEnum.multiprocessing
+N_JOBS_DEFAULT = 1
+
+
+def get_multiprocessing_ray():
+    """Get multiprocessing module for ray backend"""
+    import ray.util.multiprocessing as multiprocessing
+
     log.warning(
-        f"gammapy supports for parallelisation with {module} is still a prototype and is not fully functional."
+        "Gammapy support for parallelisation with ray is still a prototype and is not fully functional."
     )
+    return multiprocessing
 
 
 def is_ray_initialized():
+    """Check if ray is initialized"""
     try:
         from ray import is_initialized
 
         return is_initialized()
-    except (ModuleNotFoundError):
+    except ModuleNotFoundError:
+        return False
+
+
+def is_ray_available():
+    """Check if ray is available"""
+    try:
+        importlib.import_module("ray")
+        return True
+    except ModuleNotFoundError:
         return False
 
 
 class ParallelMixin:
+    """Mixin class to handle parallel processing"""
+
     @property
     def n_jobs(self):
-        if self._n_jobs is None:
-            return N_PROCESSES
-        else:
-            return self._n_jobs
+        """Number of jobs (int)"""
+        return self._n_jobs
 
     @n_jobs.setter
     def n_jobs(self, value):
-        self._n_jobs = value
+        """Number of jobs setter (int)"""
+        self._n_jobs = int(value)
 
     @property
     def parallel_backend(self):
-        if self._parallel_backend is None:
-            return MULTIPROCESSING_BACKEND
-        else:
-            return self._parallel_backend
+        """Parallel backend (str)"""
+        return self._parallel_backend
 
     @parallel_backend.setter
     def parallel_backend(self, value):
+        """Parallel backend setter (str)"""
+        value = ParallelBackendEnum(value)
+
+        if value == ParallelBackendEnum.ray and not is_ray_available():
+            log.warning("Ray is not installed, falling back to multiprocessing backend")
+            value = ParallelBackendEnum.multiprocessing
+
         self._parallel_backend = value
-
-
-def get_multiprocessing(backend=None):
-    """import multiprocessing module for a given backend"""
-
-    if backend is None:
-        backend = MULTIPROCESSING_BACKEND
-    if backend == "multiprocessing":
-        import multiprocessing
-
-        return multiprocessing
-    elif backend == "ray":
-        import ray.util.multiprocessing as multiprocessing
-
-        warning_prototype(module=backend)
-        return multiprocessing
-    else:
-        raise ValueError("Invalid multiprocessing backend")
 
 
 def run_multiprocessing(
     func,
     inputs,
-    backend=None,
+    backend=BACKEND_DEFAULT,
     pool_kwargs=None,
     method="starmap",
     method_kwargs=None,
     task_name="",
 ):
-    """Run function in a loop or in Parallel"""
+    """Run function in a loop or in Parallel
 
-    if method not in ["starmap", "apply_async"]:
-        raise ValueError("Invalid multiprocessing method")
-
-    multiprocessing = get_multiprocessing(backend)
+    Parameters
+    ----------
+    func : function
+        Function to run
+    inputs : list
+        List of arguments to pass to the function
+    backend : {'multiprocessing', 'ray'}
+        Backend to use.
+    pool_kwargs : dict
+        Keyword arguments passed to the pool
+    method : {'starmap', 'apply_async'}
+        Pool method to use.
+    method_kwargs : dict
+        Keyword arguments passed to the method
+    task_name : str
+        Name of the task to display in the progress bar
+    """
     if method_kwargs is None:
         method_kwargs = {}
+
     if pool_kwargs is None:
         pool_kwargs = {}
-    pool_kwargs.setdefault("processes", N_PROCESSES)
-    if backend == "ray":
-        from ray import is_initialized
 
-        if is_initialized():
-            address = "auto"
-        else:
-            address = None
-        pool_kwargs.setdefault("ray_address", address)
+    processes = pool_kwargs.get("processes", N_JOBS_DEFAULT)
 
-    processes = pool_kwargs["processes"]
-    if backend == "multiprocessing":
-        processes = max(processes, os.cpu_count())
+    multiprocessing = PARALLEL_BACKEND_MODULES[backend]
+
+    if backend == ParallelBackendEnum.multiprocessing:
         if multiprocessing.current_process().name != "MainProcess":
             # subprocesses cannot have childs
             processes = 1
     # TODO: check for ray
 
+    if processes == 1:
+        return run_loop(
+            func=func, inputs=inputs, method_kwargs=method_kwargs, task_name=task_name
+        )
+
+    if backend == ParallelBackendEnum.ray:
+        address = "auto" if is_ray_initialized() else None
+        pool_kwargs.setdefault("ray_address", address)
+
     log.info(f"Using {processes} processes to compute {task_name}")
 
-    if processes == 1:
-        return run_loop(func, inputs, method_kwargs, task_name)
-    else:
-        with multiprocessing.Pool(**pool_kwargs) as pool:
-            return run_pool(pool, func, inputs, method, method_kwargs, task_name)
+    with multiprocessing.Pool(**pool_kwargs) as pool:
+        pool_func = POOL_METHODS[PoolMethodEnum(method)]
+        results = pool_func(
+            pool=pool,
+            func=func,
+            inputs=inputs,
+            method_kwargs=method_kwargs,
+            task_name=task_name,
+        )
+
+    return results
 
 
 def run_loop(func, inputs, method_kwargs=None, task_name=""):
     """Loop over inputs an run function"""
-
     results = []
+
+    callback = method_kwargs.get("callback", None)
+
     for arguments in progress_bar(inputs, desc=task_name):
-        output = func(*arguments)
-        if "callback" in method_kwargs:
-            results.append(method_kwargs["callback"](output))
-        else:
-            results.append(output)
+        result = func(*arguments)
+
+        if callback is not None:
+            result = callback(result)
+
+        results.append(result)
+
     return results
 
 
-def run_pool(pool, func, inputs, method="starmap", method_kwargs=None, task_name=""):
-    """Run function in Parallel"""
+def run_pool_star_map(pool, func, inputs, method_kwargs=None, task_name=""):
+    """Run function in parallel"""
+    return pool.starmap(func, progress_bar(inputs, desc=task_name), **method_kwargs)
 
-    if method == "starmap":
-        return pool.starmap(func, progress_bar(inputs, desc=task_name), **method_kwargs)
-    elif method == "apply_async":
-        results = []
-        for arguments in progress_bar(inputs, desc=task_name):
-            result = pool.apply_async(func, arguments, **method_kwargs)
-            results.append(result)
-        # wait async run is done
-        [result.wait() for result in results]
-        return results
+
+def run_pool_async(pool, func, inputs, method_kwargs=None, task_name=""):
+    """Run function in parallel async"""
+    results = []
+
+    for arguments in progress_bar(inputs, desc=task_name):
+        result = pool.apply_async(func, arguments, **method_kwargs)
+        results.append(result)
+
+    # wait async run is done
+    [result.wait() for result in results]
+    return results
+
+
+POOL_METHODS = {
+    PoolMethodEnum.starmap: run_pool_star_map,
+    PoolMethodEnum.apply_async: run_pool_async,
+}
+
+PARALLEL_BACKEND_MODULES = {
+    ParallelBackendEnum.multiprocessing: multiprocessing,
+}
+
+if is_ray_available():
+    PARALLEL_BACKEND_MODULES[ParallelBackendEnum.ray] = get_multiprocessing_ray()
