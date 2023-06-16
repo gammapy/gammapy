5c650d223c1ac1506280d04d990400bc50b02012
Régis Terrier <rterrier@apc.in2p3.fr>
2023-06-16 10:06:40 +0200

    Modify codemeta.py script
    Signed-off-by: Régis Terrier <rterrier@apc.in2p3.fr>

--- dev/codemeta.py
+++ dev/codemeta.py
@@ -1,25 +1,55 @@
 """Utility script to work with the codemeta.json file."""
 import json
+import logging
+import click
 
-FILENAME = "../codemeta.json"
+log = logging.getLogger(__name__)
 
-# add potentially missing content
-with open(FILENAME, "r") as f:
-    data = json.load(f)
 
-data["maintainer"] = data["author"][0]
-data["readme"] = "https://gammapy.org"
-data["issueTracker"] = "https://github.com/gammapy/gammapy/issues"
+def update_codemeta(maintainer, filename):
+    log.info(f"Updating {filename}")
+    # add potentially missing content
+    with open(filename, "r") as f:
+        data = json.load(f)
 
-with open(FILENAME, "w") as f:
-    json.dump(data, f, indent=4)
+    for author in data["author"]:
+        if author["familyName"] == "Donath":
+            log.info(f"Setting maintainer to {maintainer}")
+            data["maintainer"] = author
 
-# replace bad labelled attributes
-with open(FILENAME, "r") as f:
-    content = f.read()
+    data["readme"] = "https://gammapy.org"
+    data["issueTracker"] = "https://github.com/gammapy/gammapy/issues"
 
-content = content.replace("legalName", "name")
-content = content.replace("version", "softwareVersion")
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
 
-with open(FILENAME, "w") as f:
-    f.write(content)
+    # replace bad labelled attributes
+    with open(filename, "r") as f:
+        content = f.read()
+
+    content = content.replace("legalName", "name")
+    content = content.replace("version", "softwareVersion")
+
+    with open(filename, "w") as f:
+        f.write(content)
+
+
+@click.command()
+@click.option("--maintainer", default="Donath", type=str, help="Maintainer name")
+@click.option(
+    "--filename", default="../codemeta.json", type=str, help="codemeta filename"
+)
+@click.option(
+    "--log_level",
+    default="INFO",
+    type=click.Choice(["DEBUG", "INFO", "WARNING"]),
+    help="log level",
+)
+def cli(maintainer, filename, log_level):
+    logging.basicConfig(level=log_level)
+    log.setLevel(level=log_level)
+    update_codemeta(maintainer=maintainer, filename=filename)
+
+
+if __name__ == "__main__":
+    cli()
