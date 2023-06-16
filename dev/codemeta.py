4f9ce75b8429132eb10c64c99477f82da4901519
Régis Terrier <rterrier@apc.in2p3.fr>
2023-06-16 18:51:07 +0200

    Add further inputs into codemeta.py
    Signed-off-by: Régis Terrier <rterrier@apc.in2p3.fr>

--- dev/codemeta.py
+++ dev/codemeta.py
@@ -1,12 +1,13 @@
 """Utility script to work with the codemeta.json file."""
 import json
 import logging
+from configparser import ConfigParser
 import click
 
 log = logging.getLogger(__name__)
 
 
-def update_codemeta(maintainer, filename):
+def update_codemeta(maintainer, filename, setup_file=None):
     log.info(f"Updating {filename}")
     # add potentially missing content
     with open(filename, "r") as f:
@@ -19,6 +20,17 @@ def update_codemeta(maintainer, filename):
 
     data["readme"] = "https://gammapy.org"
     data["issueTracker"] = "https://github.com/gammapy/gammapy/issues"
+    data["developmentStatus"] = ("active",)
+    data["email"] = "GAMMAPY-COORDINATION-L@IN2P3.FR"
+
+    if setup_file:
+        # complete with software requirements from setup.cfg
+        cf = ConfigParser()
+        cf.read(setup_file)
+        requirements = cf["options"]["install_requires"].split("\n")
+        if "" in requirements:
+            requirements.remove("")
+        data["softwareRequirements"] = requirements
 
     with open(filename, "w") as f:
         json.dump(data, f, indent=4)
@@ -39,16 +51,17 @@ def update_codemeta(maintainer, filename):
 @click.option(
     "--filename", default="../codemeta.json", type=str, help="codemeta filename"
 )
+@click.option("--setup_file", default="../setup.cfg", type=str, help="setup filename")
 @click.option(
     "--log_level",
     default="INFO",
     type=click.Choice(["DEBUG", "INFO", "WARNING"]),
     help="log level",
 )
-def cli(maintainer, filename, log_level):
+def cli(maintainer, filename, log_level, setup_file):
     logging.basicConfig(level=log_level)
     log.setLevel(level=log_level)
-    update_codemeta(maintainer=maintainer, filename=filename)
+    update_codemeta(maintainer=maintainer, filename=filename, setup_file=setup_file)
 
 
 if __name__ == "__main__":
