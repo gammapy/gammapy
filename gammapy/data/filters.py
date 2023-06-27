255da27ff1ece4b710028c0953a75adc505ccda3
Maxime Regeard <regeard@apc.in2p3.fr>
2023-06-27 19:07:34 +0200

    add a check for overlap of phase band and docstring
    Signed-off-by: Maxime Regeard <regeard@apc.in2p3.fr>

--- gammapy/data/filters.py
+++ gammapy/data/filters.py
@@ -1,7 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import copy
 import logging
+import numpy as np
 from astropy.table import unique, vstack
+import pandas as pd
 
 __all__ = ["ObservationFilter"]
 
@@ -24,8 +26,10 @@ class ObservationFilter:
           class that corresponds to the filter type
           (see `~gammapy.data.ObservationFilter.EVENT_FILTER_TYPES`)
 
-        The filtered event list will be an intersection of all filters. A union
-        of filters is not supported yet.
+    event_filter_method: str
+        The filterring method to use on the event_filters. Either "intersect" or "union". Default is "intersect".
+        If "intersect", the filtered event list will be the intersection of all the event filters.
+        If "union", the filtered event list will be the union of all the event fileters, under uniquness condition.
 
     Examples
     --------
@@ -45,10 +49,12 @@ class ObservationFilter:
 
     EVENT_FILTER_TYPES = dict(sky_region="select_region", custom="select_parameter")
 
-    def __init__(self, time_filter=None, event_filters=None, event_logic="and"):
+    def __init__(
+        self, time_filter=None, event_filters=None, event_filter_method="intersect"
+    ):
         self.time_filter = time_filter
-        self.event_filters = event_filters or []
-        self.event_logic = event_logic
+        self.event_filters = self._check_overlap_phase(event_filters) or []
+        self.event_filter_method = event_filter_method
 
     @property
     def livetime_fraction(self):
@@ -72,7 +78,7 @@ class ObservationFilter:
 
         filtered_events = self._filter_by_time(events)
 
-        if self.event_logic == "and":
+        if self.event_filter_method == "intersect":
 
             for f in self.event_filters:
                 method_str = self.EVENT_FILTER_TYPES[f["type"]]
@@ -80,7 +86,7 @@ class ObservationFilter:
 
             return filtered_events
 
-        elif self.event_logic == "or":
+        elif self.event_filter_method == "union":
 
             filtered_events = []
             for f in self.event_filters:
@@ -92,6 +98,11 @@ class ObservationFilter:
             )
             tot_filtered_events = EventList(table)
             return tot_filtered_events
+
+        else:
+            raise ValueError(
+                "event_filter_method has to be either 'intersect' or 'union'."
+            )
 
     def filter_gti(self, gti):
         """Apply filters to a GTI table.
@@ -131,3 +142,22 @@ class ObservationFilter:
                 fraction += band[1] - band[0]
 
         return 1 if fraction == 0 else fraction
+
+    @staticmethod
+    def _check_overlap_phase(event_filter):
+        bands = []
+        for f in event_filter:
+            if f.get("opts").get("parameter") == "PHASE":
+                bands.append(f.get("opts").get("band"))
+
+        intervals = pd.arrays.IntervalArray.from_tuples(bands)
+        interval_matrix = []
+        for b in bands:
+            interval_matrix.append(intervals.overlaps(pd.Interval(b[0], b[1])))
+
+        interval_matrix = np.array(interval_matrix)
+        overlap_array = interval_matrix[~np.eye(interval_matrix.shape[0], dtype=bool)]
+        if True in overlap_array:
+            raise ValueError(
+                "Overlapping bands in event_filters that apply to pulsar phase are not allowed."
+            )
