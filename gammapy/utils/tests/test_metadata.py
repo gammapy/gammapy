6aa9e3feb1940263747ba10ed0839060310ac28a
Régis Terrier <rterrier@apc.in2p3.fr>
2023-07-20 10:18:50 +0200

    Add further test
    Signed-off-by: Régis Terrier <rterrier@apc.in2p3.fr>

--- gammapy/utils/tests/test_metadata.py
+++ gammapy/utils/tests/test_metadata.py
@@ -1,8 +1,10 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+from typing import Optional
 import pytest
 from numpy.testing import assert_allclose
+from astropy.coordinates import SkyCoord
 from pydantic import ValidationError
-from gammapy.utils.metadata import CreatorMetaData
+from gammapy.utils.metadata import CreatorMetaData, MetaData
 
 
 def test_creator():
@@ -17,3 +19,19 @@ def test_creator():
 
     with pytest.raises(ValidationError):
         default.date = 3
+
+
+def test_subclass():
+    class TestMetaData(MetaData):
+        name: str
+        mode: Optional[SkyCoord]
+        creation: Optional[CreatorMetaData]
+
+    creator = CreatorMetaData.from_default()
+    test_meta = TestMetaData(name="test", creation=creator)
+
+    assert test_meta.name == "test"
+    assert test_meta.creation.creator.split()[0] == "Gammapy"
+
+    with pytest.raises(ValidationError):
+        test_meta.mode = "coord"
