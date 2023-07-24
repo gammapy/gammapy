228103f00193d764385c1575a88fb53769c3744e
Régis Terrier <rterrier@apc.in2p3.fr>
2023-07-24 11:54:51 +0200

    Correct error with temporal model unit
    Signed-off-by: Régis Terrier <rterrier@apc.in2p3.fr>

--- gammapy/modeling/models/temporal.py
+++ gammapy/modeling/models/temporal.py
@@ -1017,13 +1017,10 @@ class TemplatePhaseCurveTemporalModel(TemporalModel):
     @lazyproperty
     def _interpolator(self):
         x = self.table["PHASE"].data
-        y = self.table["NORM"].data
+        y = self.table["NORM"].data * u.one
 
-        return (
-            scipy.interpolate.InterpolatedUnivariateSpline(
-                x, y, k=1, ext=2, bbox=[0.0, 1.0]
-            )
-            * u.one
+        return scipy.interpolate.InterpolatedUnivariateSpline(
+            x, y, k=1, ext=2, bbox=[0.0, 1.0]
         )
 
     def evaluate(self, time, t_ref, phi_ref, f0, f1, f2):
