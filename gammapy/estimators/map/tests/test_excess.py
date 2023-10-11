0fc6dc44a84285f687c86a39c92abba59b14f2ff
Régis Terrier <rterrier@apc.in2p3.fr>
2023-10-11 22:10:10 +0200

    Modify flux sensitivity test tolerance
    Signed-off-by: Régis Terrier <rterrier@apc.in2p3.fr>

--- gammapy/estimators/map/tests/test_excess.py
+++ gammapy/estimators/map/tests/test_excess.py
@@ -204,7 +204,7 @@ def test_significance_map_estimator_map_dataset_exposure(simple_dataset):
 
     assert_allclose(result["npred_excess"].data.sum(), 19733.602, rtol=1e-3)
     assert_allclose(result["sqrt_ts"].data[0, 10, 10], 4.217129, rtol=1e-3)
-    assert_allclose(result["flux_sensitivity"].data[0, 10, 10], 5.742761e-09)
+    assert_allclose(result["flux_sensitivity"].data[0, 10, 10], 5.742761e-09, rtol=1e-3)
 
     # without mask safe
     simple_dataset_no_mask = MapDataset(
@@ -220,7 +220,7 @@ def test_significance_map_estimator_map_dataset_exposure(simple_dataset):
     result_no_mask = estimator.run(simple_dataset_no_mask)
     assert_allclose(result_no_mask["npred_excess"].data.sum(), 19733.602, rtol=1e-3)
     assert_allclose(result_no_mask["sqrt_ts"].data[0, 10, 10], 4.217129, rtol=1e-3)
-    assert_allclose(result["flux_sensitivity"].data[0, 10, 10], 5.742761e-09)
+    assert_allclose(result["flux_sensitivity"].data[0, 10, 10], 5.742761e-09, rtol=1e-3)
 
 
 def test_excess_map_estimator_map_dataset_on_off_no_correlation(
