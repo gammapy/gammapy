30da552ca514511fd6fc7dc9a26e80ee3c01938c
Bruno Khélifi <khelifi@in2p3.fr>
2023-10-19 16:36:58 +0200

    Add the function to check the version of software
    Signed-off-by: Bruno Khélifi <khelifi@in2p3.fr>

--- gammapy/utils/check.py
+++ gammapy/utils/check.py
@@ -3,12 +3,13 @@ import logging
 import os
 from gammapy.scripts.download import RELEASE, cli_download_datasets
 from gammapy.scripts.info import cli_info
+from gammapy.version import version
 
 log = logging.getLogger(__name__)
 
 
 def check_tutorials_setup(download_datasets_path="./gammapy-data"):
-    """Check tutorials setup and download data if not available
+    """Check tutorials setup and download data if not available.
 
     Parameters
     ----------
@@ -23,3 +24,27 @@ def check_tutorials_setup(download_datasets_path="./gammapy-data"):
         os.environ["GAMMAPY_DATA"] = download_datasets_path
 
     cli_info.callback(system=True, version=True, dependencies=True, envvar=True)
+
+
+def check_version(testversion):
+    """Check if the current Gammapy version is identical, older or newer than the input.
+
+    Parameters
+    ----------
+    testversion : str
+        Version to test.
+
+    Returns
+    -------
+    check : int
+        0 if identical, -1 if the current version is older than `testversion` and +1 if newer.
+    """
+
+    if testversion == version:
+        return 0
+    elif "dev" in version:
+        return 1
+    elif float(testversion) < float(version):
+        return 1
+    else:
+        return -1
