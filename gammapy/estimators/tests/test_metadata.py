8a14b7e14f584d682a5181eecc8047724c2e89a5
Bruno Khélifi <khelifi@in2p3.fr>
2023-10-27 19:31:33 +0200

    Hook formatted
    Signed-off-by: Bruno Khélifi <khelifi@in2p3.fr>

--- gammapy/estimators/tests/test_metadata.py
+++ gammapy/estimators/tests/test_metadata.py
@@ -3,13 +3,16 @@ import pytest
 import numpy as np
 from astropy import units as u
 from astropy.coordinates import SkyCoord
+from astropy.table import Table
 from pydantic import ValidationError
 from gammapy.utils.metadata import CreatorMetaData
+from gammapy.utils.testing import requires_data
 from gammapy.version import version
 from ..metadata import FluxMetaData
 
 
-def test_creator():
+@pytest.fixture()
+def default():
     default = FluxMetaData(
         creation=CreatorMetaData(date="2022-01-01", creator="gammapy", origin="CTA"),
         instrument="CTAS",
@@ -19,6 +22,11 @@ def test_creator():
         dataset_names=["aa", "tt"],
         n_sigma_ul=None,
     )
+    return default
+
+
+@requires_data()
+def test_creator(default):
 
     assert default.creation.creator == "gammapy"
     assert default.dataset_names[1] == "tt"
@@ -29,11 +37,12 @@ def test_creator():
 
     with pytest.raises(ValidationError):
         default.obs_ids = 4.2
-    with pytest.raises(ValidationError):
-        default.obs_ids = [5, "bad"]
 
     with pytest.raises(ValidationError):
         default.target_position = 2.0
+
+    with pytest.raises(ValidationError):
+        default.sed_type = "test"
 
     default = FluxMetaData.from_default()
     assert default.instrument is None
@@ -45,3 +54,37 @@ def test_creator():
         obs_ids=None,
         dataset_names=None,
     )
+
+
+def test_from_fits_header():
+    tdict = {
+        "SED_TYPE": "dnde",
+        "N_SIGMA": "4.3",
+        "TARGETNA": "RXJ",
+        "RA_OBJ": "123.5",
+        "DEC_OBJ": "-4.8",
+        "OBS_IDS": "1 2 3 6",
+        "DATASETS": "myanalysis",
+        "INSTRU": "HAWC",
+        "CREATED": "2023-10-27 16:05:09.795",
+        "ORIGIN": "Gammapy v2.3",
+        "CREATOR": "mynotebook",
+        "EPHEM": "fermi23.1",
+    }
+
+    meta = FluxMetaData.from_header(tdict)
+    print(meta)
+    assert meta.n_sigma == 4.3
+    assert meta.optional["ephem"] == "fermi23.1"
+    assert meta.target_position == SkyCoord(123.5 * u.deg, -4.8 * u.deg, frame="icrs")
+
+
+@requires_data()
+def test_to_table(default):
+    table = Table()
+    default.to_table(table)
+    print(table.meta)
+    assert table.meta["OBS_IDS"] == "1 2 3"
+    assert table.meta["ORIGIN"] == "CTA"
+    assert table.meta["RA_OBJ"] == "83.633000"
+    assert table.meta["N_SIGMA"] == "2.0"
