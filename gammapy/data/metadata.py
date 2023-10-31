59b13df79656961b60eae52db54be8a952e3b979
Régis Terrier <rterrier@apc.in2p3.fr>
2023-10-31 18:41:09 +0100

    Adapt Observation
    Signed-off-by: Régis Terrier <rterrier@apc.in2p3.fr>

--- gammapy/data/metadata.py
+++ gammapy/data/metadata.py
@@ -80,7 +80,16 @@ class ObservationMetaData(MetaData):
 
     @classmethod
     def from_header(cls, header, format="gadf"):
-        meta = super().from_header(header, format)
+        """Create and fill the observation metadata from the event list metadata.
+
+        Parameters
+        ----------
+        header : dict
+            Input FITS header.
+        format : str
+            The header data format. Default is gadf.
+        """
+        meta = super(ObservationMetaData, cls).from_header(header, format)
 
         meta.creation = CreatorMetaData()
         # Include additional gadf keywords not specified as ObservationMetaData attributes
