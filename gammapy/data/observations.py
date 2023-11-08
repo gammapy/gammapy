7b24c26764e30edc29d81459e07de49852cd4572
Maxime Regeard <regeard@apc.in2p3.fr>
2023-11-08 19:01:39 +0100

    fix mismatch
    Signed-off-by: Maxime Regeard <regeard@apc.in2p3.fr>

--- gammapy/data/observations.py
+++ gammapy/data/observations.py
@@ -614,7 +614,7 @@ class Observation:
     def copy(self, in_memory=False, **kwargs):
         """Copy observation.
 
-        Overwriting `Observation arguments requires the 'in_memory' argument to be true.
+        Overwriting `Observation` arguments requires the 'in_memory' argument to be true.
 
         Parameters
         ----------
