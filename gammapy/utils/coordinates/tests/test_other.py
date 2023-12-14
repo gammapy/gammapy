324f9c9e9a5bba9ca57e8a22bc67e411229f928a
Astro-Kirsty <AstroKirsty@gmail.com>
2023-12-14 16:07:55 +0100

    Update test for galatic utils
    Signed-off-by: Astro-Kirsty <AstroKirsty@gmail.com>

--- gammapy/utils/coordinates/tests/test_other.py
+++ gammapy/utils/coordinates/tests/test_other.py
@@ -1,4 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+from astropy.coordinates import Galactocentric
 from astropy.units import Quantity
 from gammapy.utils.coordinates import galactic
 
@@ -7,5 +8,9 @@ def test_galactic():
     x = Quantity(0, "kpc")
     y = Quantity(0, "kpc")
     z = Quantity(0, "kpc")
-    reference = (Quantity(8.122, "kpc"), Quantity(0, "deg"), Quantity(0, "deg"))
+    reference = (
+        Galactocentric().galcen_distance,
+        Quantity(0, "deg"),
+        Quantity(0, "deg"),
+    )
     assert galactic(x, y, z) == reference
