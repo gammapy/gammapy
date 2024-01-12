f036eed6bbd1be9eedf02a5626738b58aaf0b18f
Atreyee Sinha <asinha@ucm.es>
2024-01-12 18:06:47 +0100

    improve notebook

--- examples/tutorials/api/irfs.py
+++ examples/tutorials/api/irfs.py
@@ -1,8 +1,116 @@
 """
-Using asymmetric IRFs
-=====================
+Using Gammapy IRFs
+==================
+
+`gammapy.irf` contains classes for handling Instrument Response
+Functions typically stored as multi-dimensional tables. For a list of
+IRF classes internally supported, see
+https://gamma-astro-data-formats.readthedocs.io/en/v0.3/irfs/full_enclosure/index.html
+
+This tutorial is intended for advanced users typically creating IRFs
 
 """
+
+import numpy as np
+import scipy.special
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+from astropy.visualization import quantity_support
+import matplotlib.pyplot as plt
+from gammapy.irf import (
+    IRF,
+    Background3D,
+    EffectiveAreaTable2D,
+    EnergyDependentMultiGaussPSF,
+    EnergyDispersion2D,
+)
+from gammapy.irf.io import COMMON_IRF_HEADERS, IRF_DL3_HDU_SPECIFICATION
+from gammapy.makers.utils import make_edisp_kernel_map, make_map_exposure_true_energy
+from gammapy.maps import MapAxes, MapAxis, WcsGeom
+
+######################################################################
+# Inbuilt Gammapy IRFs
+# --------------------
+#
+
+irf_filename = (
+    "$GAMMAPY_DATA/cta-1dc/caldb/data/cta/1dc/bcf/South_z20_50h/irf_file.fits"
+)
+
+aeff = EffectiveAreaTable2D.read(irf_filename, hdu="EFFECTIVE AREA")
+print(aeff)
+
+
+######################################################################
+# We can see that the Effective Area Table is defined in terms of
+# “energy_true” and `offset` from the camera center
+#
+
+# To see the IRF axes binning, eg, offset
+print(aeff.axes["offset"])
+
+# To get the IRF data
+print(aeff.data)
+
+# the aeff is evaluated at a given energy and offset
+print(aeff.evaluate(energy_true=[1, 10] * u.TeV, offset=[0.2, 2.5] * u.deg))
+
+
+# The peek methods give quick looks into the irfs
+aeff.peek()
+
+
+######################################################################
+# Similarly, we can access other IRFs as well
+#
+
+psf = EnergyDependentMultiGaussPSF.read(irf_filename, hdu="Point Spread Function")
+bkg = Background3D.read(irf_filename, hdu="BACKGROUND")
+edisp = EnergyDispersion2D.read(irf_filename, hdu="ENERGY DISPERSION")
+
+print(bkg)
+
+# To evaluate the irfs, pass the values for each axis
+ev = bkg.evaluate(energy=1 * u.TeV, fov_lon=[1, 2] * u.deg, fov_lat=[1, 2] * u.deg)
+print(ev)
+
+# To evaluate the irfs, pass the values for each axis
+ev = edisp.evaluate(energy_true=1 * u.TeV, offset=[0, 1] * u.deg, migra=[1, 1.2])
+print(ev)
+
+edisp.peek()
+
+print(psf)
+
+
+######################################################################
+# The PointSpreadFunction for the CTA DC1 is stored as a combination of 3
+# Gaussians. Other PSFs, like a `PSF_TABLE` and analytic expressions
+# like KING function are also supported. All PSF claases inherit from a
+# common base `PSF` class
+#
+
+print(psf.axes.names)
+
+# To get the containment radius for a fixed fraction at a given position
+print(
+    psf.containment_radius(
+        fraction=0.68, energy_true=1.0 * u.TeV, offset=[0.2, 4.0] * u.deg
+    )
+)
+
+# Alternatively, to get the containment fraction for at a given position
+print(
+    psf.containment(
+        rad=0.05 * u.deg, energy_true=1.0 * u.TeV, offset=[0.2, 4.0] * u.deg
+    )
+)
+
+
+######################################################################
+# Support for Asymmetric IRFs
+# ---------------------------
+#
 
 
 ######################################################################
@@ -11,23 +119,17 @@ Using asymmetric IRFs
 # this to work correctly with the `MapDatasetMaker`, only variations
 # with `fov_lon` and `fov_lat` can be allowed.
 #
-# Analytic models for the PSF, or anisotropic PSFs (ie, asymmetry around
-# the source position) is not correctly supported during the data
-# reduction
+# The main idea is that the list of required axes should be correctly
+# mentioned in the class definition.
+#
+# Asymmetric PSFs are not correctly supported at present in the data
+# reduction scheme.
 #
 
-import numpy as np
-import scipy.special
-import astropy.units as u
-from astropy.coordinates import SkyCoord
-from gammapy.irf import *
-from gammapy.irf.io import COMMON_IRF_HEADERS, IRF_DL3_HDU_SPECIFICATION
-from gammapy.makers.utils import *
-from gammapy.maps import MapAxes, MapAxis, WcsGeom
 
 ######################################################################
 # Effective Area
-# --------------
+# ~~~~~~~~~~~~~~
 #
 
 
@@ -47,7 +149,9 @@ fov_lat_axis = MapAxis.from_edges([-1.5, -0.5, 0.5, 1.5] * u.deg, name="fov_lat"
 
 data = np.ones((4, 3, 3))
 for i in range(1, 4):
-    data[i] = data[i - 1] * 1.5
+    data[i] = data[i - 1] * 2.0
+    data[i][-1] = data[i][0] * 1.2
+
 
 aeff_3d = EffectiveArea3D(
     [energy_axis, fov_lon_axis, fov_lat_axis], data=data, unit=u.m**2
@@ -61,10 +165,26 @@ res = aeff_3d.evaluate(
 )
 print(res)
 
+# to visualise at a given energy
+# sphinx_gallery_thumbnail_number = 1
+aeff_eval = aeff_3d.evaluate(energy_true=[1.0] * u.TeV)
+
+ax = plt.subplot()
+with quantity_support():
+    caxes = ax.pcolormesh(
+        fov_lat_axis.edges, fov_lon_axis.edges, aeff_eval.value.squeeze()
+    )
+fov_lat_axis.format_plot_xaxis(ax)
+fov_lon_axis.format_plot_yaxis(ax)
+ax.set_title("Asymmetric effective area")
+
 
 ######################################################################
 # Serialisation
 # ~~~~~~~~~~~~~
+#
+# For serialisation, we need to add the class definition to the
+# `IRF_DL3_HDU_SPECIFICATION` dictionary
 #
 
 IRF_DL3_HDU_SPECIFICATION["aeff_3d"] = {
@@ -88,6 +208,8 @@ print(aeff_new)
 # Create exposure map
 # ~~~~~~~~~~~~~~~~~~~
 #
+# DL4 data products can be created from these IRFs.
+#
 
 axis = MapAxis.from_energy_bounds(0.1 * u.TeV, 10 * u.TeV, 6, name="energy_true")
 pointing = SkyCoord(2, 1, unit="deg")
@@ -98,6 +220,8 @@ print(geom)
 exposure_map = make_map_exposure_true_energy(
     pointing=pointing, livetime="42 h", aeff=aeff_3d, geom=geom
 )
+
+exposure_map.plot_grid(add_cbar=True)
 
 
 ######################################################################
@@ -149,26 +273,60 @@ class EnergyDispersion3D(IRF):
 
 # Make a test case
 energy_axis_true = MapAxis.from_energy_bounds(
-    "0.1 TeV", "100 TeV", nbin=50, name="energy_true"
+    "0.1 TeV", "100 TeV", nbin=3, name="energy_true"
 )
 
-migra_axis = MapAxis.from_bounds(0, 4, nbin=100, node_type="edges", name="migra")
+migra_axis = MapAxis.from_bounds(0, 4, nbin=2, node_type="edges", name="migra")
 
 fov_lon_axis = MapAxis.from_edges([-1.5, -0.5, 0.5, 1.5] * u.deg, name="fov_lon")
 fov_lat_axis = MapAxis.from_edges([-1.5, -0.5, 0.5, 1.5] * u.deg, name="fov_lat")
 
-energy_true = energy_axis_true.edges[:-1]
-sigma = 0.15 / (energy_true / (1 * u.TeV)).value ** 0.3
-bias = 1e-3 * (energy_true - 1 * u.TeV).value
-
-edisp3d = EnergyDispersion3D.from_gauss(
-    energy_axis_true=energy_axis_true,
-    migra_axis=migra_axis,
-    fov_lon_axis=fov_lon_axis,
-    fov_lat_axis=fov_lat_axis,
-    bias=bias,
-    sigma=sigma,
+data = np.array(
+    [
+        [
+            [
+                [4.99582964e-01, 4.99582964e-01, 4.99582964e-01],
+                [4.99582964e-01, 4.99582964e-01, 4.99582964e-01],
+                [4.99582964e-01, 4.99582964e-01, 4.99582964e-01],
+            ],
+            [
+                [2.06259405e-04, 2.06259405e-04, 2.06259405e-04],
+                [2.06259405e-04, 2.06259405e-04, 2.06259405e-04],
+                [2.06259405e-04, 2.06259405e-04, 2.06259405e-04],
+            ],
+        ],
+        [
+            [
+                [5.00000000e-01, 5.00000000e-01, 5.00000000e-01],
+                [5.00000000e-01, 5.00000000e-01, 5.00000000e-01],
+                [5.00000000e-01, 5.00000000e-01, 5.00000000e-01],
+            ],
+            [
+                [0.00000000e00, 0.00000000e00, 0.00000000e00],
+                [0.00000000e00, 0.00000000e00, 0.00000000e00],
+                [0.00000000e00, 0.00000000e00, 0.00000000e00],
+            ],
+        ],
+        [
+            [
+                [5.00000000e-01, 5.00000000e-01, 5.00000000e-01],
+                [5.00000000e-01, 5.00000000e-01, 5.00000000e-01],
+                [5.00000000e-01, 5.00000000e-01, 5.00000000e-01],
+            ],
+            [
+                [0.00000000e00, 0.00000000e00, 0.00000000e00],
+                [0.00000000e00, 0.00000000e00, 0.00000000e00],
+                [0.00000000e00, 0.00000000e00, 0.00000000e00],
+            ],
+        ],
+    ]
 )
+
+
+edisp3d = EnergyDispersion3D(
+    [energy_axis_true, migra_axis, fov_lon_axis, fov_lat_axis], data=data
+)
+
 print(edisp3d)
 
 energy = [1, 2] * u.TeV
@@ -181,7 +339,7 @@ edisp3d.evaluate(
     migra=migra.reshape(1, -1, 1, 1),
     fov_lon=fov_lon.reshape(1, 1, -1, 1),
     fov_lat=fov_lat.reshape(1, 1, 1, -1),
-)
+)[0][2]
 
 
 ######################################################################
@@ -220,10 +378,15 @@ edispmap = make_edisp_kernel_map(edisp3d, pointing, geom)
 
 edispmap.peek()
 
+edispmap.edisp_map.data[3][1][3]
+
 
 ######################################################################
 # PSF
 # ---
+#
+# A higher dimensional PSF Table can also be created, as shown here.
+# Support for higher dimensional analystic PSF will come later
 #
 
 
@@ -287,3 +450,9 @@ psfnd.write("test_psf.fits.gz", overwrite=True)
 psf_new = PSFnD.read("test_psf.fits.gz")
 
 psf_new == psfnd
+
+
+######################################################################
+# Support for asymmetric IRFs is priliminary at the moment, and will
+# evolve depending on feedback.
+#
