b83c066131ef579fa8b2c74ddc6471fb4cfa817e
Bruno Khélifi <khelifi@in2p3.fr>
2024-01-18 16:56:57 +0100

    Adapt the tests
    Signed-off-by: Bruno Khélifi <khelifi@in2p3.fr>

--- gammapy/data/tests/test_metadata.py
+++ gammapy/data/tests/test_metadata.py
@@ -79,8 +79,9 @@ def test_observation_metadata_from_header(hess_eventlist_header):
 
 @requires_data()
 def test_observation_metadata_bad(hess_eventlist_header):
-    with pytest.raises(ValueError):
-        ObservationMetaData.from_header(hess_eventlist_header, format="bad")
+    # The test on the format has been commented in the code
+    # with pytest.raises(ValueError):
+    #     ObservationMetaData.from_header(hess_eventlist_header, format="bad")
 
     hess_eventlist_header.pop("DEADC")
     with pytest.raises(KeyError):
