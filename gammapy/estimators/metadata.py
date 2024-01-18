7c2fadc4b1c58749c8896022d4f01eded615d57b
Bruno Khélifi <khelifi@in2p3.fr>
2024-01-18 16:28:42 +0100

    Flake8
    Signed-off-by: Bruno Khélifi <khelifi@in2p3.fr>

--- gammapy/estimators/metadata.py
+++ gammapy/estimators/metadata.py
@@ -1,11 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import logging
 from enum import Enum
-from typing import List, Optional
-import numpy as np
-from astropy.coordinates import SkyCoord
-
-# from pydantic import ValidationError, validator
+from typing import ClassVar, Literal, Optional
 from gammapy.utils.metadata import (
     METADATA_FITS_KEYS,
     CreatorMetaData,
@@ -21,17 +17,13 @@ FLUX_METADATA_FITS_KEYS = {
         "sed_type": "SED_TYPE",
         "sed_type_init": "SEDTYPEI",
         "n_sigma": "N_SIGMA",
-        "ul_conf": "UL_CONF",
-        # "n_sigma_ul": "NSIGMAUL",
+        "n_sigma_ul": "NSIGMAUL",
         "sqrt_ts_threshold_ul": "STSTHUL",
         "n_sigma_sensitivity": "NSIGMSEN",
-        # "target_name": "TARGETNA",
-        # "target_position": ["RA_OBJ", "DEC_OBJ"],
-        "obs_ids": "OBS_IDS",
-        "dataset_names": "DATASETS",
-        # "instrument": "INSTRU",
     },
 }
+
+METADATA_FITS_KEYS.update(FLUX_METADATA_FITS_KEYS)
 
 log = logging.getLogger(__name__)
 
@@ -53,28 +45,16 @@ class FluxMetaData(MetaData):
         SED type.
     sed_type_init : {"dnde", "flux", "eflux", "e2dnde", "likelihood"}, optional
         SED type of the initial data.
-    ul_conf : float, optional
-        Confidence level used for the upper limit computation.
     n_sigma : float, optional
         Significance threshold above which upper limits should be used.
-    # n_sigma_ul : float, optional
-    #     Sigma number used to compute the upper limits.
+    n_sigma_ul : float, optional
+        Significance value used for the upper limit computation.
     sqrt_ts_threshold_ul : float, optional
         Threshold on the square root of the likelihood value above which upper limits should be used.
     n_sigma_sensitivity : float, optional
         Sigma number for which the flux sensitivity is computed
     target : `~gammapy.utils.TargetMetaData`, optional
         General metadata information about the target.
-    # target_name : str, optional
-    #     Name of the target.
-    # target_position : `~astropy.coordinates.SkyCoord`, optional
-    #     Coordinates of the target.
-    obs_ids : list of str, optional
-        ID list of the used observations.
-    dataset_names : list of str, optional
-        Name list of the used datasets.
-    instrument : str, optional
-        Name of the instrument.
     creation : `~gammapy.utils.CreatorMetaData`, optional
         The creation metadata.
     optional : dict, optional
@@ -83,138 +63,15 @@ class FluxMetaData(MetaData):
     Note : these quantities are serialized in FITS header with the keywords stored in the dictionary FLUX_METADATA_FITS_KEYS
     """
 
-    _tag = "flux"
-    sed_type: Optional[SEDTYPEEnum]
-    sed_type_init: Optional[SEDTYPEEnum]
-    ul_conf: Optional[float]
-    n_sigma: Optional[float]
-    # n_sigma_ul: Optional[float]
-    sqrt_ts_threshold_ul: Optional[float]
-    n_sigma_sensitivity: Optional[float]
-    # target_name: Optional[str]
-    # target_position: Optional[SkyCoord]
-    target: Optional[TargetMetaData]
-    obs_ids: Optional[List[str]]
-    dataset_names: Optional[List[str]]
-    instrument: Optional[str]
-    creation: Optional[CreatorMetaData]
-    optional: Optional[dict]
-
-    # @validator("target_position")
-    # def validate_position(cls, v):
-    #     if v is None:
-    #         return SkyCoord(np.nan, np.nan, unit="deg", frame="icrs")
-    #     elif isinstance(v, SkyCoord):
-    #         return v.transform_to("icrs")
-    #     else:
-    #         raise ValidationError(
-    #             f"Incorrect position. Expect SkyCoord got {type(v)} instead."
-    #         )
-
-    # @validator("sed_type")
-    # def validate_sed_type(cls, v):
-    #     if isinstance(v, str):
-    #         if v not in SEDTYPE:
-    #             raise ValidationError(f"Incorrect [sed_type]. Expect {SEDTYPE}")
-    #     return v
-
-    # @validator("sed_type_init")
-    # def validate_sed_type_init(cls, v):
-    #     if isinstance(v, str):
-    #         if v not in SEDTYPE:
-    #             raise ValidationError(f"Incorrect [sed_type_init]. Expect {SEDTYPE}")
-    #     return v
-
-    @classmethod
-    def from_default(cls):
-        return cls(creation=CreatorMetaData.from_default())
-
-    @classmethod
-    def from_header(cls, data, format=None):
-        """Extract metadata from a FITS header.
-
-        Parameters
-        ----------
-        data : dict
-            Dictionary containing data.
-        format : str
-            Header format. Not yet used.
-        """
-
-        creation = CreatorMetaData.from_header(data)
-        if creation.date is None:
-            creation = CreatorMetaData.from_default()
-        meta = cls(creation=creation)
-
-        for item in FLUX_METADATA_FITS_KEYS.items():
-            if (
-                item[0] == "target_position"
-                and item[1][0] in data
-                and item[1][1] in data
-            ):
-                meta.target_position = FluxMetaData._target_from_string(
-                    data[item[1][0]], data[item[1][1]]
-                )
-            elif item[1] in data:
-                val = data[item[1]]
-                if item[0] == "obs_ids" or item[0] == "dataset_names":
-                    val = val.split()
-                setattr(meta, item[0], val)
-
-        # if "NSIGMAUL" in data:
-        #     meta.n_sigma_ul = float(data["NSIGMAUL"])
-        #     if meta.ul_conf and meta.n_sigma_ul != np.round(
-        #         stats.norm.isf(0.5 * (1 - meta.ul_conf)), 1
-        #     ):
-        #         log.warning(
-        #             f"Inconsistency between n_sigma_ul={meta.n_sigma_ul} and ul_conf={meta.ul_conf}"
-        #         )
-        # elif meta.ul_conf:
-        #     meta.n_sigma_ul = np.round(stats.norm.isf(0.5 * (1 - meta.ul_conf)), 1)
-
-        for kk in data:
-            if (
-                kk not in FLUX_METADATA_FITS_KEYS.values()
-                and kk not in METADATA_FITS_KEYS.values()
-                and kk not in FLUX_METADATA_FITS_KEYS["target_position"]
-            ):
-                if meta.optional is None:
-                    meta.optional = {}
-                meta.optional[str(kk).lower()] = data[kk]
-
-        return meta
-
-    def to_table(self, table):
-        """Write the metadata into a table. Only the non-null information are stored.
-
-        Parameters
-        ----------
-        table : `~astropy.table.Table`
-            Flux table.
-        """
-
-        for item in FLUX_METADATA_FITS_KEYS.items():
-            if item[0] == "target_position" and np.isfinite(self.target_position.ra):
-                val = FluxMetaData._target_to_string(self.target_position)
-                table.meta[item[1][0]] = val[0]
-                table.meta[item[1][1]] = val[1]
-            elif item[0] == "obs_ids" or item[0] == "dataset_names":
-                table.meta[item[1]] = " ".join(getattr(self, item[0]))
-            elif getattr(self, item[0]):
-                table.meta[item[1]] = str(getattr(self, item[0]))
-
-        table.meta.update(self.creation.to_header())
-
-        if self.optional:
-            for k in self.optional:
-                table.meta[str(k).upper()] = self.optional[k]
-
-    @staticmethod
-    def _target_from_string(v1, v2):
-        if "nan" in [v1, v2]:
-            return SkyCoord(np.nan, np.nan, unit="deg", frame="icrs")
-        return SkyCoord(v1, v2, unit="deg", frame="icrs")
-
-    @staticmethod
-    def _target_to_string(coord):
-        return coord.to_string(precision=6).split()
+    _tag: ClassVar[Literal["flux"]] = "flux"
+    sed_type: Optional[SEDTYPEEnum] = None
+    sed_type_init: Optional[SEDTYPEEnum] = None
+    n_sigma: Optional[float] = None
+    n_sigma_ul: Optional[float] = None
+    sqrt_ts_threshold_ul: Optional[float] = None
+    n_sigma_sensitivity: Optional[float] = None
+    target: Optional[TargetMetaData] = None
+    # ToDo: add obs_ids: Optional[List[int]] and instrument: Optional[str], or a List[ObsInfoMetaData]
+    # ToDo : add dataset_names: Optional[List[str]]
+    creation: Optional[CreatorMetaData] = CreatorMetaData()
+    optional: Optional[dict] = None
