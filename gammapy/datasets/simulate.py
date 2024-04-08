b4afa076ff3bacf93baf5447b89beebd16891126
Fabio Pintore <fabio.pintore@inaf.it>
2024-04-08 18:33:06 +0200

    use logging
    Signed-off-by: Fabio Pintore <fabio.pintore@inaf.it>

--- gammapy/datasets/simulate.py
+++ gammapy/datasets/simulate.py
@@ -1,6 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Simulate observations."""
 import html
+import logging
 from copy import deepcopy
 import numpy as np
 import astropy.units as u
@@ -20,6 +21,8 @@ from gammapy.utils.random import get_random_state
 from .map import create_map_dataset_from_observation
 
 __all__ = ["MapDatasetEventSampler", "ObservationEventSampler"]
+
+log = logging.getLogger(__name__)
 
 
 class MapDatasetEventSampler:
@@ -258,7 +261,7 @@ class MapDatasetEventSampler:
 
         events_all = EventList(Table())
         for idx, evaluator in enumerate(dataset.evaluators.values()):
-            print(f"Evaluating model: {evaluator.model.name}")
+            log.info(f"Evaluating model: {evaluator.model.name}")
             if evaluator.needs_update:
                 evaluator.update(
                     dataset.exposure,
@@ -311,7 +314,7 @@ class MapDatasetEventSampler:
 
         table = Table()
         if dataset.background:
-            print("Evaluating background...")
+            log.info("Evaluating background...")
             background = dataset.npred_background()
 
             temporal_model = ConstantTemporalModel()
@@ -588,7 +591,7 @@ class MapDatasetEventSampler:
 
         geom = dataset._geom
         selection = geom.contains(events.map_coord(geom))
-        print("Event sampling completed.")
+        log.info("Event sampling completed.")
         return events.select_row_subset(selection)
 
 
