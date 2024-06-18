744505c080355018ff7568df277a157a75f74fc3
cgalelli <claudio.galelli@obspm.fr>
2024-06-18 11:02:33 +0200

    Codestyle in type check
    Signed-off-by: cgalelli <claudio.galelli@obspm.fr>

--- gammapy/stats/variability.py
+++ gammapy/stats/variability.py
@@ -357,7 +357,7 @@ def TimmerKonig_lightcurve_simulator(
             "The power spectrum has to be provided as a callable function."
         )
 
-    if not type(npoints) == type(nchunks) == int:
+    if not isinstance(npoints, int) and isinstance(nchunks, int):
         raise TypeError("npoints and nchunks must be integers")
 
     if mean * spacing.value < 1 and poisson:
