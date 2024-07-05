2a7ffac31d0a4ae801f9c628a0f6b9a5e3e305b9
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2024-07-05 15:30:04 +0200

    Backport PR #5280: Add min_pix option in WcsGeom.cutout and set it to 3 for IRFs

--- gammapy/irf/core.py
+++ gammapy/irf/core.py
@@ -939,7 +939,7 @@ class IRFMap:
         """Copy IRF map."""
         return deepcopy(self)
 
-    def cutout(self, position, width, mode="trim"):
+    def cutout(self, position, width, mode="trim", min_npix=3):
         """Cutout IRF map.
 
         Parameters
@@ -952,15 +952,22 @@ class IRFMap:
         mode : {'trim', 'partial', 'strict'}, optional
             Mode option for Cutout2D, for details see `~astropy.nddata.utils.Cutout2D`.
             Default is "trim".
+        min_npix : bool, optional
+            Force width to a minimmum number of pixels.
+            Default is 3. The default is 3 pixels so interpolation is done correctly
+            if the binning of the IRF is larger than the width of the analysis region.
 
         Returns
         -------
         cutout : `IRFMap`
             Cutout IRF map.
         """
-        irf_map = self._irf_map.cutout(position, width, mode)
+
+        irf_map = self._irf_map.cutout(position, width, mode, min_npix=min_npix)
         if self.exposure_map:
-            exposure_map = self.exposure_map.cutout(position, width, mode)
+            exposure_map = self.exposure_map.cutout(
+                position, width, mode, min_npix=min_npix
+            )
         else:
             exposure_map = None
         return self.__class__(irf_map, exposure_map=exposure_map)
