835df57bacdec0ff02ae23050e91e563c52a824f
Bruno Khélifi <khelifi@in2p3.fr>
2024-07-24 20:54:59 +0200

    Ruff formatted
    Signed-off-by: Bruno Khélifi <khelifi@in2p3.fr>

--- gammapy/data/tests/test_utils.py
+++ gammapy/data/tests/test_utils.py
@@ -4,7 +4,6 @@ from gammapy.data.utils import check_time_intervals
 
 
 def test_check_time_intervals():
-
     assert check_time_intervals(1.0) is False
     assert check_time_intervals([]) is False
     assert check_time_intervals([53090.130, 53090.140]) is False
@@ -27,9 +26,14 @@ def test_check_time_intervals():
     ti = [aa, cc]
     assert check_time_intervals(ti) is True
 
-    # dd = [
-    #     Time(53080.150, format="mjd", scale="tt"),
-    #     Time(53091.160, format="mjd", scale="tt"),
-    # ]
-    # ti = [aa, bb, dd]
-    # assert check_time_intervals(ti) is False
+    dd = [
+        Time(53080.150, format="mjd", scale="tt"),
+        Time(53091.160, format="mjd", scale="tt"),
+    ]
+    ti = [aa, bb, dd]
+    assert check_time_intervals(ti) is False
+
+    from gammapy.data import utils
+
+    utils.CHECK_OVERLAPPING_TI = False
+    assert check_time_intervals(ti) is True
