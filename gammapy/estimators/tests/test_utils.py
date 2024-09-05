Fix tests error computation

gammapy/estimators/tests/test_utils.py
@@ -206,7 +206,7 @@ def test_compute_dcf():
     bins, dcf, dcf_err = compute_dcf(lighcurve, tau=5 * u.d)
     assert_allclose(bins, [-216000.0, 216000.0] * u.s)
     assert_allclose(dcf, [[-1.041667, -1.155327], [0.347222, 0.385109]], rtol=1e-6)
-    assert_allclose(dcf_err, [[0.0, 0.0], [1.388889, 1.540436]], rtol=1e-6)
+    assert_allclose(dcf_err, [[np.nan, np.nan], [0.850517, 0.943321]], rtol=1e-6)
 
 
 @requires_data()

gammapy/stats/variability.py
@@ -352,8 +352,12 @@ def discrete_correlation(flux1, flux_err1, flux2, flux_err2, time1, time2, tau, 
         [np.nanmean(udcf[bin_indices == i], axis=0) for i in range(1, len(bins))]
     )
     dcf_err = np.array(
-        [np.nanstd(udcf[bin_indices == i], axis=0) for i in range(1, len(bins))]
-    ) / (np.sqrt((len(time1) - 1) / (len(bins) - 1)))
+        [
+            np.sqrt(np.nansum((dcf[i - 1] - udcf[bin_indices == i]) ** 2, axis=0))
+            / (len(udcf[bin_indices == i]) - 1)
+            for i in range(1, len(bins))
+        ]
+    )
     bincenters = (bins[1:] + bins[:-1]) / 2
 
     return bincenters, dcf, dcf_err