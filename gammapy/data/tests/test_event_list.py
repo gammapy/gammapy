520674347ab1a5d36a54cbc8f69701b07e775907
Maxime Regeard <regeard@apc.in2p3.fr>
2024-09-20 12:59:45 +0200

    test events

--- gammapy/data/tests/test_event_list.py
+++ gammapy/data/tests/test_event_list.py
@@ -5,6 +5,7 @@ from numpy.testing import assert_allclose
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 from astropy.table import QTable, Table
+from astropy.io import fits
 from astropy.time import Time
 from regions import CircleSkyRegion, RectangleSkyRegion
 from gammapy.data import GTI, EventList, Observation, FixedPointingInfo
@@ -246,6 +247,10 @@ class TestEventListHESS:
     def test_peek(self):
         with mpl_plot_check():
             self.events.peek()
+
+    def test_select_time(self):
+        interval = Time([53090.12346118, 53090.12431144], scale="tt", format="mjd")
+        assert len(self.events.select_time(interval).time) == 499
 
 
 @requires_data()
