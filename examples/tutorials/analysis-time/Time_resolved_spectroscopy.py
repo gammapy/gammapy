4b2d9a13587ad6f427a05bb84e10d22a2406fa61
Bruno Khélifi <khelifi@in2p3.fr>
2024-10-02 15:55:50 +0200

    Update examples/tutorials/analysis-time/Time_resolved_spectroscopy.py
    Co-authored-by: Astro-Kirsty <AstroKirsty@gmail.com>

--- examples/tutorials/analysis-time/Time_resolved_spectroscopy.py
+++ examples/tutorials/analysis-time/Time_resolved_spectroscopy.py
@@ -33,7 +33,7 @@ The main idea behind doing a time resolved spectroscopy is to
 -  Perform standard data reduction on the above data
 -  Define a source model
 -  Fit the reduced data in each time bin with the source model
--  Extract relevant informations in a table
+-  Extract relevant information in a table
 
 Here, we will use the PKS 2155-304 observations from the
 `H.E.S.S. first public test data release <https://www.mpi-hd.mpg.de/hfm/HESS/pages/dl3-dr1/>`\ \_\_.
