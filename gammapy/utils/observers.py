6ed023f5c45b2b0f0b18de44050fc95b11675275
Daniel Morcuende <dmorcuende@iaa.es>
2024-10-21 12:53:07 +0200

    use ctao instead of cta in observatory location

--- gammapy/utils/observers.py
+++ gammapy/utils/observers.py
@@ -19,7 +19,7 @@ Not that with ``EarthLocation`` the orientation of angles is as follows:
 
 Available observatories (alphabetical order):
 
-- ``cta_south`` and ``cta_north`` for CTA, see
+- ``ctao_south`` and ``ctao_north`` for CTA, see
   `Website <https://www.cta-observatory.org/>`__ and
   `Wikipedia <https://en.wikipedia.org/wiki/Cherenkov_Telescope_Array>`__
 - ``hawc`` for HAWC, see
@@ -48,19 +48,19 @@ Examples
 >>> list(observatory_locations.keys())
 """
 
-# Values from https://www.cta-observatory.org/about/array-locations/chile/
+# Values from https://www.ctao.org/emission-to-discovery/array-sites/ctao-south/
 # Latitude: 24d41m0.34s South, Longitude: 70d18m58.84s West, Height: not given
 # Email from Gernot Maier on Sep 8, 2017, stating what they use in the CTA MC group:
 # lon=-70.31634499364885d, lat=-24.68342915473787d, height=2150m
-observatory_locations["cta_south"] = EarthLocation(
+observatory_locations["ctao_south"] = EarthLocation(
     lon="-70d18m58.84s", lat="-24d41m0.34s", height="2150m"
 )
 
-# Values from https://www.cta-observatory.org/about/array-locations/la-palma/
+# Values from https://www.ctao.org/emission-to-discovery/array-sites/ctao-north/
 # Latitude: 28d45m43.7904s North, Longitude: 17d53m31.218s West, Height: 2200 m
 # Email from Gernot Maier on Sep 8, 2017, stating what they use in the CTA MC group for MST-1:
 # lon=-17.891571d, lat=28.762158d, height=2147m
-observatory_locations["cta_north"] = EarthLocation(
+observatory_locations["ctao_north"] = EarthLocation(
     lon="-17d53m31.218s", lat="28d45m43.7904s", height="2147m"
 )
 
