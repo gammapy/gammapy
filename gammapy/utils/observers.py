6f88e6b8989a200ae92e660a2049bccf1e534f71
Daniel Morcuende <dmorcuende@iaa.es>
2024-10-22 17:16:43 +0200

    create a dict with observatories being able to raise deprecation warn

--- gammapy/utils/observers.py
+++ gammapy/utils/observers.py
@@ -2,109 +2,163 @@
 """Location of gamma-ray observatories."""
 import astropy.units as u
 from astropy.coordinates import EarthLocation
+import warnings
+
+warnings.simplefilter('always', DeprecationWarning)
 
 __all__ = ["observatory_locations"]
 
 
-observatory_locations = {}
-"""Gamma-ray observatory locations (dict).
+class Observatories:
+    """Gamma-ray observatory locations.
 
-This is a dict with observatory names as keys
-and values of type `~astropy.coordinates.EarthLocation`.
+    Class that defines a dict with observatory names as keys
+    and values of type `~astropy.coordinates.EarthLocation`.
 
-Not that with ``EarthLocation`` the orientation of angles is as follows:
+    Note that with ``EarthLocation`` the orientation of angles is as follows:
 
-- longitude is east for positive values and west for negative values
-- latitude is north for positive values and south for negative values
+    - longitude is east for positive values and west for negative values
+    - latitude is north for positive values and south for negative values
 
-Available observatories (alphabetical order):
+    Available observatories (alphabetical order):
 
-- ``ctao_south`` and ``ctao_north`` for CTAO, see
-  `Website <https://www.cta-observatory.org/>`__ and
-  `Wikipedia <https://en.wikipedia.org/wiki/Cherenkov_Telescope_Array>`__
-- ``hawc`` for HAWC, see
-  `Website <https://www.hawc-observatory.org/>`__ and
-  `Wikipedia <https://en.wikipedia.org/wiki/High_Altitude_Water_Cherenkov_Experiment>`__
-- ``hegra`` for HEGRA, see `Wikipedia <https://en.wikipedia.org/wiki/HEGRA>`__
-- ``hess`` for HESS, see
-  `Website <https://www.mpi-hd.mpg.de/hfm/HESS/>`__ and
-  `Wikipedia <https://en.wikipedia.org/wiki/HESS>`__
-- ``magic`` for MAGIC, see
-  `Website <https://wwwmagic.mpp.mpg.de/>`__ and
-  `Wikipedia <https://en.wikipedia.org/wiki/MAGIC_(telescope)>`__
-- ``milagro`` for MILAGRO, see
-  `Wikipedia <https://en.wikipedia.org/wiki/Milagro_(experiment)>`__)
-- ``veritas`` for VERITAS, see
-  `Website <https://veritas.sao.arizona.edu/>`__ and
-  `Wikipedia <https://en.wikipedia.org/wiki/VERITAS>`__
-- ``whipple`` for WHIPPLE, see
-  `Wikipedia <https://en.wikipedia.org/wiki/Fred_Lawrence_Whipple_Observatory>`__
+    - ``ctao_south`` and ``ctao_north`` for CTAO, see
+      `Website <https://www.cta-observatory.org/>`__ and
+      `Wikipedia <https://en.wikipedia.org/wiki/Cherenkov_Telescope_Array>`__
+    - ``hawc`` for HAWC, see
+      `Website <https://www.hawc-observatory.org/>`__ and
+      `Wikipedia <https://en.wikipedia.org/wiki/High_Altitude_Water_Cherenkov_Experiment>`__
+    - ``hegra`` for HEGRA, see `Wikipedia <https://en.wikipedia.org/wiki/HEGRA>`__
+    - ``hess`` for HESS, see
+      `Website <https://www.mpi-hd.mpg.de/hfm/HESS/>`__ and
+      `Wikipedia <https://en.wikipedia.org/wiki/HESS>`__
+    - ``magic`` for MAGIC, see
+      `Website <https://wwwmagic.mpp.mpg.de/>`__ and
+      `Wikipedia <https://en.wikipedia.org/wiki/MAGIC_(telescope)>`__
+    - ``milagro`` for MILAGRO, see
+      `Wikipedia <https://en.wikipedia.org/wiki/Milagro_(experiment)>`__)
+    - ``veritas`` for VERITAS, see
+      `Website <https://veritas.sao.arizona.edu/>`__ and
+      `Wikipedia <https://en.wikipedia.org/wiki/VERITAS>`__
+    - ``whipple`` for WHIPPLE, see
+      `Wikipedia <https://en.wikipedia.org/wiki/Fred_Lawrence_Whipple_Observatory>`__
 
-Examples
---------
+    Examples
+    --------
 
->>> from gammapy.data import observatory_locations
->>> observatory_locations['hess']
->>> list(observatory_locations.keys())
-"""
+    >>> from gammapy.data import observatory_locations
+    >>> observatory_locations['hess']
+    >>> list(observatory_locations.keys())
+    """
+    def __init__(self):
+        self._data = {
+            # Values from https://www.ctao.org/emission-to-discovery/array-sites/ctao-south/
+            # Latitude: 24d41m0.34s South, Longitude: 70d18m58.84s West, Height: not given
+            # Email from Gernot Maier on Sep 8, 2017, stating what they use in the CTA MC group:
+            # lon=-70.31634499364885d, lat=-24.68342915473787d, height=2150m
+            "ctao_south": EarthLocation(
+                lon="-70d18m58.84s", lat="-24d41m0.34s", height="2150m"
+            ),
+            # Values from https://www.ctao.org/emission-to-discovery/array-sites/ctao-north/
+            # Latitude: 28d45m43.7904s North, Longitude: 17d53m31.218s West, Height: 2200 m
+            # Email from Gernot Maier on Sep 8, 2017, stating what they use in the CTA MC group for MST-1:
+            # lon=-17.891571d, lat=28.762158d, height=2147m
+            "ctao_north": EarthLocation(
+                lon="-17d53m31.218s", lat="28d45m43.7904s", height="2147m"
+            ),
+            # HAWC location taken from https://arxiv.org/pdf/1108.6034v2.pdf
+            "hawc": EarthLocation(
+                lon="-97d18m34s", lat="18d59m48s", height="4100m"
+            ),
+            # https://en.wikipedia.org/wiki/HEGRA
+            "hegra": EarthLocation(
+                lon="28d45m42s", lat="17d53m27s", height="2200m"
+            ),
+            # Precision position of HESS from the HESS software (slightly different from Wikipedia)
+            "hess": EarthLocation(
+                lon="16d30m00.8s", lat="-23d16m18.4s", height="1835m"
+            ),
+            "magic": EarthLocation(
+                lon="-17d53m24s", lat="28d45m43s", height="2200m"
+            ),
+            "milagro": EarthLocation(
+                lon="-106.67625d", lat="35.87835d", height="2530m"
+            ),
+            "veritas": EarthLocation(
+                lon="-110d57m07.77s", lat="31d40m30.21s", height="1268m"
+            ),
+            # WHIPPLE coordinates taken from the Observatory Wikipedia page:
+            # https://en.wikipedia.org/wiki/Fred_Lawrence_Whipple_Observatory
+            "whipple": EarthLocation(
+                lon="-110d52m42s", lat="31d40m52s", height="2606m"
+            ),
+            # communication with ASTRI Project Manager
+            "astri": EarthLocation(
+                lon="-16d30m20.99s", lat="28d18m00.0s", height="2370m"
+            ),
+            # coordinates from fact-tools (based on google earth)
+            "fact": EarthLocation(
+                lat=28.761647 * u.deg,
+                lon=-17.891116 * u.deg,
+                height=2200 * u.m,
+            ),
+        }
+        # Map old key names to new ones
+        self._deprecated_keys = {
+            'cta_south': 'ctao_south',
+            'cta_north': 'ctao_north'
+        }
 
-# Values from https://www.ctao.org/emission-to-discovery/array-sites/ctao-south/
-# Latitude: 24d41m0.34s South, Longitude: 70d18m58.84s West, Height: not given
-# Email from Gernot Maier on Sep 8, 2017, stating what they use in the CTA MC group:
-# lon=-70.31634499364885d, lat=-24.68342915473787d, height=2150m
-observatory_locations["ctao_south"] = EarthLocation(
-    lon="-70d18m58.84s", lat="-24d41m0.34s", height="2150m"
-)
+    def _handle_deprecated_key(self, key):
+        """
+        Raise a deprecation warning if provided key is deprecated.
 
-# Values from https://www.ctao.org/emission-to-discovery/array-sites/ctao-north/
-# Latitude: 28d45m43.7904s North, Longitude: 17d53m31.218s West, Height: 2200 m
-# Email from Gernot Maier on Sep 8, 2017, stating what they use in the CTA MC group for MST-1:
-# lon=-17.891571d, lat=28.762158d, height=2147m
-observatory_locations["ctao_north"] = EarthLocation(
-    lon="-17d53m31.218s", lat="28d45m43.7904s", height="2147m"
-)
+        Returns the corresponding new key if the given is deprecated.
+        """
+        if key in self._deprecated_keys:
+            new_key = self._deprecated_keys[key]
+            warnings.warn(
+                f"'{key}' is deprecated and will be removed in future versions. "
+                f"Use '{new_key}' instead.",
+                DeprecationWarning,
+                stacklevel=3
+            )
+            return new_key
+        return key
 
-# HAWC location taken from https://arxiv.org/pdf/1108.6034v2.pdf
-observatory_locations["hawc"] = EarthLocation(
-    lon="-97d18m34s", lat="18d59m48s", height="4100m"
-)
+    def __getitem__(self, key):
+        key = self._handle_deprecated_key(key)
+        return self._data[key]
 
-# https://en.wikipedia.org/wiki/HEGRA
-observatory_locations["hegra"] = EarthLocation(
-    lon="28d45m42s", lat="17d53m27s", height="2200m"
-)
+    def __setitem__(self, key, value):
+        """
+        Check if the provided key is deprecated and issues a warning if it is.
 
-# Precision position of HESS from the HESS software (slightly different from Wikipedia)
-observatory_locations["hess"] = EarthLocation(
-    lon="16d30m00.8s", lat="-23d16m18.4s", height="1835m"
-)
+        It then stores the value under the appropriate key, either the original or the new key if deprecated.
+        """
+        key = self._handle_deprecated_key(key)
+        self._data[key] = value
 
-observatory_locations["magic"] = EarthLocation(
-    lon="-17d53m24s", lat="28d45m43s", height="2200m"
-)
+    def __contains__(self, key):
+        key = self._handle_deprecated_key(key)
+        return key in self._data
 
-observatory_locations["milagro"] = EarthLocation(
-    lon="-106.67625d", lat="35.87835d", height="2530m"
-)
+    def keys(self):
+        """Return the keys of the dictionary."""
+        return list(self._data.keys())
 
-observatory_locations["veritas"] = EarthLocation(
-    lon="-110d57m07.77s", lat="31d40m30.21s", height="1268m"
-)
+    def values(self):
+        """Return the values of the dictionary."""
+        return list(self._data.values())
 
-# WHIPPLE coordinates taken from the Observatory Wikipedia page:
-# https://en.wikipedia.org/wiki/Fred_Lawrence_Whipple_Observatory
-observatory_locations["whipple"] = EarthLocation(
-    lon="-110d52m42s", lat="31d40m52s", height="2606m"
-)
+    def items(self):
+        """Return the items (key-value pairs) of the dictionary."""
+        return list(self._data.items())
+  
+    def get_all(self):
+        """Return the entire dictionary."""
+        return self._data
 
-# communication with ASTRI Project Manager
-observatory_locations["astri"] = EarthLocation(
-    lon="-16d30m20.99s", lat="28d18m00.0s", height="2370m"
-)
 
-# coordinates from fact-tools (based on google earth)
-observatory_locations["fact"] = EarthLocation(
-    lat=28.761647 * u.deg,
-    lon=-17.891116 * u.deg,
-    height=2200 * u.m,
-)
+
+observatory_locations = Observatories()
