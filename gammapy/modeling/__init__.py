ddb93755df69084ccbffeab500180d00c874281b
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2024-12-17 14:03:43 +0100

    Move `_parse_datasets` to `gammapy.modeling.utils`
    Move _parse_datasets to gammapy.modeling.utils (new)
    
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/modeling/__init__.py
+++ gammapy/modeling/__init__.py
@@ -1,10 +1,12 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Models and fitting."""
+
 from .covariance import Covariance
 from .fit import CovarianceResult, Fit, FitResult, OptimizeResult
 from .parameter import Parameter, Parameters, PriorParameter, PriorParameters
 from .scipy import stat_profile_ul_scipy
 from .selection import select_nested_models
+
 
 __all__ = [
     "Covariance",
