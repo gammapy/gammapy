a627a12cf6b3a2429bf0bba6d597cf72721eb3e3
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2024-12-19 14:54:38 +0100

    add function create_observation_from_fermi_files
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/data/__init__.py
+++ gammapy/data/__init__.py
@@ -1,5 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Data and observation handling."""
+
 from gammapy.utils.observers import observatory_locations
 from .data_store import DataStore
 from .event_list import EventList
@@ -8,12 +9,13 @@ from .gti import GTI
 from .hdu_index_table import HDUIndexTable
 from .metadata import EventListMetaData, ObservationMetaData
 from .obs_table import ObservationTable
-from .observations import Observation, Observations
+from .observations import Observation, Observations, create_observation_from_fermi_files
 from .pointing import FixedPointingInfo, PointingInfo, PointingMode
 from .simulate import ObservationsEventsSampler
 from .utils import get_irfs_features
 
 __all__ = [
+    "create_observation_from_fermi_files",
     "DataStore",
     "EventList",
     "EventListMetaData",
