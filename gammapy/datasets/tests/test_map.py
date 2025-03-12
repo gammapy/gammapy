efeb20d1ca67f73e47acecac2c937aeb8040c7f2
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2025-03-12 12:36:21 +0100

    fix test
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/datasets/tests/test_map.py
+++ gammapy/datasets/tests/test_map.py
@@ -1795,13 +1795,8 @@ def test_map_dataset_on_off_to_spectrum_dataset_weights():
         center=dataset.counts.geom.center_skydir, radius=1.5 * u.deg
     )
 
-    spectrum_dataset = dataset.to_spectrum_dataset(on_region)
-
-    assert_allclose(spectrum_dataset.counts.data[:, 0, 0], [0, 2, 2])
-    assert_allclose(spectrum_dataset.counts_off.data[:, 0, 0], [0, 4, 4])
-    assert_allclose(spectrum_dataset.acceptance.data[:, 0, 0], [0, 0.08, 0.08])
-    assert_allclose(spectrum_dataset.acceptance_off.data[:, 0, 0], [0, 0.32, 0.32])
-    assert_allclose(spectrum_dataset.alpha.data[:, 0, 0], [0, 0.25, 0.25])
+    with pytest.raises(Exception):
+        dataset.to_spectrum_dataset(on_region)
 
 
 @requires_data()
