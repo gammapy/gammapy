d787bb90d2fb2f584ea0d5c30e281981b35b3807
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2025-03-28 14:53:05 +0100

    cleanup
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/data/data_store.py
+++ gammapy/data/data_store.py
@@ -342,7 +342,7 @@ class DataStore:
 
         return Observation(**kwargs)
 
-    def datastore_select(self, selection_dict=None, selection_mask=None, strict=False):
+    def select(self, selection_dict=None, selection_mask=None, strict=False):
         """Select a subset of the datastore.
 
         Parameters
