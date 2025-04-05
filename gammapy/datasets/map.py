368de08eced69d2192336b413e38f905813b04e7
PreisTo <19503558+PreisTo@users.noreply.github.com>
2025-04-05 22:37:10 +0200

    black formatting

--- gammapy/datasets/map.py
+++ gammapy/datasets/map.py
@@ -588,17 +588,25 @@ class MapDataset(Dataset):
     def background_model(self):
         if self.models is None:
             return None
-            
         bkg_model = None
         for k in self.models.names:
             v = self.models[k]
-            if isinstance(v,FoVBackgroundModel) and bkg_model is None and self.name in v.datasets_names:
+            if (
+                isinstance(v, FoVBackgroundModel)
+                and bkg_model is None
+                and self.name in v.datasets_names
+            ):
                 bkg_model = k
-            elif isinstance(v,FoVBackgroundModel) and bkg_model is not None and self.name in v.datasets_names:
-                raise AttributeError(f"Only one Background Model per Dataset - already selected {bkg_model.name}")
+            elif (
+                isinstance(v, FoVBackgroundModel)
+                and bkg_model is not None
+                and self.name in v.datasets_names
+            ):
+                raise AttributeError(
+                    f"Only one Background Model per Dataset - already selected {bkg_model.name}"
+                )
         if bkg_model is not None:
             return self.models[bkg_model]
-
 
     def __str__(self):
         str_ = f"{self.__class__.__name__}\n"
