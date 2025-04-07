f916a3215d6262e28b5154f53f0a387015d52fc9
Maxime Regeard <regeard@apc.in2p3.fr>
2025-04-07 19:08:48 +0200

    add 3PC pulse profile plot

--- gammapy/visualization/utils.py
+++ gammapy/visualization/utils.py
@@ -427,8 +427,13 @@ def plot_distribution(
 
 
 def plot_pulse_profile_3PC(
-    source, n_period=2, add_radio_profile=True, add_best_fit_profile=True
+    source,
+    n_period=2,
+    add_radio_profile=True,
+    add_best_fit_profile=True,
+    add_error=True,
 ):
+    """"""
     # Import here to avoid circular imports
     from gammapy.catalog import SourceCatalogObject3PC
 
@@ -463,8 +468,11 @@ def plot_pulse_profile_3PC(
     axes[0].tick_params(
         axis="x", direction="inout", labeltop=True, top=True, bottom=True
     )
+    axes[-1].tick_params(
+        axis="x", direction="inout", labelbottom=True, top=True, bottom=True
+    )
 
-    ax_ylim = [0, None]
+    ax_ylim = [np.inf, 0]
 
     if add_radio_profile:
         radio_profile = source.pulse_profile_radio
@@ -491,7 +499,7 @@ def plot_pulse_profile_3PC(
         axes[0].plot(
             best_fit_axis.as_plot_center,
             best_fit_profile.data.squeeze(),
-            color="#008080",
+            color="teal",
             lw=2,
             ls="--",
             label="Best-fit",
@@ -517,14 +525,30 @@ def plot_pulse_profile_3PC(
         weights=data,
         histtype="step",
         lw=1,
-        label="LAT > 100 MeV",
+        label="> 100 MeV",
     )
     ax_ylim[0] = min(data.min() - 0.1 * data.min(), ax_ylim[0])
     ax_ylim[1] = max(data.max() + 0.1 * data.max(), ax_ylim[1])
+    if add_error:
+        errors = profiles["Unc_GT100_WtCnt"].data.squeeze()
+        axes[0].errorbar(
+            axis.as_plot_center,
+            data,
+            yerr=errors,
+            fmt="none",
+            color="#202449",
+            elinewidth=0.5,
+        )
+        ax_ylim[0] = min(
+            (data - errors).min() - 0.1 * (data - errors).min(), ax_ylim[0]
+        )
+        ax_ylim[1] = max(
+            (data + errors).max() + 0.1 * (data + errors).max(), ax_ylim[1]
+        )
 
     axes[0].set_ylim(ax_ylim)
     axes[0].set_xlim(0, int(n_period))
-    axes[0].legend()
+    axes[0].legend(loc=1)
 
     for i, (ax, name) in enumerate(
         zip(np.concatenate([axes[1:2], axes[1:]]), reversed(key_names))
@@ -537,9 +561,7 @@ def plot_pulse_profile_3PC(
             kwargs["histtype"] = "stepfilled"
         axis = profiles[name].geom.axes["phase"]
         data = profiles[name].data.squeeze()
-        label = (
-            f"LAT {int(name.split('_')[0])/1000} - {int(name.split('_')[1])/1000} GeV"
-        )
+        label = f"{int(name.split('_')[0])/1000} - {int(name.split('_')[1])/1000} GeV"
         ax.hist(
             axis.as_plot_center,
             bins=axis.as_plot_edges,
@@ -548,8 +570,28 @@ def plot_pulse_profile_3PC(
             lw=1,
             **kwargs,
         )
-        ax.set_ylim(data.min() - 0.1 * data.min(), data.max() + 0.1 * data.max())
+        ylim = [data.min() - 0.1 * data.min(), data.max() + 0.1 * data.max()]
+        if add_error:
+            errors = profiles[f"Unc_{name}"].data.squeeze()
+            ax.errorbar(
+                axis.as_plot_center,
+                data,
+                yerr=errors,
+                fmt="none",
+                color=kwargs["color"],
+                elinewidth=0.5,
+            )
+            ylim[0] = min((data - errors).min() - 0.1 * (data - errors).min(), ylim[0])
+            ylim[1] = max((data + errors).max() + 0.1 * (data + errors).max(), ylim[1])
+        if i != 0:
+            ax.set_ylim(ylim)
         ax.set_xlim(0, int(n_period))
-        ax.legend()
+        ax.legend(loc=1)
+
+    for ax in axes:
+        ax.set_ylabel("Weighted Counts")
+        ax.set_xlabel("Phase")
+
+    plt.suptitle(f"PSR {source.name}")
 
     return axes
