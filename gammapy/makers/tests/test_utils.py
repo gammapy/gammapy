45d3fbeaea576ab8e68497991aa2d9385c69e4d0
Bruno Khélifi <khelifi@in2p3.fr>
2025-05-07 16:15:45 +0200

    Apply comments

--- gammapy/makers/tests/test_utils.py
+++ gammapy/makers/tests/test_utils.py
@@ -572,7 +572,9 @@ def test_make_effective_livetime_map():
 def test_data_store_get_effective_livetime(caplog):
     """Test the computation of the livetime for a test position"""
     ds = DataStore.from_dir("$GAMMAPY_DATA/hess-dl3-dr1")
-    position = SkyCoord.from_name("crab")
+    position = SkyCoord(
+        ra=83.6324, dec=22.0174, frame="icrs", unit="deg"
+    )  # Crab position
     with caplog.at_level(logging.INFO):
         livetime_maps, sel_ids = get_effective_livetime(datastore=ds, position=position)
         assert livetime_maps.geom.shape_axes == (1,)
