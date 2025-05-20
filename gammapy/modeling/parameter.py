d95c3b3b69c04f6c96cffa575cc7e80c1cd8333d
Bruno Khélifi <khelifi@in2p3.fr>
2025-05-20 17:44:25 +0200

    docstrings (#5825)
    * docstrings
    
    * Update gammapy/modeling/parameter.py
    
    Co-authored-by: Régis Terrier <regis.terrier@m4x.org>
    
    * PR comment
    
    * PR comment
    
    * Update gammapy/modeling/models/prior.py
    
    Co-authored-by: Kirsty Feijen <AstroKirsty@gmail.com>
    
    * Update gammapy/modeling/models/prior.py
    
    Co-authored-by: Kirsty Feijen <AstroKirsty@gmail.com>
    
    * Update gammapy/modeling/parameter.py
    
    Co-authored-by: Kirsty Feijen <AstroKirsty@gmail.com>
    
    * Update gammapy/modeling/models/prior.py
    
    * Update gammapy/modeling/models/prior.py
    
    * Update gammapy/modeling/models/prior.py
    
    * Update gammapy/modeling/models/prior.py
    
    * Update gammapy/modeling/parameter.py
    
    * Update gammapy/modeling/parameter.py
    
    * Update gammapy/modeling/parameter.py
    
    * Update gammapy/modeling/parameter.py
    
    * Update gammapy/modeling/parameter.py
    
    * Update gammapy/modeling/parameter.py
    
    * Update gammapy/modeling/parameter.py
    
    * Update gammapy/modeling/parameter.py
    
    * Update gammapy/modeling/parameter.py
    
    * Update gammapy/modeling/parameter.py
    
    Co-authored-by: Régis Terrier <regis.terrier@m4x.org>
    
    * Review comments
    
    * Review comments
    
    * Update gammapy/modeling/models/prior.py
    
    Co-authored-by: Kirsty Feijen <AstroKirsty@gmail.com>
    
    * Update gammapy/modeling/models/prior.py
    
    Co-authored-by: Kirsty Feijen <AstroKirsty@gmail.com>
    
    * Update gammapy/modeling/models/prior.py
    
    Co-authored-by: Kirsty Feijen <AstroKirsty@gmail.com>
    
    * cleanup
    
    Signed-off-by: Astro-Kirsty <AstroKirsty@gmail.com>
    
    ---------
    
    Signed-off-by: Astro-Kirsty <AstroKirsty@gmail.com>
    Co-authored-by: Régis Terrier <regis.terrier@m4x.org>
    Co-authored-by: Kirsty Feijen <AstroKirsty@gmail.com>

--- gammapy/modeling/parameter.py
+++ gammapy/modeling/parameter.py
@@ -70,33 +70,34 @@ class Parameter:
     scale : float, optional
         Scale (sometimes used in fitting).
     unit : `~astropy.units.Unit` or str, optional
-        Unit.
+        Unit. Default is "".
     min : float, str or `~astropy.units.quantity`, optional
-        Minimum (sometimes used in fitting). Default is None, which set the attribute to `np.nan`.
+        Minimum (sometimes used in fitting). If `None`, set to `numpy.nan`. Default is None.
     max : float, str or `~astropy.units.quantity`, optional
-        Maximum (sometimes used in fitting). Default is None, which set the attribute to `np.nan`.
+        Maximum (sometimes used in fitting). Default is `numpy.nan`.
     frozen : bool, optional
-        Frozen (used in fitting).
-    error : float
-        Parameter error.
-    scan_min : float
+        Frozen (used in fitting).  Default is False.
+    error : float, optional
+        Parameter error. Default is 0.
+    scan_min : float, optional
         Minimum value for the parameter scan. Overwrites scan_n_sigma.
-    scan_max : float
+        Default is None.
+    scan_max : float, optional
         Maximum value for the parameter scan. Overwrites scan_n_sigma.
-    scan_n_values: int
-        Number of values to be used for the parameter scan.
-    scan_n_sigma : int
-        Number of sigmas to scan.
-    scan_values: `numpy.array`
+        Default is None.
+    scan_n_values: int, optional
+        Number of values to be used for the parameter scan. Default is 11.
+    scan_n_sigma : int, optional
+        Number of sigmas to scan. Default is 2.
+    scan_values: `numpy.array`, optional
         Scan values. Overwrites all the scan keywords before.
-    scale_method : {'scale10', 'factor1', None}
-        Method used to set ``factor`` and ``scale``.  Default is "scale10".
-    interp : {"lin", "sqrt", "log"}
-        Parameter scaling to use for the scan.  Default is "lin".
-    prior : `~gammapy.modeling.models.Prior`
-        Prior set on the parameter.
-    scale_transform : {"lin", "sqrt", "log"}
-        Parameter scaling. Default is "lin".
+        Default is None.
+    scale_method : {'scale10', 'factor1', None}, optional
+        Method used to set ``factor`` and ``scale``. Default is "scale10".
+    interp : {"lin", "sqrt", "log"}, optional
+        Parameter scaling to use for the scan. Default is "lin".
+    prior : `~gammapy.modeling.models.Prior`, optional
+        Prior set on the parameter. Default is None.
     """
 
     def __init__(
@@ -253,7 +254,7 @@ class Parameter:
     def factor_min(self):
         """Factor minimum as a float (used by the optimizer).
 
-        By default when no transform is applied, ``factor_min = min / scale``,
+        By default, when no transform is applied, ``factor_min = min / scale``,
         otherwise ``factor_min = transform(min)``.
         """
         return self.transform(self.min)
@@ -275,7 +276,7 @@ class Parameter:
     def factor_max(self):
         """Factor maximum as a float (used by the optimizer).
 
-        By default when no transform is applied, ``factor_max = max / scale``,
+        By default, when no transform is applied, ``factor_max = max / scale``,
         otherwise ``factor_max = transform(max)``.
         """
         return self.transform(self.max)
@@ -294,9 +295,9 @@ class Parameter:
 
         Parameters
         ----------
-        min, max: float, `~astropy.units.Quantity`, str
+        min, max: float, `~astropy.units.Quantity` or str, optional
             Minimum and Maximum value to assign to the parameter `min` and `max`.
-            Default is None, which set `min` and `max` to `np.nan`.
+            Default is None, which set `min` and `max` to `~numpy.nan`.
         """
         if min is not None:
             self.min = min
@@ -438,7 +439,7 @@ class Parameter:
 
     @property
     def scan_values(self):
-        """Stat scan values as a `~numpy.ndarray`."""
+        """Stat scan values as a `numpy.ndarray`."""
         if self._scan_values is None:
             scale = interpolation_scale(self.interp)
             parmin, parmax = scale([self.scan_min, self.scan_max])
@@ -534,12 +535,12 @@ class Parameter:
             self._scale = value
 
     def transform(self, value, update_scale=False):
-        """Tranform from value to factor (used by the optimizer).
+        """Transform from value to factor (used by the optimizer).
 
         Parameters
         ----------
         value : float
-            Parameter value
+            Parameter value.
         update_scale : bool, optional
             Update the scaling (used by the autoscale). Default is False.
         """
@@ -550,30 +551,30 @@ class Parameter:
         return transformed_value / self.scale
 
     def inverse_transform(self, factor):
-        """Inverse tranform from factor (used by the optimizer) to value.
+        """Inverse transform from factor (used by the optimizer) to value.
 
         Parameters
         ----------
-        value : float
-            Parameter factor
+        factor : float
+            Parameter factor.
         """
         interp_scale = interpolation_scale(self.scale_transform)
         value = interp_scale.inverse(self.scale * factor)
         return value
 
     def _inverse_transform_derivative(self, factor):
-        """Inverse tranform from factor (used by the optimizer) to value.
+        """Inverse transform from factor (used by the optimizer) to value.
 
         Parameters
         ----------
-        value : float
-            Parameter factor
+        factor : float
+            Parameter factor.
         """
         interp_scale = interpolation_scale(self.scale_transform)
         return interp_scale._inverse_deriv(self.scale * factor) * self.scale
 
     def autoscale(self):
-        "Apply `interpolation_scale' and `scale_method' to the parameter."
+        "Apply `~gammapy.utils.interpolation.interpolation_scale` and `scale_method` to the parameter."
         self.factor = self.transform(self.value, update_scale=True)
 
     def reset_autoscale(self):
@@ -906,6 +907,26 @@ class restore_parameters_status:
 
 
 class PriorParameter(Parameter):
+    """Parameter of a `~gammapy.modeling.models.Prior`.
+
+    A prior is a probability density function of a model parameter and can take different forms, including but not limited to Gaussian
+    distributions and uniform distributions. The prior includes information or knowledge about the dataset or the
+    parameters of the fit.
+
+    Parameters
+    ----------
+    name : str
+        Name.
+    value : float or `~astropy.units.Quantity`
+        Value.
+    unit : `~astropy.units.Unit` or str, optional
+        Unit. Default is "".
+
+    Examples
+    --------
+    For a usage example see :doc:`/tutorials/api/priors` tutorial.
+    """
+
     def __init__(
         self,
         name,
@@ -959,6 +980,16 @@ class PriorParameter(Parameter):
 
 
 class PriorParameters(Parameters):
+    """Container of parameter priors :
+
+    - List of `PriorParameter` objects.
+
+    Parameters
+    ----------
+    parameters : list of `PriorParameter`
+        List of parameters.
+    """
+
     def __init__(self, parameters=None):
         if parameters is None:
             parameters = []
