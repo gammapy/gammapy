159336ae55a70e33fce5f718a6217aff96e576a2
Régis Terrier <rterrier@apc.in2p3.fr>
2025-05-20 10:38:59 +0200

    Add freeze decorator
    Signed-off-by: Régis Terrier <rterrier@apc.in2p3.fr>

---
+++ gammapy/utils/freeze_attr.py
@@ -0,0 +1,51 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""Decorator to freeze class attribute setting."""
+
+from functools import wraps
+
+
+def freeze(cls):
+    """Decorator function to freeze public instance attributes after calling its __init__() method.
+
+    The list of allowed attributes is obtained from the instance level attributes and the class level attributes
+    by the set_allowed_attrs function. It only considers public attributes (i.e. without _ at the start).
+
+    The __setattr__ method is overridden to check if the key is in the list of allowed attributes.
+
+    A ._frozen attribute is set to True only by the __init__() of the decorated function to support inheritance
+    of frozen classes.
+    """
+    original_init = cls.__init__
+
+    @staticmethod
+    def set_allowed_keys(obj):
+        allowed_keys = {k for k in obj.__class__.__dict__ if not k.startswith("_")}
+        allowed_keys.update({k for k in obj.__dict__ if not k.startswith("_")})
+        return allowed_keys
+
+    @wraps(original_init)
+    def __init__(self, *args, **kwargs):
+        self._frozen = False
+
+        # Call the original constructor
+        original_init(self, *args, **kwargs)
+
+        self._allowed_keys = set_allowed_keys(self)
+
+        # Freeze the object only if were are in its __init__ method
+        if self.__class__ is cls:
+            self._frozen = True
+
+    def __setattr__(self, key, value):
+        if getattr(self, "_frozen", False) and key not in getattr(
+            self, "_allowed_keys", set()
+        ):
+            raise AttributeError(
+                f"{key} is not a valid attribute of {type(self).__name__}."
+            )
+        object.__setattr__(self, key, value)
+
+    cls.__init__ = __init__
+    cls.__setattr__ = __setattr__
+
+    return cls
