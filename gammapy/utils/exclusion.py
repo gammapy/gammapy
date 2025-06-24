78e3a83dec02c8d1902ffa2c76fcf13d5a2d2ca9
Samantha Wong <samantha.wong2@mail.mcgill.ca>
2025-06-24 16:37:33 -0400

    reformatting

--- gammapy/utils/exclusion.py
+++ gammapy/utils/exclusion.py
@@ -1,3 +1,4 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 import astropy.units as u
 import numpy as np
 from astropy.table import Table
@@ -5,10 +6,12 @@ from regions import *
 from astropy.coordinates import SkyCoord
 import os
 
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Exclusion regions for source position, stars, and other gamma-ray sources"""
 
-def make_exclusion_mask(source_position,geom,rad=3*u.deg,max_star_mag=6,other_exc=[]):
+
+def make_exclusion_mask(
+    source_position, geom, rad=3 * u.deg, max_star_mag=6, other_exc=[]
+):
     """
     Create an exclusion mask for the source position, any stars in the field of view,
     and any user-provided regions.
@@ -18,37 +21,42 @@ def make_exclusion_mask(source_position,geom,rad=3*u.deg,max_star_mag=6,other_ex
     source_position : `~regions.SkyRegion`
         Source exclusion region coordinates (can be any geometry).
     geom: `~gammapy.maps.WcsGeom`
-        Map geometry for the exclusion mask. 
+        Map geometry for the exclusion mask.
     rad : `~astropy.units.Quantity`
         Radius around `source_position` to search for stellar exclusion regions.
-    max_star_mag : 
-        The maximum stellar magnitude (Johnson-Cousins B-band) to exclude. Maximum value is 8th magnitude.  
+    max_star_mag :
+        The maximum stellar magnitude (Johnson-Cousins B-band) to exclude. Maximum value is 8th magnitude.
     other_exc : list of `~regions.SkyRegion`
-        Other regions to exclude (can be any geometry). 
+        Other regions to exclude (can be any geometry).
 
     Returns
     -------
     exclusion_mask : `~gammapy.maps.WcsNDMap`
-        2D exclusion mask 
+        2D exclusion mask
     """
     exc = [source_position]
 
     for src in other_exc:
         exc.append(src)
-    star_data = np.loadtxt(os.environ.get("GAMMAPY_DATA")+"/veritas/crab-point-like-ED/Hipparcos_MAG8_1997.dat",usecols=(0, 1, 2, 3))
+    star_data = np.loadtxt(
+        os.environ.get("GAMMAPY_DATA")
+        + "/veritas/crab-point-like-ED/Hipparcos_MAG8_1997.dat",
+        usecols=(0, 1, 2, 3),
+    )
     star_cat = Table(
-    {
-        "ra": star_data[:, 0],
-        "dec": star_data[:, 1],
-        "id": star_data[:, 2],
-        "mag": star_data[:, 3],
-    }
+        {
+            "ra": star_data[:, 0],
+            "dec": star_data[:, 1],
+            "id": star_data[:, 2],
+            "mag": star_data[:, 3],
+        }
     )
     star_mask = (
-    np.sqrt(
-        (star_cat["ra"] - source_position.center.ra.deg) ** 2
-        + (star_cat["dec"] - source_position.center.dec.deg) ** 2
-    ) < rad.value
+        np.sqrt(
+            (star_cat["ra"] - source_position.center.ra.deg) ** 2
+            + (star_cat["dec"] - source_position.center.dec.deg) ** 2
+        )
+        < rad.value
     )
 
     for src in star_cat[(star_mask) & (star_cat["mag"] < max_star_mag)]:
