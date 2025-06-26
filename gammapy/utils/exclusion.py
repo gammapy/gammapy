10c9abb0d2b5cb8f1e0b17d805f479c22ab64742
Samantha Wong <samantha.wong2@mail.mcgill.ca>
2025-06-26 15:29:14 -0400

    adding requested changes

--- gammapy/utils/exclusion.py
+++ gammapy/utils/exclusion.py
@@ -1,6 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import astropy.units as u
-import numpy as np
 from astropy.table import Table
 from regions import CircleSkyRegion
 from astropy.coordinates import SkyCoord
@@ -10,7 +9,7 @@ import os
 
 
 def make_exclusion_mask(
-    source_position, geom, rad=3 * u.deg, max_star_mag=6, other_exc=[]
+    source_region, geom, max_star_mag=6, star_rad=3 * u.deg, other_exc=[]
 ):
     """
     Create an exclusion mask for the source position, any stars in the field of view,
@@ -18,14 +17,14 @@ def make_exclusion_mask(
 
     Parameters
     ----------
-    source_position : `~regions.SkyRegion`
+    source_region : `~regions.SkyRegion`
         Source exclusion region coordinates (can be any geometry).
     geom: `~gammapy.maps.WcsGeom`
         Map geometry for the exclusion mask.
-    rad : `~astropy.units.Quantity`
-        Radius around `source_position` to search for stellar exclusion regions.
     max_star_mag :
         The maximum stellar magnitude (Johnson-Cousins B-band) to exclude. Maximum value is 8th magnitude.
+    star_rad : `~astropy.units.Quantity`
+        Radius to exclude for each star.
     other_exc : list of `~regions.SkyRegion`
         Other regions to exclude (can be any geometry).
 
@@ -34,36 +33,28 @@ def make_exclusion_mask(
     exclusion_mask : `~gammapy.maps.WcsNDMap`
         2D exclusion mask
     """
-    exc = [source_position]
+    exc = [source_region]
 
     for src in other_exc:
         exc.append(src)
-    star_data = np.loadtxt(
+    star_cat = Table.read(
         os.environ.get("GAMMAPY_DATA")
         + "/veritas/crab-point-like-ED/Hipparcos_MAG8_1997.dat",
-        usecols=(0, 1, 2, 3),
+        format="ascii.commented_header",
     )
-    star_cat = Table(
-        {
-            "ra": star_data[:, 0],
-            "dec": star_data[:, 1],
-            "id": star_data[:, 2],
-            "mag": star_data[:, 3],
-        }
-    )
-    star_mask = (
-        np.sqrt(
-            (star_cat["ra"] - source_position.center.ra.deg) ** 2
-            + (star_cat["dec"] - source_position.center.dec.deg) ** 2
-        )
-        < rad.value
+    star_mask = geom.contains(
+        SkyCoord(star_cat["_RAJ2000"] * u.deg, star_cat["_DEJ2000"] * u.deg)
     )
 
-    for src in star_cat[(star_mask) & (star_cat["mag"] < max_star_mag)]:
+    for src in star_cat[
+        (star_mask) & (star_cat["B-V"] + star_cat["Vmag"] < max_star_mag)
+    ]:
         exc.append(
             CircleSkyRegion(
-                center=SkyCoord(src["ra"], src["dec"], unit="deg", frame="icrs"),
-                radius=0.3 * u.deg,
+                center=SkyCoord(
+                    src["_RAJ2000"], src["_DEJ2000"], unit="deg", frame="icrs"
+                ),
+                radius=star_rad,
             )
         )
 
