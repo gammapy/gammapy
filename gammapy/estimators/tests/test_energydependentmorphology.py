8e887170d54db6f74f14b22d107511b9a971be75
AtreyeeS <atreyee.sinha@tifr.res.in>
2025-06-30 03:48:29 +0530

    fix test
    Signed-off-by: AtreyeeS <atreyee.sinha@tifr.res.in>

--- gammapy/estimators/tests/test_energydependentmorphology.py
+++ gammapy/estimators/tests/test_energydependentmorphology.py
@@ -2,7 +2,7 @@ import pytest
 from numpy.testing import assert_allclose
 from astropy import units as u
 from astropy.coordinates import SkyCoord
-from gammapy.datasets import Datasets, MapDataset
+from gammapy.datasets import MapDataset
 from gammapy.estimators.energydependentmorphology import (
     EnergyDependentMorphologyEstimator,
     weighted_chi2_parameter,
@@ -14,7 +14,7 @@ from gammapy.modeling.models import (
 )
 
 
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def create_model():
     source_pos = SkyCoord(5.58, 0.2, unit="deg", frame="galactic")
 
@@ -44,69 +44,67 @@ def create_model():
     return model
 
 
-class TestEnergyDependentEstimator:
-    def __init__(self):
-        energy_edges = [1, 3, 5, 20] * u.TeV
+@pytest.fixture(scope="module")
+def estimator_result(create_model):
+    energy_edges = [1, 3, 5, 20] * u.TeV
+    stacked_dataset = MapDataset.read(
+        "$GAMMAPY_DATA/estimators/mock_DL4/dataset_energy_dependent.fits.gz"
+    )
+    stacked_dataset.models = create_model
+    estimator = EnergyDependentMorphologyEstimator(
+        energy_edges=energy_edges, source="source"
+    )
+    return estimator.run(stacked_dataset)
 
-        stacked_dataset = MapDataset.read(
-            "$GAMMAPY_DATA/estimators/mock_DL4/dataset_energy_dependent.fits.gz"
-        )
-        datasets = Datasets([stacked_dataset])
-        datasets.models = create_model()
 
-        estimator = EnergyDependentMorphologyEstimator(
-            energy_edges=energy_edges, source="source"
-        )
-        self.results = estimator.run(datasets)
+def test_edep(estimator_result):
+    results_edep = estimator_result["energy_dependence"]["result"]
+    assert_allclose(
+        results_edep["lon_0"],
+        [5.6067162, 5.601791, 5.6180701, 5.5973948] * u.deg,
+        rtol=1e-3,
+    )
+    assert_allclose(
+        results_edep["lat_0"],
+        [0.20237541, 0.21819575, 0.18371523, 0.18106852] * u.deg,
+        rtol=1e-3,
+    )
+    assert_allclose(
+        results_edep["sigma"],
+        [0.21563528, 0.25686477, 0.19736596, 0.13505605] * u.deg,
+        rtol=1e-3,
+    )
+    assert_allclose(estimator_result["energy_dependence"]["delta_ts"], 75.62, rtol=1e-3)
 
-    def test_edep(self):
-        results_edep = self.results["energy_dependence"]["result"]
-        assert_allclose(
-            results_edep["lon_0"],
-            [5.6067162, 5.601791, 5.6180701, 5.5973948] * u.deg,
-            atol=1e-5,
-        )
-        assert_allclose(
-            results_edep["lat_0"],
-            [0.20237541, 0.21819575, 0.18371523, 0.18106852] * u.deg,
-            atol=1e-5,
-        )
-        assert_allclose(
-            results_edep["sigma"],
-            [0.21563528, 0.25686477, 0.19736596, 0.13505605] * u.deg,
-            atol=1e-5,
-        )
-        assert_allclose(
-            self.results["energy_dependence"]["delta_ts"], 75.61713199794758, atol=1e-5
-        )
 
-    def test_significance(self):
-        results_src = self.results["src_above_bkg"]
-        assert_allclose(
-            results_src["delta_ts"],
-            [998.0521965029693, 712.8735641098574, 289.81556949490914],
-            atol=1e-5,
-        )
-        assert_allclose(
-            results_src["significance"],
-            [31.27752315246094, 26.34612970747113, 16.54625269423397],
-            atol=1e-5,
-        )
+def test_significance(estimator_result):
+    results_src = estimator_result["src_above_bkg"]
+    assert_allclose(
+        results_src["delta_ts"],
+        [998.0521965029693, 712.8735641098574, 289.81556949490914],
+        rtol=1e-3,
+    )
+    assert_allclose(
+        results_src["significance"],
+        [31.27752315246094, 26.34612970747113, 16.54625269423397],
+        rtol=1e-3,
+    )
 
-    def test_chi2(self):
-        results_edep = self.results["energy_dependence"]["result"]
-        chi2_sigma = weighted_chi2_parameter(
-            results_edep, parameters=["sigma", "lat_0", "lon_0"]
-        )
 
-        assert_allclose(
-            chi2_sigma["chi2"],
-            [87.84278516393066, 4.605432972153188, 1.320491077667271],
-            atol=1e-5,
-        )
+def test_chi2(estimator_result):
+    results_edep = estimator_result["energy_dependence"]["result"]
+    chi2_sigma = weighted_chi2_parameter(
+        results_edep, parameters=["sigma", "lat_0", "lon_0"]
+    )
 
-        assert_allclose(
-            chi2_sigma["significance"],
-            [9.107664118611664, 1.6449173252682943, 0.6484028260024965],
-            atol=1e-5,
-        )
+    assert_allclose(
+        chi2_sigma["chi2"],
+        [87.84278516393066, 4.605432972153188, 1.320491077667271],
+        rtol=1e-3,
+    )
+
+    assert_allclose(
+        chi2_sigma["significance"],
+        [9.107664118611664, 1.6449173252682943, 0.6484028260024965],
+        rtol=1e-3,
+    )
