a4156826ed11c607849dea63b2dc18fab197ca83
Samantha Wong <samantha.wong2@mail.mcgill.ca>
2025-07-07 11:50:56 -0400

    Update gammapy/utils/regions.py
    Co-authored-by: Bruno Khélifi <khelifi@in2p3.fr>

--- gammapy/utils/regions.py
+++ gammapy/utils/regions.py
@@ -433,7 +433,7 @@ def extract_bright_star_regions(geom, max_star_mag=6, star_rad=0.3 * u.deg):
 
     Parameters
     ----------
-    geom: `~gammapy.maps.WcsGeom`
+    geom : `~gammapy.maps.WcsGeom`
         Map geometry for the exclusion mask.
     max_star_mag : float, optional
         The maximum stellar magnitude (Johnson-Cousins B-band) to exclude. Maximum value is 8th magnitude.
