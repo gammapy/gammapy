0c0cc9c0d2ec6653760785fcb8770ae092a1c2c3
Atreyee Sinha <asinha@ucm.es>
2025-07-10 00:31:55 +0530

    Update docs of counts_statistic.py

--- gammapy/stats/counts_statistic.py
+++ gammapy/stats/counts_statistic.py
@@ -160,9 +160,8 @@ class CountsStatistic(abc.ABC):
     def compute_upper_limit(self, n_sigma=3):
         """Compute upper limit on the signal.
 
-        Searches the signal value for which the test statistics is n_sigma**2 away from the maximum
-        or from 0 if the measured excess is negative.
-
+        Searches the signal value for which the test statistics is n_sigma**2 away from the maximum.
+        
         Parameters
         ----------
         n_sigma : float
