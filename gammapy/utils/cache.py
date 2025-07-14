41073f5667de7837e961e0179d70d669534884e6
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2025-07-14 16:33:12 +0200

    implement class instance caching
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/utils/cache.py
+++ gammapy/utils/cache.py
@@ -118,3 +118,29 @@ def cachemethod(fn):
         return out
 
     return wrapper
+
+
+class CacheEquivalentMixin:
+    """Cache class instance using equality condition.
+    Simpler for classes with complex arguements that are not hashable.
+    """
+
+    _instances = []
+
+    def __new__(cls, *args, **kwargs):
+        # Clean up dead references
+        cls._instances = [ref for ref in cls._instances if ref() is not None]
+
+        # Create a temporary instance to compare
+        temp = super().__new__(cls)
+        temp.__init__(*args, **kwargs)
+
+        # Search for an equivalent instance
+        for ref in cls._instances:
+            instance = ref()
+            if instance is not None and instance == temp:
+                return instance
+
+        # Store a weak reference to the new instance
+        cls._instances.append(weakref.ref(temp))
+        return temp
