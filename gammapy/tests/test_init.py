0e80f3d6cfdf85f4d9bca886e65ccd84e82f6cd6
Bruno Khélifi <khelifi@in2p3.fr>
2025-07-23 08:50:25 +0200

    Update gammapy/tests/test_init.py
    Co-authored-by: Kirsty Feijen <AstroKirsty@gmail.com>

--- gammapy/tests/test_init.py
+++ gammapy/tests/test_init.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Gammapy integration and system tests.
 
-This package can be used for tests that involved several
+This package can be used for tests that involve several
 Gammapy sub-packages, or that don't fit anywhere else.
 """
 
