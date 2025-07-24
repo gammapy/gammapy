6d9e55400113f3b736efb7bb4b9925d3208c9155
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2025-07-24 20:21:50 +0200

    try to add required_dependency for download
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

--- gammapy/scripts/tests/test_tutorial_info.py
+++ gammapy/scripts/tests/test_tutorial_info.py
@@ -1,6 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+import pytest
 from gammapy.scripts.main import cli
-from gammapy.utils.testing import run_cli
+from gammapy.utils.testing import requires_dependency, run_cli
 
 
 def test_cli_tutorial_info():
@@ -8,7 +9,10 @@ def test_cli_tutorial_info():
     assert "Usage" in result.output
 
 
-def test_cli_tutorial_info_no_args():
+@requires_dependency("requests")
+@requires_dependency("tqdm")
+@pytest.mark.remote_data
+def test_cli_tutorial_info_no_args(tmp_path):
     result = run_cli(cli, ["tutorial", "setup"])
     assert "System" in result.output
     assert "Gammapy package" in result.output
