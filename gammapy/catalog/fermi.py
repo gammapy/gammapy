5b954ae2aeeff7dd2a2e6eb665d759cd92e54b6b
Astro-Kirsty <AstroKirsty@gmail.com>
2025-08-07 11:44:08 +0200

    fix fermi
    Signed-off-by: Astro-Kirsty <AstroKirsty@gmail.com>

--- gammapy/catalog/fermi.py
+++ gammapy/catalog/fermi.py
@@ -1458,6 +1458,7 @@ class SourceCatalogObject2PC(SourceCatalogObjectFermiPCBase):
         return ss
 
     def spectral_model(self):
+        """Best fit spectral model."""
         d = self.data_spectral
         if d is None:
             log.warning(f"No spectral model available for source {self.name}")
@@ -1714,6 +1715,7 @@ class SourceCatalogObject3PC(SourceCatalogObjectFermiPCBase):
     def pulse_profiles(self):
         """
         The 3PC pulse profiles are provided in different energy ranges, each represented in weighted counts.
+
         These profiles are stored in a `~gammapy.maps.Maps` of `~gammapy.maps.RegionNDMap`, one per energy bin.
 
         The `~gammapy.maps.Maps` keys correspond to specific energy ranges as follows:
@@ -1755,7 +1757,8 @@ class SourceCatalogObject3PC(SourceCatalogObjectFermiPCBase):
         return maps
 
     def spectral_model(self, fit="auto"):
-        """
+        """Best fit spectral model.
+
         In the 3PC, Fermi-LAT collaboration tried to fit a
         `~gammapy.modelling.models.SuperExpCutoffPowerLaw4FGLDR3SpectralModel` with the
         exponential index `index_2` free, or fixed to 2/3. These two models are referred
@@ -1764,7 +1767,6 @@ class SourceCatalogObject3PC(SourceCatalogObjectFermiPCBase):
 
         Parameters
         ----------
-
         fit : str, optional
             Which fitted model to return. The user can choose between "auto", "b free"
             and "b 23". "auto" will always try to return the "b free" first and fall
@@ -1815,8 +1817,7 @@ class SourceCatalogObject3PC(SourceCatalogObjectFermiPCBase):
 
     @property
     def flux_points_table(self):
-        """Flux points (`~astropy.table.Table`). Flux point is an upper limit if
-        its significance is less than 2."""
+        """Flux points (`~astropy.table.Table`). Flux point is an upper limit if its significance is less than 2."""
         fp_data = self.data_spectral
         if fp_data is None:
             log.warning(f"No flux points available for source {self.name}")
