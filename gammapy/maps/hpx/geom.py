8c996287f86b1af0aebeb9afd8730a824447b850
Astro-Kirsty <AstroKirsty@gmail.com>
2025-08-27 12:00:55 +0200

    Adapt kwargs for functionalities to finialise #3457
    Signed-off-by: Astro-Kirsty <AstroKirsty@gmail.com>

--- gammapy/maps/hpx/geom.py
+++ gammapy/maps/hpx/geom.py
@@ -219,7 +219,7 @@ class HpxGeom(Geom):
         else:
             return ravel_hpx_index(idx_local, self.npix)
 
-    def cutout(self, position, width, **kwargs):
+    def cutout(self, position, width):
         """Create a cutout around a given position.
 
         Parameters
