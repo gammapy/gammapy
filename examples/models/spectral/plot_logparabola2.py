82ef9623e3d7d34dfaa741b611b7b9f18fbe7b06
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2025-09-19 11:02:27 +0200

    Add Logparabola2 model
    Signed-off-by: Quentin Remy <quentin.remy@mpi-hd.mpg.de>

---
+++ examples/models/spectral/plot_logparabola2.py
@@ -0,0 +1,55 @@
+r"""
+.. _logparabola2-spectral-model:
+
+Log parabola spectral model
+===========================
+
+This model parametrises a log parabola spectrum.
+
+It is defined by the following equation:
+
+.. math::
+        \phi(E) = \phi_0 \left( \frac{E}{E_0} \right) ^ {
+          - \alpha - \beta \log{ \left( \frac{E}{E_s} \right) }
+        }
+
+Note that :math:`log` refers to the natural logarithm. This is consistent
+with the `Fermi Science Tools
+<https://fermi.gsfc.nasa.gov/ssc/data/analysis/scitools/source_models.html>`_
+and `ctools
+<http://cta.irap.omp.eu/ctools-devel/users/user_manual/getting_started/models.html#log-parabola>`_.
+The `Sherpa <http://cxc.harvard.edu/sherpa/ahelp/logparabola.html>`_ package, however, uses :math:`log_{10}`. If you have
+parametrization based on :math:`log_{10}` you can use the
+:func:`~gammapy.modeling.models.LogParabolaSpectralModel.from_log10` method.
+
+"""
+
+# %%
+# Example plot
+# ------------
+# Here is an example plot of the model:
+
+from astropy import units as u
+import matplotlib.pyplot as plt
+from gammapy.modeling.models import LogParabolaSpectralModel, Models, SkyModel
+
+energy_bounds = [0.1, 100] * u.TeV
+model = LogParabolaSpectralModel(
+    alpha=2.3,
+    amplitude="1e-12 cm-2 s-1 TeV-1",
+    reference=10 * u.TeV,
+    beta=0.5,
+    escale=1 * u.TeV,
+)
+model.plot(energy_bounds)
+plt.grid(which="both")
+
+# %%
+# YAML representation
+# -------------------
+# Here is an example YAML file using the model:
+
+model = SkyModel(spectral_model=model, name="log-parabola2-model")
+models = Models([model])
+
+print(models.to_yaml())
