a517cd6dcd672335d6eff20c9fafef0cbc58ddc4
Leander Schlegel <Leander.Schlegel@Rub.de>
2025-09-25 16:38:18 +0200

    adapted test as discussed with RT. added whitelines for structure of second test
    Signed-off-by: Leander Schlegel <Leander.Schlegel@Rub.de>

--- gammapy/data/tests/test_io.py
+++ gammapy/data/tests/test_io.py
@@ -12,11 +12,14 @@ from ..io import EventListReader, EventListWriter, ObservationTableReader
 
 
 @requires_data()
-def test_observationtable_reader_no_format():
+def test_observationtable_reader_unknown_hdu_extension():
     hess_obs_table = "$GAMMAPY_DATA/hess-dl3-dr1/obs-index.fits.gz"
 
-    obs_table = ObservationTableReader().read(hess_obs_table, format=None)
-    assert len(obs_table) == 105
+    with pytest.raises(BaseException):
+        obs_table = ObservationTableReader().read(
+            hess_obs_table, hdu="unknown_extension"
+        )
+        assert len(obs_table) == 105
 
 
 def test_observationtable_reader_gadf_converter():
@@ -24,17 +27,21 @@ def test_observationtable_reader_gadf_converter():
     t = Table({"OBS_ID": ["1"], "TSTART": [Time("2012-01-01T00:30:00")]}, meta={})
     with pytest.raises(RuntimeError):
         ObservationTableReader.from_gadf_table(t)
+
     t = Table({"OBS_ID": ["1"], "TSTOP": [Time("2012-01-01T00:30:00")]}, meta={})
     with pytest.raises(RuntimeError):
         ObservationTableReader.from_gadf_table(t)
+
     # OBS_ID has to be of type int64 for internal model but converter ensures this.
     t_gadf = Table({"OBS_ID": ["1"], "RA_PNT": [1.0], "DEC_PNT": [1.0]})
     obs_table = ObservationTableReader.from_gadf_table(t_gadf)
     assert obs_table["OBS_ID"].dtype == np.dtype(int)
+
     # Unit for Column objects like RA_PNT have to be specified for internal model but converter ensures this.
     t = Table({"OBS_ID": [1], "RA_PNT": [1.0]})
     obs_table = ObservationTableReader.from_gadf_table(t)
     assert obs_table["RA_PNT"].unit == u.deg
+
     # Unit of RA_PNT has to be deg for internal model but converter ensures this.
     t = Table({"OBS_ID": [1], "RA_PNT": [1.0]}, units={"OBS_ID": None, "RA_PNT": u.m})
     obs_table = ObservationTableReader.from_gadf_table(t)
