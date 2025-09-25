4353c9d27a72a839942460b9ed9f67af884afe5c
Leander Schlegel <Leander.Schlegel@Rub.de>
2025-09-25 18:36:03 +0200

    further optimizing reader
    Signed-off-by: Leander Schlegel <Leander.Schlegel@Rub.de>

--- gammapy/data/io.py
+++ gammapy/data/io.py
@@ -98,33 +98,54 @@ class ObservationTableReader:
                     warnings.warn(f"Could not convert unit for column {colname}.")
                 removed_names.append(colname)
 
-        time_ref = None
-        if "TSTART" in names_gadf or "TSTOP" in names_gadf:
-            try:
-                time_ref = time_ref_from_dict(meta_gadf)
-            except KeyError:
-                warnings.warn(
-                    "Found column TSTART or TSTOP in gadf table, but can not create columns in internal format (MixinColumn Time) due to missing header keywords in file."
-                )
-                removed_names.append("TSTART")
-                removed_names.append("TSTOP")
-            if time_ref is not None:
-                if "TIMEUNIT" in meta_gadf.keys():
-                    time_unit = meta_gadf["TIMEUNIT"]
-                else:
-                    time_unit = "second"
-                if "TSTART" in names_gadf:
-                    tstart = time_ref + Quantity(
-                        table_gadf["TSTART"].astype("float64"), time_unit
+        for colname in ["TSTART", "TSTOP"]:
+            if colname in names_gadf:
+                try:
+                    time_ref = time_ref_from_dict(meta_gadf)
+                except KeyError:
+                    warnings.warn(
+                        "Found column TSTART or TSTOP in gadf table, but can not create columns in internal format (MixinColumn Time) due to missing header keywords in file."
                     )
-                    new_table["TSTART"] = tstart
-                    removed_names.append("TSTART")
-                if "TSTOP" in names_gadf:
-                    tstop = time_ref + Quantity(
-                        table_gadf["TSTOP"].astype("float64"), time_unit
-                    )
-                    new_table["TSTOP"] = tstop
-                    removed_names.append("TSTOP")
+                    removed_names.append(colname)
+
+                if colname not in removed_names:
+                    col_typecasted = cast_func(table_gadf[colname], np.dtype(float))
+                    try:
+                        time_object = time_ref + Quantity(
+                            table_gadf[colname].astype("float64"), meta_gadf["TIMEUNIT"]
+                        )
+                        new_table[colname] = time_object
+                    except TypeError:
+                        warnings.warn(f"Could not convert unit for column {colname}.")
+                    removed_names.append(colname)
+
+        # time_ref = None
+        # if "TSTART" in names_gadf or "TSTOP" in names_gadf:
+        #     try:
+        #         time_ref = time_ref_from_dict(meta_gadf)
+        #     except KeyError:
+        #         warnings.warn(
+        #             "Found column TSTART or TSTOP in gadf table, but can not create columns in internal format (MixinColumn Time) due to missing header keywords in file."
+        #         )
+        #         removed_names.append("TSTART")
+        #         removed_names.append("TSTOP")
+        #     if time_ref is not None:
+        #         if "TIMEUNIT" in meta_gadf.keys():
+        #             time_unit = meta_gadf["TIMEUNIT"]
+        #         else:
+        #             time_unit = "second"
+        #         if "TSTART" in names_gadf:
+        #             tstart = time_ref + Quantity(
+        #                 table_gadf["TSTART"].astype("float64"), time_unit
+        #             )
+        #             new_table["TSTART"] = tstart
+        #             removed_names.append("TSTART")
+        #         if "TSTOP" in names_gadf:
+        #             tstop = time_ref + Quantity(
+        #                 table_gadf["TSTOP"].astype("float64"), time_unit
+        #             )
+        #             new_table["TSTOP"] = tstop
+        #             removed_names.append("TSTOP")
 
         for name in names_gadf:
             if name not in removed_names:
