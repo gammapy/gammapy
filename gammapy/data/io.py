faa42f7f5179a449409be03aebc4189eabda80c8
Leander Schlegel <Leander.Schlegel@Rub.de>
2025-09-26 17:33:00 +0200

    Add ObservationTableReader class based on EventListReader class
    Co-authored-by: Régis Terrier <regis.terrier@m4x.org>
    Co-authored-by: Bruno Khélifi <khelifi@in2p3.fr>
    Signed-off-by: Leander Schlegel <Leander.Schlegel@Rub.de>

--- gammapy/data/io.py
+++ gammapy/data/io.py
@@ -1,9 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import warnings
 from astropy.io import fits
+from astropy import table
 from astropy.table import Table
 import astropy.units as u
-from gammapy.data import EventListMetaData, EventList
+from astropy.units import Quantity
+from gammapy.data import EventListMetaData, EventList, ObservationTable
 from gammapy.utils.scripts import make_path
 from gammapy.utils.metadata import CreatorMetaData
 from gammapy.utils.time import time_ref_from_dict
@@ -141,3 +143,137 @@ class EventListWriter:
             raise ValueError(f"Only the 'gadf' format supported, got {format}")
 
         return self._to_gadf_table_hdu(event_list)
+
+
+class ObservationTableReader:
+    """Reader class for ObservationTable"""
+
+    def read(self, filename, hdu=None):
+        """Read ObservationTable from file.
+
+        Parameters
+        ----------
+        filename : `pathlib.Path`, str
+            Filename
+        hdu : str, optional
+            Name of observation table HDU. Default is None.
+
+        Returns
+        -------
+        ObservationTable : `~gammapy.data.ObservationTable`
+            ObservationTable from disk converted into internal data format.
+        """
+        filename = make_path(filename)
+
+        table_disk = Table.read(filename, format="fits", hdu=hdu)
+        table_disk_meta = table_disk.meta
+
+        format = table_disk_meta.get("HDUCLASS", "unknown")
+        version = table_disk_meta.get("HDUVERS", "unknown")
+
+        if (format == "unknown") or (format is None):
+            format = "GADF"
+            version = "0.3"
+            warnings.warn(
+                UserWarning(
+                    f"Could not infer fileformat from metadata in {filename}, assuming GADF."
+                )
+            )
+
+        if format == "GADF" or format == "OGIP":
+            if version == "0.2" or version == "0.3":
+                return self.from_gadf_table(table_disk)
+            else:
+                raise ValueError(f"Unknown {format} version :{version}")
+        else:
+            raise ValueError(f"Unknown fileformat :{format}")
+
+    @staticmethod
+    def from_gadf_table(table_gadf):
+        """Convert gadf observation table into internal data format.
+
+        Parameters
+        ----------
+        table_gadf : `~astropy.Table.table`
+            Table in gadf 0.2/0.3 format.
+            https://gamma-astro-data-formats.readthedocs.io/en/v0.2/data_storage/obs_index/index.html
+            https://gamma-astro-data-formats.readthedocs.io/en/v0.3/data_storage/obs_index/index.html
+
+        Returns
+        -------
+        ObservationTable : `~gammapy.data.ObservationTable`
+            ObservationTable in internal data format.
+
+            Mandatory Columns: OBS_ID
+
+            Defined Units and Types for Columns as follows:
+
+            OBS_ID	RA_PNT	DEC_PNT	ALT_PNT	AZ_PNT	TSTART	TSTOP
+                        deg	    deg	    deg	   deg
+            int64   float64	float64	float64 float64	 Time	 Time
+        """
+
+        names_gadf = table_gadf.colnames
+        meta_gadf = table_gadf.meta
+
+        required_names_gadf = [
+            "OBS_ID",
+        ]
+
+        missing_names = set(required_names_gadf).difference(names_gadf)
+        if len(missing_names) != 0:
+            raise RuntimeError(
+                f"Not all columns required to read from GADF were found in file. Missing: {missing_names}"
+            )
+
+        removed_names = []
+
+        try:
+            obs_id = table_gadf["OBS_ID"].astype("int")
+        except ValueError:
+            raise RuntimeError(
+                "Could not convert OBS_ID to int. Can not create table without OBS_ID."
+            )
+        new_table = Table({"OBS_ID": obs_id}, meta=meta_gadf)
+        new_table = table.unique(new_table, keys="OBS_ID")
+        removed_names.append("OBS_ID")
+
+        for colname in ["RA_PNT", "DEC_PNT", "ALT_PNT", "AZ_PNT"]:
+            if colname in names_gadf:
+                try:
+                    new_table[colname] = Quantity(
+                        table_gadf[colname].astype("float64"),
+                        u.Unit(table_gadf[colname].unit),
+                    ).to(u.deg)
+                except TypeError:
+                    warnings.warn(f"Could not convert unit for column {colname}.")
+                removed_names.append(colname)
+
+        for colname in ["TSTART", "TSTOP"]:
+            if colname in names_gadf:
+                try:
+                    time_ref = time_ref_from_dict(meta_gadf)
+                    time_unit = meta_gadf["TIMEUNIT"]
+                except KeyError:
+                    warnings.warn(
+                        "Found column TSTART or TSTOP in gadf table, but can not create columns in internal format (MixinColumn Time) due to missing header keywords in file."
+                    )
+                    removed_names.append(colname)
+
+                if colname not in removed_names:
+                    try:
+                        time_object = time_ref + Quantity(
+                            table_gadf[colname].astype("float64"), time_unit
+                        )
+                        new_table[colname] = time_object
+                    except ValueError:
+                        warnings.warn(f"Invalid unit for column {colname}.")
+                    except TypeError:
+                        warnings.warn(f"Could not convert type for column {colname}.")
+                    removed_names.append(colname)
+
+        for name in names_gadf:
+            if name not in removed_names:
+                new_table.add_column(table_gadf[name])
+
+        return ObservationTable(table=new_table, meta=meta_gadf)
