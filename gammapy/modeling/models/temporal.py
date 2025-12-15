eac4dbd1ddb7dad43addeb85a201b185d21d6cc0
Régis Terrier <regis.terrier@m4x.org>
2025-12-15 11:30:53 +0100

    Update gammapy/modeling/models/temporal.py
    Co-authored-by: Kirsty Feijen <AstroKirsty@gmail.com>

--- gammapy/modeling/models/temporal.py
+++ gammapy/modeling/models/temporal.py
@@ -965,7 +965,7 @@ class TemplatePhaseCurveTemporalModel(TemporalModel):
         A table with 'PHASE' vs 'NORM'.
     filename : str
         The name of the file containing the phase curve.
-    normalize : bool
+    normalize : bool, optional
         Flag to normalize phase curve integral over phase to 1. Default is True.
     t_ref : `~astropy.units.Quantity`
         The reference time in mjd.
