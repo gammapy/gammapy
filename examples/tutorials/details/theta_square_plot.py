43ea5183858362365fb761c3de105003e8a2f778
Fabio Pintore <fabio.pintore@inaf.it>
2026-02-05 14:00:26 +0100

    update theta2 plot tutorial
    Signed-off-by: Fabio Pintore <fabio.pintore@inaf.it>

--- examples/tutorials/details/theta_square_plot.py
+++ examples/tutorials/details/theta_square_plot.py
@@ -50,12 +50,40 @@ print(position)
 # Creation of the theta2 plot
 # ---------------------------
 #
+# By default, the distribution of the OFF counts in squared angular distance is calculated from the mirror reflected coordinates of the test position, assuming therefore a single OFF position.
+# However, one can set manually both the coordinates of the `off_position`.
+#
+
+separation = position.separation(observations[0].pointing.fixed_icrs)
+position_angle = 180 * u.deg
+off_position = position.directional_offset_by(
+    position_angle=position_angle, separation=separation * 2
+)
+print(off_position)
+
 
 theta2_axis = MapAxis.from_bounds(0, 0.2, nbin=20, interp="lin", unit="deg2")
 theta2_table = make_theta_squared_table(
     observations=observations,
     position=position,
     theta_squared_axis=theta2_axis,
+    position_off=off_position,
+)
+
+plt.figure(figsize=(10, 5))
+plot_theta_squared_table(theta2_table)
+plt.show()
+
+# Alternatively, it can be chosen a number of reflected OFF positions that will be calculated through the `~gammapy.makers.background.WobbleRegionsFinder`. The user should be cautios that the regions (ON and OFF) do not overlaps, otherwise only the mirror reflected region will be adopted as OFF.
+#
+
+off_regions_number = 3
+theta2_axis = MapAxis.from_bounds(0, 0.1, nbin=20, interp="lin", unit="deg2")
+theta2_table = make_theta_squared_table(
+    observations=observations,
+    position=position,
+    theta_squared_axis=theta2_axis,
+    off_regions_number=off_regions_number,
 )
 
 plt.figure(figsize=(10, 5))
