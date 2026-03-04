2b18e42e1cf691d0de95d5f6aec28ee8ade15f76
Quentin Remy <quentin.remy@live.fr>
2026-03-04 10:16:04 +0100

    Apply suggestions from code review

--- gammapy/maps/region/geom.py
+++ gammapy/maps/region/geom.py
@@ -888,9 +888,10 @@ class RegionGeom(Geom):
 
             kwargs.setdefault("facecolor", "None")
             user_color = kwargs.pop("color", kwargs.pop("c", None))
-            current_ec = kwargs.get("edgecolor", kwargs.get("ec", "C0"))
-            kwargs["edgecolor"] = user_color if user_color is not None else current_ec
-            kwargs.pop("ec", None)
+            kwargs.setdefault("edgecolor", kwargs.pop("ec", "C0"))
+            if user_color is not None:
+                kwargs["edgecolor"] = user_color
+
 
             kwargs_point.setdefault("marker", "*")
             kwargs_point.setdefault("color", kwargs["edgecolor"])
@@ -899,8 +900,12 @@ class RegionGeom(Geom):
                 key_point = ARTIST_TO_LINE_PROPERTIES.get(key, None)
                 if key_point and key_point not in kwargs_point:
                     kwargs_point[key_point] = value
+
+            if user_color is not None:
+                kwargs_point.setdefault("color", user_color)
             if "color" in kwargs_point:
-                kwargs_point["markeredgecolor"] = kwargs_point["color"]
+                kwargs_point.setdefault("markeredgecolor", kwargs_point["color"])
+
 
             for region in compound_region_to_regions(self.region):
                 region_pix = region.to_pixel(wcs=ax.wcs)
