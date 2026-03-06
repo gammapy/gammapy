240aeaaf258afc1d77aae8c5502f865988a5955e
Astro-Kirsty <AstroKirsty@gmail.com>
2026-03-06 14:36:00 +0100

    Remove inclusive and exclusive
    Signed-off-by: Astro-Kirsty <AstroKirsty@gmail.com>

--- gammapy/data/event_list.py
+++ gammapy/data/event_list.py
@@ -345,7 +345,7 @@ class EventList:
         Parameters
         ----------
         time_interval : `astropy.time.Time`
-            Start time (inclusive) and stop time (exclusive) for the selection.
+            Start and stop time for the selection.
         inverted : bool, optional
             Whether to invert selection i.e. to keep all entries outside the time range.
             Default is False.
