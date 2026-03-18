150376cd32df0ab644f32892c7af844f1625c8a2
Arnau Aguasca-Cabot <arnau.aguasca@fqa.ub.edu>
2026-03-18 22:15:43 +0100

    Update examples/models/spectral/plot_absorbed.py
    Co-authored-by: Kirsty Feijen <AstroKirsty@gmail.com>

--- examples/models/spectral/plot_absorbed.py
+++ examples/models/spectral/plot_absorbed.py
@@ -15,7 +15,7 @@ where :math:`\tau(E, z)` is the optical depth predicted by the model
 (`~gammapy.modeling.models.EBLAbsorptionNormSpectralModel`), which depends 
 on the energy of the gamma-rays and the
 redshift :math:`z` of the source, and :math:`\alpha` is a scale factor
-(default: 1) for the optical depth.
+(default is 1) for the optical depth.
 
 The available EBL models are defined in `~gammapy.modeling.models.EBL_DATA_BUILTIN`.
 """
