8b736ac57fbf6651db08b99bf53f712269987f2c
Régis Terrier <rterrier@apc.in2p3.fr>
2026-09-13 21:11:22 +0200

    Correct import
    Signed-off-by: Régis Terrier <rterrier@apc.in2p3.fr>

--- gammapy/visualization/plotters/tests/test_core.py
+++ gammapy/visualization/plotters/tests/test_core.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import pytest
 import matplotlib
-from gammapy.visualization import BasePlotter
+from gammapy.visualization.plotters import BasePlotter
 
 
 class SubClassPlotter(BasePlotter):
